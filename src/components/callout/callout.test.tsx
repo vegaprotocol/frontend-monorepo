9f6343a9f652c1c51688597252567de68e11e7dc
Dexter <dexter.edwards93@gmail.com>
2021-12-14 12:15:19 +0000

    add some basic tests

---
+++ src/components/callout/callout.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import * as React from "react";
+import { Callout } from ".";
+
+test("It renders content within callout", () => {
+  render(<Callout>Content</Callout>);
+  expect(screen.getByTestId("callout")).toHaveTextContent("Content");
+});
+
+test("It renders title and icon", () => {
+  render(<Callout icon={<div data-testid="icon" />} title="title" />);
+  expect(screen.getByTestId("icon")).toBeInTheDocument();
+  expect(screen.getByText("title")).toBeInTheDocument();
+});
+
+const intents = ["warn", "action", "error", "success"] as [
+  "warn",
+  "action",
+  "error",
+  "success"
+];
+
+intents.map((intent) =>
+  test("Applies class based on intent", () => {
+    render(<Callout intent={intent} />);
+    expect(screen.getByTestId("callout")).toHaveClass(`callout--${intent}`);
+  })
+);
