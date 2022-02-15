4b38ec6a0bf5b2ee6a9484ace1f08d77d9cafa47
Dexter <dexter.edwards93@gmail.com>
2022-02-15 14:12:27 +0000

    migrate stuff into new NX

---
+++ apps/explorer/src/app/src/App.tsx
@@ -0,0 +1,33 @@
+import './App.scss';
+
+import { ApolloProvider } from '@apollo/client';
+
+import { createClient } from './lib/apollo-client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Nav } from './components/nav';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import { Main } from './components/main';
+import React from 'react';
+import { DATA_SOURCES } from './config';
+import { TendermintWebsocketProvider } from './contexts/websocket/tendermint-websocket-provider';
+
+function App() {
+  const [client] = React.useState(createClient(DATA_SOURCES.dataNodeUrl));
+  return (
+    <TendermintWebsocketProvider>
+      <ApolloProvider client={client}>
+        <div className="app">
+          <div className="template-sidebar">
+            <Nav />
+            <Header />
+            <Main />
+            <Footer />
+          </div>
+        </div>
+      </ApolloProvider>
+    </TendermintWebsocketProvider>
+  );
+}
+
+export default App;
