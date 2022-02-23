a9a780100aab8dc66463136a7ba77e6e91e2e77b
Bartłomiej Głownia <bglownia@gmail.com>
2022-02-23 15:49:24 +0100

    Setup tailwind.css in apps/trading

---
+++ apps/trading/tailwind.config.js
@@ -0,0 +1,12 @@
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, './components/**/*.{js,ts,jsx,tsx}'),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
