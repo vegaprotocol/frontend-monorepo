76809325cbd1207bd2eed45e06b156ae9539d5a2
Matthew Russell <mattrussell36@gmail.com>
2022-02-23 16:13:21 -0800

    add nav item for home and use pubkey on portfolio page

--- apps/trading/pages/portfolio/index.page.tsx
+++ apps/trading/pages/portfolio/index.page.tsx
@@ -1,7 +1,12 @@
+import { useVegaWallet } from '@vegaprotocol/react-helpers';
+
 const Portfolio = () => {
+  const { keypair } = useVegaWallet();
   return (
     <div>
-      <h1>Portfolio</h1>
+      <h1>
+        Portfolio for: {keypair.name} {keypair.pub}
+      </h1>
     </div>
   );
 };
