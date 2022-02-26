67dc1bfbf154a76564ce02da0963084e615c735d
sam-keen <samuel.kleinmann@gmail.com>
2022-02-26 13:31:38 +0000

    Created unstyled block explorer transaction and added Prism for code blocks being displayed

--- apps/explorer/src/app/routes/txs/id/index.tsx
+++ apps/explorer/src/app/routes/txs/id/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { DATA_SOURCES } from "../../../config";
-import useFetch from "../../../hooks/use-fetch";
-import { ChainExplorerTxResponse } from "../../types/chain-explorer-response";
-import { TendermintTransactionResponse } from "../tendermint-transaction-response.d";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { DATA_SOURCES } from '../../../config';
+import useFetch from '../../../hooks/use-fetch';
+import { ChainExplorerTxResponse } from '../../types/chain-explorer-response';
+import { TendermintTransactionResponse } from '../tendermint-transaction-response.d';
+import { TxDetails, TxContent } from '../../../components/transaction';
 
 const Tx = () => {
   const { txHash } = useParams<{ txHash: string }>();
@@ -13,7 +14,7 @@ const Tx = () => {
   const { data: decodedData } = useFetch<ChainExplorerTxResponse>(
     DATA_SOURCES.chainExplorerUrl,
     {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         tx_hash: txHash,
         node_url: `${DATA_SOURCES.tendermintUrl}/`,
@@ -23,11 +24,13 @@ const Tx = () => {
 
   return (
     <section>
-      <h1>Tx</h1>
-      <h2>Tendermint Data</h2>
-      <pre>{JSON.stringify(transactionData, null, "  ")}</pre>
-      <h2>Decoded data</h2>
-      <pre>{JSON.stringify(decodedData, null, "  ")}</pre>
+      <h1>Transaction details</h1>
+      <TxDetails
+        txData={transactionData?.result}
+        pubKey={decodedData?.PubKey}
+      />
+      <h2>Transaction content</h2>
+      <TxContent data={decodedData} />
     </section>
   );
 };
