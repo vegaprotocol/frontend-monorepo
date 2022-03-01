bc0c8b7e419692849600db514a89c3937fd631ba
Bartłomiej Głownia <bglownia@gmail.com>
2022-03-01 14:50:54 +0100

    Add InputError component

---
+++ libs/ui-toolkit/src/components/inputError/inputError.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import InputError from './inputError';
+
+describe('InputError', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<InputError />);
+    expect(baseElement).toBeTruthy();
+  });
+});
