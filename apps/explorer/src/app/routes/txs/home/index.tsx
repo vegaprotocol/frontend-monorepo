2ef05b3f355ee944296cef0960fde0666d19ba24
sam-keen <samuel.kleinmann@gmail.com>
2022-03-03 13:33:45 +0000

    Transactions route set up and a component to show the transactions per block

--- apps/explorer/src/app/routes/txs/home/index.tsx
+++ apps/explorer/src/app/routes/txs/home/index.tsx
@@ -1,8 +1,27 @@
+import useFetch from '../../../hooks/use-fetch';
+import { TendermintBlockchainResponse } from '../../blocks/tendermint-blockchain-response';
+import { DATA_SOURCES } from '../../../config';
+import { BlocksTable } from '../../../components/blocks';
+import { JumpToBlock } from '../../../components/jump-to-block';
+
 const Txs = () => {
+  const {
+    state: { data },
+    refetch,
+  } = useFetch<TendermintBlockchainResponse>(
+    `${DATA_SOURCES.tendermintUrl}/blockchain`
+  );
+
   return (
-    <section>
-      <h1>Transactions</h1>
-    </section>
+    <>
+      <section>
+        <h1>Transactions</h1>
+        <button onClick={() => refetch()}>Refresh to see latest blocks</button>
+        <BlocksTable data={data} showTransactions={true} />
+      </section>
+
+      <JumpToBlock />
+    </>
   );
 };
 
