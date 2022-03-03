032f2711c3abeb8b9e23ddcaf56a843ed4cf6625
Dexter <dexter.edwards93@gmail.com>
2022-03-03 14:25:15 +0000

    add basic tailwind setup

--- apps/explorer/src/app/components/splash-screen/splash-screen.tsx
+++ apps/explorer/src/app/components/splash-screen/splash-screen.tsx
@@ -1,6 +1,6 @@
-import "./splash-screen.scss";
+// import "./splash-screen.scss";
 
-import React from "react";
+import React from 'react';
 
 export const SplashScreen = ({ children }: { children: React.ReactNode }) => {
   return <div className="splash-screen">{children}</div>;
---
+++ apps/explorer/tailwind.config.js
@@ -0,0 +1,13 @@
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+const theme = require('../../libs/tailwindcss-config/src/theme');
+
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'class',
+  theme,
+  plugins: [],
+};
