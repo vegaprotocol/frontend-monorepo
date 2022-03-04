995edc36ee26379433cce3f3820903be71f69a49
Dexter <dexter.edwards93@gmail.com>
2022-03-04 18:42:26 +0000

    add sentry

--- apps/explorer/src/main.tsx
+++ apps/explorer/src/main.tsx
@@ -1,9 +1,23 @@
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
 
 import App from './app/app';
+
+const dsn = process.env['NX_SENTRY_DSN'];
+
+/* istanbul ignore next */
+if (dsn) {
+  Sentry.init({
+    dsn,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 0.1,
+    environment: process.env['NODE_ENV'],
+  });
+}
 
 ReactDOM.render(
   <StrictMode>
