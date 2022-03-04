20a053a4f50cb1ebe9ec8b07f5eb99eabf730036
Bartłomiej Głownia <bglownia@gmail.com>
2022-03-04 17:21:24 +0100

    Adjust ui-toolkit colors

--- libs/ui-toolkit/.storybook/preview.js
+++ libs/ui-toolkit/.storybook/preview.js
@@ -11,14 +11,19 @@ export const parameters = {
 };
 
 export const decorators = [
-  (Story) => (
-    <div className="text-body">
-      <div className="dark bg-black p-16">
+  (Story, context) =>
+    context.parameters.themes === false ? (
+      <div className="text-body">
         <Story />
       </div>
-      <div className="p-16">
-        <Story />
+    ) : (
+      <div className="text-body">
+        <div className="dark bg-black p-16">
+          <Story />
+        </div>
+        <div className="p-16">
+          <Story />
+        </div>
       </div>
-    </div>
-  ),
+    ),
 ];
