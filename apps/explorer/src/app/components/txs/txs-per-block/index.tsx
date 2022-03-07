9739e4b467a7ffdc478bce9ea19f6cdfd80ad5d8
sam-keen <samuel.kleinmann@gmail.com>
2022-03-07 19:51:13 +0000

    Block id styling

--- apps/explorer/src/app/components/txs/txs-per-block/index.tsx
+++ apps/explorer/src/app/components/txs/txs-per-block/index.tsx
@@ -2,10 +2,13 @@ import useFetch from '../../../hooks/use-fetch';
 import { ChainExplorerTxResponse } from '../../../routes/types/chain-explorer-response';
 import { DATA_SOURCES } from '../../../config';
 import { Link } from 'react-router-dom';
+import { TruncateInline } from '../../truncate/truncate';
 
 interface TxsPerBlockProps {
   blockHeight: string | undefined;
 }
+
+const truncateLength = 14;
 
 export const TxsPerBlock = ({ blockHeight }: TxsPerBlockProps) => {
   const {
@@ -24,28 +27,44 @@ export const TxsPerBlock = ({ blockHeight }: TxsPerBlockProps) => {
   });
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>Transaction</td>
-          <td>From</td>
-          <td>Type</td>
-        </tr>
-      </thead>
-      <tbody>
-        {decodedBlockData &&
-          decodedBlockData.map(({ TxHash, PubKey, Type }, index) => {
-            return (
-              <tr key={index}>
-                <td>
-                  <Link to={`/txs/${TxHash}`}>{TxHash}</Link>
-                </td>
-                <td>{PubKey}</td>
-                <td>{Type}</td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </table>
+    <div className="overflow-x-auto whitespace-nowrap mb-28">
+      <table className="w-full">
+        <thead>
+          <tr className="font-mono">
+            <td>Transaction</td>
+            <td>From</td>
+            <td>Type</td>
+          </tr>
+        </thead>
+        <tbody>
+          {decodedBlockData &&
+            decodedBlockData.map(({ TxHash, PubKey, Type }, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <Link to={`/txs/${TxHash}`}>
+                      <TruncateInline
+                        text={TxHash}
+                        startChars={truncateLength}
+                        endChars={truncateLength}
+                        className="text-vega-yellow font-mono"
+                      />
+                    </Link>
+                  </td>
+                  <td>
+                    <TruncateInline
+                      text={PubKey}
+                      startChars={truncateLength}
+                      endChars={truncateLength}
+                      className="font-mono"
+                    />
+                  </td>
+                  <td>{Type}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
   );
 };
