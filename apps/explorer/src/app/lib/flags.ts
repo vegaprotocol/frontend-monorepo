98f0bfcd3b27730f151a712ad848114166060674
Dexter <dexter.edwards93@gmail.com>
2022-03-08 11:36:17 +0000

    fix process error

--- apps/explorer/src/app/lib/flags.ts
+++ apps/explorer/src/app/lib/flags.ts
@@ -1,15 +1,13 @@
-const { env } = process;
-
 const truthy = ['1', 'true'];
 
 export default {
-  assets: truthy.includes(env['NX_EXPLORER_ASSETS'] || ''),
-  genesis: truthy.includes(env['NX_EXPLORER_GENESIS'] || ''),
-  governance: truthy.includes(env['NX_EXPLORER_GOVERNANCE'] || ''),
-  markets: truthy.includes(env['NX_EXPLORER_MARKETS'] || ''),
+  assets: truthy.includes(process.env['NX_EXPLORER_ASSETS'] || ''),
+  genesis: truthy.includes(process.env['NX_EXPLORER_GENESIS'] || ''),
+  governance: truthy.includes(process.env['NX_EXPLORER_GOVERNANCE'] || ''),
+  markets: truthy.includes(process.env['NX_EXPLORER_MARKETS'] || ''),
   networkParameters: truthy.includes(
-    env['NX_EXPLORER_NETWORK_PARAMETERS'] || ''
+    process.env['NX_EXPLORER_NETWORK_PARAMETERS'] || ''
   ),
-  parties: truthy.includes(env['NX_EXPLORER_PARTIES'] || ''),
-  validators: truthy.includes(env['NX_EXPLORER_VALIDATORS'] || ''),
+  parties: truthy.includes(process.env['NX_EXPLORER_PARTIES'] || ''),
+  validators: truthy.includes(process.env['NX_EXPLORER_VALIDATORS'] || ''),
 };
