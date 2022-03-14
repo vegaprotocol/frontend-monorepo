061388938c150c14e76e8220205412acfb076c66
sam-keen <samuel.kleinmann@gmail.com>
2022-03-14 17:06:58 +0000

    Loading and error states for blocks home route

---
+++ apps/explorer/src/app/components/status-message/index.tsx
@@ -0,0 +1,12 @@
+import classnames from 'classnames';
+import React from 'react';
+
+interface StatusMessageProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const StatusMessage = ({ children, className }: StatusMessageProps) => {
+  const classes = classnames('font-alpha', 'text-h4', 'mb-28', className);
+  return <h3 className={classes}>{children}</h3>;
+};
