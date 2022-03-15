67dbb480fa33ee236875a9c471d2add882856639
Matthew Russell <mattrussell36@gmail.com>
2022-03-15 15:40:58 -0700

    inline filter logic for tif selector

--- libs/deal-ticket/src/time-in-force-selector.tsx
+++ libs/deal-ticket/src/time-in-force-selector.tsx
@@ -13,15 +13,10 @@ export const TimeInForceSelector = ({
   const options =
     order.type === OrderType.Limit
       ? Object.entries(OrderTimeInForce)
-      : Object.entries(OrderTimeInForce).filter(([key, value]) => {
-          if (
-            value === OrderTimeInForce.FOK ||
-            value === OrderTimeInForce.IOC
-          ) {
-            return true;
-          }
-          return false;
-        });
+      : Object.entries(OrderTimeInForce).filter(
+          ([key, value]) =>
+            value === OrderTimeInForce.FOK || value === OrderTimeInForce.IOC
+        );
 
   return (
     <FormGroup label="Time in force">
