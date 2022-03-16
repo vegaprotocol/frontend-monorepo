a51a04c97be9029f67989139b557da7cced7fbdc
sam-keen <samuel.kleinmann@gmail.com>
2022-03-16 12:57:24 +0000

    Added a lozenge to the ui toolkit

---
+++ libs/ui-toolkit/src/components/lozenge/lozenge.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+import classNames from 'classnames';
+
+interface LozengeProps {
+  children: ReactNode;
+  variant?: 'success' | 'warning' | 'danger' | 'highlight';
+  className?: string;
+  details?: string;
+}
+
+const getWrapperClasses = (className: LozengeProps['className']) => {
+  return classNames('inline-flex items-center gap-4', className);
+};
+
+const getLozengeClasses = (variant: LozengeProps['variant']) => {
+  return classNames(['rounded-md', 'font-mono', 'leading-none', 'p-4'], {
+    'bg-intent-success text-black': variant === 'success',
+    'bg-intent-danger text-white': variant === 'danger',
+    'bg-intent-warning text-black': variant === 'warning',
+    'bg-intent-highlight text-black': variant === 'highlight',
+    'bg-intent-help text-white': !variant,
+  });
+};
+
+export const Lozenge = ({
+  children,
+  variant,
+  className,
+  details,
+}: LozengeProps) => {
+  return (
+    <span className={getWrapperClasses(className)}>
+      <span className={getLozengeClasses(variant)}>{children}</span>
+
+      {details && <span>{details}</span>}
+    </span>
+  );
+};
