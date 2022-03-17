1bbaebd9ec45130d7cf5711a9a7b62529ba8c83b
Dexter <dexter.edwards93@gmail.com>
2022-03-17 19:37:31 +0000

    fix ignore script

--- ignore-netlify-build.js
+++ ignore-netlify-build.js
@@ -1,6 +1,6 @@
 const currentProject = process.env.NX_PROJECT_NAME;
 const execSync = require('child_process').execSync;
-const getAffected = `yarn nx print-affected`;
+const getAffected = `npx nx print-affected`;
 const output = execSync(getAffected).toString();
 //get the list of changed projects from the output
 const changedProjects = JSON.parse(output).projects; // array of affected projects
