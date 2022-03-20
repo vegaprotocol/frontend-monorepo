7473e0bef45487a892cf4bb7f78d9745297e1c15
Matthew Russell <mattrussell36@gmail.com>
2022-03-19 20:14:53 -0700

    only update row if data has changed, add test coverage

---
+++ libs/react-helpers/src/hooks/use-apply-grid-transaction.spec.ts
@@ -0,0 +1,74 @@
+import { useApplyGridTransaction } from './use-apply-grid-transaction';
+import { renderHook } from '@testing-library/react-hooks';
+import { GridApi } from 'ag-grid-community';
+
+type Items = Array<{ id: string; value: number }>;
+
+const item = {
+  id: '1',
+  value: 1,
+};
+const item2 = {
+  id: '2',
+  value: 2,
+};
+const items = [item, item2];
+
+function setup(items: Items, rowNodes: Items) {
+  const gridApiMock = {
+    applyTransaction: jest.fn(),
+    getRowNode: (id: string) => {
+      const node = rowNodes.find((i) => i.id === id);
+      if (node) {
+        return { data: node };
+      }
+      return undefined;
+    },
+  };
+  renderHook(() => useApplyGridTransaction(items, gridApiMock as any));
+  return gridApiMock;
+}
+
+test('Adds items', () => {
+  const gridApiMock = setup(items, []);
+  expect(gridApiMock.applyTransaction).toHaveBeenCalledWith({
+    update: [],
+    add: items,
+    addIndex: 0,
+  });
+});
+
+test('Doesnt update rows without changes', () => {
+  const rowNodes: Array<{ id: string; value: number }> = [...items];
+  const gridApiMock = setup(items, rowNodes);
+  expect(gridApiMock.applyTransaction).toHaveBeenCalledWith({
+    update: [],
+    add: [],
+    addIndex: 0,
+  });
+});
+
+test('Update rows with changes', () => {
+  const rowNodes = [...items];
+  const updatedItems = [
+    { id: '1', value: 10 },
+    { id: '2', value: 20 },
+  ];
+  const gridApiMock = setup(updatedItems, rowNodes);
+  expect(gridApiMock.applyTransaction).toHaveBeenCalledWith({
+    update: updatedItems,
+    add: [],
+    addIndex: 0,
+  });
+});
+
+test('Updates and adds at the same time', () => {
+  const newItem = { id: '3', value: 3 };
+  const updatedItem = { id: '2', value: 20 };
+  const gridApiMock = setup([newItem, updatedItem], [...items]);
+  expect(gridApiMock.applyTransaction).toHaveBeenCalledWith({
+    update: [updatedItem],
+    add: [newItem],
+    addIndex: 0,
+  });
+});
