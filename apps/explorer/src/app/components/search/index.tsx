ef09b807d6befb3a1f7d421b5dd7328fd4909418
Dexter <dexter.edwards93@gmail.com>
2022-03-21 10:21:05 +0000

    basic search implementation

--- apps/explorer/src/app/components/search/index.tsx
+++ apps/explorer/src/app/components/search/index.tsx
@@ -1,106 +1,72 @@
-import { gql, useQuery } from '@apollo/client';
+import { FormGroup, Input, InputError, Button } from '@vegaprotocol/ui-toolkit';
 import React from 'react';
-import { useState } from 'react';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import debounce from 'lodash.debounce';
-import { Guess, GuessVariables } from '@vegaprotocol/graphql';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { Routes } from '../../routes/router-config';
 
 const TX_LENGTH = 64;
 
-enum PossibleIdTypes {
-  Block = 'Block',
-  Tx = 'Tx',
-  Party = 'Party',
-  Market = 'Market',
-  Unknown = 'Unknown',
+interface FormFields {
+  search: string;
 }
 
-const GUESS_QUERY = gql`
-  query Guess($guess: ID!) {
-    party(id: $guess) {
-      id
-    }
-    market(id: $guess) {
-      id
-    }
-  }
-`;
-
-const usePossibleType = (search: string) => {
-  const [possibleType, setPossibleType] = useState<PossibleIdTypes>();
-  const { data, loading, error } = useQuery<Guess, GuessVariables>(
-    GUESS_QUERY,
-    {
-      variables: {
-        guess: search,
-      },
-      skip: !search,
-    }
-  );
-
-  React.useEffect(() => {
-    if (!isNaN(Number(search))) {
-      setPossibleType(PossibleIdTypes.Block);
-    } else if (data?.party) {
-      setPossibleType(PossibleIdTypes.Party);
-    } else if (data?.market) {
-      setPossibleType(PossibleIdTypes.Market);
-    } else if (search.replace('0x', '').length === TX_LENGTH) {
-      setPossibleType(PossibleIdTypes.Tx);
-    } else {
-      setPossibleType(PossibleIdTypes.Unknown);
-    }
-  }, [data?.market, data?.party, search, setPossibleType]);
-
-  return {
-    loading,
-    error,
-    possibleType,
-  };
-};
-
-const useGuess = () => {
-  const [search, setSearch] = useState<string>('');
-  const [debouncedSearch, setDebouncedSearch] = useState<string>('');
-  const { loading, error, possibleType } = usePossibleType(debouncedSearch);
-  const debouncedSearchSet = React.useMemo(
-    () => debounce(setDebouncedSearch, 1000),
-    [setDebouncedSearch]
-  );
-
-  const onChange = React.useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const search = event.target.value;
-      setSearch(search);
-      debouncedSearchSet(search);
-    },
-    [debouncedSearchSet]
-  );
-
-  return {
-    onChange,
-    search,
-    loading,
-    error,
-    possibleType,
-  };
-};
-
 const Search = () => {
-  const { search, onChange } = useGuess();
+  const { register, handleSubmit } = useForm<FormFields>();
+  const navigate = useNavigate();
+  const [error, setError] = React.useState<Error | null>(null);
+  const onSubmit = React.useCallback(
+    (fields: FormFields) => {
+      setError(null);
+
+      const search = fields.search;
+      if (!search) {
+        setError(new Error('Search required'));
+      } else if (search.startsWith('0x') && search.length === 2 + TX_LENGTH) {
+        if (Number.isNaN(Number(search))) {
+          setError(new Error('Transaction is not hexadecimal'));
+        } else {
+          navigate(`${Routes.TX}/${search}`);
+        }
+      } else if (search.length === TX_LENGTH) {
+        if (Number.isNaN(Number(`0x${search}`))) {
+          setError(new Error('Transaction is not hexadecimal'));
+        } else {
+          navigate(`${Routes.TX}/0x${search}`);
+        }
+      } else if (!Number.isNaN(Number(search))) {
+        navigate(`${Routes.BLOCKS}/${Number(search)}`);
+      } else {
+        setError(new Error("Something doesn't look right"));
+      }
+    },
+    [navigate]
+  );
   return (
-    <section>
-      <fieldset>
-        <label htmlFor="search">Search: </label>
-        <input
-          data-testid="search-field"
-          name="search"
-          value={search}
-          onChange={(e) => onChange(e)}
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex-1 flex ml-16 justify-end"
+    >
+      <FormGroup className="w-2/3" label="" labelFor="search">
+        <Input
+          {...register('search')}
+          id="search"
+          hasError={Boolean(error?.message)}
+          type="text"
+          autoFocus={true}
+          placeholder="Enter block number or transaction hash"
         />
-      </fieldset>
-    </section>
+        {error?.message ? (
+          <InputError intent="danger" className="flex-1 w-full">
+            {error.message}
+          </InputError>
+        ) : (
+          <div className="h-28"></div>
+        )}
+      </FormGroup>
+      <Button type="submit" variant="secondary">
+        Search
+      </Button>
+    </form>
   );
 };
 
