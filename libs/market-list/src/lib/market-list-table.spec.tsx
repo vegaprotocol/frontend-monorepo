8b57f6fdb1005f93a575ef12a47bc212d18e2238
Bartłomiej Głownia <bglownia@gmail.com>
2022-03-21 19:22:53 +0100

    Fix RTL warnings

--- libs/market-list/src/lib/market-list-table.spec.tsx
+++ libs/market-list/src/lib/market-list-table.spec.tsx
@@ -1,14 +1,17 @@
 import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { MockedProvider } from '@apollo/react-testing';
 import MarketListTable from './market-list-table';
 
 describe('MarketListTable', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(
-      <MockedProvider>
-        <MarketListTable />
-      </MockedProvider>
-    );
-    expect(baseElement).toBeTruthy();
+  it('should render successfully', async () => {
+    await act(async () => {
+      const { baseElement } = render(
+        <MockedProvider>
+          <MarketListTable />
+        </MockedProvider>
+      );
+      expect(baseElement).toBeTruthy();
+    });
   });
 });
