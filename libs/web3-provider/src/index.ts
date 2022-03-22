af8f106f09cc2dbd8cbd974c6cbb20b080d9cc52
Matthew Russell <mattrussell36@gmail.com>
2022-03-22 15:26:32 -0700

    rename web3-provider package to just web3

--- libs/web3-provider/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/web3-provider';
-export * from './lib/web3-connect-dialog';
--- libs/web3-provider/src/lib/types.ts
+++
@@ -1,8 +0,0 @@
-import { Web3ReactHooks } from '@web3-react/core';
-import { MetaMask } from '@web3-react/metamask';
-import { WalletConnect } from '@web3-react/walletconnect';
-
-export type Connectors = {
-  [name: string]: [Connector, Web3ReactHooks, object];
-};
-type Connector = MetaMask | WalletConnect;
