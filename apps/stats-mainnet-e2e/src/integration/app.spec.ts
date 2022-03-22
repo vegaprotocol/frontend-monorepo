666c2de6be7c9037c84fb908f60e0066f9a61347
sam-keen <samuel.kleinmann@gmail.com>
2022-03-22 15:43:40 +0000

    Correct header test for mainnet-stats app

--- apps/stats-mainnet-e2e/src/integration/app.spec.ts
+++ apps/stats-mainnet-e2e/src/integration/app.spec.ts
@@ -2,6 +2,6 @@ describe('stats-mainnet', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display header', () => {
-    cy.get('h1').should('have.text', 'Vega Explorer');
+    cy.get('h1').should('have.text', '/ Mainnet');
   });
 });
