e3aac641c2229748774eabd36dffaf1dbdd48a04
Matthew Russell <mattrussell36@gmail.com>
2022-03-22 15:26:32 -0700

    rename web3-provider package to just web3

--- libs/web3/jest.config.js
+++ libs/web3/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'web3-provider',
+  displayName: 'web3',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/web3-provider',
+  coverageDirectory: '../../coverage/libs/web3',
   setupFilesAfterEnv: ['./src/setup-tests.ts'],
 };
