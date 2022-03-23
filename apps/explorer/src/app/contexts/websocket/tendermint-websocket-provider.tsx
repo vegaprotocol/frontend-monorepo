bc61ca45e3e3631497e1f6d8cae160a108d89be0
Dexter <dexter.edwards93@gmail.com>
2022-03-23 14:15:21 +0000

    remove splash loader

--- apps/explorer/src/app/contexts/websocket/tendermint-websocket-provider.tsx
+++ apps/explorer/src/app/contexts/websocket/tendermint-websocket-provider.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import useWebSocket from "react-use-websocket";
+import React, { useState } from 'react';
+import useWebSocket from 'react-use-websocket';
 
-import { SplashLoader } from "../../components/splash-loader";
-import { SplashScreen } from "../../components/splash-screen";
-import { DATA_SOURCES } from "../../config";
-import { TendermintWebsocketContext } from "./tendermint-websocket-context";
+import { SplashScreen } from '../../components/splash-screen';
+import { DATA_SOURCES } from '../../config';
+import { TendermintWebsocketContext } from './tendermint-websocket-context';
+import { Loader } from '@vegaprotocol/ui-toolkit';
 
 /**
  * Provides a single, shared, websocket instance to the entire app to prevent recreation on every render
@@ -20,7 +20,7 @@ export const TendermintWebsocketProvider = ({
   if (!contextShape) {
     return (
       <SplashScreen>
-        <SplashLoader />
+        <Loader />
       </SplashScreen>
     );
   }
