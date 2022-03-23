3bb82730c9dfa93103a5afe99ae568730a667868
Dexter <dexter.edwards93@gmail.com>
2022-03-23 14:16:02 +0000

    remove splash

--- apps/explorer/src/app/routes/index.tsx
+++ apps/explorer/src/app/routes/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { RouteErrorBoundary } from '../components/router-error-boundary';
 
-import { SplashScreen } from '../components/splash-screen';
 import routerConfig from './router-config';
-import { Loader } from '@vegaprotocol/ui-toolkit';
+import { Loader, Splash } from '@vegaprotocol/ui-toolkit';
 
 export interface RouteChildProps {
   name: string;
@@ -14,9 +13,9 @@ export const AppRouter = () => {
   const routes = useRoutes(routerConfig);
 
   const splashLoading = (
-    <SplashScreen>
+    <Splash>
       <Loader />
-    </SplashScreen>
+    </Splash>
   );
 
   return (
