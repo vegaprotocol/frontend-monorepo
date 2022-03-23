56c00afe8c22acc4d02c0705fa7f127fcb29450d
Dexter <dexter.edwards93@gmail.com>
2022-03-23 10:54:26 +0000

    add next/previous buttons

--- apps/explorer/src/app/routes/blocks/id/index.tsx
+++ apps/explorer/src/app/routes/blocks/id/index.tsx
@@ -12,24 +12,43 @@ import {
   TableCell,
 } from '../../../components/table';
 import { TxsPerBlock } from '../../../components/txs/txs-per-block';
+import { Button } from '@vegaprotocol/ui-toolkit';
+import { Routes } from '../../router-config';
+import { SplashLoader } from '../../../components/splash-loader';
 
 const Block = () => {
   const { block } = useParams<{ block: string }>();
   const {
-    state: { data: blockData },
+    state: { data: blockData, loading, error },
   } = useFetch<TendermintBlocksResponse>(
     `${DATA_SOURCES.tendermintUrl}/block?height=${block}`
   );
 
   const header = blockData?.result.block.header;
 
-  if (!header) {
+  if (loading) {
+    return <SplashLoader />;
+  }
+
+  if (!header || error) {
     return <>Could not get block data</>;
   }
 
   return (
     <section>
       <RouteTitle>BLOCK {block}</RouteTitle>
+      <div className="grid grid-cols-2 gap-16">
+        <Link to={`/${Routes.BLOCKS}/${Number(block) - 1}`}>
+          <Button className="w-full" variant="secondary">
+            Previous
+          </Button>
+        </Link>
+        <Link to={`/${Routes.BLOCKS}/${Number(block) + 1}`}>
+          <Button className="w-full" variant="secondary">
+            Next
+          </Button>
+        </Link>
+      </div>
       <Table className="mb-28">
         <TableRow modifier="bordered">
           <TableHeader scope="row">Mined by</TableHeader>
