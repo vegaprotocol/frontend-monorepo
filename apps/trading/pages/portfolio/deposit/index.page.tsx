882e17da3f1274edbd253b67733de8afc6444683
Matthew Russell <mattrussell36@gmail.com>
2022-03-22 18:07:28 -0700

    Move ethereum config query to web3 container

--- apps/trading/pages/portfolio/deposit/index.page.tsx
+++ apps/trading/pages/portfolio/deposit/index.page.tsx
@@ -1,77 +1,18 @@
-import { gql } from '@apollo/client';
-import { NetworkParametersQuery } from '@vegaprotocol/graphql';
-import { Splash } from '@vegaprotocol/ui-toolkit';
-import { PageQueryContainer } from '../../../components/page-query-container';
 import { Web3Container } from '../../../components/web3-container';
 import { DepositForm } from './deposit-form';
-
-interface EthereumConfig {
-  network_id: string;
-  chain_id: string;
-  confirmations: number;
-  collateral_bridge_contract: {
-    address: string;
-  };
-  multisig_control_contract: {
-    address: string;
-    deployment_block_height: number;
-  };
-  staking_bridge_contract: {
-    address: string;
-    deployment_block_height: number;
-  };
-  token_vesting_contract: {
-    address: string;
-    deployment_block_height: number;
-  };
-}
-
-const NETWORK_PARAMS_QUERY = gql`
-  query NetworkParametersQuery {
-    networkParameters {
-      key
-      value
-    }
-  }
-`;
 
 const Deposit = () => {
   return (
     <Web3Container>
-      <PageQueryContainer<NetworkParametersQuery> query={NETWORK_PARAMS_QUERY}>
-        {(data) => {
-          const ethereumConfigParam = data.networkParameters.find(
-            (np) => np.key === 'blockchains.ethereumConfig'
-          );
-
-          if (!ethereumConfigParam) {
-            return (
-              <Splash>
-                <p>No ethereum config found</p>
-              </Splash>
-            );
-          }
-
-          let config: EthereumConfig;
-
-          try {
-            config = JSON.parse(ethereumConfigParam.value);
-          } catch {
-            return (
-              <Splash>
-                <p>Could not parse config</p>
-              </Splash>
-            );
-          }
-
-          return (
-            <div className="p-24">
-              <h1 className="text-h3">Deposit</h1>
-              <DepositForm />
-            </div>
-          );
-        }}
-      </PageQueryContainer>
+      {({ ethereumConfig }) => (
+        <div className="p-24">
+          <h1 className="text-h3">Deposit</h1>
+          <p>
+            Bridge address: {ethereumConfig.collateral_bridge_contract.address}
+          </p>
+          <DepositForm />
+        </div>
+      )}
     </Web3Container>
   );
 };
