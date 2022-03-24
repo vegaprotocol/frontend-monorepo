56b6933ea3c14dc1ecf2a311795b348278b44937
Joe Tsang <30622993+jtsang586@users.noreply.github.com>
2022-03-24 09:48:27 +0000

    Additional search tests added (#124)

--- apps/explorer-e2e/src/support/step_definitions/common.step.js
+++ apps/explorer-e2e/src/support/step_definitions/common.step.js
@@ -1,5 +1,20 @@
 import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+import BasePage from '../pages/base-page';
+const basePage = new BasePage();
 
 Given('I am on the homepage', () => {
   cy.visit('/');
+});
+
+When('I search for {string}', (searchText) => {
+  basePage.search(searchText);
+  basePage.clickSearch();
+});
+
+Then('I am redirected to page containing id {string}', (expectedUrl) => {
+  basePage.validateUrl(expectedUrl);
+});
+
+Then('search error message {string} is displayed', (expectedErrorMsg) => {
+  basePage.validateSearchErrorDisplayed(expectedErrorMsg);
 });
