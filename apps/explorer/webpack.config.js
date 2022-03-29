e4578a198f6ad6abd189a5e30bcabe7a82a3c210
Dexter Edwards <dexter.edwards93@gmail.com>
2022-03-29 18:29:29 +0100

    Chore/sentry (#163)
    * begin adding sentry
    
    * add sentry webpack config
    
    * rename environment variable
    
    * make sourcemaps work for sentry
    
    * set environmenet based on vega environment
    
    * rename envs
    
    * remove throw error button

---
+++ apps/explorer/webpack.config.js
@@ -0,0 +1,16 @@
+const SentryPlugin = require('@sentry/webpack-plugin');
+
+module.exports = (config, context) => {
+  const additionalPlugins = process.env.SENTRY_AUTH_TOKEN
+    ? [
+        new SentryPlugin({
+          include: './dist/apps/explorer',
+          project: 'block-explorer',
+        }),
+      ]
+    : [];
+  return {
+    ...config,
+    plugins: [...additionalPlugins, ...config.plugins],
+  };
+};
