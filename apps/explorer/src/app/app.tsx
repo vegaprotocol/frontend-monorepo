2e20df4dba576e7c9a3d654ca517645b529fd746
sam-keen <samuel.kleinmann@gmail.com>
2022-03-29 13:13:25 +0100

    Added theme switcher and tweaked header to accommodate

--- apps/explorer/src/app/app.tsx
+++ apps/explorer/src/app/app.tsx
@@ -1,4 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
+import { ThemeContext } from '@vegaprotocol/react-helpers';
+import { useThemeSwitcher } from '@vegaprotocol/react-helpers';
 import { createClient } from './lib/apollo-client';
 import { Nav } from './components/nav';
 import { Header } from './components/header';
@@ -8,22 +10,27 @@ import { DATA_SOURCES } from './config';
 import { TendermintWebsocketProvider } from './contexts/websocket/tendermint-websocket-provider';
 
 function App() {
+  const [theme, toggleTheme] = useThemeSwitcher();
+
   const client = React.useMemo(
     () => createClient(DATA_SOURCES.dataNodeUrl),
     []
   );
+
   return (
-    <TendermintWebsocketProvider>
-      <ApolloProvider client={client}>
-        <div className="antialiased m-0 min-h-[100vh] max-w-[1300px] grid grid-rows-[1fr_min-content] bg-black text-white border-white lg:border-l-1 lg:border-r-1">
-          <div className="grid grid-rows-[auto_minmax(700px,_1fr)] grid-cols-[300px_minmax(auto,_1fr)] border-b-1">
-            <Nav />
-            <Header />
-            <Main />
+    <ThemeContext.Provider value={theme}>
+      <TendermintWebsocketProvider>
+        <ApolloProvider client={client}>
+          <div className="antialiased m-0 min-h-[100vh] max-w-[1300px] grid grid-rows-[1fr_min-content] bg-white dark:bg-black text-black dark:text-white border-white lg:border-l-1 lg:border-r-1">
+            <div className="grid grid-rows-[auto_minmax(700px,_1fr)] grid-cols-[300px_minmax(auto,_1fr)] border-b-1">
+              <Nav />
+              <Header toggleTheme={toggleTheme} />
+              <Main />
+            </div>
           </div>
-        </div>
-      </ApolloProvider>
-    </TendermintWebsocketProvider>
+        </ApolloProvider>
+      </TendermintWebsocketProvider>
+    </ThemeContext.Provider>
   );
 }
 
