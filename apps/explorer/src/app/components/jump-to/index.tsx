7e3e098ae456581299e2ba8768a9265fe3f9b627
Sam Keen <samuel@vegaprotocol.io>
2022-03-29 19:08:33 +0100

    Feat/76 Use UI toolkit components for 'jump to block' (#147)
    * frontend-monorepo-76 Use UI toolkit components for 'jump to block'
    
    * frontend-monorepo-76 Use UI toolkit components for 'go to party'
    
    * Added reusable component for 'jump to block' and 'go to party'

---
+++ apps/explorer/src/app/components/jump-to/index.tsx
@@ -0,0 +1,43 @@
+import { HTMLInputTypeAttribute, SyntheticEvent } from 'react';
+import { Input, Button } from '@vegaprotocol/ui-toolkit';
+
+interface JumpToProps {
+  label: string;
+  placeholder: string;
+  inputId: string;
+  inputType: HTMLInputTypeAttribute;
+  inputName: string;
+  submitHandler: (arg0: SyntheticEvent) => void;
+}
+
+export const JumpTo = ({
+  label,
+  placeholder,
+  inputId,
+  inputType,
+  inputName,
+  submitHandler,
+}: JumpToProps) => {
+  return (
+    <form onSubmit={submitHandler}>
+      <label
+        htmlFor={inputId}
+        className="block uppercase text-h5 font-bold mb-4"
+      >
+        {label}
+      </label>
+      <div className="flex">
+        <Input
+          id={inputId}
+          type={inputType}
+          name={inputName}
+          placeholder={placeholder}
+          className="max-w-[200px]"
+        />
+        <Button variant="secondary" type="submit">
+          Go
+        </Button>
+      </div>
+    </form>
+  );
+};
