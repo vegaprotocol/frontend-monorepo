95e846d6af40f4834bd63429d8ce46a2f6ff1cc5
sam-keen <samuel.kleinmann@gmail.com>
2022-03-29 13:13:25 +0100

    Added theme switcher and tweaked header to accommodate

--- apps/explorer/src/app/components/header/header.tsx
+++ apps/explorer/src/app/components/header/header.tsx
@@ -1,8 +1,13 @@
+import { ThemeSwitcher } from '@vegaprotocol/ui-toolkit';
 import { Search } from '../search';
 
-export const Header = () => {
+interface ThemeToggleProps {
+  toggleTheme: () => void;
+}
+
+export const Header = ({ toggleTheme }: ThemeToggleProps) => {
   return (
-    <header className="flex px-16 pt-16 pb-8 border-b-1 col-start-1 col-end-3 row-start-1 row-end-2">
+    <header className="flex items-center px-16 py-16 border-b-1 col-start-1 col-end-3 row-start-1 row-end-2">
       <h1
         className="text-h3 font-alpha uppercase contextual-alternates"
         data-testid="explorer-header"
@@ -10,6 +15,7 @@ export const Header = () => {
         Vega Explorer
       </h1>
       <Search />
+      <ThemeSwitcher onToggle={toggleTheme} />
     </header>
   );
 };
