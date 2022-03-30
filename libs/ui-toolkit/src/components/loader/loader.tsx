eacaddc2acd457c19e1b35bde8af20b5a32ae79f
Matthew Russell <mattrussell36@gmail.com>
2022-03-30 15:15:52 -0700

    add small/large options for loader

--- libs/ui-toolkit/src/components/loader/loader.tsx
+++ libs/ui-toolkit/src/components/loader/loader.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 
-export const Loader = () => {
+interface LoaderProps {
+  size?: 'small' | 'large';
+}
+
+export const Loader = ({ size = 'large' }: LoaderProps) => {
   const [, forceRender] = useState(false);
 
   useEffect(() => {
@@ -11,13 +15,17 @@ export const Loader = () => {
     return () => clearInterval(interval);
   }, []);
 
+  const wrapperClasses = size === 'small' ? 'w-[15px] h-[15px]' : 'w-64 h-64';
+  const gridItemClasses = size === 'small' ? 'w-[5px] h-[5px]' : 'w-16 h-16';
+  const items = size === 'small' ? 9 : 16;
+
   return (
     <div className="flex flex-col items-center" data-testid="splash-loader">
-      <div className="w-64 h-64 flex flex-wrap">
-        {new Array(16).fill(null).map((_, i) => {
+      <div className={`${wrapperClasses} flex flex-wrap`}>
+        {new Array(items).fill(null).map((_, i) => {
           return (
             <div
-              className="w-16 h-16 dark:bg-white bg-black"
+              className={`${gridItemClasses} dark:bg-white bg-black`}
               key={i}
               style={{
                 opacity: Math.random() > 0.75 ? 1 : 0,
