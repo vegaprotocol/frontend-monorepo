ed51e4805d5bbfeb64a4c3c456ffcca43b7bfa65
sam-keen <samuel.kleinmann@gmail.com>
2022-04-01 14:59:33 +0100

    Made the logo into a link to the home route

--- apps/explorer/src/app/components/header/header.tsx
+++ apps/explorer/src/app/components/header/header.tsx
@@ -1,7 +1,9 @@
 import type { Dispatch, SetStateAction } from 'react';
+import { Link } from 'react-router-dom';
 import { ThemeSwitcher, Icon } from '@vegaprotocol/ui-toolkit';
 import { t } from '@vegaprotocol/react-helpers';
 import { Search } from '../search';
+import { Routes } from '../../routes/router-config';
 
 interface ThemeToggleProps {
   toggleTheme: () => void;
@@ -16,12 +18,14 @@ export const Header = ({
 }: ThemeToggleProps) => {
   return (
     <header className="grid grid-rows-2 grid-cols-[1fr_auto] md:flex md:col-span-2 p-16 gap-12 border-b-1">
-      <h1
-        className="text-h3 font-alpha uppercase calt"
-        data-testid="explorer-header"
-      >
-        {t('Vega Explorer')}
-      </h1>
+      <Link to={Routes.HOME}>
+        <h1
+          className="text-h3 font-alpha uppercase calt"
+          data-testid="explorer-header"
+        >
+          {t('Vega Explorer')}
+        </h1>
+      </Link>
       <button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
         <Icon name={menuOpen ? 'cross' : 'menu'} size={20} />
       </button>
