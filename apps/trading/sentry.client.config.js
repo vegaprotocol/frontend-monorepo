775a97bbaa2043185f1fce76cf58a9a1157cea05
Matthew Russell <mattrussell36@gmail.com>
2022-03-31 17:37:03 -0700

    add browser tracing plugin

--- apps/trading/sentry.client.config.js
+++ apps/trading/sentry.client.config.js
@@ -1,11 +1,12 @@
 import * as Sentry from '@sentry/nextjs';
+import { BrowserTracing } from '@sentry/tracing';
 
 const dsn = process.env['NX_TRADING_SENTRY_DSN'];
 
 if (dsn) {
   Sentry.init({
     dsn,
-    // integrations: [new BrowserTracing()],
+    integrations: [new BrowserTracing()],
     tracesSampleRate: 1,
     environment: process.env['NX_VEGA_ENV'],
   });
