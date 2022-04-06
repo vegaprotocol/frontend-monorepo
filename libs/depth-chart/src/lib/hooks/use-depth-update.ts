22889275b541695564853923385e7a6abebe2a83
johnwalley <john@walley.org.uk>
2022-04-06 17:58:32 +0100

    Refactor updateDepthUpdate function

--- libs/depth-chart/src/lib/hooks/use-depth-update.ts
+++ libs/depth-chart/src/lib/hooks/use-depth-update.ts
@@ -1,8 +1,8 @@
 import type { ApolloError } from '@apollo/client';
 import { useApolloClient } from '@apollo/client';
-import sortBy from 'lodash/sortby';
 import throttle from 'lodash/throttle';
 import * as React from 'react';
+import { updateDepthUpdate } from '../helpers';
 import {
   marketDepthQuery,
   marketDepthUpdateSubscription,
@@ -10,96 +10,11 @@ import {
 import type {
   marketDepth,
   marketDepthVariables,
-  marketDepth_market_depth,
 } from '../__generated__/marketDepth';
 import type {
   marketDepthUpdateSubscribe,
   marketDepthUpdateSubscribeVariables,
 } from '../__generated__/marketDepthUpdateSubscribe';
-
-export function updateDepthUpdate(
-  prev: marketDepth,
-  subscriptionData: { data: marketDepthUpdateSubscribe }
-): marketDepth {
-  if (!subscriptionData.data.marketDepthUpdate || !prev.market) {
-    return prev;
-  }
-
-  return {
-    ...prev,
-    market: {
-      ...prev.market,
-      ...(prev.market.data && {
-        data: {
-          ...prev.market.data,
-          midPrice:
-            subscriptionData.data.marketDepthUpdate.market.data?.midPrice ??
-            prev.market.data.midPrice,
-        },
-      }),
-      depth: {
-        ...prev.market.depth,
-        ...merge(prev.market.depth, subscriptionData.data.marketDepthUpdate),
-      },
-    },
-  };
-}
-
-type MarketDepth = Pick<marketDepth_market_depth, 'buy' | 'sell'>;
-
-function merge(snapshot: MarketDepth, update: MarketDepth): MarketDepth {
-  let buy = snapshot.buy ? [...snapshot.buy] : null;
-  let sell = snapshot.sell ? [...snapshot.sell] : null;
-
-  if (buy !== null) {
-    if (update.buy !== null) {
-      for (const priceLevel of update.buy) {
-        const index = buy.findIndex(
-          (level) => level.price === priceLevel.price
-        );
-
-        if (index !== -1) {
-          if (priceLevel.volume !== '0') {
-            buy.splice(index, 1, priceLevel);
-          } else {
-            buy.splice(index, 1);
-          }
-        } else {
-          buy.push(priceLevel);
-        }
-      }
-    }
-  } else {
-    buy = update.buy;
-  }
-
-  if (sell !== null) {
-    if (update.sell !== null) {
-      for (const priceLevel of update.sell) {
-        const index = sell.findIndex(
-          (level) => level.price === priceLevel.price
-        );
-
-        if (index !== -1) {
-          if (priceLevel.volume !== '0') {
-            sell.splice(index, 1, priceLevel);
-          } else {
-            sell.splice(index, 1);
-          }
-        } else {
-          sell.push(priceLevel);
-        }
-      }
-    }
-  } else {
-    sell = update.sell;
-  }
-
-  return {
-    buy: sortBy(buy, (d) => -parseInt(d.price)),
-    sell: sortBy(sell, (d) => parseInt(d.price)),
-  };
-}
 
 export interface QueryResult<TData> {
   data: TData | undefined;
