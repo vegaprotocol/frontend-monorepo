1864df6f123a919be119b973a01b7e3663d9d9cc
Edd <edd@vega.xyz>
2022-04-07 11:08:11 +0100

    Output ls

--- ignore-netlify-build.js
+++ ignore-netlify-build.js
@@ -4,13 +4,22 @@ const cwd = execSync('pwd').toString();
 const ls = execSync('ls').toString();
 
 const currentProject = process.env.NX_PROJECT_NAME;
+console.log(ls);
 console.log(`Checking affected for: ${currentProject} in ${cwd}`);
+
 const output = execSync(getAffected).toString();
 
 console.dir(output);
 
 //get the list of changed projects from the output
 const changedProjects = JSON.parse(output).projects; // array of affected projects
-process.exitCode = changedProjects.some((project) => project === currentProject)
+console.dir(changedProjects);
+const isThisProjectChanged = changedProjects.some(
+  (project) => project === currentProject
+)
   ? 0
   : 1;
+
+console.log(`Did this project change? ${isThisProjectChanged}`);
+
+process.exitCode = isThisProjectChanged;
