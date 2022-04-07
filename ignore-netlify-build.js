48948fad01c96af2027f87a0778a966e4bd8e5a9
Edd <edd@vega.xyz>
2022-04-07 09:54:06 +0000

    More vocal ignore file

--- ignore-netlify-build.js
+++ ignore-netlify-build.js
@@ -1,7 +1,15 @@
-const currentProject = process.env.NX_PROJECT_NAME;
 const execSync = require('child_process').execSync;
-const getAffected = `npx nx print-affected`;
+const getAffected = `npx nx@13.8.3 print-affected`;
+const cwd = execSync('pwd').toString();
+const ls = execSync('ls').toString();
+
+
+const currentProject = process.env.NX_PROJECT_NAME;
+console.log(`Checking affected for: ${currentProject} in ${cwd}`);
 const output = execSync(getAffected).toString();
+
+console.dir(output);
+
 //get the list of changed projects from the output
 const changedProjects = JSON.parse(output).projects; // array of affected projects
 process.exitCode = changedProjects.some((project) => project === currentProject)
