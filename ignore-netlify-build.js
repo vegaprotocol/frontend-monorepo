6b2c45d38d8c796777cf1e66cbd8afaf12a86edf
Edd <edd@vega.xyz>
2022-04-07 12:13:28 +0100

    Remove custom ignore

--- ignore-netlify-build.js
+++
@@ -1,9 +0,0 @@
-const currentProject = process.env.NX_PROJECT_NAME;
-const execSync = require('child_process').execSync;
-const getAffected = `npx nx print-affected`;
-const output = execSync(getAffected).toString();
-//get the list of changed projects from the output
-const changedProjects = JSON.parse(output).projects; // array of affected projects
-process.exitCode = changedProjects.some((project) => project === currentProject)
-  ? 0
-  : 1;
