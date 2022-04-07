4cebe959b43b28cfd57a7efbbc06a0894387e2cb
johnwalley <john@walley.org.uk>
2022-04-07 10:06:03 +0100

    Add padding and margin to chart container

--- apps/trading/components/chart-container/chart-container.tsx
+++ apps/trading/components/chart-container/chart-container.tsx
@@ -13,19 +13,20 @@ export const ChartContainer = ({ marketId }: ChartContainerProps) => {
   const [chartType, setChartType] = useState<ChartType>('trading');
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <ButtonRadio
-        name="chart-type"
-        options={[
-          { text: 'Trading view', value: 'trading' },
-          { text: 'Depth', value: 'depth' },
-        ]}
-        currentOption={chartType}
-        onSelect={(value) => {
-          setChartType(value);
-        }}
-      />
-
+    <div className="px-4 py-8 flex flex-col h-full">
+      <div className="mb-4">
+        <ButtonRadio
+          name="chart-type"
+          options={[
+            { text: 'Trading view', value: 'trading' },
+            { text: 'Depth', value: 'depth' },
+          ]}
+          currentOption={chartType}
+          onSelect={(value) => {
+            setChartType(value);
+          }}
+        />
+      </div>
       <div className="flex-1">
         {chartType === 'trading' ? (
           <TradingChartContainer marketId={marketId} />
