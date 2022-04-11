459540446e7f9843545c46fa9e296db3f15c375a
Elmar Gasimov <elmar@vegaprotocol.io>
2022-04-11 13:13:33 +0100

    Initial commit after nx create

---
+++ apps/simple-trading-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'simple-trading-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/simple-trading-app',
+};
