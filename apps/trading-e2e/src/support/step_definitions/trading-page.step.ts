42782c65e124afde3462f3d6574e2c39e96413e4
Joe <joe@vega.xyz>
2022-04-14 15:43:18 +0100

    Test passing for trades list

--- apps/trading-e2e/src/support/step_definitions/trading-page.step.ts
+++ apps/trading-e2e/src/support/step_definitions/trading-page.step.ts
@@ -1,4 +1,4 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps';
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 import { hasOperationName } from '..';
 import { MarketState } from '@vegaprotocol/types';
 /* eslint-disable @nrwl/nx/enforce-module-boundaries */
@@ -9,6 +9,11 @@ import {
 } from '../../../../../libs/chart/src/__tests__';
 import { generateDealTicketQuery } from '../../../../../libs/deal-ticket/src/__tests__';
 import { generateMarket } from '../../../../trading/pages/markets/__tests__';
+import TradesList from '../trading-windows/trades-list';
+import TradingPage from '../pages/trading-page';
+
+const tradesList = new TradesList();
+const tradingPage = new TradingPage();
 /* eslint-enable @nrwl/nx/enforce-module-boundaries */
 
 const mockMarket = (state: MarketState) => {
@@ -65,4 +70,21 @@ Given('I am on the trading page for a suspended market', () => {
   cy.visit('/markets/market-id');
   cy.wait('@Market');
   cy.contains('Market: SUSPENDED MARKET');
+});
+
+Then('trading page for {string} market is displayed', (marketType) => {
+  switch (marketType) {
+    case 'active':
+      mockMarket(MarketState.Active);
+      cy.wait('@Market');
+      cy.contains('Market: ACTIVE MARKET');
+      break;
+    case 'suspended':
+      mockMarket(MarketState.Suspended);
+      cy.wait('@Market');
+      cy.contains('Market: SUSPENDED MARKET');
+      break;
+  }
+  tradingPage.clickOnTradesTab();
+  tradesList.verifyTradesListDisplayed();
 });
---
+++ apps/trading-e2e/src/support/trading-windows/trades-list.js
@@ -0,0 +1,24 @@
+export default class TradesList {
+  colIdPrice = 'price';
+  colIdSize = 'size';
+  colIdCreatedAt = 'createdAt';
+
+  verifyTradesListDisplayed() {
+    cy.get(`[col-id=${this.colIdPrice}]`).each(($tradePrice) => {
+      cy.wrap($tradePrice).invoke('text').should('not.be.empty');
+    });
+    cy.get(`[col-id=${this.colIdSize}]`).each(($tradeSize) => {
+      cy.wrap($tradeSize).invoke('text').should('not.be.empty');
+    });
+
+    const dateTimeRegex =
+      /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2}):(\d{2})/gm;
+    cy.get(`[col-id=${this.colIdCreatedAt}]`).each(($tradeDateTime, index) => {
+      if (index != 0) {
+        //ignore header
+        cy.log(index);
+        cy.wrap($tradeDateTime).invoke('text').should('match', dateTimeRegex);
+      }
+    });
+  }
+}
