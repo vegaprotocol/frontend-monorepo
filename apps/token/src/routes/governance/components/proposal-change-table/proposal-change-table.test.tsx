7e6c2214cf50ae8bb6ba23f747cbe0fad0b91666
Matthew Russell <mattrussell36@gmail.com>
2022-04-19 13:25:56 -0700

    setup i18n for tests so that translations are used, proposal change table test

--- apps/token/src/routes/governance/components/proposal-change-table/proposal-change-table.test.tsx
+++ apps/token/src/routes/governance/components/proposal-change-table/proposal-change-table.test.tsx
@@ -1,74 +1,74 @@
-import { render, screen } from "@testing-library/react";
-import { format } from "date-fns";
+import { render, screen } from '@testing-library/react';
+import { format } from 'date-fns';
 
 import {
   ProposalRejectionReason,
   ProposalState,
-} from "../../../../__generated__/globalTypes";
-import { DATE_FORMAT_DETAILED } from "../../../../lib/date-formats";
-import { generateProposal } from "../../test-helpers/generate-proposals";
-import { ProposalChangeTable } from "./proposal-change-table";
+} from '../../../../__generated__/globalTypes';
+import { DATE_FORMAT_DETAILED } from '../../../../lib/date-formats';
+import { generateProposal } from '../../test-helpers/generate-proposals';
+import { ProposalChangeTable } from './proposal-change-table';
 
-it("Renders with data-testid", () => {
+it('Renders with data-testid', () => {
   const proposal = generateProposal();
   render(<ProposalChangeTable proposal={proposal} />);
-  expect(screen.getByTestId("proposal-change-table")).toBeInTheDocument();
+  expect(screen.getByTestId('proposal-change-table')).toBeInTheDocument();
 });
 
-it("Renders all data for table", () => {
+it('Renders all data for table', () => {
   const proposal = generateProposal();
   render(<ProposalChangeTable proposal={proposal} />);
-  expect(screen.getByText("id")).toBeInTheDocument();
+  expect(screen.getByText('ID')).toBeInTheDocument();
   expect(screen.getByText(proposal.id!)).toBeInTheDocument();
 
-  expect(screen.getByText("state")).toBeInTheDocument();
-  expect(screen.getByText("Open")).toBeInTheDocument();
+  expect(screen.getByText('State')).toBeInTheDocument();
+  expect(screen.getByText('Open')).toBeInTheDocument();
 
-  expect(screen.getByText("closesOn")).toBeInTheDocument();
+  expect(screen.getByText('Closes on')).toBeInTheDocument();
   expect(
     screen.getByText(
       format(new Date(proposal.terms.closingDatetime), DATE_FORMAT_DETAILED)
     )
   ).toBeInTheDocument();
 
-  expect(screen.getByText("proposedEnactment")).toBeInTheDocument();
+  expect(screen.getByText('Proposed enactment')).toBeInTheDocument();
   expect(
     screen.getByText(
       format(new Date(proposal.terms.enactmentDatetime), DATE_FORMAT_DETAILED)
     )
   ).toBeInTheDocument();
 
-  expect(screen.getByText("proposedBy")).toBeInTheDocument();
+  expect(screen.getByText('Proposed by')).toBeInTheDocument();
   expect(screen.getByText(proposal.party.id!)).toBeInTheDocument();
 
-  expect(screen.getByText("proposedOn")).toBeInTheDocument();
+  expect(screen.getByText('Proposed on')).toBeInTheDocument();
   expect(
     screen.getByText(format(new Date(proposal.datetime), DATE_FORMAT_DETAILED))
   ).toBeInTheDocument();
 
-  expect(screen.getByText("type")).toBeInTheDocument();
+  expect(screen.getByText('Type')).toBeInTheDocument();
   expect(
     screen.getByText(proposal.terms.change.__typename)
   ).toBeInTheDocument();
 });
 
-it("Changes data based on if data is in future or past", () => {
+it('Changes data based on if data is in future or past', () => {
   const proposal = generateProposal({
     state: ProposalState.Enacted,
   });
   render(<ProposalChangeTable proposal={proposal} />);
 
-  expect(screen.getByText("state")).toBeInTheDocument();
-  expect(screen.getByText("Enacted")).toBeInTheDocument();
+  expect(screen.getByText('State')).toBeInTheDocument();
+  expect(screen.getByText('Enacted')).toBeInTheDocument();
 
-  expect(screen.getByText("closedOn")).toBeInTheDocument();
+  expect(screen.getByText('Closed on')).toBeInTheDocument();
   expect(
     screen.getByText(
       format(new Date(proposal.terms.closingDatetime), DATE_FORMAT_DETAILED)
     )
   ).toBeInTheDocument();
 
-  expect(screen.getByText("enactedOn")).toBeInTheDocument();
+  expect(screen.getByText('Enacted on')).toBeInTheDocument();
   expect(
     screen.getByText(
       format(new Date(proposal.terms.enactmentDatetime), DATE_FORMAT_DETAILED)
@@ -76,16 +76,17 @@ it("Changes data based on if data is in future or past", () => {
   ).toBeInTheDocument();
 });
 
-it("Renders error details and rejection reason if present", () => {
+it('Renders error details and rejection reason if present', () => {
+  const errorDetails = 'Error message';
   const proposal = generateProposal({
-    errorDetails: "Error details",
+    errorDetails,
     rejectionReason: ProposalRejectionReason.CloseTimeTooLate,
   });
   render(<ProposalChangeTable proposal={proposal} />);
-  expect(screen.getByText("errorDetails")).toBeInTheDocument();
-  expect(screen.getByText("Error details")).toBeInTheDocument();
+  expect(screen.getByText('Error details')).toBeInTheDocument();
+  expect(screen.getByText(errorDetails)).toBeInTheDocument();
 
-  expect(screen.getByText("rejectionReason")).toBeInTheDocument();
+  expect(screen.getByText('Rejection reason')).toBeInTheDocument();
   expect(
     screen.getByText(ProposalRejectionReason.CloseTimeTooLate)
   ).toBeInTheDocument();
--- apps/token/src/setup-tests.ts
+++ apps/token/src/setup-tests.ts
@@ -2,4 +2,23 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom';
+import dev from './i18n/translations/dev.json';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// Set up i18n instance so that components have the correct default
+// en translations
+i18n.use(initReactI18next).init({
+  // we init with resources
+  resources: {
+    en: {
+      translations: {
+        ...dev,
+      },
+    },
+  },
+  fallbackLng: 'en',
+  ns: ['translations'],
+  defaultNS: 'translations',
+});
