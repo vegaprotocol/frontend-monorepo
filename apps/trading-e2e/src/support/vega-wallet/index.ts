f0fdecb490fb278c77cf01db3d96228ffcaa65fb
Joe Tsang <30622993+jtsang586@users.noreply.github.com>
2022-04-19 12:12:08 +0100

    Test/234 switch public key (#254)
    * Test for switching public key
    
    * Add missing variable
    
    * Add background step
    
    * Fix yaml error
    
    * Increase timeout for cy.wait
    
    * Assert pub key using Cypress env variable
    
    * Fix module error
    
    * Add string
    
    * Remove timeout
    
    * Removed react-helper import and added Cypress env variable

--- apps/trading-e2e/src/support/vega-wallet/index.ts
+++ apps/trading-e2e/src/support/vega-wallet/index.ts
@@ -2,12 +2,13 @@ export default class VegaWallet {
   connectVegaBtn = 'connect-vega-wallet';
   walletConnectors = 'connectors-list';
   walletForm = 'rest-connector-form';
+  selectPublicKeyBtn = 'select-keypair-button';
   walletInputError = 'input-wallet-error';
   walletFormError = 'form-error';
   inputError = 'input-error-text';
 
   openVegaWalletConnectDialog() {
-    cy.getByTestId(this.connectVegaBtn).click();
+    this.clickOnWalletConnectDialog();
     cy.contains('Connects using REST to a running Vega wallet service');
     cy.getByTestId(this.walletConnectors).find('button').click();
   }
@@ -39,5 +40,20 @@ export default class VegaWallet {
   validateWalletErrorFieldsDisplayed() {
     cy.getByTestId(this.walletInputError).should('have.text', 'Required');
     cy.getByTestId(this.inputError).should('have.text', 'Required');
+  }
+
+  validatePublicKeyDisplayed(expectedTruncatedKey: string) {
+    cy.getByTestId(this.connectVegaBtn).should(
+      'have.text',
+      expectedTruncatedKey
+    );
+  }
+
+  selectPublicKey() {
+    cy.getByTestId(this.selectPublicKeyBtn).click();
+  }
+
+  clickOnWalletConnectDialog() {
+    cy.getByTestId(this.connectVegaBtn).click();
   }
 }
