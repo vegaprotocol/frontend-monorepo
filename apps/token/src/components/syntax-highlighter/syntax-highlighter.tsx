ada6167bb3361ae69c807e44f79b1def1e85d64a
Matthew Russell <mattrussell36@gmail.com>
2022-04-19 16:40:31 -0700

    add comment and link to issue for syntax highlighter changes

--- apps/token/src/components/syntax-highlighter/syntax-highlighter.tsx
+++ apps/token/src/components/syntax-highlighter/syntax-highlighter.tsx
@@ -1,28 +1,27 @@
-import React from "react";
-import Highlighter from "react-syntax-highlighter";
+// TODO: Move to ui-toolkit https://github.com/vegaprotocol/frontend-monorepo/issues/272
+import Highlighter from 'react-syntax-highlighter';
 
 const vegaJsonTheme = {
   hljs: {
-    fontSize: "1rem",
+    fontSize: '1rem',
     fontFamily: "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
-    display: "block",
-    overflowX: "auto",
-    padding: "1em",
-    color: "#26ff8a",
-    background: "#2C2C2C",
-    border: "1px solid #696969",
+    display: 'block',
+    overflowX: 'auto',
+    padding: '1em',
+    color: '#26ff8a',
+    background: '#2C2C2C',
+    border: '1px solid #696969',
   },
-  "hljs-literal": {
-    color: "#ff2d5e",
+  'hljs-literal': {
+    color: '#ff2d5e',
   },
-  "hljs-number": {
-    color: "#ff7a1a",
+  'hljs-number': {
+    color: '#ff7a1a',
   },
-  "hljs-string": {
-    color: "#48aff0",
+  'hljs-string': {
+    color: '#48aff0',
   },
 };
-
 
 export const SyntaxHighlighter = ({ data }: { data: object }) => {
   return (
