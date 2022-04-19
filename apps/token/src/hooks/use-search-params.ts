54a4121f1c12599e59056abe1725f439aa134569
sam-keen <samuel.kleinmann@gmail.com>
2022-04-19 12:28:49 +0100

    Used 'Object.entries' instead of 'Array.from' to create iterable from object in 'use-search-params'

--- apps/token/src/hooks/use-search-params.ts
+++ apps/token/src/hooks/use-search-params.ts
@@ -1,16 +1,14 @@
-import * as React from "react";
-import { useLocation } from "react-router-dom";
+import * as React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export function useSearchParams() {
   const location = useLocation();
 
-  const queryStringParams = React.useMemo(() => {
+  return React.useMemo(() => {
     const params = new URLSearchParams(location.search);
-    return Array.from(params).reduce((obj, [key, value]) => {
+    return Object.entries(params).reduce((obj, [key, value]) => {
       obj[key] = value;
       return obj;
     }, {} as any);
   }, [location]);
-
-  return queryStringParams;
 }
