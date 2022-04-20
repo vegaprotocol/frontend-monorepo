8c6a12e66c33604c653018756c15a5e7ee3a26f0
Bartłomiej Głownia <bglownia@gmail.com>
2022-04-20 17:47:16 +0200

    [#151] Add horizontal line under header in orderbook

--- libs/orderbook/src/lib/orderbook.tsx
+++ libs/orderbook/src/lib/orderbook.tsx
@@ -8,25 +8,29 @@ interface OrderbookProps {
 }
 
 export const Orderbook = ({ data, decimalPlaces }: OrderbookProps) => (
-  <div className="grid grid-cols-4 gap-4 text-right text-ui-small">
-    <div className="text-left">{t('Bid Vol')}</div>
-    <div className="text-left">{t('Price')}</div>
-    <div className="text-left">{t('Ask Vol')}</div>
-    <div className="text-left">{t('Cumulative Vol')}</div>
-    {data?.map((data) => (
-      <OrderbookRow
-        key={data.price}
-        price={data.price}
-        decimalPlaces={decimalPlaces}
-        bidVol={data.bidVol}
-        relativeBidVol={data.relativeBidVol}
-        cumulativeRelativeBidVol={data.cumulativeVol.relativeBid}
-        askVol={data.askVol}
-        relativeAskVol={data.relativeAskVol}
-        cumulativeRelativeAskVol={data.cumulativeVol.relativeAsk}
-      />
-    ))}
-  </div>
+  <>
+    <div className="grid grid-cols-4 gap-4 border-b-1 text-ui-small mb-2 pb-2">
+      <div>{t('Bid Vol')}</div>
+      <div>{t('Price')}</div>
+      <div>{t('Ask Vol')}</div>
+      <div>{t('Cumulative Vol')}</div>
+    </div>
+    <div className="grid grid-cols-4 gap-4 text-right text-ui-small">
+      {data?.map((data) => (
+        <OrderbookRow
+          key={data.price}
+          price={data.price}
+          decimalPlaces={decimalPlaces}
+          bidVol={data.bidVol}
+          relativeBidVol={data.relativeBidVol}
+          cumulativeRelativeBidVol={data.cumulativeVol.relativeBid}
+          askVol={data.askVol}
+          relativeAskVol={data.relativeAskVol}
+          cumulativeRelativeAskVol={data.cumulativeVol.relativeAsk}
+        />
+      ))}
+    </div>
+  </>
 );
 
 export default Orderbook;
