8e0d6a7ced8a2929ecc33de56fbd4f3fb38e9c8a
sam-keen <samuel.kleinmann@gmail.com>
2022-04-21 10:09:37 +0100

    Added a light theme for syntax highlighter, added the styling for syntax highlighter to our custom classes

--- libs/tailwindcss-config/src/vega-custom-classes.js
+++ libs/tailwindcss-config/src/vega-custom-classes.js
@@ -1,9 +1,33 @@
 const plugin = require('tailwindcss/plugin');
+const theme = require('./theme');
 
 const vegaCustomClasses = plugin(function ({ addUtilities }) {
   addUtilities({
     '.calt': {
       fontFeatureSettings: "'calt'",
+    },
+    '.vega-hl': {
+      fontSize: '1rem',
+      fontFamily: "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+      display: 'block',
+      overflowX: 'auto',
+      padding: '1em',
+      background: theme.colors.white.DEFAULT,
+      color: theme.colors.intent.help,
+      border: '1px solid #696969',
+    },
+    '.dark .vega-hl': {
+      background: '#2C2C2C',
+      color: theme.colors.vega.green,
+    },
+    '.vega-hl .hljs-literal': {
+      color: theme.colors.vega.pink,
+    },
+    '.vega-hl .hljs-number': {
+      color: theme.colors.intent.warning,
+    },
+    '.vega-hl .hljs-string': {
+      color: theme.colors.blue,
     },
   });
 });
