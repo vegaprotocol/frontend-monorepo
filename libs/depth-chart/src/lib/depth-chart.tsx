15c5447e98988ec3a211b8936ff5d4851104c279
Bartłomiej Głownia <bglownia@gmail.com>
2022-04-21 10:28:44 +0200

    [#151] reuse orderbook data in market-depth chart

--- libs/depth-chart/src/lib/depth-chart.tsx
+++ libs/depth-chart/src/lib/depth-chart.tsx
@@ -1,65 +1,165 @@
-import 'pennant/dist/style.css';
-
-import { addDecimal, ThemeContext } from '@vegaprotocol/react-helpers';
 import { DepthChart } from 'pennant';
+import { produce } from 'immer';
+import { AsyncRenderer } from '@vegaprotocol/ui-toolkit';
+import {
+  useDataProvider,
+  addDecimal,
+  ThemeContext,
+} from '@vegaprotocol/react-helpers';
+import { marketDepthDataProvider } from '@vegaprotocol/orderbook';
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  useContext,
+} from 'react';
+import type {
+  MarketDepth_market_depth_buy,
+  MarketDepth_market_depth_sell,
+  MarketDepthSubscription_marketDepthUpdate_buy,
+  MarketDepthSubscription_marketDepthUpdate_sell,
+  MarketDepthSubscription_marketDepthUpdate,
+} from '@vegaprotocol/orderbook';
 import type { DepthChartProps } from 'pennant';
-import { Splash } from '@vegaprotocol/ui-toolkit';
-import type { marketDepthUpdateSubscribe_marketDepthUpdate_sell } from './__generated__/marketDepthUpdateSubscribe';
 
-import { useContext } from 'react';
-import { useDepthUpdate } from './hooks/use-depth-update';
-
-type PriceLevel = Pick<
-  marketDepthUpdateSubscribe_marketDepthUpdate_sell,
-  'price' | 'volume'
->;
-
-export type DepthChartContainerProps = {
+interface DepthChartManagerProps {
   marketId: string;
+}
+
+interface PriceLevel {
+  price: number;
+  volume: number;
+}
+
+const formatLevel = (
+  priceLevel: MarketDepth_market_depth_buy | MarketDepth_market_depth_sell,
+  decimalPlaces: number
+): PriceLevel => ({
+  price: Number(addDecimal(priceLevel.price, decimalPlaces)),
+  volume: Number(priceLevel.volume),
+});
+
+const updateLevels = (
+  levels: PriceLevel[],
+  updates: (
+    | MarketDepthSubscription_marketDepthUpdate_buy
+    | MarketDepthSubscription_marketDepthUpdate_sell
+  )[],
+  decimalPlaces: number
+) => {
+  updates.forEach((update) => {
+    const updateLevel = formatLevel(update, decimalPlaces);
+    let index = levels.findIndex((level) => level.price === updateLevel.price);
+    if (index !== -1) {
+      if (update.volume === '0') {
+        levels.splice(index, 1);
+      } else {
+        Object.assign(levels[index], updateLevel);
+      }
+    } else if (update.volume !== '0') {
+      index = levels.findIndex((level) => level.price > updateLevel.price);
+      if (index !== -1) {
+        levels.splice(index, 0, updateLevel);
+      } else {
+        levels.push(updateLevel);
+      }
+    }
+  });
+  return levels;
 };
 
-export const DepthChartContainer = ({ marketId }: DepthChartContainerProps) => {
+const formatMidPrice = (midPrice: string, decimalPlaces: number) =>
+  Number(addDecimal(midPrice, decimalPlaces));
+
+export const DepthChartContainer = ({ marketId }: DepthChartManagerProps) => {
   const theme = useContext(ThemeContext);
+  const variables = useMemo(() => ({ marketId }), [marketId]);
+  const lastUpdateRef = useRef(new Date().getTime());
+  const [depthData, setDepthData] = useState<DepthChartProps['data'] | null>(
+    null
+  );
+  const [midPrice, setMidPrice] = useState<number | undefined>(undefined);
+  const decimalPlacesRef = useRef<number>(0);
+  const dataRef = useRef<DepthChartProps['data'] | null>(null);
 
-  const { data, loading, error } = useDepthUpdate({ marketId }, 500);
+  // Apply updates to the table
+  const update = useCallback(
+    (delta: MarketDepthSubscription_marketDepthUpdate) => {
+      if (!dataRef.current) {
+        return false;
+      }
+      dataRef.current = produce(dataRef.current, (draft) => {
+        if (delta.buy) {
+          draft.buy = updateLevels(
+            draft.buy,
+            delta.buy,
+            decimalPlacesRef.current
+          );
+        }
+        if (delta.sell) {
+          draft.sell = updateLevels(
+            draft.sell,
+            delta.sell,
+            decimalPlacesRef.current
+          );
+        }
+      });
+      const now = new Date().getTime();
+      if (now - lastUpdateRef.current > 1000) {
+        setDepthData(dataRef.current);
+        setMidPrice(
+          delta.market.data?.midPrice
+            ? formatMidPrice(
+                delta.market.data?.midPrice,
+                decimalPlacesRef.current
+              )
+            : undefined
+        );
+        lastUpdateRef.current = now;
+      }
+      return true;
+    },
+    []
+  );
 
-  if (error) {
-    return <Splash>Error</Splash>;
-  }
+  const { data, error, loading } = useDataProvider(
+    marketDepthDataProvider,
+    update,
+    variables
+  );
 
-  if (loading) {
-    return <Splash>Loading...</Splash>;
-  }
-
-  if (!data || !data.market) {
-    return <Splash>No Data</Splash>;
-  }
-
-  const market = data.market;
-  const decimalPlaces = data.market.decimalPlaces;
-  const depthData: DepthChartProps['data'] = { buy: [], sell: [] };
-
-  if (market.depth) {
-    if (market.depth.buy) {
-      depthData.buy = market?.depth.buy?.map((priceLevel: PriceLevel) => ({
-        price: Number(addDecimal(priceLevel.price, decimalPlaces)),
-        volume: Number(priceLevel.volume),
-      }));
+  useEffect(() => {
+    if (!data) {
+      dataRef.current = null;
+      setDepthData(dataRef.current);
+      return;
     }
+    dataRef.current = {
+      buy:
+        data.depth.buy?.map((priceLevel) =>
+          formatLevel(priceLevel, data.decimalPlaces)
+        ) ?? [],
+      sell:
+        data.depth.sell?.map((priceLevel) =>
+          formatLevel(priceLevel, data.decimalPlaces)
+        ) ?? [],
+    };
+    setDepthData(dataRef.current);
+    setMidPrice(
+      data.data?.midPrice
+        ? formatMidPrice(data.data?.midPrice, data.decimalPlaces)
+        : undefined
+    );
+    decimalPlacesRef.current = data.decimalPlaces;
+  }, [data]);
 
-    if (market.depth.sell) {
-      depthData.sell = market?.depth.sell?.map((priceLevel: PriceLevel) => ({
-        price: Number(addDecimal(priceLevel.price, decimalPlaces)),
-        volume: Number(priceLevel.volume),
-      }));
-    }
-  }
-
-  let midPrice: number | undefined = undefined;
-
-  if (market.data?.midPrice) {
-    midPrice = Number(addDecimal(market.data.midPrice, decimalPlaces));
-  }
-
-  return <DepthChart data={depthData} midPrice={midPrice} theme={theme} />;
+  return (
+    <AsyncRenderer loading={loading} error={error} data={data}>
+      {depthData && (
+        <DepthChart data={depthData} midPrice={midPrice} theme={theme} />
+      )}
+    </AsyncRenderer>
+  );
 };
