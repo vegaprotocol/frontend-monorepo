2a569d5c58210db1ab4a82757c9c170c77860630
sam-keen <samuel.kleinmann@gmail.com>
2022-04-26 15:05:12 +0100

    Making a start with react-window-infinite-loader for the blocks infinite scrolling

---
+++ apps/explorer/src/app/components/blocks/blocks-infinite-list.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { FixedSizeList as List } from 'react-window';
+import InfiniteLoader from 'react-window-infinite-loader';
+import { t } from '@vegaprotocol/react-helpers';
+import type { BlockMeta } from '../../routes/blocks/tendermint-blockchain-response';
+
+interface BlocksInfiniteListProps {
+  hasMoreBlocks: boolean;
+  areBlocksLoading: boolean | undefined;
+  items: BlockMeta[] | undefined;
+  loadMoreBlocks: () => void;
+  error: Error | undefined;
+}
+
+interface ItemProps {
+  index: number;
+  style: React.CSSProperties;
+}
+
+export const BlocksInfiniteList = ({
+  hasMoreBlocks,
+  areBlocksLoading,
+  items,
+  loadMoreBlocks,
+  error,
+}: BlocksInfiniteListProps) => {
+  if (!items) {
+    return <div>No items</div>;
+  }
+
+  // If there are more items to be loaded then add an extra row to hold a loading indicator.
+  const itemCount = hasMoreBlocks ? items.length + 1 : items.length;
+
+  // Only load 1 page of items at a time.
+  // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const loadMoreItems = areBlocksLoading ? () => {} : loadMoreBlocks;
+
+  // Every row is loaded except for our loading indicator row.
+  const isItemLoaded = (index: number) =>
+    !hasMoreBlocks || index < items.length;
+
+  // Render an item or a loading indicator.
+  const Item = ({ index, style }: ItemProps) => {
+    let content;
+    if (!isItemLoaded(index)) {
+      content = t('Loading...');
+    } else if (error) {
+      content = t(`Error: ${error}`);
+    } else {
+      content = items[index].block_size;
+    }
+
+    return <div style={style}>{content}</div>;
+  };
+
+  return (
+    <InfiniteLoader
+      isItemLoaded={isItemLoaded}
+      itemCount={itemCount}
+      loadMoreItems={loadMoreItems}
+    >
+      {({ onItemsRendered, ref }) => (
+        <List
+          className="List"
+          height={150}
+          itemCount={itemCount}
+          itemSize={20}
+          onItemsRendered={onItemsRendered}
+          ref={ref}
+          width={300}
+        >
+          {Item}
+        </List>
+      )}
+    </InfiniteLoader>
+  );
+};
--- apps/explorer/src/app/routes/blocks/home/index.tsx
+++ apps/explorer/src/app/routes/blocks/home/index.tsx
@@ -1,29 +1,77 @@
+import { useState } from 'react';
 import { DATA_SOURCES } from '../../../config';
 import useFetch from '../../../hooks/use-fetch';
-import type { TendermintBlockchainResponse } from '../tendermint-blockchain-response';
+import type {
+  BlockMeta,
+  TendermintBlockchainResponse,
+} from '../tendermint-blockchain-response';
 import { RouteTitle } from '../../../components/route-title';
 import { RenderFetched } from '../../../components/render-fetched';
 import { BlocksData, BlocksRefetch } from '../../../components/blocks';
+import { BlocksInfiniteList } from '../../../components/blocks/blocks-infinite-list';
 import { JumpToBlock } from '../../../components/jump-to-block';
 import { t } from '@vegaprotocol/react-helpers';
 
 const Blocks = () => {
-  const {
-    state: { data, error, loading },
-    refetch,
-  } = useFetch<TendermintBlockchainResponse>(
-    `${DATA_SOURCES.tendermintUrl}/blockchain`
+  const [hasMoreBlocks, setHasMoreBlocks] = useState(true);
+  const [areBlocksLoading, setAreBlocksLoading] = useState<boolean | undefined>(
+    false
   );
+  const [error, setError] = useState<Error | undefined>(undefined);
+  const [blocksData, setBlocksData] = useState<BlockMeta[]>();
+  const [nextBlockHeightToLoad, setNextBlockHeightToLoad] = useState<
+    number | undefined
+  >(undefined);
+
+  const loadBlocks = () => {
+    const {
+      state: { data, error, loading },
+    } = useFetch<TendermintBlockchainResponse>(
+      `${DATA_SOURCES.tendermintUrl}/blockchain${
+        nextBlockHeightToLoad ? `?maxHeight=${nextBlockHeightToLoad}` : ''
+      }`
+    );
+
+    setAreBlocksLoading(loading);
+    setError(error);
+
+    if (data) {
+      const blockMetas = data.result.block_metas;
+      const lastBlockLoaded = parseInt(
+        blockMetas[blockMetas.length - 1].header.height
+      );
+
+      setBlocksData([...blocksData, ...blockMetas]);
+      setNextBlockHeightToLoad(lastBlockLoaded);
+      setHasMoreBlocks(lastBlockLoaded > 0);
+    }
+  };
+
+  // const {
+  //   state: { data, error, loading },
+  //   refetch,
+  //   // eslint-disable-next-line react-hooks/rules-of-hooks
+  // } = useFetch<TendermintBlockchainResponse>(
+  //   `${DATA_SOURCES.tendermintUrl}/blockchain`
+  // );
 
   return (
     <section>
       <RouteTitle>{t('Blocks')}</RouteTitle>
-      <RenderFetched error={error} loading={loading}>
-        <>
-          <BlocksRefetch refetch={refetch} />
-          <BlocksData data={data} className="mb-28" />
-        </>
-      </RenderFetched>
+      {/*<RenderFetched error={error} loading={loading}>*/}
+      {/*  <>*/}
+      {/*    <BlocksRefetch refetch={refetch} />*/}
+      {/*    <BlocksData data={data} className="mb-28" />*/}
+      {/*  </>*/}
+      {/*</RenderFetched>*/}
+
+      <BlocksInfiniteList
+        hasMoreBlocks={hasMoreBlocks}
+        areBlocksLoading={areBlocksLoading}
+        items={blocksData}
+        loadMoreBlocks={loadBlocks}
+        error={error}
+      />
       <JumpToBlock />
     </section>
   );
