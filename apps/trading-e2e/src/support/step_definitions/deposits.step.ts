cdd31209a4a8f7ad9c0676784ee20424b103a071
Matthew Russell <mattrussell36@gmail.com>
2022-04-27 16:52:23 -0700

    fix typo in test name and mnemonic env var

--- apps/trading-e2e/src/support/step_definitions/deposits.step.ts
+++ apps/trading-e2e/src/support/step_definitions/deposits.step.ts
@@ -5,7 +5,6 @@ import DepositsPage from '../pages/deposits-page';
 const depositsPage = new DepositsPage();
 const ethWallet = new EthereumWallet();
 
-const tBTC = '5cfa87844724df6069b94e4c8a6f03af21907d7bc251593d08e4251043ee9f7c';
 const invalidPublicKey =
   'zzz85edfa7ffdb6ed996ca912e9258998e47bf3515c885cf3c63fb56b15de36f';
 
@@ -44,7 +43,7 @@ Then('I can see validation errors present', () => {
 
 And('I enter an invalid public key', () => {
   depositsPage.updateForm({
-    asset: tBTC,
+    asset: Cypress.env('depositAsset'),
     to: invalidPublicKey,
     amount: '1',
   });
@@ -56,13 +55,13 @@ Then('Invalid Vega key is shown', () => {
 
 And('I enter an amount less than the minimum viable amount', () => {
   depositsPage.updateForm({
-    asset: tBTC,
+    asset: Cypress.env('depositAsset'),
     to: invalidPublicKey,
     amount: '0.00000000000001',
   });
 });
 
-Then('Amount to small message shown', () => {
+Then('Amount too small message shown', () => {
   depositsPage.verifyAmountTooSmall();
 });
 
