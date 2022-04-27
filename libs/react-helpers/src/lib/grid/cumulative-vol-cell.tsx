61d5a5838a89871ee9f4aba3a350e0e58e281553
Bartłomiej Głownia <bglownia@gmail.com>
2022-04-27 11:39:29 +0200

    [#151] align cmulative vol bars to left

--- libs/react-helpers/src/lib/grid/cumulative-vol-cell.tsx
+++ libs/react-helpers/src/lib/grid/cumulative-vol-cell.tsx
@@ -3,8 +3,6 @@ import type { ICellRendererParams } from 'ag-grid-community';
 
 import { BID_COLOR, ASK_COLOR } from './vol-cell';
 
-const INTERSECT_COLOR = 'darkgray';
-
 export interface CumulativeVolProps {
   relativeAsk?: number;
   relativeBid?: number;
@@ -16,43 +14,32 @@ export interface ICumulativeVolCellProps extends ICellRendererParams {
 
 export const CumulativeVol = React.memo(
   ({ relativeAsk, relativeBid }: CumulativeVolProps) => {
-    const bid = relativeBid ? (
-      <div
-        className="h-full absolute top-0 right-0"
-        style={{
-          width: `${relativeBid}%`,
-          backgroundColor:
-            relativeAsk && relativeAsk > relativeBid
-              ? INTERSECT_COLOR
-              : BID_COLOR,
-        }}
-      ></div>
-    ) : null;
     const ask = relativeAsk ? (
       <div
-        className="h-full absolute top-0 left-0"
+        className="absolute left-0 top-0"
         style={{
+          height: relativeBid && relativeAsk ? '50%' : '100%',
           width: `${relativeAsk}%`,
-          backgroundColor:
-            relativeBid && relativeBid > relativeAsk
-              ? INTERSECT_COLOR
-              : ASK_COLOR,
+          backgroundColor: ASK_COLOR,
         }}
       ></div>
     ) : null;
+    const bid = relativeBid ? (
+      <div
+        className="absolute top-0 left-0"
+        style={{
+          height: relativeBid && relativeAsk ? '50%' : '100%',
+          top: relativeBid && relativeAsk ? '50%' : '0',
+          width: `${relativeBid}%`,
+          backgroundColor: BID_COLOR,
+        }}
+      ></div>
+    ) : null;
+
     return (
       <div className="h-full relative" data-testid="vol">
-        {relativeBid && relativeAsk && relativeBid > relativeAsk ? (
-          <>
-            {ask}
-            {bid}
-          </>
-        ) : (
-          <>
-            {bid}
-            {ask}
-          </>
-        )}
+        {ask}
+        {bid}
       </div>
     );
   }
