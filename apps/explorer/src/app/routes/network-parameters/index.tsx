5c6813b9a8992347117839f72ee27d4f940cb186
madalinaraicu <madalina@raygroup.uk>
2022-04-29 16:32:35 +0300

    network parameters table with key value rows and syntax blobs only for json values

--- apps/explorer/src/app/routes/network-parameters/index.tsx
+++ apps/explorer/src/app/routes/network-parameters/index.tsx
@@ -1,8 +1,40 @@
 import { gql, useQuery } from '@apollo/client';
+import {
+  KeyValueTable,
+  KeyValueTableRow,
+  SyntaxHighlighter,
+} from '@vegaprotocol/ui-toolkit';
+import { formatNumber, t } from '@vegaprotocol/react-helpers';
 import { RouteTitle } from '../../components/route-title';
-import type { NetworkParametersQuery } from './__generated__/NetworkParametersQuery';
-import { SyntaxHighlighter } from '@vegaprotocol/ui-toolkit';
-import { t } from '@vegaprotocol/react-helpers';
+import type {
+  NetworkParametersQuery,
+  NetworkParametersQuery_networkParameters,
+} from './__generated__/NetworkParametersQuery';
+
+export const renderRow = (row: NetworkParametersQuery_networkParameters) => {
+  return (
+    <KeyValueTableRow key={row.key}>
+      {row.key}
+      {isJsonString(row.value) &&
+      Object.keys(JSON.parse(row.value)).length > 0 ? (
+        <SyntaxHighlighter data={JSON.parse(row.value)} />
+      ) : isNaN(Number(row.value)) ? (
+        row.value
+      ) : (
+        formatNumber(Number(row.value), 4)
+      )}
+    </KeyValueTableRow>
+  );
+};
+
+export const isJsonString = (str: string) => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
 
 export const NETWORK_PARAMETERS_QUERY = gql`
   query NetworkParametersQuery {
@@ -15,12 +47,15 @@ export const NETWORK_PARAMETERS_QUERY = gql`
 
 const NetworkParameters = () => {
   const { data } = useQuery<NetworkParametersQuery>(NETWORK_PARAMETERS_QUERY);
+  if (!data || !data.networkParameters) return null;
   return (
     <section>
       <RouteTitle data-testid="network-param-header">
         {t('Network Parameters')}
       </RouteTitle>
-      {data ? <SyntaxHighlighter data={data} /> : null}
+      <KeyValueTable>
+        {(data.networkParameters || []).map((row) => renderRow(row))}
+      </KeyValueTable>
     </section>
   );
 };
