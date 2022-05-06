409b82c57425b2044a129532c1c03558131c34d8
Matthew Russell <mattrussell36@gmail.com>
2022-05-06 11:51:51 +0100

    update determine id function to not use nodejs buffer

--- libs/react-helpers/src/lib/determine-id.ts
+++ libs/react-helpers/src/lib/determine-id.ts
@@ -7,17 +7,9 @@ import { remove0x } from './remove-0x';
  * Can match up the newly created order with incoming orders via a subscription
  */
 export const determineId = (sig: string) => {
-  // Prepend 0x
-  if (sig.slice(0, 2) !== '0x') {
-    sig = '0x' + sig;
-  }
-
-  // Create the ID
   const hash = new SHA3(256);
-  const bytes = ethers.utils.arrayify(sig);
-  hash.update(Buffer.from(bytes));
-  const id = ethers.utils.hexlify(hash.digest());
-
+  hash.update(sig, 'hex');
+  const id = hash.digest('hex');
   // Remove 0x as core doesn't keep them in the API
   return remove0x(id);
 };
