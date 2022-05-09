6d272d1e62cccc6af469dbd28b1b9f673025fc07
Joe <joe@vega.xyz>
2022-05-09 16:25:42 +0100

    Change back to use cy.contains

--- apps/trading-e2e/src/support/pages/markets-page.ts
+++ apps/trading-e2e/src/support/pages/markets-page.ts
@@ -55,10 +55,7 @@ export default class MarketPage extends BasePage {
   }
 
   clickOnMarket(text: string) {
-    cy.get(`[col-id=${this.marketStateColId}]`)
-      .contains(text)
-      .should('be.visible')
-      .click({ force: true });
+    cy.contains(text).should('be.visible').click({ force: true });
     cy.url().should('contain', 'portfolio=orders&trade=orderbook');
   }
 }
