fe86e15b6d66997328e3916d6ea341a45214ca5d
Elmar Gasimov <elmar@vegaprotocol.io>
2022-05-09 17:24:43 +0100

    Remove example cypress files and change config

--- apps/simple-trading-app-e2e/src/support/commands.ts
+++ apps/simple-trading-app-e2e/src/support/commands.ts
@@ -15,19 +15,3 @@ declare namespace Cypress {
     login(email: string, password: string): void;
   }
 }
-//
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
-});
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
