ecf96d6387f0ae8af575db6c9960f40813f8fae2
sam-keen <samuel.kleinmann@gmail.com>
2022-05-11 15:58:55 +0100

    Tweak of modal scss to tailwind

--- apps/token/src/components/modal/modal.tsx
+++ apps/token/src/components/modal/modal.tsx
@@ -6,7 +6,7 @@ interface ModalProps {
 }
 
 export const Modal = ({ children, title }: ModalProps) => (
-  <div className="relative top-0 left-[calc(50vw-200px)] w-[400px] my-[10vh] mx-0 bg-white text-black z-30">
+  <div className="relative top-0 left-[calc(50vw_-_200px)] w-[400px] my-[10vh] mx-0 bg-white text-black z-30">
     <h2 className="text-center py-12">{title}</h2>
     <div className="pt-0 px-20 pb-20">{children}</div>
   </div>
