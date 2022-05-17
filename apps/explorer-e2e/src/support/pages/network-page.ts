80afe8a9b74070eadca677276c37ae4c166397c4
Joe <joe@vega.xyz>
2022-05-17 12:57:45 +0100

    fix: lint

--- apps/explorer-e2e/src/support/pages/network-page.ts
+++ apps/explorer-e2e/src/support/pages/network-page.ts
@@ -3,35 +3,37 @@ import BasePage from './base-page';
 export default class NetworkParametersPage extends BasePage {
   networkParametersHeader = 'network-param-header';
   parameters = 'parameters';
-  jsonParamNameClassName = '.hljs-attr'
-  jsonParamValueStringClassName = '.hljs-string'
-  jsonParamValueNumberClassName = '.hljs-number'
-  parameterKeyValueRow = 'key-value-table-row'
+  jsonParamNameClassName = '.hljs-attr';
+  jsonParamValueStringClassName = '.hljs-string';
+  jsonParamValueNumberClassName = '.hljs-number';
+  parameterKeyValueRow = 'key-value-table-row';
 
   verifyNetworkParametersDisplayed() {
-    cy.getByTestId(this.networkParametersHeader).should('have.text', 'Network Parameters')
+    cy.getByTestId(this.networkParametersHeader).should(
+      'have.text',
+      'Network Parameters'
+    );
 
     cy.get(this.jsonParamNameClassName)
       .should('have.length.at.least', 21)
       .each(($paramName) => {
-        cy.wrap($paramName).should('not.be.empty')
-      })
+        cy.wrap($paramName).should('not.be.empty');
+      });
     cy.get(this.jsonParamValueStringClassName)
       .should('have.length.at.least', 7)
       .each(($paramValue) => {
-        cy.wrap($paramValue).should('not.be.empty')
-      })
+        cy.wrap($paramValue).should('not.be.empty');
+      });
 
-      cy.get(this.jsonParamValueNumberClassName)
+    cy.get(this.jsonParamValueNumberClassName)
       .should('have.length.at.least', 9)
       .each(($paramValue) => {
-        cy.wrap($paramValue).should('not.be.empty')
-      })
+        cy.wrap($paramValue).should('not.be.empty');
+      });
 
-    cy.getByTestId(this.parameterKeyValueRow)
-      .each(($row) => {
-        cy.wrap($row).find('dt').should('not.be.empty')
-        cy.wrap($row).find('dd').should('not.be.empty')
-      })
+    cy.getByTestId(this.parameterKeyValueRow).each(($row) => {
+      cy.wrap($row).find('dt').should('not.be.empty');
+      cy.wrap($row).find('dd').should('not.be.empty');
+    });
   }
 }
