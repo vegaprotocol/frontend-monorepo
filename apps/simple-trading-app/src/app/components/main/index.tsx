835b22881c97bd9d4508bee8aca75c4d20204af3
Elmar Gasimov <elmar@vegaprotocol.io>
2022-05-19 15:50:08 +0100

    feat(simple-trading-app): use drawer component and add navigation routes

---
+++ apps/simple-trading-app/src/app/components/main/index.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useRoutes, NavLink } from 'react-router-dom';
+import {
+  Loader,
+  Splash,
+  NavigationDrawer,
+  DrawerWrapper,
+  DrawerContainer,
+  DrawerToggle,
+  DRAWER_TOGGLE_VARIANTS,
+} from '@vegaprotocol/ui-toolkit';
+import { routerConfig } from '../../routes/router-config';
+
+export interface RouteChildProps {
+  name: string;
+}
+
+export const AppRouter = () => {
+  const routes = useRoutes(routerConfig);
+
+  const splashLoading = (
+    <Splash>
+      <Loader />
+    </Splash>
+  );
+
+  return (
+    <main className="p-20 overflow-hidden">
+      <React.Suspense fallback={splashLoading}>{routes}</React.Suspense>
+    </main>
+  );
+};
+
+export const Menu = () => (
+  <nav>
+    {routerConfig.map((r) => (
+      <NavLink
+        key={r.name}
+        to={r.path}
+        className={({ isActive }) =>
+          `text-h5 block mb-8 px-8 hover:bg-vega-yellow hover:text-black ${
+            isActive && 'bg-vega-yellow text-black'
+          }`
+        }
+      >
+        {r.text}
+      </NavLink>
+    ))}
+  </nav>
+);
+
+interface Props {
+  onToggle(): void;
+
+  isMenuOpen: boolean;
+}
+
+export const Main = ({ onToggle, isMenuOpen }: Props) => {
+  return (
+    <DrawerWrapper>
+      <NavigationDrawer onToggle={onToggle} isMenuOpen={isMenuOpen}>
+        <DrawerToggle
+          onToggle={onToggle}
+          variant={DRAWER_TOGGLE_VARIANTS.CLOSE}
+          className="self-end p-16"
+        />
+        <Menu />
+      </NavigationDrawer>
+      <DrawerContainer>
+        <AppRouter />
+      </DrawerContainer>
+    </DrawerWrapper>
+  );
+};
---
+++ apps/simple-trading-app/src/app/routes/router-config.tsx
@@ -0,0 +1,49 @@
+import { t } from '@vegaprotocol/react-helpers';
+import { DealTicketContainer } from '../components/deal-ticket';
+
+export const ROUTES = {
+  HOME: '/',
+  MARKETS: 'markets',
+  TRADING: 'trading',
+  LIQUIDITY: 'liquidity',
+  PORTFOLIO: 'portfolio',
+};
+
+export const routerConfig = [
+  {
+    path: ROUTES.HOME,
+    name: 'Home',
+    text: t('Home'),
+    element: <div>Home</div>,
+  },
+  {
+    path: ROUTES.MARKETS,
+    name: 'Markets',
+    text: t('Markets'),
+    element: <div>Markets</div>,
+  },
+  {
+    path: ROUTES.TRADING,
+    name: 'Trading',
+    text: t('Trading'),
+    element: (
+      <DealTicketContainer
+        marketId={
+          '41013c28d53a72225c07cf2660cdd415d9dd0e9317ec4574e77592332db35596'
+        }
+      />
+    ),
+  },
+  {
+    path: ROUTES.LIQUIDITY,
+    name: 'Liquidity',
+    text: t('Liquidity'),
+    element: <div>Liquidity</div>,
+  },
+  {
+    path: ROUTES.PORTFOLIO,
+    name: 'Portfolio',
+    text: t('Portfolio'),
+    element: <div>Portfolio</div>,
+  },
+];
