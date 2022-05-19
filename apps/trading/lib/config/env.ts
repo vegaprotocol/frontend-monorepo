aa17170c0fa92867893048839511640e28c4c304
Dexter <dexter.edwards93@gmail.com>
2022-05-19 11:46:09 +0100

    fix: env varibales not being added properly

--- apps/trading/lib/config/env.ts
+++ apps/trading/lib/config/env.ts
@@ -1,20 +1,35 @@
-const windowOrDefault = (key: string) => {
+const windowOrDefault = (key: string, defaultValue?: string) => {
   if (typeof window !== 'undefined') {
     if (window._env_ && window._env_[key]) {
       return window._env_[key];
     }
   }
-  return process.env[key] || '';
+  return defaultValue || '';
 };
 
+/**
+ * Need to have default value as next in-lines environment variables. Cannot figure out dynamic keys.
+ * So must provide the default with the key so that next can figure it out.
+ */
 export const ENV = {
-  // Environment
-  nodeEnv: windowOrDefault('NODE_ENV'),
-  dsn: windowOrDefault('NX_TRADING_SENTRY_DSN'),
-  envName: windowOrDefault('NX_VEGA_ENV'),
-  chainId: windowOrDefault('NX_ETHEREUM_CHAIN_ID'),
-  vegaUrl: windowOrDefault('NX_VEGA_URL'),
-  providerUrl: windowOrDefault('NX_ETHEREUM_PROVIDER_URL'),
-  etherscanUrl: windowOrDefault('NX_ETHERSCAN_URL'),
+  nodeEnv: windowOrDefault('NODE_ENV', process.env['NODE_ENV']),
+  dsn: windowOrDefault(
+    'NX_TRADING_SENTRY_DSN',
+    process.env['NX_TRADING_SENTRY_DSN']
+  ),
+  envName: windowOrDefault('NX_VEGA_ENV', process.env['NX_VEGA_ENV']),
+  chainId: windowOrDefault(
+    'NX_ETHEREUM_CHAIN_ID',
+    process.env['NX_ETHEREUM_CHAIN_ID']
+  ),
+  vegaUrl: windowOrDefault('NX_VEGA_URL', process.env['NX_VEGA_URL']),
+  providerUrl: windowOrDefault(
+    'NX_ETHEREUM_PROVIDER_URL',
+    process.env['NX_ETHEREUM_PROVIDER_URL']
+  ),
+  etherscanUrl: windowOrDefault(
+    'NX_ETHERSCAN_URL',
+    process.env['NX_ETHERSCAN_URL']
+  ),
   flags: {},
 };
