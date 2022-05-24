f10f652dafb20dffcd3e209360935a289753c21a
Botond Fekete <botond@vegaprotocol.io>
2022-05-24 12:26:30 +0100

    fix: lint

--- libs/ui-toolkit/src/components/link/link.stories.tsx
+++ libs/ui-toolkit/src/components/link/link.stories.tsx
@@ -8,9 +8,7 @@ export default {
   component: Link,
 } as ComponentMeta<typeof Link>;
 
-const Template: ComponentStory<typeof Link> = (args) => (
-  <Link {...args} />
-);
+const Template: ComponentStory<typeof Link> = (args) => <Link {...args} />;
 
 export const Text = Template.bind({});
 Text.args = {
