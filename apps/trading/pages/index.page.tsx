50d2e104bcaf20b46f0ed07f2531f6ea90efc429
madalinaraicu <madalina@vegaprotocol.io>
2022-05-25 14:35:21 +0300

    feat: [#445] redirect from index and remove other redundant logic

--- apps/trading/pages/index.page.tsx
+++ apps/trading/pages/index.page.tsx
@@ -1,11 +1,8 @@
 import { gql, useQuery } from '@apollo/client';
 import { getCurrentModal, useModalSwitcher } from '@vegaprotocol/react-helpers';
 import { MarketTradingMode } from '@vegaprotocol/types';
-import { AsyncRenderer } from '@vegaprotocol/ui-toolkit';
 import sortBy from 'lodash/sortBy';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import MarketPage from './markets/[marketId].page';
 import type { MarketsLanding } from './__generated__/MarketsLanding';
 
 const MARKETS_QUERY = gql`
@@ -35,46 +32,21 @@ export function Index() {
   // should be the oldest market that is currently trading in continuous mode(i.e. not in auction).
   const { data, error, loading } = useQuery<MarketsLanding>(MARKETS_QUERY);
   const [, setModal] = useModalSwitcher();
+  const { push } = useRouter();
   if (getCurrentModal() !== 'open') {
     setModal();
   }
-  const { pathname, push } = useRouter();
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    // conditional redirect
-    if (pathname == '/') {
-      // with router.push the page may be added to history
-      // the browser on history back will  go back to this page and then forward again to the redirected page
-      // you can prevent this behaviour using location.replace
-      push(
-        data
-          ? `/markets/${
-              marketList(data)[0]?.id
-            }?portfolio=orders&trade=orderbook&chart=candles`
-          : '/markets',
-        undefined,
-        { shallow: true }
-      );
-      // location.replace(
-      //   data
-      //     ? `/markets/${
-      //         marketList(data)[0]?.id
-      //       }?portfolio=orders&trade=orderbook&chart=candles`
-      //     : '/markets'
-      // );
-    } else {
-      setLoaded(true);
-    }
-  }, [data, pathname, push]);
-
-  return (
-    <>
-      <AsyncRenderer data={data} error={error} loading={loading && loaded}>
-        <MarketPage id={data && marketList(data)[0]?.id} />
-      </AsyncRenderer>
-    </>
-  );
+  if (data && !loading && !error) {
+    const marketId = marketList(data)[0]?.id;
+    push(
+      marketId
+        ? `/markets/${marketId}?portfolio=orders&trade=orderbook&chart=candles`
+        : '/markets'
+    );
+  }
+  return null;
 }
+
 
 Index.getInitialProps = () => ({
   page: 'home',
