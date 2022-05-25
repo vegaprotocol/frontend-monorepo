f88ee38118069420a25aca0b671cf5f602589d0b
Dexter <dexter.edwards93@gmail.com>
2022-05-25 10:19:26 +0100

    fix: render loop

--- apps/token/src/hooks/use-tranches.ts
+++ apps/token/src/hooks/use-tranches.ts
@@ -1,6 +1,6 @@
 import { useFetch } from '@vegaprotocol/react-helpers';
 import type { Networks, Tranche } from '@vegaprotocol/smart-contracts-sdk';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { APP_ENV } from '../config';
 
 import { BigNumber } from '../lib/bignumber';
@@ -20,41 +20,43 @@ export function useTranches() {
   const {
     state: { data, loading, error },
   } = useFetch<Tranche[] | null>(url);
-  const processedTrances = data
-    ?.map((t) => ({
-      ...t,
-      tranche_start: new Date(t.tranche_start),
-      tranche_end: new Date(t.tranche_end),
-      total_added: new BigNumber(t.total_added),
-      total_removed: new BigNumber(t.total_removed),
-      locked_amount: new BigNumber(t.locked_amount),
-      deposits: t.deposits.map((d) => ({
-        ...d,
-        amount: new BigNumber(d.amount),
-      })),
-      withdrawals: t.withdrawals.map((w) => ({
-        ...w,
-        amount: new BigNumber(w.amount),
-      })),
-      users: t.users.map((u) => ({
-        ...u,
-        // @ts-ignore - types are incorrect in the SDK lib
-        deposits: u.deposits.map((d) => ({
+  useEffect(() => {
+    const processedTrances = data
+      ?.map((t) => ({
+        ...t,
+        tranche_start: new Date(t.tranche_start),
+        tranche_end: new Date(t.tranche_end),
+        total_added: new BigNumber(t.total_added),
+        total_removed: new BigNumber(t.total_removed),
+        locked_amount: new BigNumber(t.locked_amount),
+        deposits: t.deposits.map((d) => ({
           ...d,
           amount: new BigNumber(d.amount),
         })),
-        // @ts-ignore - types are incorrect in the SDK lib
-        withdrawals: u.withdrawals.map((w) => ({
+        withdrawals: t.withdrawals.map((w) => ({
           ...w,
           amount: new BigNumber(w.amount),
         })),
-        total_tokens: new BigNumber(u.total_tokens),
-        withdrawn_tokens: new BigNumber(u.withdrawn_tokens),
-        remaining_tokens: new BigNumber(u.remaining_tokens),
-      })),
-    }))
-    .sort((a: Tranche, b: Tranche) => a.tranche_id - b.tranche_id);
-  setTranches(processedTrances ? processedTrances : null);
+        users: t.users.map((u) => ({
+          ...u,
+          // @ts-ignore - types are incorrect in the SDK lib
+          deposits: u.deposits.map((d) => ({
+            ...d,
+            amount: new BigNumber(d.amount),
+          })),
+          // @ts-ignore - types are incorrect in the SDK lib
+          withdrawals: u.withdrawals.map((w) => ({
+            ...w,
+            amount: new BigNumber(w.amount),
+          })),
+          total_tokens: new BigNumber(u.total_tokens),
+          withdrawn_tokens: new BigNumber(u.withdrawn_tokens),
+          remaining_tokens: new BigNumber(u.remaining_tokens),
+        })),
+      }))
+      .sort((a: Tranche, b: Tranche) => a.tranche_id - b.tranche_id);
+    setTranches(processedTrances ? processedTrances : null);
+  }, [data]);
 
   return {
     tranches,
