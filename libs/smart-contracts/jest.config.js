f2748aacb244c960ed24e43a7d19a0efbea9e978
Botond Fekete <botond@vegaprotocol.io>
2022-05-31 18:36:45 +0100

    fix: uniform ts syntax

--- libs/smart-contracts/jest.config.js
+++ libs/smart-contracts/jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   displayName: 'smart-contracts',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
