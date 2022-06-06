39fde033d85af5a912e0b390243457bbaf34343f
madalinaraicu <madalina@vegaprotocol.io>
2022-06-06 15:18:38 +0100

    fix: [#154] set nav items based on market id and show last viewed market on landing

--- apps/trading/components/navbar/navbar.tsx
+++ apps/trading/components/navbar/navbar.tsx
@@ -3,9 +3,37 @@ import { Vega } from '../icons/vega';
 import Link from 'next/link';
 import { AnchorButton } from '@vegaprotocol/ui-toolkit';
 import { LocalStorage, t } from '@vegaprotocol/react-helpers';
+import { useEffect, useState } from 'react';
 
 export const Navbar = () => {
-  const lastSelectedMarketId = LocalStorage.getItem('marketId');
+  const initNavItemsState = [
+    {
+      name: t('Portfolio'),
+      path: '/portfolio',
+      testId: 'portfolio-link',
+      slug: '',
+    },
+  ];
+  const [navItems, setNavItems] = useState(initNavItemsState);
+  const marketId = LocalStorage.getItem('marketId') ?? '';
+
+  useEffect(() => {
+    setNavItems([
+      {
+        name: t('Trading'),
+        path: '/markets',
+        testId: 'markets-link',
+        slug: marketId,
+      },
+      {
+        name: t('Portfolio'),
+        path: '/portfolio',
+        testId: 'portfolio-link',
+        slug: '',
+      },
+    ]);
+  }, [marketId]);
+
   return (
     <nav className="flex items-center">
       <Link href="/" passHref={true}>
@@ -13,16 +41,7 @@ export const Navbar = () => {
           <Vega className="fill-black dark:fill-white" />
         </a>
       </Link>
-      {[
-        {
-          name: t('Trading'),
-          path: lastSelectedMarketId
-            ? `/markets/${lastSelectedMarketId}`
-            : '/markets',
-          testId: 'markets-link',
-        },
-        { name: t('Portfolio'), path: '/portfolio', testId: 'portfolio-link' },
-      ].map((route) => (
+      {navItems.map((route) => (
         <NavLink key={route.path} {...route} />
       ))}
     </nav>
@@ -34,21 +53,29 @@ interface NavLinkProps {
   path: string;
   exact?: boolean;
   testId?: string;
+  slug?: string;
 }
 
-const NavLink = ({ name, path, exact, testId = name }: NavLinkProps) => {
+const NavLink = ({
+  name,
+  path,
+  exact,
+  testId = name,
+  slug = '',
+}: NavLinkProps) => {
   const router = useRouter();
   const isActive =
     router.asPath === path || (!exact && router.asPath.startsWith(path));
+  const href = slug !== '' ? `${path}/${slug}` : path;
   return (
     <AnchorButton
       variant={isActive ? 'accent' : 'inline'}
       className="px-16 py-6 h-[38px] uppercase border-0 self-end xs:text-ui sm:text-body-large md:text-h5 lg:text-h4"
       data-testid={testId}
-      href={path}
+      href={href}
       onClick={(e) => {
         e.preventDefault();
-        router.push(path);
+        router.push(href);
       }}
     >
       {name}
