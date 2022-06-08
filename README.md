6b2249b9db065da33f354fcf828109701867119a
madalinaraicu <madalina@vegaprotocol.io>
2022-06-08 16:40:54 +0100

    fix: [#471] set up storybook in order-list lib and add tailwind

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ The trading interface built based on a component toolkit. It will provide a way 
 
 ### [Token](./apps/token)
 
-The utlity dApp for interacting with the Vega token and using its' utility. This includes; delegation, nomination, governance and redemption of tokens.
+The utility dApp for interacting with the Vega token and using its' utility. This includes; delegation, nomination, governance and redemption of tokens.
 
 ### [Explorer](./apps/explorer)
 
@@ -47,11 +47,11 @@ For shared Cypress logic, commands and steps.
 
 ### [Web3](./libs/web3)
 
-A ulitity library for connecting to the Ethereum network and interacting with Vega Web3 contracts.
+A utility library for connecting to the Ethereum network and interacting with Vega Web3 contracts.
 
 ### [React Helpers](./libs/react-helpers)
 
-Generic react helpers that can be used across multilpe applications, along with other utilties.
+Generic react helpers that can be used across multiple applications, along with other utilities.
 
 # 💻 Develop
 
---
+++ libs/order-list/src/lib/order-list.stories.tsx
@@ -0,0 +1,79 @@
+import type { Story, Meta } from '@storybook/react';
+import {
+  OrderType,
+  OrderStatus,
+  Side,
+  OrderTimeInForce,
+} from '@vegaprotocol/types';
+import { OrderList } from './order-list';
+import type { Orders_party_orders } from './__generated__/Orders';
+
+export default {
+  component: OrderList,
+  title: 'OrderList',
+} as Meta;
+
+const marketOrder: Orders_party_orders = {
+  __typename: 'Order',
+  id: 'order-id',
+  market: {
+    __typename: 'Market',
+    id: 'market-id',
+    name: 'market-name',
+    decimalPlaces: 2,
+    tradableInstrument: {
+      __typename: 'TradableInstrument',
+      instrument: {
+        __typename: 'Instrument',
+        code: 'instrument-code',
+      },
+    },
+  },
+  size: '10',
+  type: OrderType.Market,
+  status: OrderStatus.Active,
+  side: Side.Buy,
+  remaining: '5',
+  price: '',
+  timeInForce: OrderTimeInForce.IOC,
+  createdAt: new Date().toISOString(),
+  updatedAt: null,
+  expiresAt: null,
+  rejectionReason: null,
+};
+
+const limitOrder: Orders_party_orders = {
+  __typename: 'Order',
+  id: 'order-id',
+  market: {
+    __typename: 'Market',
+    id: 'market-id',
+    name: 'market-name',
+    decimalPlaces: 2,
+    tradableInstrument: {
+      __typename: 'TradableInstrument',
+      instrument: {
+        __typename: 'Instrument',
+        code: 'instrument-code',
+      },
+    },
+  },
+  size: '10',
+  type: OrderType.Limit,
+  status: OrderStatus.Active,
+  side: Side.Sell,
+  remaining: '5',
+  price: '12345',
+  timeInForce: OrderTimeInForce.GTT,
+  createdAt: new Date('2022-3-3').toISOString(),
+  expiresAt: new Date('2022-3-5').toISOString(),
+  updatedAt: null,
+  rejectionReason: null,
+};
+
+const Template: Story = (args) => <OrderList data={args.data} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  data: [marketOrder, limitOrder],
+};
