ecda46caa599eec46d8adbfa3f1bd19c71ea9e04
botond <105208209+notbot00@users.noreply.github.com>
2022-06-10 10:15:38 +0100

    Feat/6 Network Switcher (#502)
    * feat: add network-switcher lib
    
    * feat: add env variables for some deployed app urls
    
    * feat: add network processing to environment hoook
    
    * refactor: network handling
    
    * refactor: remove dialog from provider and add env setter
    
    * feat: add network switcher dialog to the trading app
    
    * refactor: add network redirect to dialog connect callback
    
    * fix: lint
    
    * fix: jsonify env variable for possible networks
    
    * fix: add formatter file
    
    * fix: assign correct global state to network swicther
    
    * feat: add network-switcher lib
    
    * feat: add env variables for some deployed app urls
    
    * feat: add network processing to environment hoook
    
    * refactor: network handling
    
    * refactor: remove dialog from provider and add env setter
    
    * feat: add network switcher dialog to the trading app
    
    * refactor: add network redirect to dialog connect callback
    
    * fix: lint
    
    * fix: jsonify env variable for possible networks
    
    * fix: add formatter file
    
    * fix: assign correct global state to network swicther
    
    * fix: failing tests from UI changes
    
    * fix: lint
    
    * fix: lint
    
    Co-authored-by: Joe <joe@vega.xyz>
    Co-authored-by: Dexter <dexter.edwards93@gmail.com>

---
+++ libs/network-switcher/src/components/network-switcher-dialog/network-switcher-dialog.tsx
@@ -0,0 +1,37 @@
+import type { ComponentProps } from 'react';
+import { t } from '@vegaprotocol/react-helpers';
+import type { Intent } from '@vegaprotocol/ui-toolkit';
+import { Dialog } from '@vegaprotocol/ui-toolkit';
+import { NetworkSwitcher } from '../network-switcher';
+
+type NetworkSwitcherDialogProps = Pick<
+  ComponentProps<typeof NetworkSwitcher>,
+  'onConnect' | 'onError'
+> & {
+  dialogOpen: boolean;
+  setDialogOpen: (dialogOpen: boolean) => void;
+  intent?: Intent;
+};
+
+export const NetworkSwitcherDialog = ({
+  dialogOpen,
+  setDialogOpen,
+  intent,
+  onConnect,
+  onError,
+}: NetworkSwitcherDialogProps) => {
+  return (
+    <Dialog
+      open={dialogOpen}
+      onChange={setDialogOpen}
+      title={t('Choose a network')}
+      intent={intent}
+    >
+      <NetworkSwitcher
+        onConnect={onConnect}
+        onError={onError}
+        onClose={() => setDialogOpen(false)}
+      />
+    </Dialog>
+  );
+};
