c27b4a4b4d7c6710941e2cd58ed194b4a6f93d97
maciek <maciek@vegaprotocol.io>
2022-06-13 12:20:13 +0200

    feat: [simple-app] - simple market list toolbar - add some more unit tests

--- apps/simple-trading-app/src/app/components/simple-market-list/simple-market-toolbar.spec.tsx
+++ apps/simple-trading-app/src/app/components/simple-market-list/simple-market-toolbar.spec.tsx
@@ -1,36 +1,22 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
 import SimpleMarketToolbar from './simple-market-toolbar';
 import type { MockedResponse } from '@apollo/client/testing';
 import type { MarketFilters } from './__generated__/MarketFilters';
 import { FILTERS_QUERY } from './data-provider';
 import filterData from './mocks/market-filters.json';
-import { SimpleMarkets_markets } from './__generated__/SimpleMarkets';
-import { InMemoryCache } from '@apollo/client';
+
+const mockedNavigate = jest.fn();
+
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useNavigate: () => jest.fn(),
+  useNavigate: () => mockedNavigate,
   useParams: () => ({}),
 }));
 
 describe('SimpleMarketToolbar', () => {
-  const cache = new InMemoryCache({
-    typePolicies: {
-      Rocket: {
-        fields: {
-          description: {
-            read() {
-              // Read function for Rocket.description
-              return 'Placeholder rocket description';
-            },
-          },
-        },
-      },
-    },
-  });
-
   const filterMock: MockedResponse<MarketFilters> = {
     request: {
       query: FILTERS_QUERY,
@@ -57,5 +43,37 @@ describe('SimpleMarketToolbar', () => {
     expect(screen.getByTestId('market-products-menu').children).toHaveLength(3);
     expect(screen.getByTestId('market-assets-menu').children).toHaveLength(6);
     expect(screen.getByRole('combobox').children).toHaveLength(10);
+  });
+
+  it('navigation should work well', async () => {
+    await act(async () => {
+      render(
+        <MockedProvider mocks={[filterMock]} addTypename={false}>
+          <SimpleMarketToolbar />
+        </MockedProvider>
+      );
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+
+    fireEvent.click(
+      screen
+        .getByTestId('market-products-menu')
+        .children[1].querySelector('button') as HTMLButtonElement
+    );
+    expect(mockedNavigate).toHaveBeenCalledWith('/markets/Active/all/Future');
+
+    fireEvent.click(
+      screen
+        .getByTestId('market-assets-menu')
+        .children[5].querySelector('button') as HTMLButtonElement
+    );
+    expect(mockedNavigate).toHaveBeenCalledWith('/markets/Active/tEURO/Future');
+
+    fireEvent.change(screen.getByRole('combobox'), {
+      target: { value: 'Pending' },
+    });
+    expect(mockedNavigate).toHaveBeenCalledWith(
+      '/markets/Pending/tEURO/Future'
+    );
   });
 });
