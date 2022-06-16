41b9dc0bf92c675e5db0d613904fe8cecb7cef1e
Matthew Russell <mattrussell36@gmail.com>
2022-06-16 14:23:32 -0700

    fix: move MarketList wait back

--- apps/trading-e2e/src/integration/home.ts
+++ apps/trading-e2e/src/integration/home.ts
@@ -33,8 +33,8 @@ describe('home', () => {
 
       cy.visit('/');
       cy.wait('@MarketsLanding');
-      cy.wait('@MarketList');
       cy.url().should('include', `/markets/${oldestMarket.id}`); // Should redirect to oldest market
+      cy.wait('@MarketList');
     });
 
     it('redirects to a default market with the landing dialog open', () => {
