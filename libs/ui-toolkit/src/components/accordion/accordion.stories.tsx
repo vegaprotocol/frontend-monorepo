b92c90a27435d84a246f84b1443f0b5f464481f9
madalinaraicu <madalina@vegaprotocol.io>
2022-06-21 10:27:06 +0200

    fix: [#590] fix full length on radix accordion

--- libs/ui-toolkit/src/components/accordion/accordion.stories.tsx
+++ libs/ui-toolkit/src/components/accordion/accordion.stories.tsx
@@ -7,7 +7,11 @@ export default {
 } as Meta;
 
 const Template: Story = (args) => (
-  <AccordionPanel title={args.title} content={args.content} />
+  <>
+    <AccordionPanel title={args.title} content={args.content} key={'1'} />
+    <AccordionPanel title={args.title} content={args.content} key={'2'} />
+    <AccordionPanel title={args.title} content={args.content} key={'3'} />
+  </>
 );
 
 export const Default = Template.bind({});
--- libs/ui-toolkit/src/components/accordion/accordion.tsx
+++ libs/ui-toolkit/src/components/accordion/accordion.tsx
@@ -1,73 +1,67 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
+import * as Accordion from '@radix-ui/react-accordion';
+import { styled } from '@stitches/react';
 
 export interface AccordionProps {
+  key?: string;
   title: React.ReactNode;
   content: React.ReactNode;
 }
 
-export const AccordionPanel = ({ title, content }: AccordionProps) => {
-  const [active, setActive] = useState(false);
-  const [height, setHeight] = useState('0px');
-  const [rotate, setRotate] = useState(
-    'transform duration-300 ease rotate-180'
-  );
-
-  const contentSpace = useRef(null);
-
-  const toggleAccordion = () => {
-    setActive((prevState) => !prevState);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    setHeight(active ? '0px' : `${contentSpace.current.scrollHeight}px`);
-    setRotate(
-      active
-        ? 'transform duration-300 ease rotate-180'
-        : 'transform duration-300 ease'
-    );
-  };
-
+export const AccordionPanel = ({ key, title, content }: AccordionProps) => {
+  const AccordionChevron = styled(ChevronDown, {
+    transition: 'transform 300ms ease',
+    '[data-state=open] &': { transform: 'rotate(180deg)' },
+  });
   return (
-    <div className="flex flex-col">
-      <button
-        data-testid="accordion-toggle"
-        className="py-2 box-border appearance-none cursor-pointer focus:outline-none flex items-center justify-between border-b border-muted"
-        onClick={toggleAccordion}
-      >
-        <p
-          className="inline-block text-footnote font-bold text-h6 text-black dark:text-white pt-5 "
-          data-testid="accordion-title"
+    <Accordion.Root type="multiple" className="flex flex-col">
+      <Accordion.Item value={key ?? 'item'}>
+        <Accordion.Header>
+          <Accordion.Trigger
+            data-testid="accordion-toggle"
+            className="w-full py-2 box-border appearance-none cursor-pointer focus:outline-none flex items-center justify-between border-b border-muted"
+          >
+            <p
+              className="inline-block text-footnote font-bold text-h6 text-black dark:text-white pt-5"
+              data-testid="accordion-title"
+            >
+              {title}
+            </p>
+            <AccordionChevron aria-hidden />
+          </Accordion.Trigger>
+        </Accordion.Header>
+        <Accordion.Content
+          data-testid="accordion-content-ref"
+          className="overflow-auto transition-max-height duration-300 ease-in-out"
         >
-          {title}
-        </p>
-        <svg
-          width="20"
-          height="20"
-          aria-label="chevron icon"
-          data-testid="accordion-chevron-icon"
-          className={`${rotate} inline-block fill-black dark:fill-white`}
-          viewBox="0 0 20 20"
-          fill="fillCurrent"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect x="3" y="12" width="2" height="2" />
-          <rect x="5" y="10" width="2" height="2" />
-          <rect x="7" y="8" width="2" height="2" />
-          <rect x="9" y="6" width="2" height="2" />
-          <rect x="11" y="8" width="2" height="2" />
-          <rect x="13" y="10" width="2" height="2" />
-          <rect x="15" y="12" width="2" height="2" />
-        </svg>
-      </button>
-      <div
-        ref={contentSpace}
-        style={{ maxHeight: `${height}` }}
-        data-testid="accordion-content-ref"
-        className="overflow-auto transition-max-height duration-300 ease-in-out"
-      >
-        <div className="pb-5" data-testid="accordion-content">
-          {content}
-        </div>
-      </div>
-    </div>
+          <div className="pb-5" data-testid="accordion-content">
+            {content}
+          </div>
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion.Root>
+  );
+};
+
+export const ChevronDown = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      aria-label="chevron icon"
+      data-testid="accordion-chevron-icon"
+      className={`inline-block fill-black dark:fill-white`}
+      viewBox="0 0 20 20"
+      fill="fillCurrent"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="17" y="8" width="2" height="2" />
+      <rect x="15" y="10" width="2" height="2" />
+      <rect x="13" y="12" width="2" height="2" />
+      <rect x="11" y="14" width="2" height="2" />
+      <rect x="9" y="12" width="2" height="2" />
+      <rect x="7" y="10" width="2" height="2" />
+      <rect x="5" y="8" width="2" height="2" />
+    </svg>
   );
 };
