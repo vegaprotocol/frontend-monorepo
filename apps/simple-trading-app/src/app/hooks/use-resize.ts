4dd047472d1ee0f9cd6572986acba022a0628087
Elmar Gasimov <elmar@vegaprotocol.io>
2022-06-21 11:36:30 +0100

    feat(console-lite): refactor resizing into reusable hook

---
+++ apps/simple-trading-app/src/app/hooks/use-resize.ts
@@ -0,0 +1,36 @@
+import { useRef, useEffect, useState } from 'react';
+
+const useResize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const timeout = useRef(0);
+
+  const handleResize = () => {
+    if (timeout.current) {
+      window.cancelAnimationFrame(timeout.current);
+    }
+
+    // Setup the new requestAnimationFrame()
+    timeout.current = window.requestAnimationFrame(function () {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      window.cancelAnimationFrame(timeout.current);
+    };
+  }, []);
+
+  return windowSize;
+};
+
+export default useResize;
