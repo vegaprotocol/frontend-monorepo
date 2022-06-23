98c9ff263ec34625bc4fff12941b49865ed4ff81
Dexter <dexter.edwards93@gmail.com>
2022-06-23 14:52:00 +0100

    chore: address PR comments

--- libs/smart-contracts/src/utils/hexadecimalify.test.ts
+++
@@ -1,6 +0,0 @@
-import { hexadecimalify } from './hexadecimalify';
-
-test('Prepends strings with 0x', () => {
-  expect(hexadecimalify('abc')).toEqual('0xabc');
-  expect(hexadecimalify('123456789')).toEqual('0x123456789');
-});
