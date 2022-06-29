d00315a1b678818de8a04301ee5d4b6b1e5d9083
Botond <botond@vegaprotocol.io>
2022-06-29 17:34:34 +0100

    feat: add custom executor for next

--- tools/executors/serve/impl.ts
+++ tools/executors/serve/impl.ts
@@ -1,8 +1,5 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as dotenv from 'dotenv';
-import * as log from 'npmlog';
 import type { ExecutorContext } from '@nrwl/devkit';
+import setup from '../../utils/setup-environment';
 import devServerExecutor, {
   WebDevServerOptions,
 } from '@nrwl/web/src/executors/dev-server/dev-server.impl';
@@ -11,79 +8,12 @@ type Schema = WebDevServerOptions & {
   env?: string;
 };
 
-const LOGGER_SCOPE = 'tools/executors/serve';
-
-const logEnvData = (
-  envMap: Record<string, string>,
-  envFiles: string[],
-  env?: string,
-  defaultEnvFile?: string
-) => {
-  if (env && !envMap[env]) {
-    log.warn(LOGGER_SCOPE, `No environment called "${env}" found.`);
-    log.info(
-      LOGGER_SCOPE,
-      envFiles.length > 0
-        ? `You can create a new environment by putting an ".env.${env}" file in your project root, or you can use the following available ones: ${envFiles.join(
-            ', '
-          )}.`
-        : 'To get started with environments, you can create an ".env" file in your project root with the desired variables.'
-    );
-  }
-
-  if (!envMap[env]) {
-    log.info(
-      LOGGER_SCOPE,
-      defaultEnvFile
-        ? `Using "${defaultEnvFile}" as the default project environment.`
-        : 'Serving the project only using the environment variables scoped to your CLI.'
-    );
-  } else {
-    log.info(
-      LOGGER_SCOPE,
-      `Using "${envMap[env]}" as the default project environment.`
-    );
-  }
-};
-
-const filenameToEnv = (filename: string) => filename.replace('.env.', '');
-
-const getDefaultEnvFile = (envMap: Record<string, string>) => {
-  return envMap['local'] || envMap['.env'];
-};
-
-const getEnvFile = (env: string, envFiles: string[]) => {
-  const envMap = envFiles.reduce(
-    (acc, filename) => ({
-      ...acc,
-      [filenameToEnv(filename)]: filename,
-    }),
-    {}
-  );
-
-  const defaultEnvFile = getDefaultEnvFile(envMap);
-  logEnvData(envMap, envFiles, env, defaultEnvFile);
-
-  return envMap[env] || defaultEnvFile;
-};
-
 export default async function* serve(
   options: Schema,
   context: ExecutorContext
 ): ReturnType<typeof devServerExecutor> {
   const { env, ...dsOptions } = options;
-  const { root } = context.workspace.projects[context.projectName];
-  const workspacePath = path.join(context.cwd, root);
-
-  const files = await fs.promises.readdir(workspacePath);
-  const envFile = getEnvFile(
-    env,
-    files.filter((f) => f.startsWith('.env'))
-  );
-
-  if (env && envFile) {
-    dotenv.config({ path: path.join(workspacePath, envFile), override: true });
-  }
+  setup(env, context, 'tools/executors/serve');
 
   return yield* devServerExecutor(dsOptions, context);
 }
---
+++ tools/utils/setup-environment.ts
@@ -0,0 +1,88 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { execSync } from 'child_process';
+import * as log from 'npmlog';
+import * as dotenv from 'dotenv';
+import type { ExecutorContext } from '@nrwl/devkit';
+
+process.env['GIT_COMMIT_HASH'] = execSync('git rev-parse HEAD').toString();
+process.env['GIT_ORIGIN_URL'] = execSync('git remote get-url origin')
+  .toString()
+  .replace('ssh://git@', 'https://')
+  .replace('.git', '');
+
+const logEnvData = (
+  envMap: Record<string, string>,
+  envFiles: string[],
+  env?: string,
+  defaultEnvFile?: string,
+  loggerScope?: string
+) => {
+  if (env && !envMap[env]) {
+    log.warn(loggerScope, `No environment called "${env}" found.`);
+    log.info(
+      loggerScope,
+      envFiles.length > 0
+        ? `You can create a new environment by putting an ".env.${env}" file in your project root, or you can use the following available ones: ${envFiles.join(
+            ', '
+          )}.`
+        : 'To get started with environments, you can create an ".env" file in your project root with the desired variables.'
+    );
+  }
+
+  if (!envMap[env]) {
+    log.info(
+      loggerScope,
+      defaultEnvFile
+        ? `Using "${defaultEnvFile}" as the default project environment.`
+        : 'Serving the project only using the environment variables scoped to your CLI.'
+    );
+  } else {
+    log.info(
+      loggerScope,
+      `Using "${envMap[env]}" as the default project environment.`
+    );
+  }
+};
+
+const filenameToEnv = (filename: string) => filename.replace('.env.', '');
+
+const getDefaultEnvFile = (envMap: Record<string, string>) => {
+  return envMap['local'] || envMap['.env'];
+};
+
+const getEnvFile = (env: string, envFiles: string[], loggerScope?: string) => {
+  const envMap = envFiles.reduce(
+    (acc, filename) => ({
+      ...acc,
+      [filenameToEnv(filename)]: filename,
+    }),
+    {}
+  );
+
+  const defaultEnvFile = getDefaultEnvFile(envMap);
+  logEnvData(envMap, envFiles, env, defaultEnvFile, loggerScope);
+
+  return envMap[env] || defaultEnvFile;
+};
+
+export default async function setup(
+  env: string,
+  context: ExecutorContext,
+  loggerScope?: string
+) {
+  const { root } = context.workspace.projects[context.projectName];
+  const workspacePath = path.join(context.cwd, root);
+
+  const files = await fs.promises.readdir(workspacePath);
+
+  const envFile = getEnvFile(
+    env,
+    files.filter((f) => f.startsWith('.env')),
+    loggerScope
+  );
+
+  if (env && envFile) {
+    dotenv.config({ path: path.join(workspacePath, envFile), override: true });
+  }
+}
