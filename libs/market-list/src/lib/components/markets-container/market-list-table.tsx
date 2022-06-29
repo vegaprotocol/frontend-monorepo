f8364700fd4b8854ed7b71fb60652c8fb0d81e86
Bartłomiej Głownia <bglownia@gmail.com>
2022-06-29 18:10:55 +0200

    feat(#638): use infinite rowModelType in market list table

--- libs/market-list/src/lib/components/markets-container/market-list-table.tsx
+++ libs/market-list/src/lib/components/markets-container/market-list-table.tsx
@@ -1,5 +1,5 @@
 import { forwardRef } from 'react';
-import type { ValueFormatterParams } from 'ag-grid-community';
+import type { IDatasource, ValueFormatterParams } from 'ag-grid-community';
 import {
   PriceFlashCell,
   addDecimalsFormatNumber,
@@ -14,7 +14,7 @@ import type {
 } from '../__generated__/Markets';
 
 interface MarketListTableProps {
-  data: Markets_markets[] | null;
+  datasource: IDatasource;
   onRowClicked: (marketId: string) => void;
 }
 
@@ -25,12 +25,13 @@ export const getRowId = ({
 }) => data.id;
 
 export const MarketListTable = forwardRef<AgGridReact, MarketListTableProps>(
-  ({ data, onRowClicked }, ref) => {
+  ({ datasource, onRowClicked }, ref) => {
     return (
       <AgGrid
         style={{ width: '100%', height: '100%' }}
         overlayNoRowsTemplate={t('No markets')}
-        rowData={data}
+        rowModelType="infinite"
+        datasource={datasource}
         getRowId={getRowId}
         ref={ref}
         defaultColDef={{
