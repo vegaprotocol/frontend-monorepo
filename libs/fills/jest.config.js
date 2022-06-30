8e6c066b9911d8b8dd8206b0b3c949bf06541612
Matthew Russell <mattrussell36@gmail.com>
2022-06-30 00:52:25 -0700

    Feat/462 fills table mvp (no pagination) (#645)
    * feat: scaffold fills components
    
    * feat: add query for fills and populate data in basic table
    
    * feat: refactor portfolio page to use grid and add fills container
    
    * feat: add infinite scroll for fills
    
    * feat: try with data provider, get infinite scroll working
    
    * chore: remove infinite scrolling as subsequent pr will add it
    
    * chore: reorder columns
    
    * chore: remove Autosizer from portfolio grid children as its not needed
    
    * chore: move fills tab to the end
    
    * feat: add storybook, format cells
    
    * feat: add unit test for fills table
    
    * feat: convert lib to next lib
    
    * feat: add pagination variables to fills query to only get latest 300
    
    * fix: fills data provider update function to return result of produce
    
    * fix: yarn.lock
    
    * fix: cypress run by moving test helpers
    
    * fix: re add test helpers for unit tests
    
    * fix: global connection tests
    
    * fix: use fills from mocks
    
    * feat: add update handler for fills
    
    * chore: move value formatter functions into module scope

---
+++ libs/fills/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'fills',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/fills',
+  setupFilesAfterEnv: ['./src/setup-tests.ts'],
+};
---
+++ libs/fills/src/lib/test-helpers.ts
@@ -0,0 +1,134 @@
+import { Side } from '@vegaprotocol/types';
+import merge from 'lodash/merge';
+import type { PartialDeep } from 'type-fest';
+import type {
+  Fills,
+  Fills_party_tradesPaged_edges_node,
+} from './__generated__/Fills';
+
+export const generateFills = (override?: PartialDeep<Fills>): Fills => {
+  const fills: Fills_party_tradesPaged_edges_node[] = [
+    generateFill({
+      buyer: {
+        id: 'party-id',
+      },
+    }),
+    generateFill({
+      id: '1',
+      seller: {
+        id: 'party-id',
+      },
+      aggressor: Side.Sell,
+      buyerFee: {
+        infrastructureFee: '5000',
+      },
+      market: {
+        name: 'Apples Daily v3',
+        positionDecimalPlaces: 2,
+      },
+    }),
+    generateFill({
+      id: '2',
+      seller: {
+        id: 'party-id',
+      },
+      aggressor: Side.Buy,
+    }),
+    generateFill({
+      id: '3',
+      aggressor: Side.Sell,
+      market: {
+        name: 'ETHBTC Quarterly (30 Jun 2022)',
+      },
+      buyer: {
+        id: 'party-id',
+      },
+    }),
+  ];
+
+  const defaultResult: Fills = {
+    party: {
+      id: 'buyer-id',
+      tradesPaged: {
+        __typename: 'TradeConnection',
+        totalCount: 1,
+        edges: fills.map((f) => {
+          return {
+            __typename: 'TradeEdge',
+            node: f,
+            cursor: '3',
+          };
+        }),
+        pageInfo: {
+          __typename: 'PageInfo',
+          startCursor: '1',
+          endCursor: '2',
+        },
+      },
+      __typename: 'Party',
+    },
+  };
+
+  return merge(defaultResult, override);
+};
+
+export const generateFill = (
+  override?: PartialDeep<Fills_party_tradesPaged_edges_node>
+) => {
+  const defaultFill: Fills_party_tradesPaged_edges_node = {
+    __typename: 'Trade',
+    id: '0',
+    createdAt: new Date().toISOString(),
+    price: '10000000',
+    size: '50000',
+    buyOrder: 'buy-order',
+    sellOrder: 'sell-order',
+    aggressor: Side.Buy,
+    buyer: {
+      __typename: 'Party',
+      id: 'buyer-id',
+    },
+    seller: {
+      __typename: 'Party',
+      id: 'seller-id',
+    },
+    buyerFee: {
+      __typename: 'TradeFee',
+      makerFee: '100',
+      infrastructureFee: '100',
+      liquidityFee: '100',
+    },
+    sellerFee: {
+      __typename: 'TradeFee',
+      makerFee: '200',
+      infrastructureFee: '200',
+      liquidityFee: '200',
+    },
+    market: {
+      __typename: 'Market',
+      id: 'market-id',
+      name: 'UNIDAI Monthly (30 Jun 2022)',
+      positionDecimalPlaces: 0,
+      decimalPlaces: 5,
+      tradableInstrument: {
+        __typename: 'TradableInstrument',
+        instrument: {
+          __typename: 'Instrument',
+          id: 'instrument-id',
+          code: 'instrument-code',
+          product: {
+            __typename: 'Future',
+            settlementAsset: {
+              __typename: 'Asset',
+              id: 'asset-id',
+              symbol: 'SYM',
+              decimals: 18,
+            },
+          },
+        },
+      },
+    },
+  };
+
+  return merge(defaultFill, override);
+};
