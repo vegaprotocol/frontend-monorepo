e5fc3d1bc4ecd5485060e7aa0cfc6f1f78b5dd01
Botond <botond@vegaprotocol.io>
2022-07-04 14:21:56 +0100

    fix: layout styles

--- libs/environment/src/components/node-switcher/layout-row.tsx
+++ libs/environment/src/components/node-switcher/layout-row.tsx
@@ -6,7 +6,7 @@ type LayoutRowProps = {
 
 export const LayoutRow = ({ children }: LayoutRowProps) => {
   return (
-    <div className="grid gap-4 py-8 grid-cols-[minmax(200px,_1fr),_150px_125px_100px]">
+    <div className="grid gap-4 py-8 w-full grid-cols-[minmax(200px,_1fr),_150px_125px_100px]">
       {children}
     </div>
   );
