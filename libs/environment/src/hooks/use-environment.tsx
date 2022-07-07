4acf2a7f8cc09138b8dac11ba4557696873af495
Botond <botond@vegaprotocol.io>
2022-07-07 10:15:19 +0100

    fix: lint

--- libs/environment/src/hooks/use-environment.tsx
+++ libs/environment/src/hooks/use-environment.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import { useState, useEffect, createContext, useContext } from 'react';
+import { useState, createContext, useContext } from 'react';
 
 import { NodeSwitcherDialog } from '../components/node-switcher-dialog';
 import { useConfig } from './use-config';
