9cfcadd39b8f9cf573882688520bf3cb134c4acf
macqbat <kubat.maciek@gmail.com>
2022-07-11 15:27:28 +0200

    feat: [console-lite] - market selector (#726)
    * feat: [console-lite] - market selector - first commit
    
    * feat: [console-lite] - market selector - add strongly different mobile version
    
    * feat: [console-lite] - market selector - fix deal-market lint fail
    
    * feat: [console-lite] - market selector - add a bunch of improvements
    
    * feat: [console-lite] - market selector - add some int tests
    
    * feat: [console-lite] - market selector - fix dialog dimmensions
    
    Co-authored-by: maciek <maciek@vegaprotocol.io>

--- apps/simple-trading-app/src/app/components/simple-market-list/simple-market-renderer.tsx
+++ apps/simple-trading-app/src/app/components/simple-market-list/simple-market-renderer.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import type { SimpleMarkets_markets } from './__generated__/SimpleMarkets';
+import type { MarketNames_markets } from '@vegaprotocol/deal-ticket';
 import SimpleMarketExpires from './simple-market-expires';
 
 interface Props {
-  data: SimpleMarkets_markets;
+  market: MarketNames_markets;
 }
 
-const MarketNameRenderer = ({ data }: Props) => {
+const MarketNameRenderer = ({ market }: Props) => {
   return (
     <div className="flex h-full items-center grid grid-rows-2 grid-flow-col gap-x-8 gap-y-0 grid-cols-[min-content,1fr,1fr]">
       <div className="w-60 row-span-2 bg-pink rounded-full w-44 h-44 bg-gradient-to-br from-white-60 to--white-80 opacity-30" />
       <div className="col-span-2 uppercase justify-start text-black dark:text-white text-market self-end">
-        {data.name}{' '}
+        {market.name}{' '}
         <SimpleMarketExpires
-          tags={data.tradableInstrument.instrument.metadata.tags}
+          tags={market.tradableInstrument.instrument.metadata.tags}
         />
       </div>
-      <div className="col-span-2 ui-small text-deemphasise dark:text-midGrey self-start leading-3">
-        {data.tradableInstrument.instrument.product.quoteName}
+      <div className="col-span-2 text-ui-small text-deemphasise dark:text-midGrey self-start leading-3">
+        {market.tradableInstrument.instrument.product.quoteName}
       </div>
     </div>
   );
