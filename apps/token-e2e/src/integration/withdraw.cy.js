359414f353d3dac4595af38590d364d86d757076
Radosław Szpiech <szpiechrados@gmail.com>
2022-07-13 17:23:51 +0200

    chore(token): get rid of locators files (#749)
    * chore(token): get rid of locators files
    
    * chore(token): refactor some functions and locators
    
    Co-authored-by: Rado <rado@vegaprotocol.io>

--- apps/token-e2e/src/integration/withdraw.cy.js
+++ apps/token-e2e/src/integration/withdraw.cy.js
@@ -1,36 +1,28 @@
-import navigation from '../locators/navigation.locators';
-import withdraw from '../locators/withdraw.locators';
+const connectToVegaBtn = '[data-testid="connect-to-vega-wallet-btn"]';
+const warning = '[data-testid="callout"]';
 
 context('Withdraw Page - verify elements on page', function () {
   before('navigate to withdraw page', function () {
-    cy.visit('/')
-      .get(navigation.section)
-      .within(() => {
-        cy.get(navigation.withdraw).click();
-      });
+    cy.visit('/').navigate_to('withdraw');
   });
 
   describe('with wallets disconnected', function () {
     it('should have withdraw tab highlighted', function () {
-      cy.get(navigation.section).within(() => {
-        cy.get(navigation.withdraw).should('have.attr', 'aria-current');
-      });
+      cy.verify_tab_highlighted('withdraw');
     });
 
     it('should have WITHDRAW header visible', function () {
-      cy.get(withdraw.pageHeader)
-        .should('be.visible')
-        .and('have.text', 'Withdraw');
+      cy.verify_page_header('Withdraw');
     });
 
     it('should have connect Vega wallet button', function () {
-      cy.get(withdraw.connectToVegaBtn)
+      cy.get(connectToVegaBtn)
         .should('be.visible')
         .and('have.text', 'Connect Vega wallet');
     });
 
     it('should have withdraw information box', function () {
-      cy.get(withdraw.warning).should('be.visible');
+      cy.get(warning).should('be.visible');
     });
   });
 });
--- apps/token-e2e/src/support/common.functions.js
+++ apps/token-e2e/src/support/common.functions.js
@@ -1,7 +1,33 @@
 Cypress.Commands.add(
-  'convertTokenValueToNumber',
+  'convert_token_value_to_number',
   { prevSubject: true },
   (subject) => {
     return parseFloat(subject.replace(/,/g, ''));
   }
 );
+
+const navigation = {
+  section: 'nav',
+  home: '[href="/"]',
+  vesting: '[href="/vesting"]',
+  staking: '[href="/staking"]',
+  rewards: '[href="/rewards"]',
+  withdraw: '[href="/withdraw"]',
+  governance: '[href="/governance"]',
+};
+
+Cypress.Commands.add('navigate_to', (page) => {
+  return cy.get(navigation.section).within(() => {
+    cy.get(navigation[page]).click();
+  });
+});
+
+Cypress.Commands.add('verify_tab_highlighted', (page) => {
+  return cy.get(navigation.section).within(() => {
+    cy.get(navigation[page]).should('have.attr', 'aria-current');
+  });
+});
+
+Cypress.Commands.add('verify_page_header', (text) => {
+  return cy.get('header h1').should('be.visible').and('have.text', text);
+});
