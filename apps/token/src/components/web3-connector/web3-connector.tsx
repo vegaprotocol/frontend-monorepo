cd82c674fb07700a0bb524cf3089d8bbd365c902
Dexter <dexter.edwards93@gmail.com>
2022-07-13 14:58:20 +0100

    fix: white text on disconnect screen

--- apps/token/src/components/web3-connector/web3-connector.tsx
+++ apps/token/src/components/web3-connector/web3-connector.tsx
@@ -79,7 +79,9 @@ export const Web3Content = ({ children, appChainId }: Web3ContentProps) => {
     return (
       <Splash>
         <div className="flex flex-col items-center gap-12">
-          <p>This app only works on chain ID: {appChainId}</p>
+          <p className="text-white">
+            This app only works on chain ID: {appChainId}
+          </p>
           <Button onClick={() => connector.deactivate()}>Disconnect</Button>
         </div>
       </Splash>
