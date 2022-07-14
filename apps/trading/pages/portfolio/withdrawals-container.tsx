ab77e99f96346d04b3c6afefab98fd30c510e69f
Matthew Russell <mattrussell36@gmail.com>
2022-07-14 17:12:28 +0100

    fix: withdrawals and depsits (#754)
    * feat: handle withdrawal limits
    
    * feat: add withdraw limit ui to withdraw form
    
    * chore: lint error
    
    * fix: mock network param query for e2e tests
    
    * fix: wrong translation in tests
    
    * fix: withdrawals test and revert change in text for trade grid elements
    
    * fix: add check for signature length before progressing withdraw

--- apps/trading/pages/portfolio/withdrawals-container.tsx
+++ apps/trading/pages/portfolio/withdrawals-container.tsx
@@ -1,6 +1,7 @@
 import orderBy from 'lodash/orderBy';
-import { AsyncRenderer } from '@vegaprotocol/ui-toolkit';
+import { AsyncRenderer, Button } from '@vegaprotocol/ui-toolkit';
 import { useWithdrawals, WithdrawalsTable } from '@vegaprotocol/withdraws';
+import Link from 'next/link';
 
 export const WithdrawalsContainer = () => {
   const { data, loading, error } = useWithdrawals();
@@ -16,7 +17,16 @@ export const WithdrawalsContainer = () => {
           (w) => new Date(w.createdTimestamp).getTime(),
           'desc'
         );
-        return <WithdrawalsTable withdrawals={withdrawals} />;
+        return (
+          <div className="grid grid-cols-[1fr_min-content] gap-12 h-full">
+            <WithdrawalsTable withdrawals={withdrawals} />
+            <div className="p-12">
+              <Link href="/portfolio/withdraw" passHref={true}>
+                <Button data-testid="start-withdrawal">Withdraw</Button>
+              </Link>
+            </div>
+          </div>
+        );
       }}
     />
   );
