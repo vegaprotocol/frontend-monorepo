cdd8407404f670e9ebe6076d60204b0f0294ba84
madalinaraicu <madalina@vegaprotocol.io>
2022-07-14 15:19:50 +0300

    fix: format volume with positionDecimalPlaces

--- apps/trading/pages/markets/__generated__/Market.ts
+++ apps/trading/pages/markets/__generated__/Market.ts
@@ -145,6 +145,12 @@ export interface Market_market {
    */
   decimalPlaces: number;
   /**
+   * positionDecimalPlaces indicated the number of decimal places that an integer must be shifted in order to get a correct size (uint64).
+   * i.e. 0 means there are no fractional orders for the market, and order sizes are always whole sizes.
+   * 2 means sizes given as 10^2 * desired size, e.g. a desired size of 1.23 is represented as 123 in this market.
+   */
+  positionDecimalPlaces: number;
+  /**
    * marketData for the given market
    */
   data: Market_market_data | null;
