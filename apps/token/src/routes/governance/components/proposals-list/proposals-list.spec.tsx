a98a3c5d1f2782a543a1d975baffa45eb818b832
sam-keen <samuel.kleinmann@gmail.com>
2022-07-18 10:20:32 +0100

    frontend-monorepo-671: Readded commented out tests

--- apps/token/src/routes/governance/components/proposals-list/proposals-list.spec.tsx
+++ apps/token/src/routes/governance/components/proposals-list/proposals-list.spec.tsx
@@ -81,66 +81,66 @@ const renderComponent = (proposals: Proposals_proposals[]) => (
   </Router>
 );
 
-// it('Culls failed proposals', () => {
-//   render(renderComponent([failedProposal]));
-//   expect(screen.queryByTestId('open-proposals')).not.toBeInTheDocument();
-//   expect(screen.getByTestId('no-open-proposals')).toBeInTheDocument();
-//   expect(screen.queryByTestId('closed-proposals')).not.toBeInTheDocument();
-//   expect(screen.getByTestId('no-closed-proposals')).toBeInTheDocument();
-// });
-//
-// it('Will hide filter if no proposals', () => {
-//   render(renderComponent([]));
-//   expect(screen.queryByTestId('proposals-list-filter')).not.toBeInTheDocument();
-// });
-//
-// it('Will show filter if there are proposals', () => {
-//   render(renderComponent([enactedProposalClosedLastWeek]));
-//   expect(screen.queryByTestId('proposals-list-filter')).toBeInTheDocument();
-// });
-//
-// it('Places proposals correctly in open or closed lists', () => {
-//   render(
-//     renderComponent([
-//       openProposalClosesNextWeek,
-//       openProposalClosesNextMonth,
-//       enactedProposalClosedLastWeek,
-//       rejectedProposalClosedLastMonth,
-//     ])
-//   );
-//   const openProposals = screen.getByTestId('open-proposals');
-//   const closedProposals = screen.getByTestId('closed-proposals');
-//   expect(
-//     openProposals.querySelectorAll("[data-testid='proposals-list-item']").length
-//   ).toBe(2);
-//   expect(
-//     closedProposals.querySelectorAll("[data-testid='proposals-list-item']")
-//       .length
-//   ).toBe(2);
-// });
-//
-// it('Orders proposals correctly by closingDateTime', () => {
-//   render(
-//     renderComponent([
-//       rejectedProposalClosedLastMonth,
-//       openProposalClosesNextMonth,
-//       openProposalClosesNextWeek,
-//       enactedProposalClosedLastWeek,
-//     ])
-//   );
-//   const openProposals = screen.getByTestId('open-proposals');
-//   const closedProposals = screen.getByTestId('closed-proposals');
-//   const openProposalsItems = openProposals.querySelectorAll(
-//     "[data-testid='proposals-list-item']"
-//   );
-//   const closedProposalsItems = closedProposals.querySelectorAll(
-//     "[data-testid='proposals-list-item']"
-//   );
-//   expect(openProposalsItems[0]).toHaveAttribute('id', 'proposal1');
-//   expect(openProposalsItems[1]).toHaveAttribute('id', 'proposal2');
-//   expect(closedProposalsItems[0]).toHaveAttribute('id', 'proposal4');
-//   expect(closedProposalsItems[1]).toHaveAttribute('id', 'proposal3');
-// });
+it('Culls failed proposals', () => {
+  render(renderComponent([failedProposal]));
+  expect(screen.queryByTestId('open-proposals')).not.toBeInTheDocument();
+  expect(screen.getByTestId('no-open-proposals')).toBeInTheDocument();
+  expect(screen.queryByTestId('closed-proposals')).not.toBeInTheDocument();
+  expect(screen.getByTestId('no-closed-proposals')).toBeInTheDocument();
+});
+
+it('Will hide filter if no proposals', () => {
+  render(renderComponent([]));
+  expect(screen.queryByTestId('proposals-list-filter')).not.toBeInTheDocument();
+});
+
+it('Will show filter if there are proposals', () => {
+  render(renderComponent([enactedProposalClosedLastWeek]));
+  expect(screen.queryByTestId('proposals-list-filter')).toBeInTheDocument();
+});
+
+it('Places proposals correctly in open or closed lists', () => {
+  render(
+    renderComponent([
+      openProposalClosesNextWeek,
+      openProposalClosesNextMonth,
+      enactedProposalClosedLastWeek,
+      rejectedProposalClosedLastMonth,
+    ])
+  );
+  const openProposals = screen.getByTestId('open-proposals');
+  const closedProposals = screen.getByTestId('closed-proposals');
+  expect(
+    openProposals.querySelectorAll("[data-testid='proposals-list-item']").length
+  ).toBe(2);
+  expect(
+    closedProposals.querySelectorAll("[data-testid='proposals-list-item']")
+      .length
+  ).toBe(2);
+});
+
+it('Orders proposals correctly by closingDateTime', () => {
+  render(
+    renderComponent([
+      rejectedProposalClosedLastMonth,
+      openProposalClosesNextMonth,
+      openProposalClosesNextWeek,
+      enactedProposalClosedLastWeek,
+    ])
+  );
+  const openProposals = screen.getByTestId('open-proposals');
+  const closedProposals = screen.getByTestId('closed-proposals');
+  const openProposalsItems = openProposals.querySelectorAll(
+    "[data-testid='proposals-list-item']"
+  );
+  const closedProposalsItems = closedProposals.querySelectorAll(
+    "[data-testid='proposals-list-item']"
+  );
+  expect(openProposalsItems[0]).toHaveAttribute('id', 'proposal1');
+  expect(openProposalsItems[1]).toHaveAttribute('id', 'proposal2');
+  expect(closedProposalsItems[0]).toHaveAttribute('id', 'proposal4');
+  expect(closedProposalsItems[1]).toHaveAttribute('id', 'proposal3');
+});
 
 it('Displays info on no proposals', () => {
   render(renderComponent([]));
@@ -150,78 +150,78 @@ it('Displays info on no proposals', () => {
   expect(screen.getByTestId('no-closed-proposals')).toBeInTheDocument();
 });
 
-// it('Displays info on no open proposals if only closed are present', () => {
-//   render(renderComponent([enactedProposalClosedLastWeek]));
-//   expect(screen.queryByTestId('open-proposals')).not.toBeInTheDocument();
-//   expect(screen.getByTestId('no-open-proposals')).toBeInTheDocument();
-//   expect(screen.getByTestId('closed-proposals')).toBeInTheDocument();
-//   expect(screen.queryByTestId('no-closed-proposals')).not.toBeInTheDocument();
-// });
-//
-// it('Displays info on no closed proposals if only open are present', () => {
-//   render(renderComponent([openProposalClosesNextWeek]));
-//   expect(screen.getByTestId('open-proposals')).toBeInTheDocument();
-//   expect(screen.queryByTestId('no-open-proposals')).not.toBeInTheDocument();
-//   expect(screen.queryByTestId('closed-proposals')).not.toBeInTheDocument();
-//   expect(screen.getByTestId('no-closed-proposals')).toBeInTheDocument();
-// });
-//
-// it('Opens filter form when button is clicked', () => {
-//   render(
-//     renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
-//   );
-//   fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
-//   expect(screen.getByTestId('open-proposals-list-filter')).toBeInTheDocument();
-// });
-//
-// it('Filters list by text - proposal id', () => {
-//   render(
-//     renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
-//   );
-//   fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
-//   fireEvent.change(screen.getByTestId('filter-input'), {
-//     target: { value: 'proposal1' },
-//   });
-//   const container = screen.getByTestId('open-proposals');
-//   expect(container.querySelector('#proposal1')).toBeInTheDocument();
-//   expect(container.querySelector('#proposal2')).not.toBeInTheDocument();
-// });
-//
-// it('Filters list by text - party id', () => {
-//   render(
-//     renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
-//   );
-//   fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
-//   fireEvent.change(screen.getByTestId('filter-input'), {
-//     target: { value: 'bvcx' },
-//   });
-//   const container = screen.getByTestId('open-proposals');
-//   expect(container.querySelector('#proposal2')).toBeInTheDocument();
-//   expect(container.querySelector('#proposal1')).not.toBeInTheDocument();
-// });
-//
-// it('Filters list by text - proposal rationale', () => {
-//   render(
-//     renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
-//   );
-//   fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
-//   fireEvent.change(screen.getByTestId('filter-input'), {
-//     target: { value: 'proposal that closes next month' },
-//   });
-//   const container = screen.getByTestId('open-proposals');
-//   expect(container.querySelector('#proposal1')).toBeInTheDocument();
-//   expect(container.querySelector('#proposal2')).not.toBeInTheDocument();
-// });
-//
-// it('Filters list by text - check for substring matching', () => {
-//   render(
-//     renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
-//   );
-//   fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
-//   fireEvent.change(screen.getByTestId('filter-input'), {
-//     target: { value: 'next month' },
-//   });
-//   const container = screen.getByTestId('open-proposals');
-//   expect(container.querySelector('#proposal1')).toBeInTheDocument();
-//   expect(container.querySelector('#proposal2')).not.toBeInTheDocument();
-// });
+it('Displays info on no open proposals if only closed are present', () => {
+  render(renderComponent([enactedProposalClosedLastWeek]));
+  expect(screen.queryByTestId('open-proposals')).not.toBeInTheDocument();
+  expect(screen.getByTestId('no-open-proposals')).toBeInTheDocument();
+  expect(screen.getByTestId('closed-proposals')).toBeInTheDocument();
+  expect(screen.queryByTestId('no-closed-proposals')).not.toBeInTheDocument();
+});
+
+it('Displays info on no closed proposals if only open are present', () => {
+  render(renderComponent([openProposalClosesNextWeek]));
+  expect(screen.getByTestId('open-proposals')).toBeInTheDocument();
+  expect(screen.queryByTestId('no-open-proposals')).not.toBeInTheDocument();
+  expect(screen.queryByTestId('closed-proposals')).not.toBeInTheDocument();
+  expect(screen.getByTestId('no-closed-proposals')).toBeInTheDocument();
+});
+
+it('Opens filter form when button is clicked', () => {
+  render(
+    renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
+  );
+  fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
+  expect(screen.getByTestId('open-proposals-list-filter')).toBeInTheDocument();
+});
+
+it('Filters list by text - proposal id', () => {
+  render(
+    renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
+  );
+  fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
+  fireEvent.change(screen.getByTestId('filter-input'), {
+    target: { value: 'proposal1' },
+  });
+  const container = screen.getByTestId('open-proposals');
+  expect(container.querySelector('#proposal1')).toBeInTheDocument();
+  expect(container.querySelector('#proposal2')).not.toBeInTheDocument();
+});
+
+it('Filters list by text - party id', () => {
+  render(
+    renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
+  );
+  fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
+  fireEvent.change(screen.getByTestId('filter-input'), {
+    target: { value: 'bvcx' },
+  });
+  const container = screen.getByTestId('open-proposals');
+  expect(container.querySelector('#proposal2')).toBeInTheDocument();
+  expect(container.querySelector('#proposal1')).not.toBeInTheDocument();
+});
+
+it('Filters list by text - proposal rationale', () => {
+  render(
+    renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
+  );
+  fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
+  fireEvent.change(screen.getByTestId('filter-input'), {
+    target: { value: 'proposal that closes next month' },
+  });
+  const container = screen.getByTestId('open-proposals');
+  expect(container.querySelector('#proposal1')).toBeInTheDocument();
+  expect(container.querySelector('#proposal2')).not.toBeInTheDocument();
+});
+
+it('Filters list by text - check for substring matching', () => {
+  render(
+    renderComponent([openProposalClosesNextMonth, openProposalClosesNextWeek])
+  );
+  fireEvent.click(screen.getByTestId('set-proposals-filter-visible'));
+  fireEvent.change(screen.getByTestId('filter-input'), {
+    target: { value: 'next month' },
+  });
+  const container = screen.getByTestId('open-proposals');
+  expect(container.querySelector('#proposal1')).toBeInTheDocument();
+  expect(container.querySelector('#proposal2')).not.toBeInTheDocument();
+});
