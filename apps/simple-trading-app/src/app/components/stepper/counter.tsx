39ced284740fec3409aff7e18b045ebcd6a0118a
Elmar Gasimov <elmar@vegaprotocol.io>
2022-07-18 15:57:09 +0100

    chore(console-lite): refactor stepper counter styles to make it more legible

--- apps/simple-trading-app/src/app/components/stepper/counter.tsx
+++ apps/simple-trading-app/src/app/components/stepper/counter.tsx
@@ -8,18 +8,38 @@ interface CounterProps {
   isActive: boolean;
 }
 
+type TStyleMap = {
+  [key: string]: string;
+};
+
+const reduceStyles = (styleMap: TStyleMap) =>
+  Object.keys(styleMap).reduce((acc, curr) => acc + `${styleMap[curr]} `, '');
+
 export const Counter = ({ label, isActive, className }: CounterProps) => {
+  const generalStyles: TStyleMap = {
+    shape: 'rounded-full relative aspect-square clip-path-rounded',
+    border: 'border-black dark:border-white',
+    colours: 'bg-white dark:bg-black text-black dark:text-white',
+    children: 'flex items-center justify-center text-center',
+  };
+
+  const unselected: TStyleMap = {
+    border: 'border',
+    size: 'w-[30px] md:w-40',
+    margin: 'mt-[5px] md:ml-[10px] md:mt-0',
+  };
+
+  const selected: TStyleMap = {
+    colours: 'text-xl text-white',
+    size: 'w-40 md:w-[60px]',
+  };
+
   const classes = classNames(
-    'rounded-full relative border-black dark:border-white bg-white dark:bg-black text-black dark:text-white aspect-square clip-path-rounded flex items-center justify-center text-center',
-    {
-      border: !isActive,
-      'mt-[5px] md:ml-[10px] md:mt-0': !isActive,
-      'w-[30px] md:w-40': !isActive,
-      'w-40 md:w-[60px]': isActive,
-      'text-xl text-white': isActive,
-    },
+    reduceStyles(generalStyles),
+    isActive ? reduceStyles(selected) : reduceStyles(unselected),
     className
-  );
+  ).replace(/  +/g, ' '); // remove double space
+
   return (
     <div className={classes} aria-hidden>
       {isActive && (
