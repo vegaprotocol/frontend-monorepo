8b4ea1118c581bbb0c6ad21941226c235e2ec68e
Elmar Gasimov <elmar@vegaprotocol.io>
2022-07-19 00:31:47 +0100

    feat(console-lit): change balance styles

--- apps/simple-trading-app/src/app/components/deal-ticket/deal-ticket-balance.tsx
+++ apps/simple-trading-app/src/app/components/deal-ticket/deal-ticket-balance.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import classNames from 'classNames';
 import type { DealTicketQuery_market_tradableInstrument_instrument_product_settlementAsset } from '@vegaprotocol/deal-ticket';
 import type { PartyBalanceQuery_party_accounts } from './__generated__/PartyBalanceQuery';
 import { useSettlementAccount } from '../../hooks/use-settlement-account';
@@ -8,12 +9,14 @@ interface DealTicketBalanceProps {
   settlementAsset: DealTicketQuery_market_tradableInstrument_instrument_product_settlementAsset;
   accounts: PartyBalanceQuery_party_accounts[];
   isWalletConnected: boolean;
+  className?: string;
 }
 
 export const DealTicketBalance = ({
   settlementAsset,
   accounts,
   isWalletConnected,
+  className = '',
 }: DealTicketBalanceProps) => {
   const settlementAssetId = settlementAsset?.id;
   const settlementAssetSymbol = settlementAsset?.symbol;
@@ -26,28 +29,22 @@ export const DealTicketBalance = ({
       settlementAccount.asset.decimals
     );
 
-  const balance = settlementAccount ? (
-    <p>
-      {t(
-        `${formatedNumber} ${settlementAccount.asset.symbol} available to trade`
-      )}
+  const balance = (
+    <p className="text-blue text-lg font-semibold">
+      {settlementAccount
+        ? t(`${formatedNumber}`)
+        : `No ${settlementAssetSymbol} left to trade`}
     </p>
-  ) : (
-    <p>{t(`You have no ${settlementAssetSymbol} available to trade`)}</p>
   );
 
   const connectWallet = (
-    <p>
-      {t(
-        "Please connect your Vega wallet to see your balance for this market's settlement asset"
-      )}
-    </p>
+    <p>{t('Please connect your Vega wallet to see your balance')}</p>
   );
 
   return (
-    <div className="text-right">
-      <div className="border border-current p-16 inline-block">
-        <small className="text-text">{t('Balance')}</small>
+    <div className={classNames('text-right', className)}>
+      <div className="inline-block">
+        <span className="text-blue">{settlementAssetSymbol}</span>
         {isWalletConnected ? balance : connectWallet}
       </div>
     </div>
