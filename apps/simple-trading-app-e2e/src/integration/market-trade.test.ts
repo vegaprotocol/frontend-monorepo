2c249bd9b8fbaf46556a6ae43683ae6b7b239a47
maciek <maciek@vegaprotocol.io>
2022-07-19 16:43:11 +0200

    feat: [console-lite] - trading - direction selector - add int tests

---
+++ apps/simple-trading-app-e2e/src/integration/market-trade.test.ts
@@ -0,0 +1,67 @@
+import { aliasQuery } from '@vegaprotocol/cypress';
+import { generateSimpleMarkets } from '../support/mocks/generate-markets';
+import { generateDealTicket } from '../support/mocks/generate-deal-ticket';
+
+describe('Market trade', () => {
+  let markets;
+  beforeEach(() => {
+    cy.mockGQL((req) => {
+      aliasQuery(req, 'SimpleMarkets', generateSimpleMarkets());
+      aliasQuery(req, 'DealTicketQuery', generateDealTicket());
+    });
+    cy.visit('/markets');
+    cy.wait('@SimpleMarkets').then((response) => {
+      if (response.response?.body?.data?.markets?.length) {
+        markets = response.response?.body?.data?.markets;
+      }
+    });
+  });
+  it('side selector should work well', () => {
+    if (markets) {
+      cy.visit(`/trading/${markets[0].id}`);
+      cy.get('#step-1-control [aria-label^="Selected value"]').should(
+        'have.text',
+        'Long'
+      );
+      cy.get('#step-1-control [aria-label^="Selected value"]').click();
+      cy.get('button[aria-label="Open short position"]').click();
+      cy.get('#step-2-control').click();
+      cy.get('#step-1-control [aria-label^="Selected value"]').should(
+        'have.text',
+        'Short'
+      );
+    }
+  });
+
+  it('mobile view should work well', () => {
+    if (markets) {
+      cy.viewport('iphone-xr');
+      cy.visit(`/trading/${markets[0].id}`);
+      cy.get('button').contains('Next').click();
+
+      cy.get('button[aria-label="Open long position"]').should(
+        'have.class',
+        'selected'
+      );
+      cy.get('button[aria-label="Open short position"]').should(
+        'not.have.class',
+        'selected'
+      );
+
+      cy.get('button[aria-label="Open short position"]').click();
+      cy.get('button[aria-label="Open long position"]').should(
+        'not.have.class',
+        'selected'
+      );
+      cy.get('button[aria-label="Open short position"]').should(
+        'have.class',
+        'selected'
+      );
+      cy.get('button').contains('Next').click();
+      cy.get('#step-1-control').should(
+        'contain.html',
+        'aria-label="Selected value Short"'
+      );
+    }
+  });
+});
