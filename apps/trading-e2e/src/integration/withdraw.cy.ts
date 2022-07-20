59993658a1cf980f9282a37daa4a0606deb3daa4
Botond <botond@vegaprotocol.io>
2022-07-20 11:46:42 +0100

    fix: eth address in e2e

--- apps/trading-e2e/src/integration/withdraw.cy.ts
+++ apps/trading-e2e/src/integration/withdraw.cy.ts
@@ -11,6 +11,9 @@ describe('withdraw', () => {
   const amountField = 'input[name="amount"]';
   const useMaximumAmount = 'use-maximum';
   const submitWithdrawBtn = 'submit-withdrawal';
+  const ethAddressValue = `${Cypress.env(
+    'ETHEREUM_WALLET_ADDRESS'
+  )}/address/0x265Cc6d39a1B53d0d92068443009eE7410807158`;
 
   beforeEach(() => {
     cy.mockWeb3Provider();
@@ -42,10 +45,7 @@ describe('withdraw', () => {
     cy.getByTestId(formFieldError).should('have.length', 2);
 
     // Test for Ethereum address
-    cy.get(toAddressField).should(
-      'have.value',
-      process.env['ETHEREUM_WALLET_ADDRESS']
-    );
+    cy.get(toAddressField).should('have.value', ethAddressValue);
 
     // Test min amount
     cy.get(assetSelectField).select('Asset 1'); // Select asset so we have a min viable amount calculated
