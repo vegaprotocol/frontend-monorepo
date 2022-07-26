6f374264c0db58f29d08f75fe623fb41f1cf7c29
Sam Keen <samuel@vegaprotocol.io>
2022-07-26 20:26:12 +0100

    Feat/45 - Validator list (#833)
    * frontend-monorepo-45: Removed party data from validators table
    
    * frontend-monorepo-45: Removed redundant query fields
    
    * frontend-monorepo-45: Validator AG Grid working and fields formatted
    
    * frontend-monorepo-45: Removed old node list tests (as now handled by AG Grid)
    
    * Frontend-monorepo-45: Removed unwanted auto-added imports
    
    * Frontend-monorepo-45: Fixing cypress tests
    
    * Frontend-monorepo-45: Removed unnecessary class added as part of layout test
    
    * Frontend-monorepo-45: Removed unnecessary refactor added as part of layout test
    
    * Frontend-monorepo-45: Removed unnecessary grid export added as part of grid setup
    
    * frontend-monorepo-45: Removed unwanted font-size and adjusted avatar size in proportion
    
    * frontend-monorepo-45: Expanded node-list tests
    
    * frontend-monorepo-45: Used toString on translations to make node list coldefs a bit clearer
    
    * frontend-monorepo-45: Converted nodeFactory to use lodash merge and PartialDeep
    
    * frontend-monorepo-45: Refactored so node list AG grid field names are decoupled from translations
    
    * frontend-monorepo-45: Added refreshing when epoch changes
    
    * frontend-monorepo-45: Used epoch data already passed to node list instead of a new query
    
    * frontend-monorepo-45: Node list - used formatNumber from libs/react-helpers
    
    * frontend-monorepo-45: formatNumber now accepts BigNumbers

--- apps/token/src/routes/staking/node-list.spec.tsx
+++ apps/token/src/routes/staking/node-list.spec.tsx
@@ -1,49 +1,44 @@
-import { render, screen, waitFor, within } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import merge from 'lodash/merge';
 import { NodeList, NODES_QUERY } from './node-list';
 import { MockedProvider } from '@apollo/client/testing';
 import { MemoryRouter } from 'react-router-dom';
 import { addDecimal } from '@vegaprotocol/react-helpers';
 import type { Nodes_nodes } from './__generated__/Nodes';
+import type { PartialDeep } from 'type-fest';
 
 jest.mock('../../components/epoch-countdown', () => ({
   EpochCountdown: () => <div data-testid="epoch-info"></div>,
 }));
 
-const nodeFactory = (overrides?: Partial<Nodes_nodes>) => ({
-  id: 'ccc022b7e63a4d0a6d3a193c3940c88574060e58a184964c994998d86835a1b4',
-  name: 'Skynet',
-  pubkey: '6abc23391a9f888ab240415bf63d6844b03fc360be822f4a1d2cd832d87b2917',
-  infoUrl: 'https://en.wikipedia.org/wiki/Skynet_(Terminator)',
-  location: '',
-  stakedByOperator: '3000000000000000000000',
-  stakedByDelegates: '11182454495731682635157',
-  stakedTotal: '14182454495731682635157',
-  pendingStake: '0',
-  stakedByOperatorFormatted: addDecimal(
-    overrides?.stakedByOperator || '3000000000000000000000',
-    18
-  ),
-  stakedByDelegatesFormatted: addDecimal(
-    overrides?.stakedByDelegates || '11182454495731682635157',
-    18
-  ),
-  stakedTotalFormatted: addDecimal(
-    overrides?.stakedTotal || '14182454495731682635157',
-    18
-  ),
-  pendingStakeFormatted: addDecimal(overrides?.pendingStake || '0', 18),
-  epochData: null,
-  status: 'Validator',
-  rankingScore: {
-    rankingScore: '0.67845061012234727427532760837568',
-    stakeScore: '0.3392701644525644',
-    performanceScore: '0.9998677767864936',
-    votingPower: '2407',
-    __typename: 'RankingScore',
-  },
-  __typename: 'Node',
-  ...overrides,
-});
+const nodeFactory = (overrides?: PartialDeep<Nodes_nodes>) => {
+  const defaultNode = {
+    id: 'ccc022b7e63a4d0a6d3a193c3940c88574060e58a184964c994998d86835a1b4',
+    name: 'Skynet',
+    avatarUrl: 'https://upload.wikimedia.org/wikipedia/en/2/25/Marvin-TV-3.jpg',
+    pubkey: '6abc23391a9f888ab240415bf63d6844b03fc360be822f4a1d2cd832d87b2917',
+    infoUrl: 'https://en.wikipedia.org/wiki/Skynet_(Terminator)',
+    location: '',
+    stakedByOperator: '3000000000000000000000',
+    stakedByDelegates: '11182454495731682635157',
+    stakedTotal: '14182454495731682635157',
+    stakedTotalFormatted: addDecimal('14182454495731682635157', 18),
+    pendingStake: '0',
+    pendingStakeFormatted: addDecimal('0', 18),
+    epochData: null,
+    status: 'Validator',
+    rankingScore: {
+      rankingScore: '0.67845061012234727427532760837568',
+      stakeScore: '0.3392701644525644',
+      performanceScore: '0.9998677767864936',
+      votingPower: '2407',
+      status: 'tendermint',
+      __typename: 'RankingScore',
+    },
+    __typename: 'Node',
+  };
+  return merge(defaultNode, overrides);
+};
 
 const MOCK_NODES = {
   nodes: [
@@ -53,15 +48,14 @@ const MOCK_NODES = {
       name: 'T-800 Terminator',
       pubkey:
         'ccc3b8362c25b09d20df8ea407b0a476d6b24a0e72bc063d0033c8841652ddd4',
-      infoUrl: 'https://en.wikipedia.org/wiki/Terminator_(character)',
-      stakedByOperator: '3000000000000000000000',
-      stakedByDelegates: '6618711883996159534058',
       stakedTotal: '9618711883996159534058',
+      stakedTotalFormatted: addDecimal('9618711883996159534058', 18),
       rankingScore: {
         rankingScore: '0.4601942440481428',
         stakeScore: '0.2300971220240714',
         performanceScore: '1',
         votingPower: '2408',
+        status: 'tendermint',
         __typename: 'RankingScore',
       },
     }),
@@ -70,16 +64,15 @@ const MOCK_NODES = {
       name: 'NCC-1701-E',
       pubkey:
         '0931a8fd8cc935458f470e435a05414387cea6f329d648be894fcd44bd517a2b',
-      infoUrl: 'https://en.wikipedia.org/wiki/USS_Enterprise_(NCC-1701-E)',
-      stakedByOperator: '3000000000000000000000',
-      stakedByDelegates: '1041343338923442976709',
       stakedTotal: '4041343338923442976709',
+      stakedTotalFormatted: addDecimal('4041343338923442976709', 18),
       pendingStake: '0',
       rankingScore: {
         rankingScore: '0.1932810100133910357676209647912',
         stakeScore: '0.0966762995515676',
         performanceScore: '0.999629748500531',
         votingPower: '1163',
+        status: 'tendermint',
         __typename: 'RankingScore',
       },
     }),
@@ -95,14 +88,14 @@ const MOCK_NODES = {
   },
 };
 
-const renderNodeList = () => {
+const renderNodeList = (data = MOCK_NODES) => {
   return render(
     <MemoryRouter>
       <MockedProvider
         mocks={[
           {
             request: { query: NODES_QUERY },
-            result: { data: MOCK_NODES },
+            result: { data },
           },
         ]}
       >
@@ -115,27 +108,6 @@ const renderNodeList = () => {
               start: new Date(0).toISOString(),
               end: '',
               expiry: new Date(1000 * 60 * 60 * 24).toISOString(),
-            },
-          }}
-          party={{
-            __typename: 'Party',
-            id: 'foo',
-            delegations: [
-              {
-                __typename: 'Delegation',
-                amount: '0',
-                amountFormatted: '0',
-                epoch: 1,
-                node: {
-                  __typename: 'Node',
-                  id: 'bar',
-                },
-              },
-            ],
-            stake: {
-              __typename: 'PartyStake',
-              currentStakeAvailable: '0',
-              currentStakeAvailableFormatted: '0',
             },
           }}
         />
@@ -162,43 +134,101 @@ describe('Nodes list', () => {
     expect(screen.getByTestId('epoch-info')).toBeInTheDocument();
   });
 
-  it('should a lit of all nodes', async () => {
+  it('should render a list of all nodes', async () => {
     renderNodeList();
 
     await waitFor(() => {
       expect(screen.getByText(MOCK_NODES.nodes[0].name)).toBeInTheDocument();
+      expect(screen.getByText(MOCK_NODES.nodes[1].name)).toBeInTheDocument();
+      expect(screen.getByText(MOCK_NODES.nodes[2].name)).toBeInTheDocument();
     });
-    const items = screen.queryAllByTestId('node-list-item');
-    expect(items).toHaveLength(3);
-    for (const item of MOCK_NODES.nodes) {
-      expect(screen.getByText(item.name)).toBeInTheDocument();
-    }
   });
 
-  it('should list the total stake and rewards of each node', async () => {
-    renderNodeList();
+  it('should display the correctly formatted fields in the correct columns', async () => {
+    const MOCK_NODE = {
+      nodes: [
+        nodeFactory({
+          id: '966438c6bffac737cfb08173ffcb3f393c4692b099ad80cb45a82e2dc0a8cf99',
+          name: 'T-800 Terminator',
+          avatarUrl:
+            'https://upload.wikimedia.org/wikipedia/en/9/94/T-800_%28Model_101%29.png',
+          pubkey:
+            'ccc3b8362c25b09d20df8ea407b0a476d6b24a0e72bc063d0033c8841652ddd4',
+          stakedTotal: '9618711883996159534058',
+          stakedTotalFormatted: addDecimal('9618711883996159534058', 18),
+          rankingScore: {
+            rankingScore: '0.4601942440481428',
+            stakeScore: '0.2300971220240714',
+            performanceScore: '1',
+            votingPower: '2408',
+            status: 'tendermint',
+            __typename: 'RankingScore',
+          },
+        }),
+      ],
+      nodeData: {
+        stakedTotal: '9618711883996159534058',
+        stakedTotalFormatted: addDecimal('9618711883996159534058', 18),
+        totalNodes: 1,
+        inactiveNodes: 0,
+        validatingNodes: 1,
+        uptime: 1560.266845703125,
+        __typename: 'NodeData',
+      },
+    };
 
+    renderNodeList(MOCK_NODE);
     await waitFor(() => {
-      expect(screen.getByText(MOCK_NODES.nodes[0].name)).toBeInTheDocument();
+      expect(screen.getByText(MOCK_NODE.nodes[0].name)).toBeInTheDocument();
     });
-    const items = screen.queryAllByTestId('node-list-item');
-    const item = within(items[0]);
-    const rows = item.getAllByRole('row');
 
-    const expectedValues = [
-      ['Total stake', '14,182.45 (50.94%)'],
-      ['Ranking score', '0.6785'],
-      ['Stake score', '0.3393'],
-      ['Performance score', '0.9999'],
-      ['Voting score', '2,407.0000'],
-    ];
+    const grid = screen.getByTestId('validators-grid');
 
-    for (const [i, r] of rows.entries()) {
-      const row = within(r);
-      const cell = row.getByRole('cell');
-      const header = row.getByRole('rowheader');
-      expect(header).toHaveTextContent(expectedValues[i][0]);
-      expect(cell).toHaveTextContent(expectedValues[i][1]);
-    }
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="validator"]')
+    ).toHaveTextContent('T-800 Terminator');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="validator"] img')
+    ).toHaveAttribute(
+      'src',
+      'https://upload.wikimedia.org/wikipedia/en/9/94/T-800_%28Model_101%29.png'
+    );
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="status"]')
+    ).toHaveTextContent('Consensus');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="totalStakeThisEpoch"]')
+    ).toHaveTextContent('9,618.71');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="share"]')
+    ).toHaveTextContent('100%');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="validatorStake"]')
+    ).toHaveTextContent('9,618.71');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="pendingStake"]')
+    ).toHaveTextContent('0');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="rankingScore"]')
+    ).toHaveTextContent('0.46019');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="stakeScore"]')
+    ).toHaveTextContent('0.23010');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="performanceScore"]')
+    ).toHaveTextContent('1.00000');
+
+    expect(
+      grid.querySelector('[role="gridcell"][col-id="votingPower"]')
+    ).toHaveTextContent('2408');
   });
 });
