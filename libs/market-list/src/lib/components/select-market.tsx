b1073d6d7c1b48997b38a51e1744f6dd90d3faa2
madalinaraicu <madalina@vegaprotocol.io>
2022-08-02 20:19:18 +0100

    fix: #30 show markets where you have open positions

--- libs/market-list/src/lib/components/select-market.tsx
+++ libs/market-list/src/lib/components/select-market.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/client';
-import { t } from '@vegaprotocol/react-helpers';
+import { t, useDataProvider } from '@vegaprotocol/react-helpers';
 import { Interval } from '@vegaprotocol/types';
 import {
   ArrowDown,
@@ -10,12 +10,15 @@ import {
 } from '@vegaprotocol/ui-toolkit';
 import classNames from 'classnames';
 import isNil from 'lodash/isNil';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { MARKET_LIST_QUERY } from '../markets-data-provider';
 import { SelectMarketList } from './select-market-list';
 import type { MarketList } from '../__generated__';
+import { useVegaWallet } from '@vegaprotocol/wallet';
+import { positionsDataProvider } from '@vegaprotocol/positions';
 
 export const SelectMarketPopover = ({ marketName }: { marketName: string }) => {
+  const { keypair } = useVegaWallet();
   const [open, setOpen] = useState(false);
   const yesterday = Math.round(new Date().getTime() / 1000) - 24 * 3600;
   const yTimestamp = new Date(yesterday * 1000).toISOString();
@@ -26,6 +29,21 @@ export const SelectMarketPopover = ({ marketName }: { marketName: string }) => {
   const isOpen = !isNil(data) && open;
   const headerTriggerButtonClassName =
     'flex items-center gap-8 shrink-0 font-medium text-h5 hover:bg-black/10 dark:hover:bg-white/20';
+  const variables = useMemo(() => ({ partyId: keypair?.pub }), [keypair?.pub]);
+
+  const { data: marketDataPositions } = useDataProvider({
+    dataProvider: positionsDataProvider,
+    variables,
+  });
+  const positionMarkets: MarketList = {
+    markets:
+      data?.markets?.filter((market) =>
+        marketDataPositions?.find(
+          (position) => position.market.id === market.id
+        )
+      ) || null,
+  };
+
   return (
     <Popover
       open={isOpen}
@@ -57,6 +75,21 @@ export const SelectMarketPopover = ({ marketName }: { marketName: string }) => {
       }
     >
       <div className="m-20">
+        {keypair && (
+          <>
+            <h1
+              className={`text-h4 font-bold text-black-95 dark:text-white-95 mt-0 mb-6`}
+              data-testid="dialog-title"
+            >
+              {t('My markets')}
+            </h1>
+            <SelectMarketList
+              data={positionMarkets}
+              onSelect={() => setOpen(false)}
+              detailed={true}
+            />
+          </>
+        )}
         <h1
           className={`text-h4 font-bold text-black-95 dark:text-white-95 mt-0 mb-6`}
           data-testid="dialog-title"
