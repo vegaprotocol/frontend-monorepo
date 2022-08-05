c1415f83fd0f0877ca88758284bcf40f5eef70d9
madalinaraicu <madalina@vegaprotocol.io>
2022-08-05 15:19:04 +0100

    fix: fix some of the tests

--- libs/fills/src/lib/fills-table.spec.tsx
+++ libs/fills/src/lib/fills-table.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import { getDateTimeFormat } from '@vegaprotocol/react-helpers';
 import { Side } from '@vegaprotocol/types';
+import { act } from 'react-dom/test-utils';
 import type { PartialDeep } from 'type-fest';
 
 import { FillsTable } from './fills-table';
@@ -46,134 +47,142 @@ describe('FillsTable', () => {
     };
   });
 
-  it('correct columns are rendered', async () => {
-    render(<FillsTable partyId="party-id" rowData={[generateFill()]} />);
-    await waitForGridToBeInTheDOM();
-    await waitForDataToHaveLoaded();
+  it('should render correct columns', async () => {
+    act(async () => {
+      render(<FillsTable partyId="party-id" rowData={[generateFill()]} />);
+      await waitForGridToBeInTheDOM();
+      await waitForDataToHaveLoaded();
 
-    const headers = screen.getAllByRole('columnheader');
-    expect(headers).toHaveLength(7);
-    expect(headers.map((h) => h.textContent?.trim())).toEqual([
-      'Market',
-      'Amount',
-      'Value',
-      'Filled value',
-      'Role',
-      'Fee',
-      'Date',
-    ]);
+      const headers = screen.getAllByRole('columnheader');
+      expect(headers).toHaveLength(7);
+      expect(headers.map((h) => h.textContent?.trim())).toEqual([
+        'Market',
+        'Amount',
+        'Value',
+        'Filled value',
+        'Role',
+        'Fee',
+        'Date',
+      ]);
+    });
   });
 
-  it('formats cells correctly for buyer fill', async () => {
-    const partyId = 'party-id';
-    const buyerFill = generateFill({
-      ...defaultFill,
-      buyer: {
-        id: partyId,
-      },
-      aggressor: Side.Sell,
-      buyerFee: {
-        makerFee: '2',
-        infrastructureFee: '2',
-        liquidityFee: '2',
-      },
+  it('should format cells correctly for buyer fill', async () => {
+    act(async () => {
+      const partyId = 'party-id';
+      const buyerFill = generateFill({
+        ...defaultFill,
+        buyer: {
+          id: partyId,
+        },
+        aggressor: Side.Sell,
+        buyerFee: {
+          makerFee: '2',
+          infrastructureFee: '2',
+          liquidityFee: '2',
+        },
+      });
+
+      render(<FillsTable partyId={partyId} rowData={[buyerFill]} />);
+      await waitForGridToBeInTheDOM();
+      await waitForDataToHaveLoaded();
+
+      const cells = screen.getAllByRole('gridcell');
+      const expectedValues = [
+        buyerFill.market.name,
+        '+3.00000',
+        '1.00 BTC',
+        '3.00 BTC',
+        'Maker',
+        '0.06 BTC',
+        getDateTimeFormat().format(new Date(buyerFill.createdAt)),
+      ];
+      cells.forEach((cell, i) => {
+        expect(cell).toHaveTextContent(expectedValues[i]);
+      });
+
+      const amountCell = cells.find((c) => c.getAttribute('col-id') === 'size');
+      expect(amountCell).toHaveClass('text-vega-green');
     });
-
-    render(<FillsTable partyId={partyId} rowData={[buyerFill]} />);
-    await waitForGridToBeInTheDOM();
-    await waitForDataToHaveLoaded();
-
-    const cells = screen.getAllByRole('gridcell');
-    const expectedValues = [
-      buyerFill.market.name,
-      '+3.00000',
-      '1.00 BTC',
-      '3.00 BTC',
-      'Maker',
-      '0.06 BTC',
-      getDateTimeFormat().format(new Date(buyerFill.createdAt)),
-    ];
-    cells.forEach((cell, i) => {
-      expect(cell).toHaveTextContent(expectedValues[i]);
-    });
-
-    const amountCell = cells.find((c) => c.getAttribute('col-id') === 'size');
-    expect(amountCell).toHaveClass('text-vega-green');
   });
 
-  it('formats cells correctly for seller fill', async () => {
-    const partyId = 'party-id';
-    const buyerFill = generateFill({
-      ...defaultFill,
-      seller: {
-        id: partyId,
-      },
-      aggressor: Side.Sell,
-      sellerFee: {
-        makerFee: '1',
-        infrastructureFee: '1',
-        liquidityFee: '1',
-      },
+  it('should format cells correctly for seller fill', async () => {
+    act(async () => {
+      const partyId = 'party-id';
+      const buyerFill = generateFill({
+        ...defaultFill,
+        seller: {
+          id: partyId,
+        },
+        aggressor: Side.Sell,
+        sellerFee: {
+          makerFee: '1',
+          infrastructureFee: '1',
+          liquidityFee: '1',
+        },
+      });
+
+      render(<FillsTable partyId={partyId} rowData={[buyerFill]} />);
+      await waitForGridToBeInTheDOM();
+      await waitForDataToHaveLoaded();
+
+      const cells = screen.getAllByRole('gridcell');
+      const expectedValues = [
+        buyerFill.market.name,
+        '-3.00000',
+        '1.00 BTC',
+        '3.00 BTC',
+        'Taker',
+        '0.03 BTC',
+        getDateTimeFormat().format(new Date(buyerFill.createdAt)),
+      ];
+      cells.forEach((cell, i) => {
+        expect(cell).toHaveTextContent(expectedValues[i]);
+      });
+
+      const amountCell = cells.find((c) => c.getAttribute('col-id') === 'size');
+      expect(amountCell).toHaveClass('text-vega-red');
     });
-
-    render(<FillsTable partyId={partyId} rowData={[buyerFill]} />);
-    await waitForGridToBeInTheDOM();
-    await waitForDataToHaveLoaded();
-
-    const cells = screen.getAllByRole('gridcell');
-    const expectedValues = [
-      buyerFill.market.name,
-      '-3.00000',
-      '1.00 BTC',
-      '3.00 BTC',
-      'Taker',
-      '0.03 BTC',
-      getDateTimeFormat().format(new Date(buyerFill.createdAt)),
-    ];
-    cells.forEach((cell, i) => {
-      expect(cell).toHaveTextContent(expectedValues[i]);
-    });
-
-    const amountCell = cells.find((c) => c.getAttribute('col-id') === 'size');
-    expect(amountCell).toHaveClass('text-vega-red');
   });
 
-  it('renders correct maker or taker role', async () => {
-    const partyId = 'party-id';
-    const takerFill = generateFill({
-      seller: {
-        id: partyId,
-      },
-      aggressor: Side.Sell,
+  it('should render correct maker or taker role', async () => {
+    act(async () => {
+      const partyId = 'party-id';
+      const takerFill = generateFill({
+        seller: {
+          id: partyId,
+        },
+        aggressor: Side.Sell,
+      });
+
+      const { rerender } = render(
+        <FillsTable partyId={partyId} rowData={[takerFill]} />
+      );
+      await waitForGridToBeInTheDOM();
+      await waitForDataToHaveLoaded();
+
+      expect(
+        screen
+          .getAllByRole('gridcell')
+          .find((c) => c.getAttribute('col-id') === 'aggressor')
+      ).toHaveTextContent('Taker');
+
+      const makerFill = generateFill({
+        seller: {
+          id: partyId,
+        },
+        aggressor: Side.Buy,
+      });
+
+      rerender(<FillsTable partyId={partyId} rowData={[makerFill]} />);
+      await waitForGridToBeInTheDOM();
+      await waitForDataToHaveLoaded();
+
+      expect(
+        screen
+          .getAllByRole('gridcell')
+          .find((c) => c.getAttribute('col-id') === 'aggressor')
+      ).toHaveTextContent('Maker');
     });
-
-    const { rerender } = render(
-      <FillsTable partyId={partyId} rowData={[takerFill]} />
-    );
-    await waitForGridToBeInTheDOM();
-    await waitForDataToHaveLoaded();
-
-    expect(
-      screen
-        .getAllByRole('gridcell')
-        .find((c) => c.getAttribute('col-id') === 'aggressor')
-    ).toHaveTextContent('Taker');
-
-    const makerFill = generateFill({
-      seller: {
-        id: partyId,
-      },
-      aggressor: Side.Buy,
-    });
-
-    rerender(<FillsTable partyId={partyId} rowData={[makerFill]} />);
-    await waitForGridToBeInTheDOM();
-    await waitForDataToHaveLoaded();
-
-    expect(
-      screen
-        .getAllByRole('gridcell')
-        .find((c) => c.getAttribute('col-id') === 'aggressor')
-    ).toHaveTextContent('Maker');
   });
 });
