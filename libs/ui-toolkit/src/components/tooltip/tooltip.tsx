ae90638518fbef6f9f853f0a189f5a3be009fa9f
Botond <botond@vegaprotocol.io>
2022-08-05 12:51:35 +0100

    feat: add tooltip for market trading modes

--- libs/ui-toolkit/src/components/tooltip/tooltip.tsx
+++ libs/ui-toolkit/src/components/tooltip/tooltip.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import {
   Provider,
   Root,
@@ -9,7 +9,7 @@ import {
 
 export interface TooltipProps {
   children: React.ReactElement;
-  description?: string;
+  description?: string | ReactNode;
   open?: boolean;
   align?: 'start' | 'center' | 'end';
 }
@@ -20,19 +20,19 @@ export const Tooltip = ({ children, description, open, align }: TooltipProps) =>
     <Provider delayDuration={200} skipDelayDuration={100}>
       <Root open={open}>
         <Trigger asChild>{children}</Trigger>
-        <Content align={align} alignOffset={5}>
+        <Content align={align} alignOffset={8}>
           <div className="relative z-0 p-8 bg-black-50 border border-black-60 text-white rounded-sm max-w-sm">
             {description}
           </div>
           <Arrow
             width={10}
             height={5}
-            className="z-[1] fill-black-60 dark:fill-white-60 z-0 translate-x-[1px] translate-y-[-1px]"
+            className="z-[1] mx-8 fill-black-60 dark:fill-white-60 z-0 translate-x-[1px] translate-y-[-1px]"
           />
           <Arrow
             width={8}
             height={4}
-            className="z-[1] translate-y-[-1px] fill-black-50"
+            className="z-[1] mx-8 translate-y-[-1px] fill-black-50"
           />
         </Content>
       </Root>
