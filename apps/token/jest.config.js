49fb77f69d5cc060f4217bd59b43e3df35d6ac8d
Madalina Raicu <madalina@vegaprotocol.io>
2022-08-09 14:10:10 +0100

    chore: add moduleNameMapper for d3 in token jest.config.js

--- apps/token/jest.config.js
+++ apps/token/jest.config.js
@@ -4,8 +4,12 @@ module.exports = {
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
+    '/.[jt]sx?$/': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/token',
   setupFilesAfterEnv: ['./src/setup-tests.ts'],
+  moduleNameMapper: {
+    '^d3-(.*)$': `d3-$1/dist/d3-$1`,
+  },
 };
