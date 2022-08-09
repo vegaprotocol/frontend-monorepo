427057faa0a63b4e875dac32b62a90985e261223
Botond <botond@vegaprotocol.io>
2022-08-09 13:29:04 +0100

    fix: simple market test imports

--- apps/simple-trading-app/src/app/hooks/use-local-values.spec.ts
+++ apps/simple-trading-app/src/app/hooks/use-local-values.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react';
 import useLocalValues from './use-local-values';
 
 describe('local values hook', () => {
