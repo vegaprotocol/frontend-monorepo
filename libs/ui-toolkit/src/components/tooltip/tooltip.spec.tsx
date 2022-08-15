e7347508ed5fc579ac44fad4e4567f6491c7051b
Botond <botond@vegaprotocol.io>
2022-08-15 11:39:04 +0100

    fix: remove event testing for now and check only controlled tooltips

--- libs/ui-toolkit/src/components/tooltip/tooltip.spec.tsx
+++ libs/ui-toolkit/src/components/tooltip/tooltip.spec.tsx
@@ -5,24 +5,25 @@ it('Renders a tooltip', async () => {
   const props = {
     description: 'description',
     children: <button>Tooltip</button>,
+    open: true,
   };
   render(<Tooltip {...props} />);
   // radix applies the data-state attribute
-  expect(screen.getByRole('button')).toHaveAttribute('data-state', 'closed');
-  fireEvent.mouseOver(screen.getByRole('button'));
-  expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+  const trigger = screen.getByRole('button');
+  expect(trigger).toHaveAttribute('data-state', 'instant-open');
+  expect(screen.queryByRole('tooltip')).toBeInTheDocument();
 });
 
 it('Doesnt render a tooltip if no description provided', () => {
   const props = {
     description: undefined,
     children: <button>Tooltip</button>,
+    open: true,
   };
   render(<Tooltip {...props} />);
   expect(screen.getByRole('button')).not.toHaveAttribute(
     'data-state',
     'closed'
   );
-  fireEvent.mouseOver(screen.getByRole('button'));
   expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
 });
