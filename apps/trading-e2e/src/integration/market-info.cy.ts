7edfca971acb387af775fe43ad81ee1cb577be83
Madalina Raicu <madalina@vegaprotocol.io>
2022-08-17 09:34:35 +0100

    fix: add  market-info.cy.ts case

--- apps/trading-e2e/src/integration/market-info.cy.ts
+++ apps/trading-e2e/src/integration/market-info.cy.ts
@@ -31,13 +31,12 @@ describe('market info is displayed', () => {
     validateMarketDataRow(2, 'Best Offer Volume', '1');
     validateMarketDataRow(3, 'Best Static Bid Volume', '5');
     validateMarketDataRow(4, 'Best Static Offer Volume', '1');
+    validateMarketDataRow(5, 'Open Interest', '0');
   });
 
-  it('market price and interest displayed', () => {
-    cy.getByTestId(marketTitle).contains('Market price and interest').click();
-
+  it('market price', () => {
+    cy.getByTestId(marketTitle).contains('Market price').click();
     validateMarketDataRow(0, 'Mark Price', '57.49');
-    validateMarketDataRow(1, 'Open Interest', '0.00');
   });
 
   it('key details displayed', () => {
@@ -67,7 +66,7 @@ describe('market info is displayed', () => {
     validateMarketDataRow(1, 'Symbol', 'tBTC');
     validateMarketDataRow(
       2,
-      'ID',
+      'Asset ID',
       '5cfa87844724df6069b94e4c8a6f03af21907d7bc251593d08e4251043ee9f7c'
     );
   });
