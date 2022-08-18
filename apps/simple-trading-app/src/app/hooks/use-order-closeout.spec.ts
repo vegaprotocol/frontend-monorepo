2e02efedf5e93002f602a07b03a0325d5b56fc7b
Dexter <dexter@vega.xyz>
2022-08-18 09:59:07 +0100

    test: fix test import

--- apps/simple-trading-app/src/app/hooks/use-order-closeout.spec.ts
+++ apps/simple-trading-app/src/app/hooks/use-order-closeout.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import useOrderCloseOut from './use-order-closeout';
 import type { Order } from '@vegaprotocol/orders';
 import type { DealTicketQuery_market } from '@vegaprotocol/deal-ticket';
