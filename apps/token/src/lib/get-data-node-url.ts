7cc389ff5639f654a93939adf6ef17782507bd83
Dexter <dexter@vega.xyz>
2022-08-22 16:29:27 +0100

    fix: don't assume GQL URL path

--- apps/token/src/lib/get-data-node-url.ts
+++ apps/token/src/lib/get-data-node-url.ts
@@ -5,9 +5,8 @@ export function getDataNodeUrl() {
   if (!base) {
     throw new Error('Environment variable NX_VEGA_URL must be set');
   }
-  const gqlPath = 'query';
-  const urlHTTP = new URL(gqlPath, base);
-  const urlWS = new URL(gqlPath, base);
+  const urlHTTP = new URL(base);
+  const urlWS = new URL(base);
   // Replace http with ws, preserving if its a secure connection eg. https => wss
   urlWS.protocol = urlWS.protocol.replace('http', 'ws');
 
