609b94ff43ae494eb1706348581dde65cde4734d
Madalina Raicu <madalina@vegaprotocol.io>
2022-08-23 20:31:16 +0100

    fix: revert to testnet changes

--- libs/types/apollo.config.js
+++ libs/types/apollo.config.js
@@ -2,7 +2,8 @@ module.exports = {
   client: {
     service: {
       name: 'vega',
-      url: 'https://api.n11.testnet.vega.xyz/graphql',
+      url:
+        process.env.NX_VEGA_URL || 'https://api.n11.testnet.vega.xyz/graphql',
     },
     includes: ['../../{apps,libs}/**/*.{ts,tsx,js,jsx,graphql}'],
     excludes: ['**/generic-data-provider.ts'],
