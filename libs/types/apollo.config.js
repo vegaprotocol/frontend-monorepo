5eaef8de06c6279ca3709dfdb04835f02ac2ff2a
Madalina Raicu <madalina@vegaprotocol.io>
2022-08-23 19:34:51 +0100

    fix: add back appolo generate command

--- libs/types/apollo.config.js
+++ libs/types/apollo.config.js
@@ -2,7 +2,8 @@ module.exports = {
   client: {
     service: {
       name: 'vega',
-      url: process.env.NX_VEGA_URL,
+      url:
+        process.env.NX_VEGA_URL || 'https://api.n11.testnet.vega.xyz/graphql',
     },
     includes: ['../../{apps,libs}/**/*.{ts,tsx,js,jsx,graphql}'],
     excludes: ['**/generic-data-provider.ts'],
