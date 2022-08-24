eec2eac94e7aba39e09097df60943b37e3916f6a
Madalina Raicu <madalina@vegaprotocol.io>
2022-08-24 07:34:51 +0100

    fix: remove liquidity query from deal-ticket

--- libs/deal-ticket/src/components/market-info/__generated__/index.ts
+++ libs/deal-ticket/src/components/market-info/__generated__/index.ts
@@ -1,2 +1,1 @@
 export * from './MarketInfoQuery';
-export * from './MarketLiquidity';
--- libs/deal-ticket/src/components/market-info/index.ts
+++ libs/deal-ticket/src/components/market-info/index.ts
@@ -1,4 +1,3 @@
 export * from './__generated__';
 export * from './info-market-query';
 export * from './info-market';
-export * from './liquidity-query';
