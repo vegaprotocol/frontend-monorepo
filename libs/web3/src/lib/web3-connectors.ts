2c4f2be2403ebabb6565e97e50d1305f5bf4373e
Elmar Gasimov <elmar@vegaprotocol.io>
2022-08-24 10:03:12 +0100

    feat(web3): make web3 container component reusable

--- libs/web3/src/lib/web3-connectors.ts
+++ libs/web3/src/lib/web3-connectors.ts
@@ -3,14 +3,15 @@ import { initializeConnector } from '@web3-react/core';
 import { MetaMask } from '@web3-react/metamask';
 import { WalletConnect } from '@web3-react/walletconnect';
 
-const [metamask, metamaskHooks] = initializeConnector<MetaMask>(
-  (actions) => new MetaMask(actions)
-);
-
 export const createConnectors = (providerUrl: string, chainId: number) => {
   if (isNaN(chainId)) {
     throw new Error('Invalid Ethereum chain ID for environment');
   }
+
+  const [metamask, metamaskHooks] = initializeConnector<MetaMask>(
+    (actions) => new MetaMask(actions)
+  );
+
   const [walletconnect, walletconnectHooks] =
     initializeConnector<WalletConnect>(
       (actions) =>
