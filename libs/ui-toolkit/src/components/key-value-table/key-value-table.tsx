58a8cacadea0cdabae8bd63141bec6ad3d9044a2
PeteWilliams <me@petewilliams.info>
2022-08-24 09:53:49 +0100

    Revert "chore: tweaked styling for fees tooltip"
    This reverts commit 3f1a34d835eb0cbea82847e37e66c5a4c8bf723d.

--- libs/ui-toolkit/src/components/key-value-table/key-value-table.tsx
+++ libs/ui-toolkit/src/components/key-value-table/key-value-table.tsx
@@ -19,11 +19,6 @@ export const KeyValueTable = ({
   const TitleTag: keyof JSX.IntrinsicElements = headingLevel
     ? `h${headingLevel}`
     : 'div';
-  const divClassName = classNames(
-    'w-full border-collapse [border-spacing:0] break-all',
-    { 'mb-8': !tooltip },
-    className
-  );
   return (
     <React.Fragment>
       {title && <TitleTag className={`text-xl my-2`}>{title}</TitleTag>}
@@ -54,7 +49,6 @@ export interface KeyValueTableRowProps
   numerical?: boolean; // makes all values monospace
   inline?: boolean;
   noBorder?: boolean;
-  tooltip?: boolean;
   dtClassName?: string;
   ddClassName?: string;
 }
@@ -65,7 +59,6 @@ export const KeyValueTableRow = ({
   numerical,
   inline = true,
   noBorder = false,
-  tooltip = false,
   dtClassName,
   ddClassName,
   id,
