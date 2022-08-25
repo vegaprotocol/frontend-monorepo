c485c317f95a40a4ede8ae8a113e544c132cc63e
asiaznik <artur@vegaprotocol.io>
2022-08-25 14:21:55 +0200

    fix: added distinction between wallet and contract if they have the same key

--- apps/token/src/routes/staking/disassociate/disassociate-page.tsx
+++ apps/token/src/routes/staking/disassociate/disassociate-page.tsx
@@ -9,15 +9,27 @@ import { useAppState } from '../../../contexts/app-state/app-state-context';
 import { useRefreshAssociatedBalances } from '../../../hooks/use-refresh-associated-balances';
 import { useRemoveStake } from './hooks';
 import type { RemoveStakePayload } from './hooks';
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useCallback } from 'react';
+import type { ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
 import type { BigNumber } from '../../../lib/bignumber';
 import { truncateMiddle } from '../../../lib/truncate-middle';
 
 type Association = {
+  /**
+   * An unique id of association (combination of staking method and wallet key)
+   */
+  id: string;
+  /**
+   * A vega wallet key
+   */
   key: string;
-  value: BigNumber;
+  /**
+   * Amount of associated tokens
+   */
+  amount: BigNumber;
   stakingMethod: StakingMethod;
+  label: string;
 };
 
 const toListOfAssociations = (
@@ -26,11 +38,13 @@ const toListOfAssociations = (
 ): Association[] =>
   Object.keys(obj)
     .map((k) => ({
+      id: `${stakingMethod.toLowerCase()}-${remove0x(k)}`,
       key: remove0x(k),
-      value: obj[k],
+      amount: obj[k],
       stakingMethod,
+      label: '',
     }))
-    .filter((k) => k.value.isGreaterThan(0));
+    .filter((k) => k.amount.isGreaterThan(0));
 
 export const DisassociatePage = ({
   address,
@@ -48,11 +62,16 @@ export const DisassociatePage = ({
   } = useAppState();
 
   const associations = useMemo(
-    () => [
-      ...toListOfAssociations(stakingAssociations, StakingMethod.Wallet),
-      ...toListOfAssociations(vestingAssociations, StakingMethod.Contract),
-    ],
-    [stakingAssociations, vestingAssociations]
+    () =>
+      [
+        ...toListOfAssociations(stakingAssociations, StakingMethod.Wallet),
+        ...toListOfAssociations(vestingAssociations, StakingMethod.Contract),
+      ].map((a) => ({
+        ...a,
+        label: `${truncateMiddle(a.key)} ${t(`via${a.stakingMethod}`)} 
+                (${formatNumber(a.amount, 18)} ${t('tokens')})`,
+      })),
+    [stakingAssociations, vestingAssociations, t]
   );
 
   useEffect(() => {
@@ -61,7 +80,7 @@ export const DisassociatePage = ({
 
   const [chosen, setChosen] = useState<Association>();
 
-  const maximum = chosen?.value || toBigNum(0, 0);
+  const maximum = chosen?.amount || toBigNum(0, 0);
   const [amount, setAmount] = useState<string>('');
 
   const refreshBalances = useRefreshAssociatedBalances();
@@ -83,6 +102,18 @@ export const DisassociatePage = ({
       refreshBalances(address, chosen?.key || '');
     }
   }, [txState, refreshBalances, address, chosen]);
+
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      if (!e.target.value) return;
+      const chosen = associations.find((a) => a.id === e.target.value);
+      if (chosen) {
+        setChosen(chosen);
+        setAmount('');
+      }
+    },
+    [associations]
+  );
 
   if (txState.txState !== TxState.Default && payload) {
     return (
@@ -111,20 +142,12 @@ export const DisassociatePage = ({
           className="font-mono"
           disabled={associations.length === 1}
           id="vega-key-selector"
-          onChange={(e) => {
-            if (!e.target.value) return;
-            const chosen = associations.find((k) => k.key === e.target.value);
-            if (chosen) {
-              setChosen(chosen);
-              setAmount('');
-            }
-          }}
-          value={chosen?.key}
+          onChange={onChange}
+          value={chosen?.id}
         >
-          {associations.map((k) => (
-            <option key={k.key} value={k.key}>
-              {truncateMiddle(k.key)} {t(`via${k.stakingMethod}`)} (
-              {formatNumber(k.value, 18)} {t('tokens')})
+          {associations.map((a) => (
+            <option key={a.id} value={a.id}>
+              {a.label}
             </option>
           ))}
         </Select>
