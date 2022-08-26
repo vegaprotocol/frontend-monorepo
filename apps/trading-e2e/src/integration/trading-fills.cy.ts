68a5dff174fb96f4e5b3074c657a52b5ca4c1bb5
Joe <joe@vega.xyz>
2022-08-26 09:57:45 +0100

    test: trading fills tab on accounts

--- apps/trading-e2e/src/integration/trading-fills.cy.ts
+++ apps/trading-e2e/src/integration/trading-fills.cy.ts
@@ -1,76 +1,92 @@
 import { aliasQuery } from '@vegaprotocol/cypress';
 import { generateFill, generateFills } from '../support/mocks/generate-fills';
 import { Side } from '@vegaprotocol/types';
+import { MarketState } from '@vegaprotocol/types';
 import { connectVegaWallet } from '../support/vega-wallet';
+import { mockTradingPage } from '../support/trading';
 import { generateNetworkParameters } from '../support/mocks/generate-network-parameters';
 
+const fills = [
+  generateFill({
+    buyer: {
+      id: Cypress.env('VEGA_PUBLIC_KEY'),
+    },
+  }),
+  generateFill({
+    id: '1',
+    seller: {
+      id: Cypress.env('VEGA_PUBLIC_KEY'),
+    },
+    aggressor: Side.SIDE_SELL,
+    buyerFee: {
+      infrastructureFee: '5000',
+    },
+    market: {
+      name: 'Apples Daily v3',
+      positionDecimalPlaces: 2,
+    },
+  }),
+  generateFill({
+    id: '2',
+    seller: {
+      id: Cypress.env('VEGA_PUBLIC_KEY'),
+    },
+    aggressor: Side.SIDE_BUY,
+  }),
+  generateFill({
+    id: '3',
+    aggressor: Side.SIDE_SELL,
+    market: {
+      name: 'ETHBTC Quarterly (30 Jun 2022)',
+    },
+    buyer: {
+      id: Cypress.env('VEGA_PUBLIC_KEY'),
+    },
+  }),
+];
+const result = generateFills({
+  party: {
+    tradesConnection: {
+      edges: fills.map((f, i) => {
+        return {
+          __typename: 'TradeEdge',
+          node: f,
+          cursor: i.toString(),
+        };
+      }),
+    },
+  },
+});
+
 describe('fills', () => {
-  before(() => {
-    const fills = [
-      generateFill({
-        buyer: {
-          id: Cypress.env('VEGA_PUBLIC_KEY'),
-        },
-      }),
-      generateFill({
-        id: '1',
-        seller: {
-          id: Cypress.env('VEGA_PUBLIC_KEY'),
-        },
-        aggressor: Side.SIDE_SELL,
-        buyerFee: {
-          infrastructureFee: '5000',
-        },
-        market: {
-          name: 'Apples Daily v3',
-          positionDecimalPlaces: 2,
-        },
-      }),
-      generateFill({
-        id: '2',
-        seller: {
-          id: Cypress.env('VEGA_PUBLIC_KEY'),
-        },
-        aggressor: Side.SIDE_BUY,
-      }),
-      generateFill({
-        id: '3',
-        aggressor: Side.SIDE_SELL,
-        market: {
-          name: 'ETHBTC Quarterly (30 Jun 2022)',
-        },
-        buyer: {
-          id: Cypress.env('VEGA_PUBLIC_KEY'),
-        },
-      }),
-    ];
-    const result = generateFills({
-      party: {
-        tradesConnection: {
-          edges: fills.map((f, i) => {
-            return {
-              __typename: 'TradeEdge',
-              node: f,
-              cursor: i.toString(),
-            };
-          }),
-        },
-      },
-    });
+  beforeEach(() => {
     cy.mockGQL((req) => {
       aliasQuery(req, 'Fills', result);
       aliasQuery(req, 'NetworkParamsQuery', generateNetworkParameters());
     });
-    cy.visit('/portfolio');
-    cy.get('main[data-testid="portfolio"]').should('exist');
   });
 
-  it('renders fills', () => {
+  it('renders fills on portfolio page', () => {
+    cy.visit('/portfolio');
+    cy.get('main[data-testid="portfolio"]').should('exist');
     cy.getByTestId('Fills').click();
     cy.getByTestId('tab-fills').contains('Connect your Vega wallet');
-
     connectVegaWallet();
+    validateFillsDisplayed();
+  });
 
+  it('renders fills on trading tab', () => {
+    cy.mockGQL((req) => {
+      mockTradingPage(req, MarketState.STATE_ACTIVE);
+    });
+    cy.visit('/markets/market-0');
+    cy.getByTestId('Fills').click();
+    cy.getByTestId('tab-fills').contains('Please connect Vega wallet');
+    connectVegaWallet();
+    validateFillsDisplayed();
+  });
+
+  function validateFillsDisplayed() {
     cy.getByTestId('tab-fills').should('be.visible');
 
     cy.getByTestId('tab-fills')
@@ -118,5 +134,5 @@ describe('fills', () => {
         cy.wrap($tradeDateTime).invoke('text').should('match', dateTimeRegex);
       }
     });
-  });
+  }
 });
