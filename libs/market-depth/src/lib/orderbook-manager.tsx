2857406b9529fe517605842c6fa5ce0ae150ebcf
maciek <maciek@vegaprotocol.io>
2022-08-31 13:07:08 +0200

    feat: [console-lite] - slippage - add back order book model outside hook

--- libs/market-depth/src/lib/orderbook-manager.tsx
+++ libs/market-depth/src/lib/orderbook-manager.tsx
@@ -1,7 +1,16 @@
+import throttle from 'lodash/throttle';
 import { AsyncRenderer } from '@vegaprotocol/ui-toolkit';
 import { Orderbook } from './orderbook';
-import { useMemo, useState } from 'react';
-import { useOrderBookData } from './use-orderbook-data';
+import { useDataProvider } from '@vegaprotocol/react-helpers';
+import dataProvider from './market-depth-data-provider';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import type { MarketDepthSubscription_marketDepthUpdate } from './__generated__/MarketDepthSubscription';
+import {
+  compactRows,
+  updateCompactedRows,
+  mapMarketData,
+} from './orderbook-data';
+import type { OrderbookData } from './orderbook-data';
 
 interface OrderbookManagerProps {
   marketId: string;
@@ -10,11 +19,92 @@ interface OrderbookManagerProps {
 export const OrderbookManager = ({ marketId }: OrderbookManagerProps) => {
   const [resolution, setResolution] = useState(1);
   const variables = useMemo(() => ({ marketId }), [marketId]);
-
-  const { orderbookData, loading, error, data } = useOrderBookData({
-    variables,
-    resolution,
+  const resolutionRef = useRef(resolution);
+  const [orderbookData, setOrderbookData] = useState<OrderbookData>({
+    rows: null,
   });
+  const dataRef = useRef<OrderbookData>({ rows: null });
+  const deltaRef = useRef<MarketDepthSubscription_marketDepthUpdate>();
+  const updateOrderbookData = useRef(
+    throttle(() => {
+      if (!deltaRef.current) {
+        return;
+      }
+      dataRef.current = {
+        ...deltaRef.current.market.data,
+        ...mapMarketData(deltaRef.current.market.data, resolutionRef.current),
+        rows: updateCompactedRows(
+          dataRef.current.rows ?? [],
+          deltaRef.current.sell,
+          deltaRef.current.buy,
+          resolutionRef.current
+        ),
+      };
+      deltaRef.current = undefined;
+      setOrderbookData(dataRef.current);
+    }, 1000)
+  );
+
+  const update = useCallback(
+    ({ delta }: { delta: MarketDepthSubscription_marketDepthUpdate }) => {
+      if (!dataRef.current.rows) {
+        return false;
+      }
+      if (deltaRef.current) {
+        deltaRef.current.market = delta.market;
+        if (delta.sell) {
+          if (deltaRef.current.sell) {
+            deltaRef.current.sell.push(...delta.sell);
+          } else {
+            deltaRef.current.sell = delta.sell;
+          }
+        }
+        if (delta.buy) {
+          if (deltaRef.current.buy) {
+            deltaRef.current.buy.push(...delta.buy);
+          } else {
+            deltaRef.current.buy = delta.buy;
+          }
+        }
+      } else {
+        deltaRef.current = delta;
+      }
+      updateOrderbookData.current();
+      return true;
+    },
+    // using resolutionRef.current to avoid using resolution as a dependency - it will cause data provider restart on resolution change
+    []
+  );
+
+  const { data, error, loading, flush } = useDataProvider({
+    dataProvider,
+    update,
+    variables,
+  });
+
+  useEffect(() => {
+    const throttleRunnner = updateOrderbookData.current;
+    if (!data) {
+      dataRef.current = { rows: null };
+      setOrderbookData(dataRef.current);
+      return;
+    }
+    dataRef.current = {
+      ...data.data,
+      rows: compactRows(data.depth.sell, data.depth.buy, resolution),
+      ...mapMarketData(data.data, resolution),
+    };
+    setOrderbookData(dataRef.current);
+
+    return () => {
+      throttleRunnner.cancel();
+    };
+  }, [data, resolution]);
+
+  useEffect(() => {
+    resolutionRef.current = resolution;
+    flush();
+  }, [resolution, flush]);
 
   return (
     <AsyncRenderer loading={loading} error={error} data={data}>
