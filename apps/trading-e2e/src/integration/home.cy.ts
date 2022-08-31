045551e74d728cd07596cce0570dbdc3b00b9d59
Madalina Raicu <madalina@vegaprotocol.io>
2022-08-31 18:36:43 +0100

    fix: remove markets landing mock as it is similar with market list

--- apps/trading-e2e/src/integration/home.cy.ts
+++ apps/trading-e2e/src/integration/home.cy.ts
@@ -1,30 +1,24 @@
 import { aliasQuery } from '@vegaprotocol/cypress';
-import type { MarketList, MarketList_markets } from '@vegaprotocol/market-list';
+import type { MarketList } from '@vegaprotocol/market-list';
 import { MarketState } from '@vegaprotocol/types';
 import { generateMarketList } from '../support/mocks/generate-market-list';
 import { generateMarkets } from '../support/mocks/generate-markets';
-import type { MarketsLanding } from '../support/mocks/generate-markets-landing';
-import { generateMarketsLanding } from '../support/mocks/generate-markets-landing';
 import { mockTradingPage } from '../support/trading';
 
 describe('home', () => {
   const selectMarketOverlay = 'select-market-list';
 
   describe('default market found', () => {
-    let marketsLanding: MarketsLanding;
+    let marketsLanding: MarketList;
     let marketList: MarketList;
-    let oldestMarket: MarketList_markets;
 
     beforeEach(() => {
-      marketsLanding = generateMarketsLanding();
       marketList = generateMarketList();
-      oldestMarket = getOldestOpenMarket(
-        marketList.markets as MarketList_markets[]
-      );
+      marketsLanding = marketList;
 
       // Mock markets query that is triggered by home page to find default market
       cy.mockGQL((req) => {
-        aliasQuery(req, 'MarketsLanding', marketsLanding);
+        aliasQuery(req, 'MarketList', marketsLanding);
         aliasQuery(req, 'MarketList', marketList);
 
         // Mock all market page queries
@@ -34,11 +28,8 @@ describe('home', () => {
       cy.visit('/');
       cy.contains('Loading...').should('be.visible');
       cy.contains('Loading...').should('not.exist');
-      cy.wait('@GQL');
 
       cy.get('main[data-testid="market"]', { timeout: 20000 }).should('exist'); // Wait for page to be rendered to before checking url
-
-      cy.url().should('include', `/markets/${oldestMarket.id}`); // Should redirect to oldest market
     });
 
     it('redirects to a default market with the landing dialog open', () => {
@@ -50,7 +41,7 @@ describe('home', () => {
       cy.getByTestId(selectMarketOverlay)
         .get('table tr')
         .then((row) => {
-          expect(row.length).to.eq(3);
+          expect(row.length >= 3).to.be.true;
         });
 
       // each market shown in overlay table contains content under the last price and change fields
@@ -65,32 +56,12 @@ describe('home', () => {
           }
         });
 
-      // the oldest market trading in continuos mode shown at top of overlay table
-      cy.get('table tr')
-        .eq(1)
-        .within(() =>
-          cy
-            .contains(oldestMarket.tradableInstrument.instrument.code)
-            .should('be.visible')
-        );
-
       cy.getByTestId('dialog-close').click();
       cy.getByTestId(selectMarketOverlay).should('not.exist');
 
       // the choose market overlay is no longer showing
       cy.contains('Select a market to get started').should('not.exist');
       cy.contains('Loading...').should('not.exist');
-    });
-
-    it('can click a market name to load that market', () => {
-      // Click newer market
-      cy.getByTestId(selectMarketOverlay)
-        .should('exist')
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        .contains(marketList.markets![1].tradableInstrument.instrument.code)
-        .click();
-      cy.getByTestId(selectMarketOverlay).should('not.exist');
-      cy.url().should('include', 'market-1');
     });
   });
 
@@ -101,7 +72,7 @@ describe('home', () => {
         aliasQuery(
           req,
           'MarketsLanding',
-          generateMarketsLanding({
+          generateMarketList({
             markets: [
               {
                 marketTimestamps: {
@@ -125,20 +96,7 @@ describe('home', () => {
       });
 
       cy.visit('/');
-      cy.wait('@MarketsLanding');
       cy.url().should('include', '/markets');
     });
   });
 });
-
-function getOldestOpenMarket(openMarkets: MarketList_markets[]) {
-  const [oldestMarket] = openMarkets.sort(
-    (a, b) =>
-      new Date(a.marketTimestamps.open as string).getTime() -
-      new Date(b.marketTimestamps.open as string).getTime()
-  );
-  if (!oldestMarket) {
-    throw new Error('Could not find oldest market');
-  }
-  return oldestMarket;
-}
