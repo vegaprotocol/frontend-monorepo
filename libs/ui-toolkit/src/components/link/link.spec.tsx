b5e78240d69e901178f0c368c7ab0f3dec77476c
asiaznik <artur@vegaprotocol.io>
2022-09-01 17:44:10 +0200

    feat: link to market proposal (707)

--- libs/ui-toolkit/src/components/link/link.spec.tsx
+++ libs/ui-toolkit/src/components/link/link.spec.tsx
@@ -1,34 +1,63 @@
 import { render, screen } from '@testing-library/react';
-import { Link } from '.';
+import exp from 'constants';
+import { ExternalLink, Link } from '.';
 
-it('renders a link with a text', () => {
-  render(
-    <Link href="127.0.0.1" title="Link title">
-      Link text
-    </Link>
-  );
-  const link = screen.getByRole('link', { name: 'Link title' });
-  expect(link).toBeInTheDocument();
-  expect(link).toHaveAttribute('data-testid', 'link');
-  expect(link).toHaveAttribute('referrerPolicy', 'strict-origin');
-  expect(link).toHaveAttribute('href', '127.0.0.1');
-  expect(link).toHaveAttribute('title', 'Link title');
-  expect(link).toHaveClass('cursor-pointer');
-  expect(link).toHaveClass('underline');
+describe('Link', () => {
+  it('renders a link with a text', () => {
+    render(
+      <Link href="127.0.0.1" title="Link title">
+        Link text
+      </Link>
+    );
+    const link = screen.getByRole('link', { name: 'Link title' });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('data-testid', 'link');
+    expect(link).toHaveAttribute('referrerPolicy', 'strict-origin');
+    expect(link).toHaveAttribute('href', '127.0.0.1');
+    expect(link).toHaveAttribute('title', 'Link title');
+    expect(link).toHaveClass('cursor-pointer');
+    expect(link).toHaveClass('underline');
+  });
+
+  it('renders a link with children elements', () => {
+    render(
+      <Link href="127.0.0.1" title="Link title">
+        <span>Link text</span>
+      </Link>
+    );
+    const link = screen.getByRole('link', { name: 'Link title' });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('data-testid', 'link');
+    expect(link).toHaveAttribute('referrerPolicy', 'strict-origin');
+    expect(link).toHaveAttribute('href', '127.0.0.1');
+    expect(link).toHaveAttribute('title', 'Link title');
+    expect(link).toHaveClass('cursor-pointer');
+    expect(link).not.toHaveClass('underline');
+  });
 });
 
-it('renders a link with children elements', () => {
-  render(
-    <Link href="127.0.0.1" title="Link title">
-      <span>Link text</span>
-    </Link>
-  );
-  const link = screen.getByRole('link', { name: 'Link title' });
-  expect(link).toBeInTheDocument();
-  expect(link).toHaveAttribute('data-testid', 'link');
-  expect(link).toHaveAttribute('referrerPolicy', 'strict-origin');
-  expect(link).toHaveAttribute('href', '127.0.0.1');
-  expect(link).toHaveAttribute('title', 'Link title');
-  expect(link).toHaveClass('cursor-pointer');
-  expect(link).not.toHaveClass('underline');
+describe('ExternalLink', () => {
+  it('should have an icon indicating that it is an external link', () => {
+    render(<ExternalLink href="https://vega.xyz/">Go to Vega</ExternalLink>);
+    const link = screen.getByTestId('external-link');
+    expect(link.children.length).toEqual(2);
+    expect(link.children[1].tagName.toUpperCase()).toEqual('SVG');
+  });
+
+  it('should have an underlined text part', () => {
+    render(<ExternalLink href="https://vega.xyz/">Go to Vega</ExternalLink>);
+    const link = screen.getByTestId('external-link');
+    expect(link.children[0]).toHaveClass('underline');
+  });
+
+  it('should not have an icon or underlined text if wrapping an element', () => {
+    render(
+      <ExternalLink href="https://vega.xyz/">
+        <div className="inner">inner element</div>
+      </ExternalLink>
+    );
+    const link = screen.getByTestId('external-link');
+    expect(link.children.length).toEqual(1);
+    expect(link.children[0]).toHaveClass('inner');
+  });
 });
