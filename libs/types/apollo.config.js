b70df74aba1d860da7bd47263b5b31a239db149c
Botond <botond@vegaprotocol.io>
2022-09-01 17:02:13 +0200

    fix: format

--- libs/types/apollo.config.js
+++ libs/types/apollo.config.js
@@ -3,7 +3,8 @@ module.exports = {
     service: {
       name: 'vega',
       url:
-        process.env.GRAPHQL_SCHEMA_PATH || 'https://api.n07.testnet.vega.xyz/graphql',
+        process.env.GRAPHQL_SCHEMA_PATH ||
+        'https://api.n07.testnet.vega.xyz/graphql',
     },
     includes: [
       '../../{apps,libs}/**/*.{ts,tsx,js,jsx}',
