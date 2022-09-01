4d09368aec636d6a8fe2328f73bfd72c9dc252a4
Botond <botond@vegaprotocol.io>
2022-09-01 19:40:56 +0200

    fix: spacing

--- apps/trading/components/risk-notice-dialog/risk-notice-dialog.tsx
+++ apps/trading/components/risk-notice-dialog/risk-notice-dialog.tsx
@@ -12,18 +12,18 @@ export const RiskNoticeDialog = ({
 }: RiskNoticeDialogProps) => {
   return (
     <Dialog open={dialogOpen} title={t('WARNING')} size="medium">
-      <h4 className="text-h4 mb-8 mt-16">
+      <h4 className="text-xl mb-4 mt-8">
         {t('Regulation may apply to use of this app')}
       </h4>
-      <p className="text-ui mb-16">
+      <p className="text-ui mb-4">
         {t(
           'This decentralised application allows you to connect to and use publicly available blockchain services operated by third parties that may include trading, financial products, or other services that may be subject to legal and regulatory restrictions in your jurisdiction. This application is a front end only and does not hold any funds or provide any products or services. It is available to anyone with an internet connection via IPFS and other methods, and the ability to access it does not imply any right to use any services or that it is legal for you to do so. By using this application you accept that it is your responsibility to ensure that your use of the application and any blockchain services accessed through it is compliant with applicable laws and regulations in your jusrisdiction.'
         )}
       </p>
-      <h4 className="text-h4 mb-8">
+      <h4 className="text-xl mb-4">
         {t('Technical and financial risk of loss')}
       </h4>
-      <p className="text-ui mb-8">
+      <p className="text-ui mb-4">
         {t(
           'The public blockchain services accessible via this decentralised application are operated by third parties and may carry significant risks including the potential loss of all funds that you deposit or hold with these services. Technical risks include the risk of loss in the event of the failure or compromise of the public blockchain infrastructure or smart contracts that provide any services you use. Financial risks include but are not limited to losses due to volatility, excessive leverage, low liquidity, and your own lack of understanding of the services you use. By using this decentralised application you accept that it is your responsibility to ensure that you understand any services you use and the technical and financial risks inherent in your use. Do not risk what you cannot afford to lose.'
         )}
