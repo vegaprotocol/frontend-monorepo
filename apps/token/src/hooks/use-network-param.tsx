dff6809ad234fb2ff5841bdedaddb1566fe538e0
Madalina Raicu <madalina@vegaprotocol.io>
2022-09-02 12:15:00 +0300

    fix: use one param only in network params query

--- apps/token/src/hooks/use-network-param.tsx
+++ apps/token/src/hooks/use-network-param.tsx
@@ -12,13 +12,12 @@ export const NETWORK_PARAMS_QUERY = gql`
   }
 `;
 
-export function useNetworkParam(params: string[]) {
+export function useNetworkParam(param: string) {
   const { data, loading, error } = useQuery<NetworkParams, never>(
     NETWORK_PARAMS_QUERY
   );
   const foundParams = data?.networkParameters
-    ?.filter((p) => params.includes(p.key))
-    .sort((a, b) => params.indexOf(a.key) - params.indexOf(b.key));
+    ?.filter((p) => param === p.key);
   return {
     data: foundParams ? foundParams.map((f) => f.value) : null,
     loading,
