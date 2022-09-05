dd87d7770ab9ad94e96d9b449b172d6266130bc5
Joe <joe@vega.xyz>
2022-09-05 16:34:26 +0100

    test: update position validation to include updates

--- apps/trading-e2e/src/integration/trading-positions.cy.ts
+++ apps/trading-e2e/src/integration/trading-positions.cy.ts
@@ -46,9 +46,23 @@ describe('positions', () => {
         .should('contain.text', '11.29935') // entry price
         .should('contain.text', '9.21954'); // liquidation price
 
+      cy.get('[col-id="currentLeverage"]').should('contain.text', '1.1');
+
       cy.get('[col-id="capitalUtilisation"]') // margin allocated
         .should('contain.text', '0.00%')
         .should('contain.text', '1,000.01000');
+
+      cy.get('[col-id="unrealisedPNL"]').each(($unrealisedPnl) => {
+        cy.wrap($unrealisedPnl).invoke('text').should('not.be.empty');
+      });
+
+      cy.getByTestId('flash-cell').should('contain.text', '2,785.19482'); // Total tDAI position
+      cy.getByTestId('flash-cell').should('contain.text', '0.00100'); // Total Realised PNL
+      cy.get('[col-id="unrealisedPNL"]').should('contain.text', '17.90000'); // Total Unrealised PNL
     });
+
+    cy.getByTestId('balance').eq(0).should('have.text', '1,000.01000'); // Asset balance
+
+    cy.getByTestId('close-position').should('be.visible').and('have.length', 3);
   }
 });
