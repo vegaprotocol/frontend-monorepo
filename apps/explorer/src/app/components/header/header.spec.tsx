b528fafe670941a16c6da32ca26de4e5f267c650
Madalina Raicu <madalina@vegaprotocol.io>
2022-09-05 21:27:49 +0300

    fix: #723 add test to see if network switcher gets rendered

--- apps/explorer/src/app/components/header/header.spec.tsx
+++ apps/explorer/src/app/components/header/header.spec.tsx
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react';
 import { Header } from './header';
 import { MemoryRouter } from 'react-router-dom';
 
-jest.mock('@vegaprotocol/environment');
+jest.mock('@vegaprotocol/environment', () => ({
+  NetworkSwitcher: () => <div data-testid="network-switcher">NetworkSwitcher</div>,
+}));
 
 jest.mock('../search', () => ({
   Search: () => <div data-testid="search">OrderList</div>,
@@ -26,5 +28,11 @@ describe('Header', () => {
     render(renderComponent());
 
     expect(screen.getByTestId('search')).toBeInTheDocument();
+  });
+
+  it('should render network switcher', () => {
+    render(renderComponent());
+
+    expect(screen.getByTestId('network-switcher')).toBeInTheDocument();
   });
 });
