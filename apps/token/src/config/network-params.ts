52b1c24070f06c8a6a19d244bdca161b95eaddea
sam-keen <samuel.kleinmann@gmail.com>
2022-09-05 22:48:04 +0100

    Feat/800: Updated network params, added new method to get param keys as well as values, updated generated types

--- apps/token/src/config/network-params.ts
+++ apps/token/src/config/network-params.ts
@@ -4,22 +4,50 @@ export const NetworkParams = {
   REWARD_PAYOUT_DURATION: 'reward.staking.delegation.payoutDelay',
   GOV_UPDATE_MARKET_REQUIRED_MAJORITY:
     'governance.proposal.updateMarket.requiredMajority',
+  GOV_NEW_MARKET_MIN_CLOSE: 'governance.proposal.market.minClose',
+  GOV_NEW_MARKET_MAX_CLOSE: 'governance.proposal.market.maxClose',
+  GOV_NEW_MARKET_MIN_ENACT: 'governance.proposal.market.minEnact',
+  GOV_NEW_MARKET_MAX_ENACT: 'governance.proposal.market.maxEnact',
+  GOV_UPDATE_MARKET_MIN_CLOSE: 'governance.proposal.updateMarket.minClose',
+  GOV_UPDATE_MARKET_MAX_CLOSE: 'governance.proposal.updateMarket.maxClose',
+  GOV_UPDATE_MARKET_MIN_ENACT: 'governance.proposal.updateMarket.minEnact',
+  GOV_UPDATE_MARKET_MAX_ENACT: 'governance.proposal.updateMarket.maxEnact',
+  GOV_ASSET_MIN_CLOSE: 'governance.proposal.asset.minClose',
+  GOV_ASSET_MAX_CLOSE: 'governance.proposal.asset.maxClose',
+  GOV_ASSET_MIN_ENACT: 'governance.proposal.asset.minEnact',
+  GOV_ASSET_MAX_ENACT: 'governance.proposal.asset.maxEnact',
+  GOV_UPDATE_NET_PARAM_MIN_CLOSE: 'governance.proposal.asset.minClose',
+  GOV_UPDATE_NET_PARAM_MAX_CLOSE: 'governance.proposal.asset.maxClose',
+  GOV_UPDATE_NET_PARAM_MIN_ENACT: 'governance.proposal.asset.minEnact',
+  GOV_UPDATE_NET_PARAM_MAX_ENACT: 'governance.proposal.asset.maxEnact',
+  GOV_FREEFORM_MIN_CLOSE: 'governance.proposal.freeform.minClose',
+  GOV_FREEFORM_MAX_CLOSE: 'governance.proposal.freeform.maxClose',
   GOV_UPDATE_MARKET_REQUIRED_PARTICIPATION:
     'governance.proposal.updateMarket.requiredParticipation',
+  GOV_UPDATE_MARKET_MIN_PROPOSER_BALANCE:
+    'governance.proposal.updateMarket.minProposerBalance',
   GOV_NEW_MARKET_REQUIRED_MAJORITY:
     'governance.proposal.market.requiredMajority',
   GOV_NEW_MARKET_REQUIRED_PARTICIPATION:
     'governance.proposal.market.requiredParticipation',
+  GOV_NEW_MARKET_MIN_PROPOSER_BALANCE:
+    'governance.proposal.market.minProposerBalance',
   GOV_ASSET_REQUIRED_MAJORITY: 'governance.proposal.asset.requiredMajority',
   GOV_ASSET_REQUIRED_PARTICIPATION:
     'governance.proposal.asset.requiredParticipation',
+  GOV_ASSET_MIN_PROPOSER_BALANCE:
+    'governance.proposal.asset.minProposerBalance',
   GOV_UPDATE_NET_PARAM_REQUIRED_MAJORITY:
     'governance.proposal.updateNetParam.requiredMajority',
   GOV_UPDATE_NET_PARAM_REQUIRED_PARTICIPATION:
     'governance.proposal.updateNetParam.requiredParticipation',
+  GOV_UPDATE_NET_PARAM_MIN_PROPOSER_BALANCE:
+    'governance.proposal.updateNetParam.minProposerBalance',
   GOV_FREEFORM_REQUIRED_PARTICIPATION:
     'governance.proposal.freeform.requiredParticipation',
   GOV_FREEFORM_REQUIRED_MAJORITY:
     'governance.proposal.freeform.requiredMajority',
+  GOV_FREEFORM_MIN_PROPOSER_BALANCE:
+    'governance.proposal.freeform.minProposerBalance',
   VALIDATOR_DELEGATION_MIN_AMOUNT: 'validators.delegation.minAmount',
 };
--- apps/token/src/hooks/use-network-param.tsx
+++ apps/token/src/hooks/use-network-param.tsx
@@ -16,11 +16,44 @@ export function useNetworkParam(params: string[]) {
   const { data, loading, error } = useQuery<NetworkParams, never>(
     NETWORK_PARAMS_QUERY
   );
+
   const foundParams = data?.networkParameters
     ?.filter((p) => params.includes(p.key))
     .sort((a, b) => params.indexOf(a.key) - params.indexOf(b.key));
+
   return {
     data: foundParams ? foundParams.map((f) => f.value) : null,
+    loading,
+    error,
+  };
+}
+
+export function useNetworkParamWithKeys(params: string[]) {
+  const { data, loading, error } = useQuery<NetworkParams, never>(
+    NETWORK_PARAMS_QUERY
+  );
+
+  const foundParams =
+    params.length === 0
+      ? data?.networkParameters
+      : data?.networkParameters?.filter((p) => params.includes(p.key));
+
+  // if foundParams length is greater than 0, sort params by key property
+  const sortedParams =
+    foundParams && foundParams.length > 0
+      ? [...foundParams].sort((a, b) => {
+          if (a.key < b.key) {
+            return -1;
+          }
+          if (a.key > b.key) {
+            return 1;
+          }
+          return 0;
+        })
+      : null;
+
+  return {
+    data: sortedParams,
     loading,
     error,
   };
