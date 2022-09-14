b109038f8dbb7e1ca744c9d34bc0935a727e5998
Malin Antonsson <mantonsson90@gmail.com>
2022-09-14 16:21:49 +0200

    feat: add env variables & render a headline

--- apps/liquidity-provision-dashboard-e2e/src/e2e/app.cy.ts
+++ apps/liquidity-provision-dashboard-e2e/src/e2e/app.cy.ts
@@ -4,10 +4,7 @@ describe('liquidity-provision-dashboard', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome liquidity-provision-dashboard');
+    getGreeting().contains('Top liquidity opportunities');
   });
 });
