eb7b8633424430f2230f0599a418754d3de51bf8
Malin Antonsson <mantonsson90@gmail.com>
2022-09-14 14:27:31 +0200

    feat: generate new nx application

---
+++ apps/liquidity-provision-dashboard/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NxWelcome from './nx-welcome';
+
+import '../styles.scss';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="liquidity-provision-dashboard" />
+      <div />
+    </>
+  );
+}
+
+export default App;
