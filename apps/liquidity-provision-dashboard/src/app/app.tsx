9169165a03f559e75fbcdf6073a200d2606dfd20
malinantonsson <mantonsson90@gmail.com>
2022-09-15 17:37:02 +0200

    feat: lp dashboard setup (#1327)
    * feat: generate new nx application
    
    * feat: add env variables & render a headline
    
    * feat: add netlify config
    
    * feat: add cypress projectId and delete unused files

---
+++ apps/liquidity-provision-dashboard/src/app/app.tsx
@@ -0,0 +1,11 @@
+import '../styles.scss';
+
+export function App() {
+  return (
+    <div className="flex items-stretch px-6 py-6">
+      <h1 className="text-3xl">Top liquidity opportunities</h1>
+    </div>
+  );
+}
+
+export default App;
