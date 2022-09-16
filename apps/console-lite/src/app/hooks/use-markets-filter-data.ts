5452bcd7a4955c38005068aa32b935f948462df7
maciek <maciek@vegaprotocol.io>
2022-09-16 11:49:23 +0200

    feat: [subscription-update] - improve wrongly renamed hook 3

--- apps/console-lite/src/app/hooks/use-markets-filter-data.ts
+++ apps/console-lite/src/app/hooks/use-markets-filter-data.ts
@@ -4,44 +4,49 @@ import { MarketState } from '@vegaprotocol/types';
 import type { MarketsListData } from '@vegaprotocol/market-list';
 
 const useMarketsFilterData = (data: MarketsListData, params: RouterParams) => {
-  const markets =
-    data?.markets?.filter((item) => {
-      if (
-        params.product &&
-        params.product !== item.tradableInstrument.instrument.product.__typename
-      ) {
-        return false;
-      }
-      if (
-        params.asset &&
-        params.asset !== 'all' &&
-        params.asset !==
-          item.tradableInstrument.instrument.product.settlementAsset.symbol
-      ) {
-        return false;
-      }
-      const state =
-        params.state === 'all'
-          ? ''
-          : params.state
-          ? params.state
-          : MarketState.STATE_ACTIVE;
-      if (state && state !== item.state) {
-        return false;
-      }
-      return true;
-    }) || [];
+  return useMemo(() => {
+    const markets =
+      data?.markets?.filter((item) => {
+        if (
+          params.product &&
+          params.product !==
+            item.tradableInstrument.instrument.product.__typename
+        ) {
+          return false;
+        }
+        if (
+          params.asset &&
+          params.asset !== 'all' &&
+          params.asset !==
+            item.tradableInstrument.instrument.product.settlementAsset.symbol
+        ) {
+          return false;
+        }
+        const state =
+          params.state === 'all'
+            ? ''
+            : params.state
+            ? params.state
+            : MarketState.STATE_ACTIVE;
+        if (state && state !== item.state) {
+          return false;
+        }
+        return true;
+      }) || [];
 
-  return useMemo(
-    () =>
-      markets.map((market) => ({
-        ...market,
-        candles: (data?.marketsCandles || [])
-          .filter((c) => c.marketId === market.id)
-          .map((c) => c.candles),
-      })),
-    [markets, data?.marketsCandles]
-  );
+    return markets.map((market) => ({
+      ...market,
+      candles: (data?.marketsCandles || [])
+        .filter((c) => c.marketId === market.id)
+        .map((c) => c.candles),
+    }));
+  }, [
+    data?.marketsCandles,
+    data?.markets,
+    params.product,
+    params.asset,
+    params.state,
+  ]);
 };
 
 export default useMarketsFilterData;
