15ac79182412e7ec16b1734bb5bcab01208b97c2
Bartłomiej Głownia <bglownia@gmail.com>
2022-09-20 10:37:09 +0200

    chore: simplify node subscription probe

--- libs/environment/src/utils/__generated__/BlockTime.ts
+++ libs/environment/src/utils/__generated__/BlockTime.ts
@@ -7,26 +7,12 @@
 // GraphQL subscription operation: BlockTime
 // ====================================================
 
-export interface BlockTime_busEvents_event_MarketEvent {
-  __typename: "MarketEvent" | "TransferResponses" | "PositionResolution" | "Order" | "Trade" | "Account" | "Party" | "MarginLevels" | "Proposal" | "Vote" | "MarketData" | "NodeSignature" | "LossSocialization" | "SettlePosition" | "Market" | "Asset" | "MarketTick" | "SettleDistressed" | "AuctionEvent" | "RiskFactor" | "Deposit" | "Withdrawal" | "OracleSpec" | "LiquidityProvision";
-}
-
-export interface BlockTime_busEvents_event_TimeUpdate {
-  __typename: "TimeUpdate";
-  /**
-   * RFC3339Nano time of new block time
-   */
-  timestamp: string;
-}
-
-export type BlockTime_busEvents_event = BlockTime_busEvents_event_MarketEvent | BlockTime_busEvents_event_TimeUpdate;
-
 export interface BlockTime_busEvents {
   __typename: "BusEvent";
   /**
-   * The payload - the wrapped event
+   * The ID for this event
    */
-  event: BlockTime_busEvents_event;
+  id: string;
 }
 
 export interface BlockTime {
