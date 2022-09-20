4c882255ece71b87cec21b0fdf029ee930656af4
Bartłomiej Głownia <bglownia@gmail.com>
2022-09-20 09:18:25 +0200

    chore: revert error policy ignore

--- libs/react-helpers/src/lib/generic-data-provider.ts
+++ libs/react-helpers/src/lib/generic-data-provider.ts
@@ -241,7 +241,6 @@ function makeDataProviderInternal<QueryData, Data, SubscriptionData, Delta>({
         pagination: paginationVariables,
       },
       fetchPolicy: fetchPolicy || 'no-cache',
-      errorPolicy: 'ignore',
     });
     const insertionData = getData(res.data);
     const insertionPageInfo = pagination.getPageInfo(res.data);
@@ -271,7 +270,6 @@ function makeDataProviderInternal<QueryData, Data, SubscriptionData, Delta>({
           ? { ...variables, pagination: { first: pagination.first } }
           : variables,
         fetchPolicy,
-        errorPolicy: 'ignore',
       });
       data = getData(res.data);
       if (data && pagination) {
