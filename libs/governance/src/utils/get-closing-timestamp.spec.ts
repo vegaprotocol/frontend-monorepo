0017d487013b9aa1233d8e29cd7146fe0c3cd79d
sam-keen <samuel.kleinmann@gmail.com>
2022-09-20 11:20:54 +0100

    Feat/800: Result of running 'nx format:write'

--- libs/governance/src/utils/get-closing-timestamp.spec.ts
+++ libs/governance/src/utils/get-closing-timestamp.spec.ts
@@ -3,16 +3,18 @@ import { addHours, addMinutes, getTime } from 'date-fns';
 
 describe('getClosingTimestamp', () => {
   it('should return the correct timestamp if the proposalVoteDeadline is 1 (when 2 mins are added)', () => {
-    const proposalVoteDeadline = "1";
+    const proposalVoteDeadline = '1';
     const expected = Math.floor(
-      getTime(addHours(addMinutes(new Date(), 2), Number(proposalVoteDeadline))) / 1000
+      getTime(
+        addHours(addMinutes(new Date(), 2), Number(proposalVoteDeadline))
+      ) / 1000
     );
     const actual = getClosingTimestamp(proposalVoteDeadline);
     expect(actual).toEqual(expected);
   });
 
   it('should return the correct timestamp if the proposalVoteDeadline is 2 (when no extra mins are added)', () => {
-    const proposalVoteDeadline = "2";
+    const proposalVoteDeadline = '2';
     const expected = Math.floor(
       getTime(addHours(new Date(), Number(proposalVoteDeadline))) / 1000
     );
