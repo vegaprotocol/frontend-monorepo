1c842ea99839668045ffe7991d090c65edaee5cb
Malin Antonsson <mantonsson90@gmail.com>
2022-09-21 12:04:12 +0200

    feat: render LP grid

--- apps/liquidity-provision-dashboard/src/app/app.tsx
+++ apps/liquidity-provision-dashboard/src/app/app.tsx
@@ -1,14 +1,114 @@
+import { useQuery, gql } from '@apollo/client';
+
+//import { MarketTradingMode } from '@vegaprotocol/types';
+//import { calcCandleVolume } from '@vegaprotocol/market-info';
+
 import '../styles.scss';
 import Header from './components/header';
 import Intro from './components/intro';
 import MarketList from './components/market-list';
 
+const TWENTY_FOUR_H_AGO = Date.now() - 86400 * 1000;
+console.log('TWENTY_FOUR_H_AGO: ', TWENTY_FOUR_H_AGO);
+// "1663236997"
+const GET_MARKETS = gql`
+  query GetMarkets {
+    marketsConnection {
+      edges {
+        node {
+          id,
+          tradingMode,
+          
+          tradableInstrument {
+            instrument {
+              name,
+              product {
+                ... on Future {
+                  settlementAsset {
+                    name,
+                    id
+                  }
+                }
+              }
+            }
+          },
+          
+          liquidityProvisionsConnection {
+            edges {
+              node {
+                id
+              }
+            }
+          },
+          
+          tradesConnection(dateRange: { start: "${TWENTY_FOUR_H_AGO}" }) {
+            edges {
+              node {
+                id,
+                price
+              }
+            }
+          } 
+        }
+      }
+    }
+  }
+`;
+
+interface MarketQuery {
+  marketsConnection: {
+    edges: {
+      node: {
+        id: string;
+        tradingMode: string;
+        tradableInstrument: {
+          instrument: {
+            name: string;
+            product: {
+              settlementAsset: {
+                name: string;
+              };
+            };
+          };
+        };
+      };
+    }[];
+  };
+}
+
+const formatData = (data: MarketQuery) => {
+  // console.log('calcCandleVolume: ', calcCandleVolume());
+  return data.marketsConnection.edges.map((n) => {
+    return {
+      id: n.node.id,
+      name: n.node.tradableInstrument.instrument.name,
+      settlementAsset:
+        n.node.tradableInstrument.instrument.product.settlementAsset.name,
+      status: n.node.tradingMode,
+      volume: 'calc this',
+      liquidity: '250,000',
+      health: 'health',
+      apy: '7-11%',
+    };
+  });
+};
+
 export function App() {
+  // console.log('MarketTradingMode: ', MarketTradingMode);
+  const { loading, error, data } = useQuery(GET_MARKETS);
+
+  console.log('data: ', data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( </p>;
+  const TYPES = formatData(data);
+  console.log('TYPES: ', TYPES);
+
   return (
-    <div className="max-h-full min-h-full bg-white text-neutral-800 grid grid-rows-[min-content,1fr]">
+    <div className="max-h-full min-h-full bg-white flex flex-col">
       <Header />
       <Intro />
-      <MarketList />
+      <MarketList data={TYPES} />
     </div>
   );
 }
--- apps/liquidity-provision-dashboard/src/app/components/market-list/market-list.tsx
+++ apps/liquidity-provision-dashboard/src/app/components/market-list/market-list.tsx
@@ -1,7 +1,99 @@
-const MarketList = () => {
+import React, { useState, useMemo } from 'react';
+import { t } from '@vegaprotocol/react-helpers';
+// import type { AgGridReact } from 'ag-grid-react';
+import { AgGridDynamic as AgGrid } from '@vegaprotocol/ui-toolkit';
+import { AgGridColumn } from 'ag-grid-react';
+import type {
+  GroupCellRendererParams,
+  ValueFormatterParams,
+} from 'ag-grid-community';
+
+import type { MarketTradingMode } from '@vegaprotocol/types';
+import { MarketTradingModeMapping } from '@vegaprotocol/types';
+// import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+// import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+
+interface Markets {
+  data: {
+    id: string;
+    name: string;
+    status: string;
+  }[];
+}
+
+const MarketList = ({ data }: Markets) => {
+  console.log('market data: ', data);
+
+  const marketNameCellRenderer = (props: GroupCellRendererParams) => {
+    const { value, data } = props;
+
+    return (
+      <div className="flex flex-col">
+        <div>{value}</div>
+        <div>{data?.settlementAsset}</div>
+      </div>
+    );
+  };
+
+  const healthCellRenderer = ({ value }: GroupCellRendererParams) => {
+    return (
+      <div>
+        <div>{value}</div>
+      </div>
+    );
+  };
+
   return (
-    <div className="px-6 py-6" data-testid="market-list">
-      market list
+    <div
+      className="px-6 py-6 ag-theme-alpine grow"
+      data-testid="market-list"
+      style={{ height: 500, overflow: 'hidden', flexGrow: 1 }}
+    >
+      <AgGrid
+        rowData={data}
+        defaultColDef={{
+          sortable: true,
+          unSortIcon: true,
+        }}
+        className="h-full"
+        rowHeight={52}
+      >
+        <AgGridColumn
+          headerName={t('Market (futures)')}
+          field="name"
+          headerTooltip={t('This is the tooltip')}
+          cellRenderer={marketNameCellRenderer}
+        />
+        <AgGridColumn
+          headerName={t('Volume 24h')}
+          field="volume"
+          headerTooltip={t('This is the volume tooltip')}
+        />
+        <AgGridColumn
+          headerName={t('Committed liquidity')}
+          field="liquidity"
+          headerTooltip={t('This is the liquidity tooltip')}
+        />
+        <AgGridColumn
+          headerName={t('Status')}
+          field="status"
+          headerTooltip={t('This is the status tooltip')}
+          valueFormatter={({ value }: { value: MarketTradingMode }) =>
+            `${MarketTradingModeMapping[value]}`
+          }
+        />
+        <AgGridColumn
+          headerName={t('Health')}
+          field="health"
+          headerTooltip={t('This is the health tooltip')}
+          cellRenderer={healthCellRenderer}
+        />
+        <AgGridColumn
+          headerName={t('Est. return / APY')}
+          field="apy"
+          headerTooltip={t('This is the APY tooltip')}
+        />
+      </AgGrid>
     </div>
   );
 };
