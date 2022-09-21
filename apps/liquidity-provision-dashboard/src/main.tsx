82a5074dc771196ee78bbd8bf03beea08600235e
Malin Antonsson <mantonsson90@gmail.com>
2022-09-21 12:04:12 +0200

    feat: render LP grid

--- apps/liquidity-provision-dashboard/src/main.tsx
+++ apps/liquidity-provision-dashboard/src/main.tsx
@@ -1,13 +1,45 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ThemeContext } from '@vegaprotocol/react-helpers';
 
 import App from './app/app';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Market: {
+      merge: true,
+    },
+    Party: {
+      merge: true,
+    },
+    Query: {},
+    Account: {
+      keyFields: false,
+      fields: {
+        balanceFormatted: {},
+      },
+    },
+    Node: {
+      keyFields: false,
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: 'https://api.n01.stagnet3.vega.xyz/graphql',
+  cache,
+});
 
 const rootElement = document.getElementById('root');
 const root = rootElement && createRoot(rootElement);
 
 root?.render(
   <StrictMode>
-    <App />
+    <ThemeContext.Provider value="light">
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </ThemeContext.Provider>
   </StrictMode>
 );
