a9c15b81e5f06976a0a1f527da8950f68e813544
Dexter <dexter.edwards93@gmail.com>
2022-09-21 12:41:07 +0100

    feat: add not found page

--- apps/token/src/routes/governance/proposal/proposal-container.tsx
+++ apps/token/src/routes/governance/proposal/proposal-container.tsx
@@ -1,6 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
 import { AsyncRenderer } from '@vegaprotocol/ui-toolkit';
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 
 import { Proposal } from '../components/proposal';
@@ -19,6 +20,20 @@ export const PROPOSAL_QUERY = gql`
   }
 `;
 
+const ProposalNotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <section>
+      <header data-testid="proposal-title">
+        <h2 className="text-lg mx-0 mt-0 mb-1 font-semibold">
+          {t('ProposalNotFound')}
+        </h2>
+      </header>
+      <p>{t('ProposalNotFoundDetails')}</p>
+    </section>
+  );
+};
+
 export const ProposalContainer = () => {
   const params = useParams<{ proposalId: string }>();
   const { data, loading, error, refetch } = useQuery<
@@ -26,6 +41,7 @@ export const ProposalContainer = () => {
     ProposalVariables
   >(PROPOSAL_QUERY, {
     fetchPolicy: 'network-only',
+    errorPolicy: 'ignore',
     variables: { proposalId: params.proposalId || '' },
     skip: !params.proposalId,
   });
@@ -37,7 +53,11 @@ export const ProposalContainer = () => {
 
   return (
     <AsyncRenderer loading={loading} error={error} data={data}>
-      {data && data.proposal && <Proposal proposal={data.proposal} />}
+      {data?.proposal ? (
+        <Proposal proposal={data.proposal} />
+      ) : (
+        <ProposalNotFound />
+      )}
     </AsyncRenderer>
   );
 };
