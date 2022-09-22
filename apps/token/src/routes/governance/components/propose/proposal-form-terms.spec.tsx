247b9a37598c33acfc37d8d6297474e52a7d2ecf
sam-keen <samuel.kleinmann@gmail.com>
2022-09-22 10:30:19 +0100

    Feat/800: Added extra test for docs link in the proposal-form-terms component

--- apps/token/src/routes/governance/components/propose/proposal-form-terms.spec.tsx
+++ apps/token/src/routes/governance/components/propose/proposal-form-terms.spec.tsx
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { ProposalFormTerms } from './proposal-form-terms';
 
+jest.mock('@vegaprotocol/environment', () => ({
+  useEnvironment: () => ({
+    VEGA_DOCS_URL: 'https://docs.vega.xyz',
+  }),
+}));
+
 describe('Proposal Form Terms', () => {
   it('should display error text', () => {
     const register = jest.fn();
@@ -11,5 +17,32 @@ describe('Proposal Form Terms', () => {
       />
     );
     expect(screen.getByText('Error text')).toBeInTheDocument();
+  });
+
+  it('should render the generic docs link if no custom override', () => {
+    const register = jest.fn();
+    render(
+      <ProposalFormTerms
+        registerField={register('proposalTerms')}
+        errorMessage="Error text"
+      />
+    );
+    expect(
+      screen.getByText('https://docs.vega.xyz/tutorials/proposals')
+    ).toBeInTheDocument();
+  });
+
+  it('should render the custom docs link if provided', () => {
+    const register = jest.fn();
+    render(
+      <ProposalFormTerms
+        registerField={register('proposalTerms')}
+        errorMessage="Error text"
+        customDocLink="/custom"
+      />
+    );
+    expect(
+      screen.getByText('https://docs.vega.xyz/tutorials/proposals/custom')
+    ).toBeInTheDocument();
   });
 });
