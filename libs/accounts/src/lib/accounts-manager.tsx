ee5fe3367f27800b40b9c76b94cb9004bc99423a
Madalina Raicu <madalina@vegaprotocol.io>
2022-09-25 16:59:25 +0300

    fix: #847 remove unused getRows

--- libs/accounts/src/lib/accounts-manager.tsx
+++ libs/accounts/src/lib/accounts-manager.tsx
@@ -1,7 +1,6 @@
 import type { Asset } from '@vegaprotocol/react-helpers';
 import { useDataProvider } from '@vegaprotocol/react-helpers';
 import { AsyncRenderer } from '@vegaprotocol/ui-toolkit';
-import type { IGetRowsParams } from 'ag-grid-community';
 import type { AgGridReact } from 'ag-grid-react';
 import { useRef, useMemo, useCallback } from 'react';
 import type { AccountFields } from './accounts-data-provider';
@@ -14,10 +13,6 @@ interface AccountManagerProps {
   onClickAsset: (asset?: string | Asset) => void;
   onClickWithdraw?: (assetId?: string) => void;
   onClickDeposit?: (assetId?: string) => void;
-}
-
-export interface GetRowsParams extends Omit<IGetRowsParams, 'successCallback'> {
-  successCallback(rowsThisBlock: AccountFields[], lastRow?: number): void;
 }
 
 export const AccountManager = ({
@@ -45,18 +40,6 @@ export const AccountManager = ({
     AccountEventsSubscription['accounts']
   >({ dataProvider: accountsDataProvider, update, variables });
   dataRef.current = data;
-  const getRows = async ({
-    successCallback,
-    startRow,
-    endRow,
-  }: GetRowsParams) => {
-    const rowsThisBlock = dataRef.current
-      ? dataRef.current.slice(startRow, endRow)
-      : [];
-    const lastRow = dataRef.current?.length ?? -1;
-    console.log('rowsThisBlock', rowsThisBlock, 'lastRow', lastRow);
-    successCallback(rowsThisBlock, lastRow);
-  };
   return (
     <AsyncRenderer loading={loading} error={error} data={data}>
       {data && (
