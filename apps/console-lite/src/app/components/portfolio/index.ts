b5e2513d536b277a97699609781d73fa2a161aeb
macqbat <kubat.maciek@gmail.com>
2022-09-26 09:59:52 +0200

    chore: [console-lite] - portfolio - use more advanced react router (#1467)
    * chore: [console-lite] - portfolio use mora advanced react router
    
    * chore: [console-lite] - portfolio - fix container dimms
    
    Co-authored-by: maciek <maciek@vegaprotocol.io>

--- apps/console-lite/src/app/components/portfolio/index.ts
+++ apps/console-lite/src/app/components/portfolio/index.ts
@@ -1,1 +1,6 @@
 export * from './portfolio';
+export * from './accounts';
+export * from './fills';
+export * from './orders';
+export * from './positions';
+export * as constants from './constants';
--- apps/console-lite/src/app/components/portfolio/portfolio.tsx
+++ apps/console-lite/src/app/components/portfolio/portfolio.tsx
@@ -1,54 +1,27 @@
-import React, { useMemo } from 'react';
-import { AccountManager } from './accounts';
+import React from 'react';
 import { useVegaWallet } from '@vegaprotocol/wallet';
 import ConnectWallet from '../wallet-connector';
-import { DepositContainer } from '../deposits';
-import { useParams } from 'react-router-dom';
+import { useOutlet, useLocation } from 'react-router-dom';
 import { HorizontalMenu } from '../horizontal-menu';
 import * as constants from './constants';
-import { PositionsManager } from './positions';
-import { OrdersManager } from './orders';
-import { FillsManager } from './fills';
-
-type RouterParams = {
-  module?: string;
-};
 
 export const Portfolio = () => {
   const { keypair } = useVegaWallet();
-  const params = useParams<RouterParams>();
-
-  const module = useMemo(() => {
-    if (!keypair) {
-      return (
-        <section className="xl:w-1/2">
-          <ConnectWallet />
-        </section>
-      );
-    }
-
-    switch (params?.module) {
-      case constants.PORTFOLIO_ASSETS:
-      default:
-        return <AccountManager partyId={keypair.pub} />;
-      case constants.PORTFOLIO_POSITIONS:
-        return <PositionsManager partyId={keypair.pub} />;
-      case constants.PORTFOLIO_ORDERS:
-        return <OrdersManager partyId={keypair.pub} />;
-      case constants.PORTFOLIO_FILLS:
-        return <FillsManager partyId={keypair.pub} />;
-      case constants.PORTFOLIO_DEPOSITS:
-        return <DepositContainer />;
-    }
-  }, [params?.module, keypair]);
+  const { pathname } = useLocation();
+  const module = pathname.split('/portfolio/')?.[1] ?? '';
+  const outlet = useOutlet({ partyId: keypair?.pub || '' });
+  if (!keypair) {
+    return (
+      <section className="xl:w-1/2">
+        <ConnectWallet />
+      </section>
+    );
+  }
 
   return (
     <div className="h-full p-4 md:p-6 grid grid-rows-[min-content_1fr]">
-      <HorizontalMenu
-        active={params?.module}
-        items={constants.PORTFOLIO_ITEMS}
-      />
-      {module}
+      <HorizontalMenu active={module} items={constants.PORTFOLIO_ITEMS} />
+      {outlet}
     </div>
   );
 };
