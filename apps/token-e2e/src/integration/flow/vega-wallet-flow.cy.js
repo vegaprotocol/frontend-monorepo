366abcf7fb7cccc28d469c0ffe8db97f554071be
AndyWhiteVega <andy@vegaprotocol.io>
2022-09-26 13:19:58 +0100

    test: lint

--- apps/token-e2e/src/integration/flow/vega-wallet-flow.cy.js
+++ apps/token-e2e/src/integration/flow/vega-wallet-flow.cy.js
@@ -8,97 +8,87 @@ context(
   'Vega wallet flow - with vega wallet connected',
   { tags: '@slow' },
   function () {
-    before('visit token front - send-faucet assets to connected vega wallet', function () {
-      cy.vega_wallet_import();
-      cy.vega_wallet_top_up_with_asset('USDC (fake)', '10')
-      cy.vega_wallet_top_up_with_asset('BTC (fake)', '6')
-      cy.vega_wallet_top_up_with_asset('EURO (fake)', '8')
-      cy.vega_wallet_top_up_with_asset('DAI (fake)', '2')
-      cy.visit('/');
-      cy.verify_page_header('The $VEGA token');
-      cy.vega_wallet_connect();
-      cy.ethereum_wallet_connect();
-    });
+    before(
+      'visit token front - send-faucet assets to connected vega wallet',
+      function () {
+        cy.vega_wallet_import();
+        cy.vega_wallet_top_up_with_asset('USDC (fake)', '10');
+        cy.vega_wallet_top_up_with_asset('BTC (fake)', '6');
+        cy.vega_wallet_top_up_with_asset('EURO (fake)', '8');
+        cy.vega_wallet_top_up_with_asset('DAI (fake)', '2');
+        cy.visit('/');
+        cy.verify_page_header('The $VEGA token');
+        cy.vega_wallet_connect();
+        cy.ethereum_wallet_connect();
+      }
+    );
 
     describe('Vega wallet - Check assets show', function () {
-
       it('Able to see fUSDC assets - within vega wallet', function () {
-        let currencyId = 'fUSDC'
+        let currencyId = 'fUSDC';
         cy.get(vegaWalletContainer).within(() => {
-            
-            cy.get(vegaWalletCurrencyTitle, txTimeout).contains(
-                currencyId,
-                txTimeout
-            )
+          cy.get(vegaWalletCurrencyTitle, txTimeout)
+            .contains(currencyId, txTimeout)
             .should('be.visible');
 
-            cy.get(vegaWalletCurrencyTitle)
-                .contains(currencyId)
-                .parent()
-                .siblings()
-                .within(() => {
-                    cy.contains(`10.00000`).should('be.visible')
-                })
+          cy.get(vegaWalletCurrencyTitle)
+            .contains(currencyId)
+            .parent()
+            .siblings()
+            .within(() => {
+              cy.contains(`10.00000`).should('be.visible');
+            });
         });
       });
 
       it('Able to see fBTC assets - within vega wallet', function () {
-        let currencyId = 'fBTC'
+        let currencyId = 'fBTC';
         cy.get(vegaWalletContainer).within(() => {
-
-            cy.get(vegaWalletCurrencyTitle, txTimeout).contains(
-                currencyId,
-                txTimeout
-            )
+          cy.get(vegaWalletCurrencyTitle, txTimeout)
+            .contains(currencyId, txTimeout)
             .should('be.visible');
 
-            cy.get(vegaWalletCurrencyTitle)
-                .contains(currencyId)
-                .parent()
-                .siblings()
-                .within(() => {
-                    cy.contains(`6.00000`).should('be.visible')
-                })
+          cy.get(vegaWalletCurrencyTitle)
+            .contains(currencyId)
+            .parent()
+            .siblings()
+            .within(() => {
+              cy.contains(`6.00000`).should('be.visible');
+            });
         });
       });
 
       it('Able to see fEURO assets - within vega wallet', function () {
-        let currencyId = 'fEURO'
+        let currencyId = 'fEURO';
         cy.get(vegaWalletContainer).within(() => {
-
-            cy.get(vegaWalletCurrencyTitle, txTimeout).contains(
-                currencyId,
-                txTimeout
-            )
+          cy.get(vegaWalletCurrencyTitle, txTimeout)
+            .contains(currencyId, txTimeout)
             .should('be.visible');
 
-            cy.get(vegaWalletCurrencyTitle)
-                .contains(currencyId)
-                .parent()
-                .siblings()
-                .within(() => {
-                    cy.contains(`8.00000`).should('be.visible')
-                })
+          cy.get(vegaWalletCurrencyTitle)
+            .contains(currencyId)
+            .parent()
+            .siblings()
+            .within(() => {
+              cy.contains(`8.00000`).should('be.visible');
+            });
         });
       });
 
       it('Able to see fDAI assets - within vega wallet', function () {
-        let currencyId = 'fDAI'
+        let currencyId = 'fDAI';
         cy.get(vegaWalletContainer).within(() => {
-
-            cy.get(vegaWalletCurrencyTitle, txTimeout).contains(
-                currencyId,
-                txTimeout
-            )
+          cy.get(vegaWalletCurrencyTitle, txTimeout)
+            .contains(currencyId, txTimeout)
             .should('be.visible');
-            
-            cy.get(vegaWalletCurrencyTitle)
-                .contains(currencyId)
-                .parent()
-                .siblings()
-                .within(() => {
-                    cy.contains(`2.00000`).should('be.visible')
-                })
+
+          cy.get(vegaWalletCurrencyTitle)
+            .contains(currencyId)
+            .parent()
+            .siblings()
+            .within(() => {
+              cy.contains(`2.00000`).should('be.visible');
+            });
         });
       });
 
@@ -110,5 +100,6 @@ context(
           }
         }
       );
-    })
-})
+    });
+  }
+);
