bd4f0afbe9057b8c28ce7c38f08a8a3181bfaf38
AndyWhiteVega <andy@vegaprotocol.io>
2022-09-26 09:07:40 +0100

    test: update

--- apps/token-e2e/src/support/wallet-vega.functions.js
+++ apps/token-e2e/src/support/wallet-vega.functions.js
@@ -43,3 +43,76 @@ Cypress.Commands.add('vega_wallet_connect', () => {
   });
   cy.get(vegaWalletNameElement).should('be.visible');
 });
+
+
+Cypress.Commands.add(
+  'vega_wallet_top_up_with_asset',
+  function (assetName, amount) {
+    cy.highlight(`Topping up vega wallet with ${assetName}, amount: ${amount}`);
+    cy.get_asset_information().then((assets) => {
+      let asset = assets[assetName];
+      for (let i = 0; i < asset.decimals; i++) amount += '0';
+      cy.exec(
+        `curl -X POST -d '{"amount": "${amount}", "asset": "${asset.id}", "party": "${vegaWalletPublicKey}"}' -u "hedgehogandvega:hiccup" http://localhost:1790/api/v1/mint`
+      );
+      cy.get(vegaWalletContainer).within(() => {
+        cy.get(vegaWalletCurrencyTitle, txTimeout).contains(
+          asset.id,
+          txTimeout
+        );
+      });
+    });
+  }
+);
+
+Cypress.Commands.add('get_asset_information', () => {
+  let mutation =
+    '{ assets {name id decimals globalRewardPoolAccount {balance}}}';
+  cy.request({
+    method: 'POST',
+    url: `http://localhost:3028/query`,
+    body: {
+      query: mutation,
+    },
+    headers: { 'content-type': 'application/json' },
+  })
+    .its(`body.data.assets`)
+    .then(function (response) {
+      let object = response.reduce(function (assets, entry) {
+        assets[entry.name] = {
+          rewardPoolBalance: entry.globalRewardPoolAccount.balance,
+          id: entry.id,
+          decimals: entry.decimals,
+        };
+        return assets;
+      }, {});
+
+      return object;
+    });
+});
+
+Cypress.Commands.add('get_vega_wallet_asset_info', () => {
+  let mutation =
+    '{ partiesConnection {edges {node {accountsConnection {edges {node {type asset {name id decimals}}}}}}}}';
+  cy.request({
+    method: 'POST',
+    url: `http://localhost:3028/query`,
+    body: {
+      query: mutation,
+    },
+    headers: { 'content-type': 'application/json' },
+  })
+    .its(`body.data.partiesConnection.edges.1.node.accountsConnection.edges`)
+    .then(function (response) {
+      let object = [];
+      response.forEach((vegaAsset) => {
+        let asset = {
+          name: vegaAsset.node.asset.name,
+          id: vegaAsset.node.asset.id,
+          decimals: vegaAsset.node.asset.decimals,
+        };
+        object[asset.id] = asset;
+      });
+      return object;
+    });
+});
