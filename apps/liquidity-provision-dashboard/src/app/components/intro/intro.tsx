f8a45402a66bc703ab7e44c060ebe9b1371f276a
Malin Antonsson <mantonsson90@gmail.com>
2022-09-28 17:16:20 +0200

    feat: add EnvironmentProvider

--- apps/liquidity-provision-dashboard/src/app/components/intro/intro.tsx
+++ apps/liquidity-provision-dashboard/src/app/components/intro/intro.tsx
@@ -1,8 +1,9 @@
+import { t } from '@vegaprotocol/react-helpers';
 const Intro = () => {
   return (
     <div className="px-6 py-6" data-testid="intro">
-      <h2 className="text-2xl">Become a liquidity provider</h2>
-      <p className="text-base">Earn a cut...</p>
+      <h2 className="text-2xl">{t('Become a liquidity provider')}</h2>
+      <p className="text-base">{t('Earn a cut...')}</p>
     </div>
   );
 };
--- apps/liquidity-provision-dashboard/src/main.tsx
+++ apps/liquidity-provision-dashboard/src/main.tsx
@@ -1,50 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { ThemeContext } from '@vegaprotocol/react-helpers';
+import { createClient } from './app/lib/apollo-client';
+import { EnvironmentProvider, NetworkLoader } from '@vegaprotocol/environment';
 
 import App from './app/app';
-
-const cache = new InMemoryCache({
-  typePolicies: {
-    Market: {
-      merge: true,
-    },
-    Party: {
-      merge: true,
-    },
-    Query: {},
-    Account: {
-      keyFields: false,
-      fields: {
-        balanceFormatted: {},
-      },
-    },
-    Node: {
-      keyFields: false,
-    },
-  },
-});
-// https://api.n01.stagnet3.vega.xyz/graphql
-// //api.n01.stagnet3.vega.xyz/graphql/
-// //api.n07.testnet.vega.xyz/graphql/
-
-// https://api.n08.testnet.vega.xyz/graphql
-
-const client = new ApolloClient({
-  uri: 'https://api.n01.stagnet3.vega.xyz/graphql',
-  cache,
-});
 
 const rootElement = document.getElementById('root');
 const root = rootElement && createRoot(rootElement);
 
 root?.render(
   <StrictMode>
-    <ThemeContext.Provider value="light">
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </ThemeContext.Provider>
+    <EnvironmentProvider>
+      <ThemeContext.Provider value="light">
+        <NetworkLoader createClient={createClient}>
+          <App />
+        </NetworkLoader>
+      </ThemeContext.Provider>
+    </EnvironmentProvider>
   </StrictMode>
 );
