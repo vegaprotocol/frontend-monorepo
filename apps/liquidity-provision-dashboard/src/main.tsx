a7d711ba97ef964db6195760dfbbc47317b2a278
Malin Antonsson <mantonsson90@gmail.com>
2022-09-28 17:16:20 +0200

    feat: add EnvironmentProvider

--- apps/liquidity-provision-dashboard/src/main.tsx
+++ apps/liquidity-provision-dashboard/src/main.tsx
@@ -6,37 +6,6 @@ import { createClient } from './app/lib/apollo-client';
 
 import App from './app/app';
 
-const cache = new InMemoryCache({
-  typePolicies: {
-    Market: {
-      merge: true,
-    },
-    Party: {
-      merge: true,
-    },
-    Query: {},
-    Account: {
-      keyFields: false,
-      fields: {
-        balanceFormatted: {},
-      },
-    },
-    Node: {
-      keyFields: false,
-    },
-  },
-});
-// https://api.n01.stagnet3.vega.xyz/graphql
-// //api.n01.stagnet3.vega.xyz/graphql/
-// //api.n07.testnet.vega.xyz/graphql/
-
-// https://api.n08.testnet.vega.xyz/graphql
-
-const client = new ApolloClient({
-  uri: 'https://api.n01.stagnet3.vega.xyz/graphql',
-  cache,
-});
-
 const rootElement = document.getElementById('root');
 const root = rootElement && createRoot(rootElement);
 
