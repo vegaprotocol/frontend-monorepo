e24557270ec92d22d6efd1399ea4e697bfda1047
Malin Antonsson <mantonsson90@gmail.com>
2022-09-28 17:16:20 +0200

    feat: add EnvironmentProvider

--- apps/liquidity-provision-dashboard/src/app/components/header/header.tsx
+++ apps/liquidity-provision-dashboard/src/app/components/header/header.tsx
@@ -1,9 +1,10 @@
+import { t } from '@vegaprotocol/react-helpers';
 const Header = () => {
   return (
     <div className="flex items-stretch px-6 py-6" data-testid="header">
-      <h1 className="text-3xl">Top liquidity opportunities</h1>
+      <h1 className="text-3xl">{t('Top liquidity opportunities')}</h1>
       <div className="flex items-center gap-2 ml-auto relative z-10">
-        Network switcher
+        {t('Network switcher')}
       </div>
     </div>
   );
