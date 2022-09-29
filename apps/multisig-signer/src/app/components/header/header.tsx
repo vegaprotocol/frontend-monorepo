4272fefb0b75e639bb85cf6c50d8de33516232ea
Sam Keen <samuel@vegaprotocol.io>
2022-09-29 20:10:53 +0100

    feat(#807): adds app for validator add and remove signer (#1402)
    * Feat/807: ABI and classes for the contract methods
    
    * Feat/807: Added a new multisig-signer app
    
    * Feat/807: Added a new multisig-signer app
    
    * Feat/800: Untested signer forms
    
    * Feat/800: Moved reused bg video into ui-toolkit to use in multisig-signer project, and cleaned up some spacing that was overlooked in the stats theme changes
    
    * Feat/800: Componentised a bit, made the app look ok
    
    * Feat/800: Linting, prettifying, removing some unneeded tests, ensuring e2e tests run
    
    * Feat/800: Bit of translation
    
    * chore: fix type errors
    
    * chore: some parts error handling
    
    * feat: handle error and not found cases
    
    * feat: add changes to remove signer form as well
    
    * chore: rename component
    
    * chore: fix type issues
    
    * feat: add web3 connector logic
    
    * feat: allow disconnecting and show connected eth wallet info
    
    * Feat/800: Removed unused 'useApolloClient'
    
    * Feat/800: Ensure bundle.nonce and bundle.signatures have '0x' prepended
    
    * Feat/800: Removed unused e2e directory
    
    * Feat/800: Removed unnecessary app test
    
    * Feat/800: Removed unnecessary router
    
    * Feat/800: Capturing GQL errors in Sentry
    
    * Feat/800: Removing references to the unused e2e test directory
    
    * Feat/807: Consistent react hook imports
    
    * Feat/807: Removed unnecessary spreads
    
    Co-authored-by: Dexter <dexter.edwards93@gmail.com>

--- apps/multisig-signer/src/app/components/header/header.tsx
+++ apps/multisig-signer/src/app/components/header/header.tsx
@@ -1,19 +1,18 @@
 import {
   BackgroundVideo,
-  VegaLogo,
   ThemeSwitcher,
+  VegaLogo,
 } from '@vegaprotocol/ui-toolkit';
 
-interface ThemeToggleProps {
+interface HeaderProps {
   theme: 'light' | 'dark';
   toggleTheme: () => void;
 }
 
-export const Header = ({ theme, toggleTheme }: ThemeToggleProps) => {
+export const Header = ({ theme, toggleTheme }: HeaderProps) => {
   return (
-    <header className="relative overflow-hidden py-2 mb-10 md:mb-16">
+    <header className="relative overflow-hidden py-2 mb-8">
       <BackgroundVideo />
-
       <div className="relative flex justify-center px-2 dark:bg-black bg-white">
         <div className="w-full max-w-3xl p-5 flex items-center justify-between">
           <VegaLogo />
