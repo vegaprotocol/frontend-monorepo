ad77b518a490d6d58bb1923b77775fc0e5874e5c
asiaznik <artur@vegaprotocol.io>
2022-09-29 13:46:54 +0200

    added cy.log

--- apps/trading-e2e/src/support/vega-wallet.ts
+++ apps/trading-e2e/src/support/vega-wallet.ts
@@ -3,7 +3,7 @@ export const connectVegaWallet = () => {
   const manageVegaBtn = 'manage-vega-wallet';
   const walletName = Cypress.env('TRADING_TEST_VEGA_WALLET_NAME');
   const walletPassphrase = Cypress.env('TRADING_TEST_VEGA_WALLET_PASSPHRASE');
-
+  cy.log(`WALLET PASSPHRASE: ${walletPassphrase}`);
   cy.getByTestId('connect-vega-wallet').click();
   cy.getByTestId('connectors-list').find('button').click();
   cy.getByTestId(form).find('#wallet').click().type(walletName);
