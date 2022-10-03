edb31e0f46da89084ca00013ac4f429d3909b322
AndyWhiteVega <106072061+AndyWhiteVega@users.noreply.github.com>
2022-10-03 09:50:36 +0100

    Test/expand explorer tests to include parties page (#1530)
    * test: parties test suite
    
    * test: lint
    
    * test: improve error handling
    
    * test: lint
    
    * test: extra error handling and deletion of redundant curl params
    
    * test: lint
    
    * test: lint
    
    * test: update
    
    * test: skip test
    
    * test: tweaks
    
    * test: lint
    
    * test: lint

--- apps/token-e2e/src/integration/flow/token-association-flow.cy.js
+++ apps/token-e2e/src/integration/flow/token-association-flow.cy.js
@@ -280,8 +280,8 @@ context(
       after(
         'teardown environment to prevent test data bleeding into other tests',
         function () {
-          if (Cypress.env('TEARDOWN_NETWORK_AFTER_FLOWS')) {
-            cy.restartVegacapsuleNetwork();
+          if (Cypress.env('CYPRESS_TEARDOWN_NETWORK_AFTER_FLOWS')) {
+            cy.restart_vegacapsule_network();
           }
         }
       );
