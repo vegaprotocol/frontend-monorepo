de5df4b8e601bcf090661e2d2d81c2cf656dd4d5
AndyWhiteVega <andy@vegaprotocol.io>
2022-10-03 09:32:32 +0100

    test: lint

--- apps/explorer-e2e/src/integration/parties.cy.js
+++ apps/explorer-e2e/src/integration/parties.cy.js
@@ -1,17 +1,17 @@
-import { forEach } from "lodash";
+import { forEach } from 'lodash';
 
 const vegaWalletPublicKey = Cypress.env('vegaWalletPublicKey');
 const partiesMenuHeader = 'a[href="/parties"]';
 const partiesSearchBox = '[data-testid="party-input"]';
 const partiesSearchAction = '[data-testid="go-submit"]';
-const partiesJsonSection = '[data-testid="parties-json"]'
+const partiesJsonSection = '[data-testid="parties-json"]';
 const txTimeout = Cypress.env('txTimeout');
 
-let assetData = { 
-  fUSDC: {id: 'fUSDC', name: 'USDC (fake)', amount: '10' },
-  fBTC: {id: 'fBTC', name: 'BTC (fake)', amount: '6' },
-  fEURO: {id: 'fEURO', name: 'EURO (fake)', amount: '8' },
-  fDAI: {id: 'fDAI', name: 'DAI (fake)', amount: '2' },
+let assetData = {
+  fUSDC: { id: 'fUSDC', name: 'USDC (fake)', amount: '10' },
+  fBTC: { id: 'fBTC', name: 'BTC (fake)', amount: '6' },
+  fEURO: { id: 'fEURO', name: 'EURO (fake)', amount: '8' },
+  fDAI: { id: 'fDAI', name: 'DAI (fake)', amount: '2' },
 };
 
 const assetsInTest = Object.keys(assetData);
@@ -19,13 +19,13 @@ const assetsInTest = Object.keys(assetData);
 context('Parties page', { tags: '@regression' }, function () {
   before('send-faucet assets to connected vega wallet', function () {
     cy.vega_wallet_import();
-    assetsInTest.forEach(asset => {
+    assetsInTest.forEach((asset) => {
       cy.vega_wallet_receive_fauceted_asset(
         assetData[asset].name,
         assetData[asset].amount,
         vegaWalletPublicKey
       );
-    })
+    });
     cy.visit('/');
   });
 
@@ -45,7 +45,7 @@ context('Parties page', { tags: '@regression' }, function () {
     });
 
     it('should see each asset - within asset data section', function () {
-      assetsInTest.forEach(asset => {
+      assetsInTest.forEach((asset) => {
         cy.contains(assetData[asset].name, txTimeout).should('be.visible');
         cy.contains(assetData[asset].name)
           .siblings()
@@ -55,20 +55,20 @@ context('Parties page', { tags: '@regression' }, function () {
           .parent()
           .siblings()
           .within(() => {
-            cy.get_asset_decimals(asset).then(assetDecimals => {
-              cy.contains_exactly(assetData[asset].amount + '.' + assetDecimals).should('be.visible');
-            })
-          })
-      })
+            cy.get_asset_decimals(asset).then((assetDecimals) => {
+              cy.contains_exactly(
+                assetData[asset].amount + '.' + assetDecimals
+              ).should('be.visible');
+            });
+          });
+      });
     });
 
     it('should be able to copy the party address id', function () {
       cy.monitor_clipboard().as('clipboard');
-      cy.contains('Address')
-        .siblings()
-        .last()
-        .click()
-      cy.get('@clipboard').get_copied_text_from_clipboard()
+      cy.contains('Address').siblings().last().click();
+      cy.get('@clipboard')
+        .get_copied_text_from_clipboard()
         .should('equal', vegaWalletPublicKey);
     });
 
@@ -79,26 +79,31 @@ context('Parties page', { tags: '@regression' }, function () {
       cy.contains(assetData.fDAI.name)
         .siblings()
         .within(() => {
-          cy.get('[data-state="closed"]').last().click({force:true})
+          cy.get('[data-state="closed"]').last().click({ force: true });
         });
-        
-      cy.get('@clipboard').get_copied_text_from_clipboard().should('equal', assetData.fDAI.id);
+
+      cy.get('@clipboard')
+        .get_copied_text_from_clipboard()
+        .should('equal', assetData.fDAI.id);
     });
 
     it('should be able to see JSON of each asset containing correct balance and decimals', function () {
       cy.get(partiesJsonSection).should('be.visible');
 
-      assetsInTest.forEach(asset => {
+      assetsInTest.forEach((asset) => {
         cy.get(partiesJsonSection)
           .invoke('text')
           .convert_string_json_to_js_object()
           .get_party_accounts_data()
-          .then(accounts => {
-            cy.get_asset_decimals(asset).then(assetDecimals => {
-              assert.equal(accounts[asset].balance, assetData[asset].amount + assetDecimals);
-            })
-          })
-      })
+          .then((accounts) => {
+            cy.get_asset_decimals(asset).then((assetDecimals) => {
+              assert.equal(
+                accounts[asset].balance,
+                assetData[asset].amount + assetDecimals
+              );
+            });
+          });
+      });
     });
 
     after(
@@ -115,28 +120,37 @@ context('Parties page', { tags: '@regression' }, function () {
         const assetDecimals = assetsInfo[assetData[assetID].name].decimals;
         let decimals = '';
         for (let i = 0; i < assetDecimals; i++) decimals += '0';
-        return decimals
-      })
-    })
-
-    Cypress.Commands.add('convert_string_json_to_js_object',
-    { prevSubject: true }, (jsonBlobString) => {
-      return JSON.parse(jsonBlobString);
+        return decimals;
+      });
     });
 
-    Cypress.Commands.add('get_party_accounts_data',
-    { prevSubject: true }, (jsObject) => {
-      const accounts = jsObject.party.accounts.reduce(function (account, entry) {
-        account[entry.asset.id] = {
-          balance: entry.balance,
-          id: entry.asset.id,
-          decimals: entry.asset.decimals,
-          name: entry.asset.name,
-        };
-        return account;
-      }, {});
-      return accounts
-    });
+    Cypress.Commands.add(
+      'convert_string_json_to_js_object',
+      { prevSubject: true },
+      (jsonBlobString) => {
+        return JSON.parse(jsonBlobString);
+      }
+    );
 
+    Cypress.Commands.add(
+      'get_party_accounts_data',
+      { prevSubject: true },
+      (jsObject) => {
+        const accounts = jsObject.party.accounts.reduce(function (
+          account,
+          entry
+        ) {
+          account[entry.asset.id] = {
+            balance: entry.balance,
+            id: entry.asset.id,
+            decimals: entry.asset.decimals,
+            name: entry.asset.name,
+          };
+          return account;
+        },
+        {});
+        return accounts;
+      }
+    );
   });
 });
--- apps/explorer-e2e/src/support/common.functions.js
+++ apps/explorer-e2e/src/support/common.functions.js
@@ -53,14 +53,17 @@ Cypress.Commands.add('common_verify_json_int_values', function (expectedNum) {
 });
 
 Cypress.Commands.add('monitor_clipboard', () => {
-  cy.window().then(win => {
-    return cy.stub(win, 'prompt').returns(win.prompt)
-  })
+  cy.window().then((win) => {
+    return cy.stub(win, 'prompt').returns(win.prompt);
+  });
 });
 
-Cypress.Commands.add('get_copied_text_from_clipboard',
-{ prevSubject: true }, (clipboard) => {
-  // Must first setup with cy.monitor_clipboard().as('clipboard')
-  // This function then chained off a cy.get('@clipboard') 
-  return clipboard.args[0][1]
-});
+Cypress.Commands.add(
+  'get_copied_text_from_clipboard',
+  { prevSubject: true },
+  (clipboard) => {
+    // Must first setup with cy.monitor_clipboard().as('clipboard')
+    // This function then chained off a cy.get('@clipboard')
+    return clipboard.args[0][1];
+  }
+);
