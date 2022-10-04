d889b8e970974aede90907d23de1fae772c72696
AndyWhiteVega <106072061+AndyWhiteVega@users.noreply.github.com>
2022-10-04 11:42:15 +0100

    Test/block explorer assets tests (#1602)
    * test: update asset tests
    
    * test: lint
    
    * test: heading tweak
    
    * test: remove comments
    
    * test: lint

--- libs/cypress/src/lib/commands/get-asset-information.ts
+++ libs/cypress/src/lib/commands/get-asset-information.ts
@@ -12,7 +12,11 @@ export function addGetAssetInformation() {
   // @ts-ignore - ignoring Cypress type error which gets resolved when Cypress uses the command
   Cypress.Commands.add('get_asset_information', () => {
     const mutation =
-      '{ assets {name id decimals globalRewardPoolAccount {balance}}}';
+      '{ assets {name id symbol decimals source{__typename \
+        ... on ERC20{contractAddress} \
+        ... on BuiltinAsset{maxFaucetAmountMint}} \
+        infrastructureFeeAccount{__typename type balance} \
+        globalRewardPoolAccount {balance}}}';
     cy.request({
       method: 'POST',
       url: `http://localhost:3028/query`,
@@ -25,14 +29,9 @@ export function addGetAssetInformation() {
       .then(function (response) {
         // @ts-ignore - ignoring Cypress type error which gets resolved when Cypress uses the command
         const object = response.reduce(function (assets, entry) {
-          assets[entry.name] = {
-            rewardPoolBalance: entry.globalRewardPoolAccount.balance,
-            id: entry.id,
-            decimals: entry.decimals,
-          };
+          assets[entry.name] = entry;
           return assets;
         }, {});
-
         return object;
       });
   });
