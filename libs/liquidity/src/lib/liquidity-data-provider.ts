56727257540fc594feb6c44bf4e35c4c8fc6001e
Madalina Raicu <madalina@vegaprotocol.io>
2022-10-04 15:30:21 +0100

    fix: #1537 fix generated types in LP and deposits

--- libs/liquidity/src/lib/liquidity-data-provider.ts
+++ libs/liquidity/src/lib/liquidity-data-provider.ts
@@ -6,10 +6,8 @@ import type {
   LiquidityProvisionsSubscription,
   MarketLiquidityQuery,
 } from './__generated___/MarketLiquidity';
-import {
-  MarketLiquidityDocument,
-  LiquidityProvisionsDocument,
-} from './__generated___/MarketLiquidity';
+import { LiquidityProvisionsDocument } from './__generated___/MarketLiquidity';
+import { MarketLiquidityDocument } from './__generated___/MarketLiquidity';
 
 export function isLpFragment(
   lp:
@@ -99,101 +97,3 @@ export const marketLiquidityDataProvider = makeDataProvider<
     return delta;
   },
 });
-
-// export const marketLiquidityDataProvider = makeDataProvider<
-//   MarketLiquidityQuery,
-//   never>){
-//   query: MarketLiquidityDocument
-//   }
-
-// export const liquidityProviderFeeShareDataProvider = makeDataProvider<
-//   MarketLiquidityQuery,
-
-// export const useLiquidityProvision = ({
-//   marketId,
-//   partyId,
-// }: {
-//   partyId?: string;
-//   marketId?: string;
-// }) => {
-//   const { param: stakeToCcySiskas } = useNetworkParam(
-//     NetworkParams.market_liquidity_stakeToCcySiskas
-//   );
-//   const stakeToCcySiska = stakeToCcySiskas && stakeToCcySiskas[0];
-//   const { data, loading, error } = useMarketLiquidityQuery({
-//     variables: { marketId: marketId || '' },
-//   });
-//   const liquidityData: LiquidityData = getLiquidityData(
-//     data,
-//     partyId,
-//     stakeToCcySiska
-//   );
-//   return { data: liquidityData, loading, error };
-// };
-
-// function getLiquidityData(
-//   data: MarketLiquidityQuery | undefined,
-//   partyId: string | undefined,
-//   stakeToCcySiska: string
-// ) {
-//   const liquidityProviders = (
-//     data?.market?.data?.liquidityProviderFeeShare || []
-//   )
-//     ?.filter((p) => !partyId || p.party.id === partyId) // if partyId is provided, filter out other parties
-//     .map((provider) => {
-//       const liquidityProvisionConnection =
-//         data?.market?.liquidityProvisionsConnection?.edges?.find(
-//           (e) => e?.node.party.id === provider.party.id
-//         );
-//       const balance =
-//         liquidityProvisionConnection?.node?.party.accountsConnection?.edges?.reduce(
-//           (acc, e) => {
-//             return e?.node.type === AccountType.ACCOUNT_TYPE_BOND // just an extra check to make sure we only use bond accounts
-//               ? acc.plus(new BigNumber(e?.node.balance ?? 0))
-//               : acc;
-//           },
-//           new BigNumber(0)
-//         );
-//       const obligation =
-//         stakeToCcySiska &&
-//         new BigNumber(stakeToCcySiska)
-//           .times(liquidityProvisionConnection?.node?.commitmentAmount ?? 1)
-//           .toString();
-//       const supplied =
-//         stakeToCcySiska &&
-//         new BigNumber(stakeToCcySiska).times(balance ?? 1).toString();
-//       return {
-//         party: provider.party.id,
-//         createdAt: liquidityProvisionConnection?.node?.createdAt,
-//         updatedAt: liquidityProvisionConnection?.node?.updatedAt,
-//         commitmentAmount: liquidityProvisionConnection?.node?.commitmentAmount,
-//         fee: liquidityProvisionConnection?.node?.fee,
-//         status: liquidityProvisionConnection?.node?.status,
-//         equityLikeShare: provider.equityLikeShare,
-//         averageEntryValuation: provider.averageEntryValuation,
-//         obligation,
-//         supplied,
-//       };
-//     });
-//   const liquidityData: LiquidityData = {
-//     liquidityProviders,
-//     suppliedStake: data?.market?.data?.suppliedStake,
-//     targetStake: data?.market?.data?.targetStake,
-//     decimalPlaces: data?.market?.decimalPlaces,
-//     positionDecimalPlaces: data?.market?.positionDecimalPlaces,
-//     code: data?.market?.tradableInstrument.instrument.code,
-//     name: data?.market?.tradableInstrument.instrument.name,
-//     assetDecimalPlaces:
-//       data?.market?.tradableInstrument.instrument.product.settlementAsset
-//         .decimals,
-//     symbol:
-//       data?.market?.tradableInstrument.instrument.product.settlementAsset
-//         .symbol,
-//   };
-//   return liquidityData;
-// }
-
-// export const lpDataProvider = makeDerivedDataProvider<LiquidityData, never>(
-//   [liquidityDataProvider],
-//   (parts) => parts[0]
-// );
