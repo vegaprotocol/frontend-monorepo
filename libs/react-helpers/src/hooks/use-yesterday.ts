b9c6dbcaf398aea492d6e3107e19295382a8e55c
asiaznik <artur@vegaprotocol.io>
2022-10-05 18:14:18 +0200

    chore: added useYesterday hook

---
+++ libs/react-helpers/src/hooks/use-yesterday.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+const DEFAULT_ROUND_BY_MS = 5 * 60 * 1000;
+const TWENTY_FOUR_HOURS_MS = 24 * 60 * 60 * 1000;
+
+export const now = (roundBy = 1) =>
+  Math.floor((Math.round(new Date().getTime() / 1000) * 1000) / roundBy) *
+  roundBy;
+
+/**
+ * Returns the yesterday's timestamp rounded by given number (in milliseconds; 5 minutes by default)
+ */
+export const useYesterday = (roundBy = DEFAULT_ROUND_BY_MS) => {
+  const yesterday = useRef<number>(now(roundBy) - TWENTY_FOUR_HOURS_MS);
+  useEffect(() => {
+    const i = setInterval(() => {
+      yesterday.current = now(roundBy) - TWENTY_FOUR_HOURS_MS;
+    }, roundBy);
+    return () => clearInterval(i);
+  }, [roundBy]);
+  return yesterday.current;
+};
