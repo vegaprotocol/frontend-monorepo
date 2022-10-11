dc4701892fad02a7f68376418968de69ceb1dca9
Dexter <dexter.edwards93@gmail.com>
2022-10-11 16:23:26 +0100

    fix: incorrect connection logic on the associate page

--- apps/token/src/routes/staking/disassociate/disassociate-page-container.tsx
+++ apps/token/src/routes/staking/disassociate/disassociate-page-container.tsx
@@ -1,14 +1,17 @@
-import { StakingWalletsContainer } from '../staking-wallets-container';
+import { useVegaWallet } from '@vegaprotocol/wallet';
+import { useWeb3React } from '@web3-react/core';
+import { EthConnectPrompt } from '../../../components/eth-connect-prompt';
 import { DisassociatePage } from './disassociate-page';
 
 export const DisassociateContainer = () => {
-  return (
-    <StakingWalletsContainer needsEthereum={true} needsVega={false}>
-      {({ address, pubKey }) => (
-        <DisassociatePage address={address} vegaKey={pubKey ?? ''} />
-      )}
-    </StakingWalletsContainer>
-  );
+  const { account } = useWeb3React();
+  const { pubKey } = useVegaWallet();
+
+  if (!account) {
+    return <EthConnectPrompt />;
+  }
+
+  return <DisassociatePage address={account} vegaKey={pubKey ?? ''} />;
 };
 
 export default DisassociateContainer;
