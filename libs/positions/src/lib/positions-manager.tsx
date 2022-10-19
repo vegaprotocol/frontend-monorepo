8d2fe118ecca7f77fb880c720097145a8791e1c7
Art <artur@vegaprotocol.io>
2022-10-20 01:59:36 +0200

    chore: single position table (1645) (#1749)
    * chore: single position table (1645)
    
    chore: single position table (1645)
    
    * chore: tests fixed
    
    * chore: remove unused withSummaryRow arg
    
    * chore: use ag grid value formatter type helper
    
    * chore: update console-lite to use value formatter params helper
    
    * chore: fix e2e test by ignoring pinned row
    
    Co-authored-by: Rado <szpiechrados@gmail.com>
    Co-authored-by: Matthew Russell <mattrussell36@gmail.com>

--- libs/positions/src/lib/positions-manager.tsx
+++ libs/positions/src/lib/positions-manager.tsx
@@ -1,8 +1,8 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import { AsyncRenderer } from '@vegaprotocol/ui-toolkit';
 import type { Position } from './positions-data-providers';
-import { Positions } from './positions';
-import { useClosePosition, usePositionsAssets } from '../';
+import { PositionsTable, useClosePosition, usePositionsData } from '../';
+import type { AgGridReact } from 'ag-grid-react';
 
 interface PositionsManagerProps {
   partyId: string;
@@ -17,18 +17,20 @@ export const PositionsManager = ({ partyId }: PositionsManagerProps) => {
     [submit]
   );
 
-  const { data, error, loading, assetSymbols } = usePositionsAssets(partyId);
+  const gridRef = useRef<AgGridReact | null>(null);
+  const { data, error, loading, getRows } = usePositionsData(partyId, gridRef);
   return (
     <>
       <AsyncRenderer loading={loading} error={error} data={data}>
-        {assetSymbols?.map((assetSymbol) => (
-          <Positions
-            partyId={partyId}
-            assetSymbol={assetSymbol}
-            key={assetSymbol}
-            onClose={onClose}
-          />
-        ))}
+        <PositionsTable
+          domLayout="autoHeight"
+          style={{ width: '100%' }}
+          ref={gridRef}
+          rowModelType={data?.length ? 'infinite' : 'clientSide'}
+          rowData={data?.length ? undefined : []}
+          datasource={{ getRows }}
+          onClose={onClose}
+        />
       </AsyncRenderer>
 
       <Dialog>
