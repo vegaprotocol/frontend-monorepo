a6576132b1e2f7296a2163a687b1a6f7d8285197
Bartłomiej Głownia <bglownia@gmail.com>
2022-10-19 11:14:18 +0200

    chore: generic data provider improvments (#1772)

--- libs/react-helpers/src/lib/pagination.ts
+++ libs/react-helpers/src/lib/pagination.ts
@@ -1,5 +1,6 @@
 import type { IGetRowsParams } from 'ag-grid-community';
-import type { Load } from './generic-data-provider';
+import type { Load, DerivedPart } from './generic-data-provider';
+import type { Node, Edge } from './generic-data-provider';
 import type { MutableRefObject } from 'react';
 
 const getLastRow = (
@@ -57,3 +58,39 @@ export const makeInfiniteScrollGetRows =
       failCallback();
     }
   };
+
+export const paginatedCombineDelta = <
+  DataNode extends Node,
+  DeltaNode extends Node
+>(
+  data: (Edge<DataNode> | null)[],
+  parts: DerivedPart[]
+): DataNode[] | undefined => {
+  if (!parts[0].isUpdate) {
+    return;
+  }
+  const updatedIds = (parts[0].delta as DeltaNode[]).map((node) => node.id);
+  return data
+    .filter<Edge<DataNode>>(
+      (edge): edge is Edge<DataNode> =>
+        edge !== null && updatedIds.includes(edge.node.id)
+    )
+    .map((edge) => edge.node);
+};
+
+export const paginatedCombineInsertionData = <DataNode extends Node>(
+  data: (Edge<DataNode> | null)[],
+  parts: DerivedPart[]
+): Edge<DataNode>[] | undefined => {
+  if (!parts[0].isInsert) {
+    return;
+  }
+  const insertedIds = (parts[0].insertionData as DataNode[]).map(
+    (node) => node.id
+  );
+  // get updated orders
+  return data.filter<Edge<DataNode>>(
+    (edge): edge is Edge<DataNode> =>
+      edge !== null && insertedIds.includes(edge.node.id)
+  );
+};
