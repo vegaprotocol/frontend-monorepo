6d9375768cdee5c3146c4a36c0c67e1ade77fb65
Madalina Raicu <madalina@raygroup.uk>
2022-10-19 09:51:30 +0100

    fix: #1486 fix deal-ticket-steps est close out null handling

--- apps/console-lite/src/app/components/deal-ticket/deal-ticket-steps.tsx
+++ apps/console-lite/src/app/components/deal-ticket/deal-ticket-steps.tsx
@@ -200,7 +200,7 @@ export const DealTicketSteps = ({ market }: DealTicketMarketProps) => {
                   .symbol
               }
               notionalSize={notionalSize || emptyString}
-              estCloseOut={estCloseOut}
+              estCloseOut={estCloseOut || '-'}
               fees={fees || emptyString}
               estMargin={estMargin?.margin || emptyString}
             />
@@ -230,7 +230,7 @@ export const DealTicketSteps = ({ market }: DealTicketMarketProps) => {
             isDisabled={isDisabled}
             transactionStatus={transactionStatus}
             order={order}
-            estCloseOut={estCloseOut}
+            estCloseOut={estCloseOut || '-'}
             estMargin={estMargin?.margin || emptyString}
             price={formattedPrice || emptyString}
             quoteName={
