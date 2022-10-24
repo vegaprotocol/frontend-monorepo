98705082dbd07fad65eb054327b696c0eea8a9df
Botond <botond@vegaprotocol.io>
2022-10-24 08:52:53 +0100

    chore: migrate governance types

--- libs/governance/src/lib/proposals-hooks/use-proposal-event.ts
+++ libs/governance/src/lib/proposals-hooks/use-proposal-event.ts
@@ -1,29 +1,15 @@
-import { useApolloClient, gql } from '@apollo/client';
+import { useApolloClient } from '@apollo/client';
 import { useCallback, useEffect, useRef } from 'react';
+import {
+  ProposalEventDocument,
+} from './__generated__/Proposal';
 import type {
-  ProposalEvent,
-  ProposalEventVariables,
-  ProposalEvent_busEvents_event_Proposal,
-} from './__generated__/ProposalEvent';
+  ProposalEventSubscriptionVariables,
+  ProposalEventSubscription,
+  ProposalEventFieldsFragment,
+} from './__generated__/Proposal';
 import type { Subscription } from 'zen-observable-ts';
 import type { VegaTxState } from '@vegaprotocol/wallet';
-
-export const PROPOSAL_EVENT_SUB = gql`
-  subscription ProposalEvent($partyId: ID!) {
-    busEvents(partyId: $partyId, batchSize: 0, types: [Proposal]) {
-      type
-      event {
-        ... on Proposal {
-          id
-          reference
-          state
-          rejectionReason
-          errorDetails
-        }
-      }
-    }
-  }
-`;
 
 export const useProposalEvent = (transaction: VegaTxState) => {
   const client = useApolloClient();
@@ -33,11 +19,11 @@ export const useProposalEvent = (transaction: VegaTxState) => {
     (
       id: string,
       partyId: string,
-      callback: (proposal: ProposalEvent_busEvents_event_Proposal) => void
+      callback: (proposal: ProposalEventFieldsFragment) => void
     ) => {
       subRef.current = client
-        .subscribe<ProposalEvent, ProposalEventVariables>({
-          query: PROPOSAL_EVENT_SUB,
+        .subscribe<ProposalEventSubscription, ProposalEventSubscriptionVariables>({
+          query: ProposalEventDocument,
           variables: { partyId },
         })
         .subscribe(({ data }) => {
