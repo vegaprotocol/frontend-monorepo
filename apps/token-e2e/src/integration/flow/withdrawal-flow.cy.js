4c73797910e5b1877a73fa72fd464ab55224f651
Joe <joe@vega.xyz>
2022-10-25 15:47:11 +0100

    chore: functions for depositing assets

--- apps/token-e2e/src/integration/flow/withdrawal-flow.cy.js
+++ apps/token-e2e/src/integration/flow/withdrawal-flow.cy.js
@@ -2,16 +2,25 @@ const withdraw = 'withdraw';
 const selectAsset = 'select-asset';
 const ethAddressInput = 'eth-address-input';
 const amountInput = 'amount-input';
+const UsdcEthAddress = '0x1b8a1B6CBE5c93609b46D1829Cc7f3Cb8eeE23a0';
 
 context(
   'Withdrawals - with eth and vega wallet connected',
   { tags: '@slow' },
   function () {
     before('visit withdrawals and connect vega wallet', function () {
+      // cy.updateCapsuleMultiSig(); // When running tests locally, will fail if run without restarting capsule
       cy.vega_wallet_import();
+      cy.deposit_asset(UsdcEthAddress);
       cy.visit('/');
       cy.navigate_to('withdrawals');
       cy.vega_wallet_connect();
+      cy.ethereum_wallet_connect();
+    });
+
+    beforeEach('Navigate to withdrawal page', function () {
+      cy.visit('/');
+      cy.navigate_to('withdrawals');
     });
 
     it('Able to open withdrawal form with vega wallet connected', function () {
---
+++ libs/cypress/src/lib/commands/add-validators-to-multisig.ts
@@ -0,0 +1,21 @@
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Chainable<Subject> {
+      updateCapsuleMultiSig(): void;
+    }
+  }
+}
+
+export function addUpdateCapsuleMultiSig() {
+  Cypress.Commands.add('updateCapsuleMultiSig', () => {
+    Cypress.on('uncaught:exception', () => {
+      return false;
+    });
+    cy.log('Adding validators to multisig');
+    cy.exec('vegacapsule ethereum multisig init', { failOnNonZeroExit: false })
+      .its('stderr')
+      .should('contain', 'Updated multisig threshold');
+  });
+}
