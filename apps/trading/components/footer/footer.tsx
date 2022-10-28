ac4dad50bb5b431282a4e8c1e250f4a2cb3c94eb
Matthew Russell <mattrussell36@gmail.com>
2022-10-27 18:03:12 -0700

    feat: add node url to footer, add link to change node

--- apps/trading/components/footer/footer.tsx
+++ apps/trading/components/footer/footer.tsx
@@ -1,9 +1,27 @@
+import { useEnvironment } from '@vegaprotocol/environment';
+import { t } from '@vegaprotocol/react-helpers';
+import { ButtonLink, Link } from '@vegaprotocol/ui-toolkit';
+
 export const Footer = () => {
+  const { VEGA_URL, setNodeSwitcherOpen } = useEnvironment();
   return (
-    <footer className="px-4 py-2 text-xs border-t border-default bg-neutral-100 dark:bg-neutral-800">
+    <footer className="px-4 py-1 text-xs border-t border-default">
       <div className="flex justify-between">
-        <div>Status</div>
+        <div className="flex gap-2">
+          {VEGA_URL && <NodeUrl url={VEGA_URL} />}
+          <ButtonLink onClick={setNodeSwitcherOpen}>Change</ButtonLink>
+        </div>
       </div>
     </footer>
+  );
+};
+
+const NodeUrl = ({ url }: { url: string }) => {
+  const urlObj = new URL(url);
+  const nodeUrl = urlObj.origin.replace(/^[^.]+\./g, '');
+  return (
+    <Link href={'https://' + nodeUrl} target="_blank">
+      {nodeUrl}
+    </Link>
   );
 };
