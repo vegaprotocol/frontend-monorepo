83a677be6c6099935cfac91aa294f63ab1c20461
Elmar Gasimov <elmar@vegaprotocol.io>
2022-11-03 14:22:57 +0000

    feat(explorer): update styles tx details page

--- apps/explorer/src/app/routes/txs/id/tx-details.tsx
+++ apps/explorer/src/app/routes/txs/id/tx-details.tsx
@@ -1,15 +1,9 @@
 import { Routes } from '../../route-names';
-import { CopyWithTooltip, Icon } from '@vegaprotocol/ui-toolkit';
-import {
-  TableWithTbody,
-  TableCell,
-  TableHeader,
-  TableRow,
-} from '../../../components/table';
 import { t } from '@vegaprotocol/react-helpers';
-import { HighlightedLink } from '../../../components/highlighted-link';
 import type { BlockExplorerTransactionResult } from '../../../routes/types/block-explorer-response';
 import React from 'react';
+import { TruncateInline } from '../../../components/truncate/truncate';
+import { Link } from 'react-router-dom';
 
 interface TxDetailsProps {
   txData: BlockExplorerTransactionResult | undefined;
@@ -24,40 +18,32 @@ export const TxDetails = ({ txData, pubKey, className }: TxDetailsProps) => {
     return <>{t('Awaiting Block Explorer transaction details')}</>;
   }
 
+  const truncatedSubmitter = (
+    <TruncateInline text={pubKey || ''} startChars={5} endChars={5} />
+  );
+
   return (
-    <TableWithTbody className={className}>
-      <TableRow modifier="bordered">
-        <TableCell>{t('Hash')}</TableCell>
-        <TableCell modifier="bordered" data-testid="hash">
-          {txData.hash}
-          <CopyWithTooltip text={txData.hash}>
-            <button
-              title={t('Copy tx to clipboard')}
-              data-testid="copy-tx-to-clipboard"
-              className="underline"
-            >
-              <Icon name="duplicate" className="ml-2" />
-            </button>
-          </CopyWithTooltip>
-        </TableCell>
-      </TableRow>
-      <TableRow modifier="bordered">
-        <TableHeader scope="row" className="w-[160px]">
-          {t('Submitted by')}
-        </TableHeader>
-        <TableCell modifier="bordered" data-testid="submitted-by">
-          <HighlightedLink to={`/${Routes.PARTIES}/${pubKey}`} text={pubKey} />
-        </TableCell>
-      </TableRow>
-      <TableRow modifier="bordered">
-        <TableCell>{t('Block')}</TableCell>
-        <TableCell modifier="bordered" data-testid="block">
-          <HighlightedLink
-            to={`/${Routes.BLOCKS}/${txData.block}`}
-            text={txData.block}
-          />
-        </TableCell>
-      </TableRow>
-    </TableWithTbody>
+    <section className="mb-10">
+      <h3 className="text-3xl xl:text-4xl uppercase font-alpha mb-4">
+        Order submission by{' '}
+        <Link
+          className="font-bold underline"
+          to={`/${Routes.PARTIES}/${pubKey}`}
+        >
+          {truncatedSubmitter}
+        </Link>
+      </h3>
+      <p className="text-xl xl:text-2xl uppercase font-alpha">
+        Block{' '}
+        <Link
+          className="font-bold underline"
+          to={`/${Routes.BLOCKS}/${txData.block}`}
+        >
+          {txData.block}
+        </Link>
+        {', '}
+        Index {txData.index}
+      </p>
+    </section>
   );
 };
