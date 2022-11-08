c44421e31fc1cfff493a842383c2428edefba17c
Rado <szpiechrados@gmail.com>
2022-11-08 17:04:56 +0100

    test: test

--- apps/stats-e2e/src/integration/app.test.ts
+++ apps/stats-e2e/src/integration/app.test.ts
@@ -4,6 +4,7 @@ describe('stats', () => {
   beforeEach(() => cy.visit('/'));
 
   it.skip('should display header based on environment name', () => {
+    console.log('test');
     cy.get('h3', { timeout: 10000 }).should('have.text', `/ ${textToCheck}`);
   });
 });
