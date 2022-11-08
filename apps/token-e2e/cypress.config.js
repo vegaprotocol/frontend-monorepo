65dfa2aaa804339bc61628bf9b1406990c6fbf1f
Joe <joe@vega.xyz>
2022-11-08 17:21:18 +0000

    chore: update teardown in workdflows

--- apps/token-e2e/cypress.config.js
+++ apps/token-e2e/cypress.config.js
@@ -51,6 +51,6 @@ module.exports = defineConfig({
     grepTags: '@regression @smoke @slow',
     grepFilterSpecs: true,
     grepOmitFiltered: true,
-    TEARDOWN_NETWORK_AFTER_FLOWS: false
+    TEARDOWN_NETWORK_AFTER_FLOWS: false,
   },
 });
