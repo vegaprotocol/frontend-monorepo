9a517294c18a5f8dc724ed96af9176778edddd58
Rado <szpiechrados@gmail.com>
2022-11-08 17:06:51 +0100

    test: test

--- apps/stats-e2e/src/integration/app.test.ts
+++ apps/stats-e2e/src/integration/app.test.ts
@@ -1,9 +1,12 @@
 const textToCheck = Cypress.env('VEGA_ENV');
 // breaking changes fail
-describe('stats', () => {
+describe('stats', { tags: '@smoke' }, () => {
   beforeEach(() => cy.visit('/'));
 
   it.skip('should display header based on environment name', () => {
     cy.get('h3', { timeout: 10000 }).should('have.text', `/ ${textToCheck}`);
+  });
+  it('should fail', () => {
+    expect(true).to.equal(false);
   });
 });
