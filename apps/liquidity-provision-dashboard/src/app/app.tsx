c21a14f5efd84fc721da64b3292a50117ab4704c
Dexter <dexter.edwards93@gmail.com>
2022-11-09 10:20:48 +0000

    chore: resolve conflicts

--- apps/liquidity-provision-dashboard/src/app/app.tsx
+++ apps/liquidity-provision-dashboard/src/app/app.tsx
@@ -1,7 +1,4 @@
-import { ThemeContext } from '@vegaprotocol/react-helpers';
 import { useRoutes } from 'react-router-dom';
-import { EnvironmentProvider, NetworkLoader } from '@vegaprotocol/environment';
-import { createClient } from './lib/apollo-client';
 
 import '../styles.scss';
 import { Navbar } from './components/navbar';
@@ -12,16 +9,10 @@ const AppRouter = () => useRoutes(routerConfig);
 
 export function App() {
   return (
-    <EnvironmentProvider>
-      <ThemeContext.Provider value="light">
-        <NetworkLoader createClient={createClient}>
-          <div className="max-h-full min-h-full bg-white">
-            <Navbar />
-            <AppRouter />
-          </div>
-        </NetworkLoader>
-      </ThemeContext.Provider>
-    </EnvironmentProvider>
+    <div className="max-h-full min-h-full bg-white">
+      <Navbar />
+      <AppRouter />
+    </div>
   );
 }
 
