48fca4b66003b28df87f93abe3fd9152d31f8f49
sam-keen <samuel.kleinmann@gmail.com>
2022-11-10 13:56:52 +0000

    feat(1879): Delegations - removed from explorer queries as currently unused

--- apps/explorer/src/app/routes/parties/__generated___/PartyAssets.ts
+++ apps/explorer/src/app/routes/parties/__generated___/PartyAssets.ts
@@ -8,21 +8,13 @@ export type PartyAssetsQueryQueryVariables = Types.Exact<{
 }>;
 
 
-export type PartyAssetsQueryQuery = { __typename?: 'Query', party?: { __typename?: 'Party', id: string, delegations?: Array<{ __typename?: 'Delegation', amount: string, epoch: number, node: { __typename?: 'Node', id: string, name: string } }> | null, stakingSummary: { __typename?: 'StakingSummary', currentStakeAvailable: string }, accounts?: Array<{ __typename?: 'AccountBalance', type: Types.AccountType, balance: string, asset: { __typename?: 'Asset', name: string, id: string, decimals: number, symbol: string, source: { __typename: 'BuiltinAsset' } | { __typename: 'ERC20', contractAddress: string } } }> | null } | null };
+export type PartyAssetsQueryQuery = { __typename?: 'Query', party?: { __typename?: 'Party', id: string, stakingSummary: { __typename?: 'StakingSummary', currentStakeAvailable: string }, accounts?: Array<{ __typename?: 'AccountBalance', type: Types.AccountType, balance: string, asset: { __typename?: 'Asset', name: string, id: string, decimals: number, symbol: string, source: { __typename: 'BuiltinAsset' } | { __typename: 'ERC20', contractAddress: string } } }> | null } | null };
 
 
 export const PartyAssetsQueryDocument = gql`
     query PartyAssetsQuery($partyId: ID!) {
   party(id: $partyId) {
     id
-    delegations {
-      amount
-      node {
-        id
-        name
-      }
-      epoch
-    }
     stakingSummary {
       currentStakeAvailable
     }
