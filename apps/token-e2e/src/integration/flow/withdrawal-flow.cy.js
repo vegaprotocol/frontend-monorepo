789002724e9282fd228f4e73dd8f77be1c40a46b
Joe <joe@vega.xyz>
2022-11-10 09:42:05 +0000

    chore: turn off teardown and revert tag change

--- apps/token-e2e/src/integration/flow/withdrawal-flow.cy.js
+++ apps/token-e2e/src/integration/flow/withdrawal-flow.cy.js
@@ -23,7 +23,7 @@ const txTimeout = Cypress.env('txTimeout');
 
 context(
   'Withdrawals - with eth and vega wallet connected',
-  { tags: '@withdrawal' },
+  { tags: '@slow' },
   function () {
     before('visit withdrawals and connect vega wallet', function () {
       cy.updateCapsuleMultiSig(); // When running tests locally, will fail if run without restarting capsule
