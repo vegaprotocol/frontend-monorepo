6160fa0856bc0ca4f9ac16bbb71602391d8087da
m.ray <16125548+MadalinaRaicu@users.noreply.github.com>
2022-11-17 09:19:09 -0500

    fix(#2144): fix positions table notional value and remove total row

--- libs/positions/src/lib/use-positions-data.tsx
+++ libs/positions/src/lib/use-positions-data.tsx
@@ -1,40 +1,12 @@
 import { useCallback, useMemo, useRef } from 'react';
 import type { RefObject } from 'react';
-import { BigNumber } from 'bignumber.js';
 import type { AgGridReact } from 'ag-grid-react';
 import type { Position } from './positions-data-providers';
 import { positionsMetricsProvider } from './positions-data-providers';
 import type { PositionsMetricsProviderVariables } from './positions-data-providers';
-import { t, toBigNum, useDataProvider } from '@vegaprotocol/react-helpers';
+import { useDataProvider } from '@vegaprotocol/react-helpers';
 
 export const getRowId = ({ data }: { data: Position }) => data.marketId;
-
-export const getSummaryRowData = (positions: Position[]) => {
-  const summaryRow = {
-    notional: new BigNumber(0),
-    realisedPNL: BigInt(0),
-    unrealisedPNL: BigInt(0),
-  };
-  positions.forEach((position) => {
-    summaryRow.notional = summaryRow.notional.plus(
-      toBigNum(position.notional, position.marketDecimalPlaces)
-    );
-    summaryRow.realisedPNL += BigInt(position.realisedPNL);
-    summaryRow.unrealisedPNL += BigInt(position.unrealisedPNL);
-  });
-  const decimals = positions[0]?.decimals || 0;
-  return {
-    marketName: t('Total'),
-    // we are using asset decimals instead of market decimals because each market can have different decimals
-    notional: summaryRow.notional
-      .multipliedBy(10 ** decimals)
-      .toFixed()
-      .toString(),
-    realisedPNL: summaryRow.realisedPNL.toString(),
-    unrealisedPNL: summaryRow.unrealisedPNL.toString(),
-    decimals,
-  };
-};
 
 export const usePositionsData = (
   partyId: string,
@@ -76,18 +48,6 @@ export const usePositionsData = (
           add,
           addIndex: 0,
         });
-        const summaryRowNode = gridRef.current.api.getPinnedBottomRow(0);
-        if (summaryRowNode && dataRef.current) {
-          summaryRowNode.data = getSummaryRowData(dataRef.current);
-          gridRef.current.api.refreshCells({
-            force: true,
-            rowNodes: [summaryRowNode],
-          });
-        } else {
-          gridRef.current.api.setPinnedBottomRowData(
-            dataRef.current ? [getSummaryRowData(dataRef.current)] : []
-          );
-        }
       }
       return true;
     },
