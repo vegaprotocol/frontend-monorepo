56cf380a231f8143293158e9c38cb8c39e4d368a
Dexter <dexter.edwards93@gmail.com>
2022-11-29 14:46:04 +0000

    chore: make navbar full width

--- apps/token/src/components/page-templates/template-sidebar.tsx
+++ apps/token/src/components/page-templates/template-sidebar.tsx
@@ -9,16 +9,18 @@ export interface TemplateSidebarProps {
 
 export function TemplateSidebar({ children, sidebar }: TemplateSidebarProps) {
   return (
-    <div className="w-full border-b border-neutral-700 lg:grid lg:grid-rows-[min-content_1fr] lg:grid-cols-[1fr_450px]">
+    <>
       <Nav />
-      <main className="col-start-1 p-4">{children}</main>
-      <aside className="col-start-2 row-start-1 row-span-2 hidden lg:block p-4 bg-banner bg-contain border-l border-neutral-700">
-        {sidebar.map((Component, i) => (
-          <section className="mb-4 last:mb-0" key={i}>
-            {Component}
-          </section>
-        ))}
-      </aside>
-    </div>
+      <div className="w-full border-b border-neutral-700 lg:grid lg:grid-rows-[min-content_1fr] lg:grid-cols-[1fr_450px]">
+        <main className="col-start-1 p-4">{children}</main>
+        <aside className="col-start-2 row-start-1 row-span-2 hidden lg:block p-4 bg-banner bg-contain border-l border-neutral-700">
+          {sidebar.map((Component, i) => (
+            <section className="mb-4 last:mb-0" key={i}>
+              {Component}
+            </section>
+          ))}
+        </aside>
+      </div>
+    </>
   );
 }
