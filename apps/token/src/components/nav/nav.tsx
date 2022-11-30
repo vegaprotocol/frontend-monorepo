a42bb9f036447963a870ed903a5252dc8fdb8486
Dexter <dexter.edwards93@gmail.com>
2022-11-30 11:09:04 +0000

    chore: make navbar reactive and support draw

--- apps/token/src/components/nav/nav.tsx
+++ apps/token/src/components/nav/nav.tsx
@@ -1,20 +1,46 @@
 import classNames from 'classnames';
 import { NavLink, Link } from 'react-router-dom';
 import { NetworkSwitcher } from '@vegaprotocol/environment';
-import { ThemeSwitcher } from '@vegaprotocol/ui-toolkit';
-// import { Vega } from '../icons/vega';
 import type { HTMLAttributeAnchorTarget } from 'react';
-// import testnetBg from '../../assets/green-cloud.png';
+import { useEffect, useState } from 'react';
 import Routes from '../../routes/routes';
 import { useTranslation } from 'react-i18next';
 import vegaWhite from '../../images/vega_white.png';
+import {
+  AppStateActionType,
+  useAppState,
+} from '../../contexts/app-state/app-state-context';
+import * as Dialog from '@radix-ui/react-dialog';
+import { EthWallet } from '../eth-wallet';
+import { VegaWallet } from '../vega-wallet';
+import debounce from 'lodash/debounce';
 
 type NavbarTheme = 'inherit' | 'dark' | 'yellow';
 interface NavbarProps {
   navbarTheme?: NavbarTheme;
 }
 
+interface Route {
+  name: string;
+  path: string;
+}
+
 export const Nav = ({ navbarTheme = 'inherit' }: NavbarProps) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const isDesktop = windowWidth > 995;
+
+  useEffect(() => {
+    const handleResizeDebounced = debounce(() => {
+      setWindowWidth(window.innerWidth);
+    }, 300);
+
+    window.addEventListener('resize', handleResizeDebounced);
+
+    return () => {
+      window.removeEventListener('resize', handleResizeDebounced);
+    };
+  }, []);
+
   const { t } = useTranslation();
   const themeWrapperClasses = classNames({
     dark: navbarTheme === 'dark',
@@ -30,59 +56,65 @@ export const Nav = ({ navbarTheme = 'inherit' }: NavbarProps) => {
     }
   );
 
+  const routes = [
+    {
+      name: t('Proposals'),
+      path: Routes.PROPOSALS,
+    },
+    {
+      name: t('Validators'),
+      path: Routes.VALIDATORS,
+    },
+    {
+      name: t('Rewards'),
+      path: Routes.REWARDS,
+    },
+    {
+      name: t('Token'),
+      path: Routes.TOKEN,
+    },
+    {
+      name: t('Redeem'),
+      path: Routes.REDEEM,
+    },
+    {
+      name: t('Withdraw'),
+      path: Routes.WITHDRAWALS,
+    },
+    {
+      name: t('Supply & Vesting'),
+      path: Routes.TRANCHES,
+    },
+  ];
+
   return (
     <div className={themeWrapperClasses}>
-      <div
-        className={navbarClasses}
-        style={
-          {
-            // backgroundImage: isYellow ? `url("${testnetBg.src}")` : '',
-          }
-        }
-      >
+      <div className={navbarClasses}>
         <div className="flex gap-4 items-center">
           <Link to="/">
             <img alt="Vega" src={vegaWhite} height={30} width={30} />
           </Link>
+          <h1
+            className={classNames(
+              'h-full flex flex-col my-0 justify-center font-alpha calt',
+              { 'text-black': isYellow, 'text-white': !isYellow }
+            )}
+          >
+            {t('Governance')}
+          </h1>
           <NetworkSwitcher />
         </div>
-        <nav className="flex items-center flex-1 px-2">
-          <AppNavLink
-            name={t('Proposals')}
-            path={Routes.PROPOSALS}
-            navbarTheme={navbarTheme}
-          />
-          <AppNavLink
-            name={t('Validators')}
-            path={Routes.VALIDATORS}
-            navbarTheme={navbarTheme}
-          />
-          <AppNavLink
-            name={t('Rewards')}
-            path={Routes.REWARDS}
-            navbarTheme={navbarTheme}
-          />
-          <AppNavLink
-            name={t('Token')}
-            path={Routes.TOKEN}
-            navbarTheme={navbarTheme}
-          />
-          <AppNavLink
-            name={t('Redeem')}
-            path={Routes.REDEEM}
-            navbarTheme={navbarTheme}
-          />
-          <AppNavLink
-            name={t('Withdraw')}
-            path={Routes.WITHDRAWALS}
-            navbarTheme={navbarTheme}
-          />
-          <AppNavLink
-            name={t('Supply & Vesting')}
-            path={Routes.TRANCHES}
-            navbarTheme={navbarTheme}
-          />
-        </nav>
+        {isDesktop ? (
+          <nav className="flex items-center flex-1 px-2">
+            {routes.map((r) => (
+              <AppNavLink {...r} navbarTheme={navbarTheme} />
+            ))}
+          </nav>
+        ) : (
+          <nav className="flex items-center flex-1 px-2 justify-end">
+            <NavDrawer inverted={isYellow} routes={routes} />
+          </nav>
+        )}
       </div>
     </div>
   );
@@ -115,6 +147,7 @@ const AppNavLink = ({
       to={{ pathname: path }}
       className={getNavLinkClassNames(navbarTheme, alignRight)}
       target={target}
+      end={true}
     >
       {({ isActive }) => {
         return (
@@ -151,4 +184,114 @@ const getActiveNavLinkClassNames = (
     'text-black': isActive && navbarTheme === 'yellow',
     'text-black/60 hover:text-black': !isActive && navbarTheme === 'yellow',
   });
+};
+
+const DrawerSection = ({ children }: { children: React.ReactNode }) => (
+  <div className="px-4 my-4">{children}</div>
+);
+
+const IconLine = ({ inverted }: { inverted: boolean }) => (
+  <span className={`block w-6 h-[2px] ${inverted ? 'bg-black' : 'bg-white'}`} />
+);
+
+const DrawNavLinks = ({
+  isInverted,
+  routes,
+}: {
+  isInverted?: boolean;
+  routes: Route[];
+}) => {
+  const { appDispatch } = useAppState();
+  const linkProps = {
+    end: true,
+    onClick: () =>
+      appDispatch({ type: AppStateActionType.SET_DRAWER, isOpen: false }),
+  };
+  const navClasses = classNames('flex flex-col');
+
+  return (
+    <nav className={navClasses}>
+      {routes.map(({ name, path }) => {
+        return (
+          <NavLink
+            {...linkProps}
+            to={{ pathname: path }}
+            className={({ isActive }) =>
+              classNames({
+                'bg-vega-yellow text-black': !isInverted && isActive,
+                'bg-transparent text-white hover:text-vega-yellow':
+                  !isInverted && !isActive,
+                'bg-black text-white': isInverted && isActive,
+                'bg-transparent text-black hover:text-white':
+                  isInverted && !isActive,
+                'border-t border-white p-4': true,
+              })
+            }
+          >
+            {name}
+          </NavLink>
+        );
+      })}
+    </nav>
+  );
+};
+
+const NavDrawer = ({
+  inverted,
+  routes,
+}: {
+  inverted: boolean;
+  routes: Route[];
+}) => {
+  const { appState, appDispatch } = useAppState();
+
+  const drawerContentClasses = classNames(
+    'drawer-content', // needed for css animation
+    // Positions the modal in the center of screen
+    'fixed w-[80vw] max-w-[420px] top-0 right-0',
+    'flex flex-col flex-nowrap justify-between h-full bg-banner overflow-y-scroll border-l border-white',
+    'bg-black text-neutral-200'
+  );
+  return (
+    <>
+      <button
+        onClick={() =>
+          appDispatch({
+            type: AppStateActionType.SET_DRAWER,
+            isOpen: true,
+          })
+        }
+        className="flex flex-col flex-nowrap gap-1"
+      >
+        <IconLine inverted={inverted} />
+        <IconLine inverted={inverted} />
+        <IconLine inverted={inverted} />
+      </button>
+
+      <Dialog.Root
+        open={appState.drawerOpen}
+        onOpenChange={(isOpen) =>
+          appDispatch({
+            type: AppStateActionType.SET_DRAWER,
+            isOpen,
+          })
+        }
+      >
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 bg-white/15" />
+          <Dialog.Content className={drawerContentClasses}>
+            <div>
+              <DrawerSection>
+                <EthWallet />
+              </DrawerSection>
+              <DrawerSection>
+                <VegaWallet />
+              </DrawerSection>
+            </div>
+            <DrawNavLinks routes={routes} />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </>
+  );
 };
