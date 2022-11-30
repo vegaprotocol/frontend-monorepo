c43073a7f3a8e9ebaa5db07a598ebed60522663e
Dexter <dexter.edwards93@gmail.com>
2022-11-30 09:43:28 +0000

    feat: new navbar bases on trading navbar

--- apps/token/src/components/page-templates/template-sidebar.tsx
+++ apps/token/src/components/page-templates/template-sidebar.tsx
@@ -1,3 +1,5 @@
+import { Networks, useEnvironment } from '@vegaprotocol/environment';
+import { useThemeSwitcher } from '@vegaprotocol/react-helpers';
 import React from 'react';
 
 import { Nav } from '../nav';
@@ -8,9 +10,10 @@ export interface TemplateSidebarProps {
 }
 
 export function TemplateSidebar({ children, sidebar }: TemplateSidebarProps) {
+  const { VEGA_ENV } = useEnvironment();
   return (
     <>
-      <Nav />
+      <Nav navbarTheme={VEGA_ENV === Networks.TESTNET ? 'yellow' : 'dark'} />
       <div className="w-full border-b border-neutral-700 lg:grid lg:grid-rows-[min-content_1fr] lg:grid-cols-[1fr_450px]">
         <main className="col-start-1 p-4">{children}</main>
         <aside className="col-start-2 row-start-1 row-span-2 hidden lg:block p-4 bg-banner bg-contain border-l border-neutral-700">
