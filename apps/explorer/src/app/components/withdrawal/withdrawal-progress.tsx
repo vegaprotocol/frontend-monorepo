174a565dcc7b5d38499dc9a7d9cee24dce4f93c6
Edd <edd@vega.xyz>
2022-12-07 17:27:13 +0000

    feat(explorer): progress bar

--- apps/explorer/src/app/components/withdrawal/withdrawal-progress.tsx
+++ apps/explorer/src/app/components/withdrawal/withdrawal-progress.tsx
@@ -1,3 +1,4 @@
+import { t } from '@vegaprotocol/react-helpers';
 import { Time } from '../time';
 import { useExplorerWithdrawalQuery } from './__generated__/Withdrawal';
 
@@ -21,6 +22,26 @@ const classes = {
   textIncomplete:
     'absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500',
 };
+
+/**
+ * Shows a user the *current* state of a proposal. Which sound easy, but.. strap in
+ * 1. Step one is the submission and acceptance of the WithdrawalSubmission. If it is not rejected, it is accepted and we can mark it as complete
+ * 2. Is complete when there is a multisig bundle available to the user. This means that their funds have been moved to an account locked for
+ *    withdrawal. To move out of this phase, the user (any user - but realistically the owner of the recipient address) needs to execute the withdrawal
+ *    bundle on Ethereum, then we progress to.
+ * 3. The funds have left the ERC20 bridge and been sent to the user. This is complete.
+ *
+ * There could actually be some extra complexity:
+ * - If a deposit is above { asset { withdrawalThreshold } } then the multisig bundle is still produced (step 2) but cannot be submitted for a period
+ *   of time. The period of time is defined on the ERC20 bridge. Attempts to submit the multisig bundle before that would be rejected.
+ * - The bridge could be paused, which would cause attempts to complete step three to be rejected
+ *
+ * Also:
+ * - 1 -> 2 is completed automatically by the system
+ * - 2 -> 3 requires action from the user
+ * - Any user with an Ethereum wallet can complete 2 -> 3 as above, but it will likely be the person who owns the vega public key of the source and
+ *   the ethereum public key of the recipient. Whoever is looking at this page may be none of those people.
+ */
 export const WithdrawalProgress = ({ id, txStatus }: TxsStatsInfoProps) => {
   const { data } = useExplorerWithdrawalQuery({ variables: { id } });
 
@@ -46,7 +67,7 @@ export const WithdrawalProgress = ({ id, txStatus }: TxsStatsInfoProps) => {
                 txStatus === 0 ? classes.textComplete : classes.textFailed
               }
             >
-              Requested
+              {txStatus === 0 ? t('Requested') : t('Rejected')}
               <br />
             </div>
           </div>
@@ -70,7 +91,7 @@ export const WithdrawalProgress = ({ id, txStatus }: TxsStatsInfoProps) => {
                 step2Date ? classes.textComplete : classes.textIncomplete
               }
             >
-              {step2Date ? 'Prepared' : 'Not prepared'}
+              {step2Date ? t('Prepared') : t('Not prepared')}
               <br />
               {step2Date ? <Time date={step2Date} /> : null}
             </div>
@@ -95,7 +116,7 @@ export const WithdrawalProgress = ({ id, txStatus }: TxsStatsInfoProps) => {
                 step3Date ? classes.textComplete : classes.textIncomplete
               }
             >
-              {step3Date ? 'Complete' : 'Not complete'}
+              {step3Date ? t('Complete') : t('Not complete')}
               <br />
               {step3Date ? <Time date={step3Date} /> : null}
             </div>
