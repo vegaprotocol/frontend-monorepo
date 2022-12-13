630cc3d00aa1fb2586be697f2fa8da21be3d25fe
Bartłomiej Głownia <bglownia@gmail.com>
2022-12-13 11:15:17 +0100

    feat: add use eth transaction manager tests

--- libs/web3/src/lib/use-ethereum-transaction-manager.spec.tsx
+++ libs/web3/src/lib/use-ethereum-transaction-manager.spec.tsx
@@ -1,5 +1,98 @@
-describe('use-ethereum-transaction-manager', () => {
-  it('exists', () => {
-    expect(true).toBeTruthy();
+import { useEthTransactionManager } from './use-ethereum-transaction-manager';
+import { renderHook } from '@testing-library/react';
+import waitForNextTick from 'flush-promises';
+import type { CollateralBridge } from '@vegaprotocol/smart-contracts';
+import type { EthTransactionStore } from './use-ethereum-transaction-store';
+
+import { EthTxStatus } from './use-ethereum-transaction';
+
+const txHash = 'txHash';
+
+const requestedTransactionUpdate = {
+  status: EthTxStatus.Requested,
+  error: null,
+  confirmations: 0,
+};
+
+const mockDepositAsset = jest.fn();
+const mockDepositAssetStatic = jest.fn();
+const mockTxWait = jest.fn(() => ({ confirmations: 5 }));
+mockDepositAsset.mockResolvedValue({
+  hash: txHash,
+  wait: mockTxWait,
+});
+
+const contract = {
+  contract: {
+    callStatic: {
+      deposit_asset: mockDepositAssetStatic,
+    },
+  },
+  deposit_asset: mockDepositAsset,
+} as unknown as CollateralBridge;
+const methodName = 'deposit_asset';
+const args: string[] = [];
+
+const update = jest.fn();
+const defaultState: Partial<EthTransactionStore> = {
+  transactions: [
+    {
+      id: 0,
+      status: EthTxStatus.Default,
+      createdAt: new Date('2022-12-12T11:24:40.301Z'),
+      updatedAt: new Date('2022-12-12T11:24:40.301Z'),
+      contract,
+      methodName,
+      args,
+      requiredConfirmations: 1,
+      requiresConfirmation: false,
+      error: null,
+      confirmations: 0,
+      dialogOpen: false,
+      txHash: null,
+      receipt: null,
+    },
+    {
+      id: 1,
+      status: EthTxStatus.Default,
+      createdAt: new Date('2022-12-12T11:24:40.301Z'),
+      updatedAt: new Date('2022-12-12T11:24:40.301Z'),
+      contract,
+      methodName,
+      args,
+      requiredConfirmations: 1,
+      requiresConfirmation: false,
+      error: null,
+      confirmations: 0,
+      dialogOpen: false,
+      txHash: null,
+      receipt: null,
+    },
+  ],
+  update,
+};
+
+const mockTransactionStoreState = jest.fn<Partial<EthTransactionStore>, []>();
+
+jest.mock('./use-ethereum-transaction-store', () => ({
+  useEthTransactionStore: (
+    selector: (state: Partial<EthTransactionStore>) => void
+  ) => selector(mockTransactionStoreState()),
+}));
+
+describe('useVegaTransactionManager', () => {
+  beforeEach(() => {
+    update.mockReset();
+    mockTransactionStoreState.mockReset();
+  });
+
+  it('sendTx of first pending transaction', async () => {
+    mockTransactionStoreState.mockReturnValue(defaultState);
+    const { rerender } = renderHook(useEthTransactionManager);
+    await waitForNextTick();
+    rerender();
+    await waitForNextTick();
+    expect(update.mock.calls[0]).toEqual([0, requestedTransactionUpdate]);
+    expect(update.mock.calls[4]).toEqual([1, requestedTransactionUpdate]);
   });
 });
--- libs/web3/src/lib/use-ethereum-transaction-manager.tsx
+++ libs/web3/src/lib/use-ethereum-transaction-manager.tsx
@@ -1,5 +1,5 @@
 import type { ethers } from 'ethers';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import type { EthereumError } from './ethereum-error';
 import { isExpectedEthereumError } from './ethereum-error';
 import { isEthereumError } from './ethereum-error';
@@ -16,11 +16,11 @@ export const useEthTransactionManager = () => {
         !processed.current.has(transaction.id)
     )
   );
-  if (!transaction) {
-    return;
-  }
-  processed.current.add(transaction.id);
-  (async () => {
+  useEffect(() => {
+    if (!transaction) {
+      return;
+    }
+    processed.current.add(transaction.id);
     update(transaction.id, {
       status: EthTxStatus.Requested,
       error: null,
@@ -33,72 +33,74 @@ export const useEthTransactionManager = () => {
       requiredConfirmations,
       requiresConfirmation,
     } = transaction;
-    try {
-      if (
-        !contract ||
-        // @ts-ignore method vary depends on contract
-        typeof contract[methodName] !== 'function' ||
-        typeof contract.contract.callStatic[methodName] !== 'function'
-      ) {
-        throw new Error('method not found on contract');
-      }
-      await contract.contract.callStatic[methodName](...args);
-    } catch (err) {
-      update(transaction.id, {
-        status: EthTxStatus.Error,
-        error: err as EthereumError,
-      });
-      return;
-    }
-
-    try {
-      // @ts-ignore method vary depends on contract
-      const method = contract[methodName];
-
-      if (!method || typeof method !== 'function') {
-        throw new Error('method not found on contract');
-      }
-
-      // @ts-ignore args will vary depends on contract and method
-      const tx = await method.call(contract, ...args);
-
-      let receipt: ethers.ContractReceipt | null = null;
-
-      update(transaction.id, {
-        status: EthTxStatus.Pending,
-        txHash: tx.hash,
-      });
-
-      for (let i = 1; i <= requiredConfirmations; i++) {
-        receipt = await tx.wait(i);
-        update(transaction.id, {
-          confirmations: receipt
-            ? receipt.confirmations
-            : requiredConfirmations,
-        });
-      }
-
-      if (!receipt) {
-        throw new Error('no receipt after confirmations are met');
-      }
-
-      if (requiresConfirmation) {
-        update(transaction.id, { status: EthTxStatus.Complete, receipt });
-      } else {
-        update(transaction.id, { status: EthTxStatus.Confirmed, receipt });
-      }
-    } catch (err) {
-      if (err instanceof Error || isEthereumError(err)) {
-        if (!isExpectedEthereumError(err)) {
-          update(transaction.id, { status: EthTxStatus.Error, error: err });
+    (async () => {
+      try {
+        if (
+          !contract ||
+          // @ts-ignore method vary depends on contract
+          typeof contract[methodName] !== 'function' ||
+          typeof contract.contract.callStatic[methodName] !== 'function'
+        ) {
+          throw new Error('method not found on contract');
         }
-      } else {
+        await contract.contract.callStatic[methodName](...args);
+      } catch (err) {
         update(transaction.id, {
           status: EthTxStatus.Error,
-          error: new Error('Something went wrong'),
+          error: err as EthereumError,
         });
+        return;
       }
-      return;
-    }
-  })();
+
+      try {
+        // @ts-ignore method vary depends on contract
+        const method = contract[methodName];
+
+        if (!method || typeof method !== 'function') {
+          throw new Error('method not found on contract');
+        }
+
+        // @ts-ignore args will vary depends on contract and method
+        const tx = await method.call(contract, ...args);
+
+        let receipt: ethers.ContractReceipt | null = null;
+
+        update(transaction.id, {
+          status: EthTxStatus.Pending,
+          txHash: tx.hash,
+        });
+
+        for (let i = 1; i <= requiredConfirmations; i++) {
+          receipt = await tx.wait(i);
+          update(transaction.id, {
+            confirmations: receipt
+              ? receipt.confirmations
+              : requiredConfirmations,
+          });
+        }
+
+        if (!receipt) {
+          throw new Error('no receipt after confirmations are met');
+        }
+
+        if (requiresConfirmation) {
+          update(transaction.id, { status: EthTxStatus.Complete, receipt });
+        } else {
+          update(transaction.id, { status: EthTxStatus.Confirmed, receipt });
+        }
+      } catch (err) {
+        if (err instanceof Error || isEthereumError(err)) {
+          if (!isExpectedEthereumError(err)) {
+            update(transaction.id, { status: EthTxStatus.Error, error: err });
+          }
+        } else {
+          update(transaction.id, {
+            status: EthTxStatus.Error,
+            error: new Error('Something went wrong'),
+          });
+        }
+        return;
+      }
+    })();
+  }, [transaction, update]);
 };
