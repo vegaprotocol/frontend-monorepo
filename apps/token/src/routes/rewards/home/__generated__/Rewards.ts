9e425cf9aca915dd7bcbccceaca7db1b596225a4
Dexter <dexter.edwards93@gmail.com>
2022-12-13 16:27:16 +0000

    fix: rewards page

--- apps/token/src/routes/rewards/home/__generated__/Rewards.ts
+++ apps/token/src/routes/rewards/home/__generated__/Rewards.ts
@@ -3,132 +3,9 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { AccountType } from "./../../../../../../../libs/types/src/__generated__/globalTypes";
-
 // ====================================================
 // GraphQL query operation: Rewards
 // ====================================================
-
-export interface Rewards_party_rewardDetails_asset {
-  __typename: "Asset";
-  /**
-   * The id of the asset
-   */
-  id: string;
-  /**
-   * The symbol of the asset (e.g: GBP)
-   */
-  symbol: string;
-}
-
-export interface Rewards_party_rewardDetails_rewards_asset {
-  __typename: "Asset";
-  /**
-   * The id of the asset
-   */
-  id: string;
-}
-
-export interface Rewards_party_rewardDetails_rewards_party {
-  __typename: "Party";
-  /**
-   * Party identifier
-   */
-  id: string;
-}
-
-export interface Rewards_party_rewardDetails_rewards_epoch {
-  __typename: "Epoch";
-  /**
-   * Presumably this is an integer or something. If there's no such thing, disregard
-   */
-  id: string;
-}
-
-export interface Rewards_party_rewardDetails_rewards {
-  __typename: "Reward";
-  /**
-   * The type of reward
-   */
-  rewardType: AccountType;
-  /**
-   * The asset this reward is paid in
-   */
-  asset: Rewards_party_rewardDetails_rewards_asset;
-  /**
-   * Party receiving the reward
-   */
-  party: Rewards_party_rewardDetails_rewards_party;
-  /**
-   * Epoch for which this reward was distributed
-   */
-  epoch: Rewards_party_rewardDetails_rewards_epoch;
-  /**
-   * Amount received for this reward
-   */
-  amount: string;
-  /**
-   * The amount field formatted by the client
-   */
-  amountFormatted: string;
-  /**
-   * Percentage out of the total distributed reward
-   */
-  percentageOfTotal: string;
-  /**
-   * Time at which the rewards was received
-   */
-  receivedAt: string;
-}
-
-export interface Rewards_party_rewardDetails {
-  __typename: "RewardPerAssetDetail";
-  /**
-   * Asset in which the reward was paid
-   */
-  asset: Rewards_party_rewardDetails_asset;
-  /**
-   * A list of rewards received for this asset
-   */
-  rewards: (Rewards_party_rewardDetails_rewards | null)[] | null;
-  /**
-   * The total amount of rewards received for this asset.
-   */
-  totalAmount: string;
-  /**
-   * The total amount field formatted by the client
-   */
-  totalAmountFormatted: string;
-}
-
-export interface Rewards_party_delegations {
-  __typename: "Delegation";
-  /**
-   * Amount delegated
-   */
-  amount: string;
-  /**
-   * The amount field formatted by the client
-   */
-  amountFormatted: string;
-  /**
-   * Epoch of delegation
-   */
-  epoch: number;
-}
-
-export interface Rewards_party {
-  __typename: "Party";
-  /**
-   * Party identifier
-   */
-  id: string;
-  /**
-   * return reward information
-   */
-  rewardDetails: (Rewards_party_rewardDetails | null)[] | null;
-  delegations: Rewards_party_delegations[] | null;
-}
 
 export interface Rewards_epoch_timestamps {
   __typename: "EpochTimestamps";
@@ -159,10 +36,6 @@ export interface Rewards_epoch {
 }
 
 export interface Rewards {
-  /**
-   * An entity that is trading on the VEGA network
-   */
-  party: Rewards_party | null;
   /**
    * get data for a specific epoch, if id omitted it gets the current epoch. If the string is 'next', fetch the next epoch
    */
