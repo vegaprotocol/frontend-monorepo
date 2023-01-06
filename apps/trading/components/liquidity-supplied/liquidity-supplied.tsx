0f5f73ccef8b85fb0f475c016d4eb06a7b3e61ea
Madalina Raicu <madalina@raygroup.uk>
2023-01-06 15:08:19 +0200

    fix: add sufficient liquidity still auction message

--- apps/trading/components/liquidity-supplied/liquidity-supplied.tsx
+++ apps/trading/components/liquidity-supplied/liquidity-supplied.tsx
@@ -124,7 +124,7 @@ export const MarketLiquiditySupplied = ({
       {showMessage && (
         <p className="mt-4">
           {t(
-            'If it is sufficient liquidity, the market is in an auction because there are no priced limit orders, which are required to deploy liquidity provision pegged orders, so the orderbook is empty on one or both sides.'
+            'If there is sufficient liquidity, the market is in an auction because there are no priced limit orders, which are required to deploy liquidity provision pegged orders. This means the order book is empty on one or both sides.'
           )}
         </p>
       )}
