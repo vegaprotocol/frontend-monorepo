38df0aec15d883b975f3ea168adaf488b4c6bbbf
Matthew Russell <mattrussell36@gmail.com>
2023-01-16 14:11:11 -0800

    chore: suppress fetch logs to help with debugging

--- libs/cypress/src/lib/commands/create-market.ts
+++ libs/cypress/src/lib/commands/create-market.ts
@@ -12,6 +12,19 @@ declare global {
     }
   }
 }
+
+const originalLog = Cypress.log;
+// @ts-ignore fuck with log to help with debug
+Cypress.log = function (options, ...rest) {
+  // @ts-ignore fuck with log to help with debug
+  const isRequest = ['fetch', 'xhr'].includes(options.displayName);
+  if (isRequest) {
+    return;
+  }
+
+  return originalLog(options, ...rest);
+};
+
 export const addCreateMarket = () => {
   Cypress.Commands.add('createMarket', () => {
     const config = {
