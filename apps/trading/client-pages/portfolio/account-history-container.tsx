4ad9d57708977e463096362505a22e32f9472f8a
Madalina Raicu <madalina@raygroup.uk>
2023-01-16 14:29:50 +0000

    fix(#2556): asset dropdown list fix

--- apps/trading/client-pages/portfolio/account-history-container.tsx
+++ apps/trading/client-pages/portfolio/account-history-container.tsx
@@ -9,7 +9,6 @@ import compact from 'lodash/compact';
 import type { ChangeEvent } from 'react';
 import { useMemo, useState } from 'react';
 import type { AccountHistoryQuery } from './__generated__/AccountHistory';
-import { useAccountsWithBalanceQuery } from './__generated__/AccountHistory';
 import { useAccountHistoryQuery } from './__generated__/AccountHistory';
 import * as Schema from '@vegaprotocol/types';
 import type { AssetFieldsFragment } from '@vegaprotocol/assets';
@@ -26,6 +25,8 @@ import {
 import { AccountTypeMapping } from '@vegaprotocol/types';
 import { PriceChart } from 'pennant';
 import 'pennant/dist/style.css';
+import { accountsOnlyDataProvider } from '@vegaprotocol/accounts';
+import { useDataProvider } from '@vegaprotocol/react-helpers';
 
 const DateRange = {
   RANGE_1D: '1D',
@@ -95,17 +96,15 @@ const AccountHistoryManager = ({
     [pubKey]
   );
 
-  const assetsWithBalanceHistory = useAccountsWithBalanceQuery({
+  const { data: assetsWithBalanceHistory } = useDataProvider({
+    dataProvider: accountsOnlyDataProvider,
     variables: variablesForOneTimeQuery,
     skip: !pubKey,
   });
 
   const assetsWithBalance = useMemo(
-    () =>
-      assetsWithBalanceHistory.data?.balanceChanges.edges.map(
-        (e) => e?.node.assetId
-      ) || [],
-    [assetsWithBalanceHistory.data?.balanceChanges.edges]
+    () => assetsWithBalanceHistory?.map((e) => e?.asset?.id) || [],
+    [assetsWithBalanceHistory]
   );
 
   const assets = useMemo(
