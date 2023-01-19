b65db3cdad8e2cc4e36ce409bb92be4637193bd4
Dexter Edwards <dexter.edwards93@gmail.com>
2023-01-19 15:59:49 +0000

    chore: remove console lite (#2670)

--- apps/console-lite/src/app/lib/vega-connectors.ts
+++
@@ -1,9 +0,0 @@
-import { RestConnector, JsonRpcConnector } from '@vegaprotocol/wallet';
-
-export const rest = new RestConnector();
-export const jsonRpc = new JsonRpcConnector();
-
-export const Connectors = {
-  rest,
-  jsonRpc,
-};
