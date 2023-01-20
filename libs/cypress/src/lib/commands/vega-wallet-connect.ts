bd8ee6f1414b202e054e90674dcc9107112fcd02
Rado <szpiechrados@gmail.com>
2023-01-20 19:48:08 +0100

    chore: change connect wallet mock t to use auth header

--- libs/cypress/src/lib/commands/vega-wallet-connect.ts
+++ libs/cypress/src/lib/commands/vega-wallet-connect.ts
@@ -1,4 +1,4 @@
-import { aliasWalletQuery } from '../mock-rest';
+import { aliasWalletConnectQuery } from '../mock-rest';
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -15,12 +15,8 @@ declare global {
 }
 
 export const mockConnectWallet = () => {
-  const data = {
-    token: Cypress.env('VEGA_WALLET_API_TOKEN'),
-  };
-  console.log('mockConnectWallet', data);
   cy.mockWallet((req) => {
-    aliasWalletQuery(req, 'client.connect_wallet', data);
+    aliasWalletConnectQuery(req, Cypress.env('VEGA_WALLET_API_TOKEN'));
   });
 };
 
