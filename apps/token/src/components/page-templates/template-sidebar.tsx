d714e8b8c2541904b62648e5d8c9380b6b61a739
Dexter <dexter.edwards93@gmail.com>
2023-01-24 11:09:42 +0000

    feat: splash loader more central

--- apps/token/src/components/page-templates/template-sidebar.tsx
+++ apps/token/src/components/page-templates/template-sidebar.tsx
@@ -21,7 +21,7 @@ export function TemplateSidebar({ children, sidebar }: TemplateSidebarProps) {
       ) : (
         <div />
       )}
-      <div className="w-full border-b border-neutral-700 lg:grid lg:grid-rows-[min-content_1fr] lg:grid-cols-[1fr_450px]">
+      <div className="w-full border-b border-neutral-700 lg:grid lg:grid-rows-[1fr] lg:grid-cols-[1fr_450px]">
         <main className="col-start-1 p-4">{children}</main>
         <aside className="col-start-2 row-start-1 row-span-2 hidden lg:block p-4 bg-banner bg-contain border-l border-neutral-700">
           {sidebar.map((Component, i) => (
