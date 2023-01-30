9b36071477c8f8c91ef81cfa0fa2b24a7ffc600c
Dexter <dexter.edwards93@gmail.com>
2023-01-30 08:50:26 +0000

    chore: staking facilities leaving

--- apps/token/src/routes/staking/staking-form.tsx
+++ apps/token/src/routes/staking/staking-form.tsx
@@ -157,6 +157,7 @@ export const StakingForm = ({
     return [
       'efbdf943443bd7595e83b0d7e88f37b7932d487d1b94aab3d004997273bb43fc',
       '5db9794f44c85b4b259907a00c8ea2383ad688dfef6ffb72c8743b6ae3eaefd4',
+      'f3022974212780ea1196af08fd2e8a9c0d784d0be8e97637bd5e763ac4c219bd',
     ].includes(nodeId);
   }, [nodeId]);
 
