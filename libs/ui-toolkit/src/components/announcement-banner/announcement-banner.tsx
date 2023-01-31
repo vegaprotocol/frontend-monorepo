b16840fd8beca2e5de709f9e4168838c56feae66
sam-keen <samuel.kleinmann@gmail.com>
2023-01-31 15:44:03 +0000

    feat(common): moved image asset into static site

--- libs/ui-toolkit/src/components/announcement-banner/announcement-banner.tsx
+++ libs/ui-toolkit/src/components/announcement-banner/announcement-banner.tsx
@@ -1,5 +1,4 @@
 import classnames from 'classnames';
-import background from './banner-bg.jpg';
 import type { ReactNode } from 'react';
 
 export interface BannerProps {
@@ -7,14 +6,10 @@ export interface BannerProps {
 }
 
 export const AnnouncementBanner = ({ children }: BannerProps) => {
-  const bannerClasses = classnames('bg-cover bg-center bg-no-repeat', 'p-4');
-
-  return (
-    <div
-      style={{ backgroundImage: `url(${background})` }}
-      className={bannerClasses}
-    >
-      {children}
-    </div>
+  const bannerClasses = classnames(
+    "bg-[url('https://static.vega.xyz/assets/img/banner-bg.jpg')] bg-cover bg-center bg-no-repeat",
+    'p-4'
   );
+
+  return <div className={bannerClasses}>{children}</div>;
 };
