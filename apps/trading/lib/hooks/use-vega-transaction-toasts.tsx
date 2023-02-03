90958e290d628f31ad2723f9f88ae7b0db450a48
Madalina Raicu <madalina@raygroup.uk>
2023-02-03 21:56:50 +0000

    fix(trading): when order rejected change tx toast intent

--- apps/trading/lib/hooks/use-vega-transaction-toasts.tsx
+++ apps/trading/lib/hooks/use-vega-transaction-toasts.tsx
@@ -50,6 +50,13 @@ const intentMap: { [s in VegaTxStatus]: Intent } = {
   Complete: Intent.Success,
 };
 
+const getIntent = (tx: VegaStoredTxState) => {
+  if (tx.order?.rejectionReason) {
+    return Intent.Danger;
+  }
+  return intentMap[tx.status];
+};
+
 const isClosePositionTransaction = (tx: VegaStoredTxState) => {
   if (isBatchMarketInstructionsTransaction(tx.body)) {
     const amendments =
@@ -463,6 +470,26 @@ const VegaTxCompleteToastsContent = ({ tx }: VegaTxToastContentProps) => {
     );
   }
 
+  if (isOrderSubmissionTransaction(tx.body) && tx.order?.rejectionReason) {
+    return (
+      <div>
+        <h3 className="font-bold">{t('Order rejected')}</h3>
+        <p>{t('Your order was rejected.')}</p>
+        {tx.txHash && (
+          <p className="break-all">
+            <ExternalLink
+              href={explorerLink(EXPLORER_TX.replace(':hash', tx.txHash))}
+              rel="noreferrer"
+            >
+              {t('View in block explorer')}
+            </ExternalLink>
+          </p>
+        )}
+        <VegaTransactionDetails tx={tx} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <h3 className="font-bold">{t('Confirmed')}</h3>
@@ -547,7 +574,7 @@ export const useVegaTransactionToasts = () => {
       }
       return {
         id: `vega-${tx.id}`,
-        intent: intentMap[tx.status],
+        intent: getIntent(tx),
         onClose: () => dismissVegaTransaction(tx.id),
         loader: tx.status === VegaTxStatus.Pending,
         content,
