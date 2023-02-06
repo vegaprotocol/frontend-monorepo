09b43ba71f25fccf1b434b5e63e16353ac585ec3
Edd <edd@vega.xyz>
2023-02-06 18:06:38 +0000

    feat(explorer): change party to pubkey

--- apps/explorer/src/app/routes/parties/id/components/party-id-error.tsx
+++ apps/explorer/src/app/routes/parties/id/components/party-id-error.tsx
@@ -14,7 +14,7 @@ type PartyIdErrorProps = {
 const PartyIdError = ({ id, error }: PartyIdErrorProps) => {
   const end = isValidPartyId(id)
     ? t('No accounts or transactions found for: ')
-    : 'Invalid party id: ';
+    : 'Invalid pubkey: ';
   return (
     <section>
       <p>
