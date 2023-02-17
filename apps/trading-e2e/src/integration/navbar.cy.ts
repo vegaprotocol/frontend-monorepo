2a8094ed6d3e8d73f4560d907199e69290886641
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-02-17 09:16:41 +0100

    test: added wait for market

--- apps/trading-e2e/src/integration/navbar.cy.ts
+++ apps/trading-e2e/src/integration/navbar.cy.ts
@@ -23,7 +23,9 @@ describe('Desktop view', { tags: '@smoke' }, () => {
             cy.wrap(element)
               .get('span.absolute.md\\:h-1.w-full')
               .should('exist');
-            cy.location('hash').should('equal', hashes[index]);
+            cy.location('hash').should('equal', hashes[index], {
+              timeout: 300,
+            });
           });
       });
     });
@@ -68,7 +70,9 @@ describe('Mobile view', { tags: '@smoke' }, () => {
       cy.getByTestId('button-menu-drawer').click();
       cy.getByTestId('menu-drawer').within((el) => {
         cy.wrap(el).getByTestId('Trading').click();
-        cy.location('hash').should('equal', '#/markets/market-1');
+        cy.location('hash').should('equal', '#/markets/market-1', {
+          timeout: 300,
+        });
       });
     });
     it('Portfolio should be correctly rendered', () => {
