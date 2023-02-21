6c72fff23c390118fd7ab32ca71a37af5f0286e0
Edd <edd@vega.xyz>
2023-02-21 17:32:24 +0000

    feat(explorer): basic proposal overview

---
+++ apps/explorer/src/app/components/txs/details/proposal/summary.tsx
@@ -0,0 +1,66 @@
+import { t } from '@vegaprotocol/react-helpers';
+import type { ProposalTerms } from '../tx-proposal';
+import { useState } from 'react';
+import type { components } from '../../../../../types/explorer';
+import { JsonViewerDialog } from '../../../dialogs/json-viewer-dialog';
+import ProposalLink from '../../../links/proposal-link/proposal-link';
+
+type Rationale = components['schemas']['vegaProposalRationale'];
+
+type ProposalTermsDialog = {
+  open: boolean;
+  title: string;
+  content: unknown;
+};
+
+interface ProposalSummaryProps {
+  id: string;
+  rationale?: Rationale;
+  terms?: ProposalTerms;
+}
+
+/**
+ * Effectively a 'preview' for what the proposal is about, and a link to the
+ * Token site for full breakdown of votes
+ */
+export const ProposalSummary = ({
+  id,
+  rationale,
+  terms,
+}: ProposalSummaryProps) => {
+  const [dialog, setDialog] = useState<ProposalTermsDialog>({
+    open: false,
+    title: '',
+    content: null,
+  });
+
+  const openDialog = () => {
+    if (!terms) return;
+    setDialog({
+      open: true,
+      title: rationale?.title || t('Proposal details'),
+      content: terms ? terms : {},
+    });
+  };
+
+  return (
+    <div className="w-auto max-w-lg border-2 border-solid border-vega-light-100 dark:border-vega-dark-200 p-5">
+      {rationale?.title && <h1 className="text-xl pb-3">{rationale.title}</h1>}
+      {rationale?.description && (
+        <p className="pt-2">{rationale.description}</p>
+      )}
+      <p className="pt-5">
+        <button className="underline max-md:hidden mr-5" onClick={openDialog}>
+          {t('View terms')}
+        </button>{' '}
+        <ProposalLink id={id} text={t('Full details')} />
+      </p>
+      <JsonViewerDialog
+        open={dialog.open}
+        onChange={(isOpen) => setDialog({ ...dialog, open: isOpen })}
+        title={dialog.title}
+        content={dialog.content}
+      />
+    </div>
+  );
+};
--- apps/explorer/src/app/components/txs/details/tx-proposal.tsx
+++ apps/explorer/src/app/components/txs/details/tx-proposal.tsx
@@ -1,19 +1,60 @@
 import { t } from '@vegaprotocol/react-helpers';
 import type { BlockExplorerTransactionResult } from '../../../routes/types/block-explorer-response';
 import type { TendermintBlocksResponse } from '../../../routes/blocks/tendermint-blocks-response';
-import { TxDetailsShared } from './shared/tx-details-shared';
+import { sharedHeaderProps, TxDetailsShared } from './shared/tx-details-shared';
 import { TableCell, TableRow, TableWithTbody } from '../../table';
 import ProposalLink from '../../links/proposal-link/proposal-link';
 import type { components } from '../../../../types/explorer';
-import Hash from '../../links/hash';
 import { txSignatureToDeterministicId } from '../lib/deterministic-ids';
+import has from 'lodash/has';
+import { ProposalSummary } from './proposal/summary';
 
-type Proposal = components['schemas']['v1ProposalSubmission'];
+export type Proposal = components['schemas']['v1ProposalSubmission'];
+export type ProposalTerms = components['schemas']['vegaProposalTerms'];
 
 interface TxProposalProps {
   txData: BlockExplorerTransactionResult | undefined;
   pubKey: string | undefined;
   blockData: TendermintBlocksResponse | undefined;
+}
+
+/**
+ * Detects if a given proposal requires a signature bundle to be emitted by the validators for
+ * the proposal to be completed after it is accepted by the governance vote
+ *
+ * @param t The details of the proposal
+ * @returns boolean True if a signature bundle is required. Used to fetch a signature bundle
+ */
+export function proposalRequiresSignatureBundle(terms: ProposalTerms): boolean {
+  return !!['newAsset', 'updateAsset'].filter((requiredIfExists) =>
+    has(terms, requiredIfExists)
+  );
+}
+
+/**
+ * Given a proposal, returns a nice text label for the proposal type
+ *
+ * @param terms The details of the proposal
+ * @returns string Proposal type
+ */
+export function proposalTypeLabel(terms?: ProposalTerms): string {
+  if (has(terms, 'newAsset')) {
+    return t('New asset proposal');
+  } else if (has(terms, 'updateAsset')) {
+    return t('Update asset proposal');
+  } else if (has(terms, 'newMarket')) {
+    return t('New market proposal');
+  } else if (has(terms, 'updateMarket')) {
+    return t('Update market proposal');
+  } else if (has(terms, 'updateNetworkParameter')) {
+    return t('Update network parameter');
+  } else if (has(terms, 'newFreeform')) {
+    return t('Freeform proposal');
+  }
+
+  // The list above contains all currently known types. This will be triggered if a new
+  // unrecognised proposal type is added.
+  return t('Governance proposal');
 }
 
 /**
@@ -34,14 +75,30 @@ export const TxProposal = ({ txData, pubKey, blockData }: TxProposalProps) => {
   }
 
   return (
-    <TableWithTbody className="mb-8" allowWrap={true}>
-      <TxDetailsShared txData={txData} pubKey={pubKey} blockData={blockData} />
-      <TableRow modifier="bordered">
-        <TableCell>{t('Proposal details')}</TableCell>
-        <TableCell>
-          <ProposalLink id={deterministicId} />
-        </TableCell>
-      </TableRow>
-    </TableWithTbody>
+    <>
+      <TableWithTbody className="mb-8" allowWrap={true}>
+        <TableRow modifier="bordered">
+          <TableCell {...sharedHeaderProps}>{t('Type')}</TableCell>
+          <TableCell>{proposalTypeLabel(proposal.terms)}</TableCell>
+        </TableRow>
+        {/* TODO: Disable type row */}
+        <TxDetailsShared
+          txData={txData}
+          pubKey={pubKey}
+          blockData={blockData}
+        />
+        <TableRow modifier="bordered">
+          <TableCell>{t('Proposal details')}</TableCell>
+          <TableCell>
+            <ProposalLink id={deterministicId} />
+          </TableCell>
+        </TableRow>
+      </TableWithTbody>
+      <ProposalSummary
+        id={deterministicId}
+        rationale={proposal.rationale}
+        terms={proposal?.terms}
+      />
+    </>
   );
 };
