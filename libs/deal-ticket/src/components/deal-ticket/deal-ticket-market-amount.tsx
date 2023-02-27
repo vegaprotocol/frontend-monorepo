39f5efa84f47cb9915f4858e81fabb6d636303ba
Madalina Raicu <madalina@raygroup.uk>
2023-02-27 17:19:57 +0000

    fix(trading): fix margin gap to mb-2 in deal ticket

--- libs/deal-ticket/src/components/deal-ticket/deal-ticket-market-amount.tsx
+++ libs/deal-ticket/src/components/deal-ticket/deal-ticket-market-amount.tsx
@@ -29,7 +29,7 @@ export const DealTicketMarketAmount = ({
     : undefined;
 
   return (
-    <div className="mb-4">
+    <div className="mb-2">
       <div className="flex items-end gap-4 mb-2">
         <div className="flex-1 text-sm">{t('Size')}</div>
         <div />
