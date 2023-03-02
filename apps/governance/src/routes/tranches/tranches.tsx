79b0868ff7589ccca6388135519c841b713c0042
Sam Keen <samuel@vegaprotocol.io>
2023-03-02 14:35:46 +0000

    feat(governance): remove graph from tranches page (#3049)

--- apps/governance/src/routes/tranches/tranches.tsx
+++ apps/governance/src/routes/tranches/tranches.tsx
@@ -1,15 +1,12 @@
 import { useWeb3React } from '@web3-react/core';
-import React from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
-import { SubHeading } from '../../components/heading';
-import { TrancheItem } from '../redemption/tranche-item';
-import { TrancheLabel } from './tranche-label';
-import { VestingChart } from './vesting-chart';
 import { ButtonLink } from '@vegaprotocol/ui-toolkit';
 import { useEthereumConfig } from '@vegaprotocol/web3';
-import type { Tranche } from '../../lib/tranches/tranches-store';
+import { TrancheItem } from '../redemption/tranche-item';
+import { TrancheLabel } from './tranche-label';
 import { useTranches } from '../../lib/tranches/tranches-store';
+import type { Tranche } from '../../lib/tranches/tranches-store';
 
 const trancheMinimum = 10;
 
@@ -18,7 +15,7 @@ const shouldShowTranche = (t: Tranche) =>
 
 export const Tranches = () => {
   const tranches = useTranches((state) => state.tranches);
-  const [showAll, setShowAll] = React.useState<boolean>(false);
+  const [showAll, setShowAll] = useState<boolean>(false);
   const { t } = useTranslation();
   const { chainId } = useWeb3React();
   const { config } = useEthereumConfig();
@@ -30,10 +27,6 @@ export const Tranches = () => {
 
   return (
     <section>
-      <SubHeading title={t('chartTitle')} />
-      <p>{t('chartAbove')}</p>
-      <VestingChart />
-      <p>{t('chartBelow')}</p>
       {tranches?.length ? (
         <ul role="list">
           {(showAll ? tranches : filteredTranches).map((tranche) => {
@@ -55,6 +48,7 @@ export const Tranches = () => {
       ) : (
         <p>{t('No tranches')}</p>
       )}
+
       <section className="text-center mt-4">
         <ButtonLink onClick={() => setShowAll(!showAll)}>
           {showAll
