b34c82f830c60efcf5b1eb347d583c77520a737d
maciek <maciek@vegaprotocol.io>
2023-03-06 23:21:39 +0100

    chore: make footer lower buttons floating over table rows - fix lint error

--- libs/withdraws/src/lib/withdrawals-table.tsx
+++ libs/withdraws/src/lib/withdrawals-table.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from 'react';
+import { useRef } from 'react';
 import type { AgGridReact } from 'ag-grid-react';
 import { AgGridColumn } from 'ag-grid-react';
 import {
