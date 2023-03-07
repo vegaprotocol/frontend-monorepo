243d2d46f8df2ed35c3a0f181b85855d51eb8fae
ciaran- <ciaran@softbrew.io>
2023-03-07 14:47:42 +0000

    refactor: use ui-toolkit tooltip for health bar

--- apps/liquidity-provision-dashboard/src/app/components/health-bar/health-bar.tsx
+++ apps/liquidity-provision-dashboard/src/app/components/health-bar/health-bar.tsx
@@ -3,111 +3,123 @@ import type * as Schema from '@vegaprotocol/types';
 import { addDecimalsFormatNumber } from '@vegaprotocol/utils';
 import { t } from '@vegaprotocol/i18n';
 import { BigNumber } from 'bignumber.js';
-import type { ReactNode } from 'react';
+import { Tooltip } from '@vegaprotocol/ui-toolkit';
 
 import { getColorForStatus } from '../../lib/utils';
 
 import { Indicator } from '../indicator';
 
 const Remainder = () => (
-  <div className="bg-greys-light-200 h-[inherit] relative flex-1"></div>
+  <div className="bg-greys-light-200 h-[inherit] relative flex-1" />
 );
 
-const COPY_CLASS =
-  'text-sm font-medium whitespace-nowrap text-white font-alpha calt';
-
-const Tooltip = ({
-  children,
-  isExpanded,
-}: {
-  children: ReactNode;
-  isExpanded: boolean;
-}) => {
-  return (
-    <div
-      className={classNames(
-        'absolute top-0 left-1/2 -translate-x-2/4 -translate-y-[80%] p-2 z-10 bg-greys-light-400 group-hover:flex rounded',
-        {
-          flex: isExpanded,
-          hidden: !isExpanded,
-        }
-      )}
-    >
-      {children}
-    </div>
-  );
-};
-
 const Target = ({
+  target,
   targetPercent,
+  decimals,
   isLarge,
-  children,
 }: {
   targetPercent: number;
   isLarge: boolean;
-  children: ReactNode;
+  target: string;
+  decimals: number;
 }) => {
   return (
-    <div
-      className={classNames(
-        'absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-1/2 px-1.5 group'
-      )}
-      style={{ left: `${targetPercent}%` }}
+    <Tooltip
+      description={
+        <>
+          <div className="mt-1.5 inline-flex">
+            <Indicator />
+          </div>
+          <span>
+            {t('Target stake')} {addDecimalsFormatNumber(target, decimals)}
+          </span>
+        </>
+      }
     >
       <div
         className={classNames(
-          'health-target w-0.5 bg-black group-hover:scale-x-150 group-hover:scale-y-108',
-          {
-            'h-6': !isLarge,
-            'h-12': isLarge,
-          }
+          'absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-1/2 px-1.5 group'
         )}
-      ></div>
-      {children}
-    </div>
+        style={{ left: `${targetPercent}%` }}
+      >
+        <div
+          className={classNames(
+            'health-target w-0.5 bg-black group-hover:scale-x-150 group-hover:scale-y-108',
+            {
+              'h-6': !isLarge,
+              'h-12': isLarge,
+            }
+          )}
+        />
+      </div>
+    </Tooltip>
   );
 };
 
 const Level = ({
-  children,
   commitmentAmount,
   total,
   backgroundColor,
   opacity,
+  status,
+  fee,
+  prevLevel,
+  decimals,
 }: {
-  children: ReactNode;
   commitmentAmount: number;
   total: number;
   backgroundColor: string;
   opacity: number;
+  status: Schema.MarketTradingMode;
+  fee: string;
+  prevLevel: number;
+  decimals: number;
 }) => {
   const width = new BigNumber(commitmentAmount)
     .div(total)
     .multipliedBy(100)
     .toNumber();
 
-  return (
-    <div
-      className={classNames(`relative h-[inherit] w-full group min-w-[1px]`)}
-      style={{
-        width: `${width}%`,
-      }}
-    >
-      <div
-        className="relative w-full h-[inherit] group-hover:scale-y-150"
-        style={{
-          opacity,
-          backgroundColor,
-        }}
-      ></div>
+  const tooltipContent = (
+    <>
+      <div className="mt-1.5 inline-flex">
+        <Indicator status={status} opacity={opacity} />
+      </div>
+      <span>
+        {fee}% {t('Fee')}
+      </span>
+      <div className="flex  flex-col">
+        <span>
+          {prevLevel ? addDecimalsFormatNumber(prevLevel, decimals) : '0'} -{' '}
+          {addDecimalsFormatNumber(commitmentAmount, decimals)}
+        </span>
+      </div>
+    </>
+  );
 
-      {children}
-    </div>
+  return (
+    <Tooltip description={tooltipContent}>
+      <div
+        className={classNames(`relative h-[inherit] w-full group min-w-[1px]`)}
+        style={{
+          width: `${width}%`,
+        }}
+      >
+        <div
+          className="relative w-full h-[inherit] group-hover:scale-y-150"
+          style={{
+            opacity,
+            backgroundColor,
+          }}
+        />
+      </div>
+    </Tooltip>
   );
 };
 
 const Full = () => (
-  <div className="bg-transparent w-full h-[inherit] absolute bottom-0 left-0"></div>
+  <div className="bg-transparent w-full h-[inherit] absolute bottom-0 left-0" />
 );
 
 interface Levels {
@@ -121,13 +133,11 @@ export const HealthBar = ({
   decimals,
   levels,
   size = 'small',
-  isExpanded = false,
 }: {
   status: Schema.MarketTradingMode;
   target: string;
   decimals: number;
   levels: Levels[];
-  isExpanded?: boolean;
   size?: 'small' | 'large';
 }) => {
   const targetNumber = parseInt(target, 10);
@@ -162,6 +172,7 @@ export const HealthBar = ({
 
           <div className="health-bars h-[inherit] flex w-full gap-0.5">
             {levels.map((p, index) => {
+              console.log('level: ', p);
               const { commitmentAmount, fee } = p;
               const prevLevel = levels[index - 1]?.commitmentAmount;
               const opacity = 1 - 0.2 * index;
@@ -171,24 +182,11 @@ export const HealthBar = ({
                   total={total}
                   backgroundColor={backgroundColor}
                   opacity={opacity}
-                >
-                  <Tooltip isExpanded={isExpanded}>
-                    <div className="mt-1.5 inline-flex">
-                      <Indicator status={status} opacity={opacity} />
-                    </div>
-                    <div className="flex  flex-col">
-                      <span className={COPY_CLASS}>
-                        {fee}% {t('Fee')}
-                      </span>
-                      <span className={classNames(COPY_CLASS, 'opacity-60')}>
-                        {prevLevel
-                          ? addDecimalsFormatNumber(prevLevel, decimals)
-                          : '0'}{' '}
-                        - {addDecimalsFormatNumber(commitmentAmount, decimals)}
-                      </span>
-                    </div>
-                  </Tooltip>
-                </Level>
+                  status={status}
+                  fee={fee}
+                  prevLevel={prevLevel}
+                  decimals={decimals}
+                />
               );
             })}
             {(total !== committedNumber || levels.length === 0) && (
@@ -196,17 +194,12 @@ export const HealthBar = ({
             )}
           </div>
         </div>
-
-        <Target targetPercent={targetPercent} isLarge={isLarge}>
-          <Tooltip isExpanded={isExpanded}>
-            <div className="mt-1.5 inline-flex">
-              <Indicator />
-            </div>
-            <span className={COPY_CLASS}>
-              {t('Target stake')} {addDecimalsFormatNumber(target, decimals)}
-            </span>
-          </Tooltip>
-        </Target>
+        <Target
+          targetPercent={targetPercent}
+          isLarge={isLarge}
+          target={target}
+          decimals={decimals}
+        />
       </div>
     </div>
   );
