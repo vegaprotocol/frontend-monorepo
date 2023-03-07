a2feff77dc510d98f5f699d90bfe1d336f5189ca
Matthew Russell <mattrussell36@gmail.com>
2023-03-06 16:07:48 -0800

    chore: clean up order store tests by mocking zustand correctly

--- libs/orders/src/lib/order-hooks/use-order-store.spec.ts
+++ libs/orders/src/lib/order-hooks/use-order-store.spec.ts
@@ -4,8 +4,11 @@ import {
   useOrder,
   useOrderStore,
 } from './use-order-store';
-import { act, cleanup, renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { OrderType } from '@vegaprotocol/types';
+import { t } from '@vegaprotocol/i18n';
+
+jest.mock('zustand');
 
 describe('useOrderStore', () => {
   const setup = () => {
@@ -13,19 +16,15 @@ describe('useOrderStore', () => {
   };
 
   afterEach(() => {
-    useOrderStore.setState({ orders: {} });
     localStorage.clear();
   });
 
-  it('has a empty default state', () => {
+  it('has a empty default state', async () => {
     const { result } = setup();
     expect(result.current).toEqual({
       orders: {},
       update: expect.any(Function),
     });
-
-    // https://github.com/pmndrs/zustand/issues/363
-    cleanup();
   });
 
   it('can update', () => {
@@ -52,9 +51,6 @@ describe('useOrderStore', () => {
       },
       version: 0,
     });
-
-    // https://github.com/pmndrs/zustand/issues/363
-    cleanup();
   });
 
   it('can update without persisting', () => {
@@ -79,9 +75,6 @@ describe('useOrderStore', () => {
       },
       version: 0,
     });
-
-    // https://github.com/pmndrs/zustand/issues/363
-    cleanup();
   });
 });
 
@@ -91,11 +84,10 @@ describe('useOrder', () => {
   };
 
   afterEach(() => {
-    useOrderStore.setState({ orders: {} });
     localStorage.clear();
   });
 
-  it('creates a new order if it doesnt exist', () => {
+  it('creates a new order if it doesnt exist which is only persisted after editing', () => {
     const marketId = 'market-id';
     const expectedOrder = {
       ...getDefaultOrder(marketId),
@@ -103,8 +95,28 @@ describe('useOrder', () => {
     };
     const { result } = setup(marketId);
     expect(result.current).toEqual([expectedOrder, expect.any(Function)]);
+  });
 
-    // https://github.com/pmndrs/zustand/issues/363
-    cleanup();
+  it('only persists an order if edited', () => {
+    const marketId = 'market-id';
+    const expectedOrder = {
+      ...getDefaultOrder(marketId),
+      persist: false,
+    };
+    const { result } = setup(marketId);
+    expect(result.current[0]).toMatchObject({
+      price: expectedOrder.price,
+      persist: false,
+    });
+
+    const update = { price: '500' };
+    act(() => {
+      result.current[1](update);
+    });
+
+    expect(result.current[0]).toMatchObject({
+      ...update,
+      persist: true,
+    });
   });
 });
