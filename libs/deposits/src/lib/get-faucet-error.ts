de76df95d4c7a8534cb2f2daa6725474896c7863
Matthew Russell <mattrussell36@gmail.com>
2023-03-08 15:16:38 -0800

    chore: update faucet feedback i18n

---
+++ libs/deposits/src/lib/get-faucet-error.ts
@@ -0,0 +1,25 @@
+import { t } from '@vegaprotocol/i18n';
+import type { TxError } from '@vegaprotocol/web3';
+
+export const getFaucetError = (error: TxError | null, symbol: string) => {
+  const reasonMap: {
+    [reason: string]: string;
+  } = {
+    'faucet not enabled': t(
+      'The %s faucet is not available at this time',
+      symbol
+    ),
+    'must wait faucetCallLimit between faucet calls': t(
+      'You have exceeded the maximum number of faucet attempts',
+      symbol
+    ),
+    'user rejected transaction': t(
+      'The faucet transaction was rejected by the connected Ethereum wallet'
+    ),
+  };
+  // render a customized failure message from the map above or fallback
+  // to a non generic error message
+  return error && 'reason' in error && reasonMap[error.reason]
+    ? reasonMap[error.reason]
+    : t('Faucet of %s failed', symbol);
+};
