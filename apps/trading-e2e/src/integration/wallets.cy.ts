2725bef159c7b8cbe03cb57e3fd45ca3cc976263
daro-maj <119658839+daro-maj@users.noreply.github.com>
2023-03-09 14:11:46 +0100

    test(trading): increase coverage for 0002 wcon (#3137)

--- apps/trading-e2e/src/integration/wallets.cy.ts
+++ apps/trading-e2e/src/integration/wallets.cy.ts
@@ -17,6 +17,13 @@ describe('connect hosted wallet', { tags: '@smoke' }, () => {
   });
 
   it('can connect', () => {
+    // 0002-WCON-002
+    // 0002-WCON-003
+    // 0002-WCON-039
+    // 0002-WCON-017
+    // 0002-WCON-018
+    // 0002-WCON-019
+
     // Mock authentication
     cy.intercept('POST', 'https://wallet.testnet.vega.xyz/api/v1/auth/token', {
       body: {
@@ -41,6 +48,9 @@ describe('connect hosted wallet', { tags: '@smoke' }, () => {
       },
     });
     cy.getByTestId(connectVegaBtn).click();
+    cy.contains(
+      'Choose wallet app to connect, or to change port or server URL enter a custom wallet location first'
+    );
     cy.contains('Connect Vega wallet');
     cy.contains('Hosted Fairground wallet');
 
@@ -51,9 +61,13 @@ describe('connect hosted wallet', { tags: '@smoke' }, () => {
     cy.getByTestId(form).find('#passphrase').click().type('pass');
     cy.getByTestId('rest-connector-form').find('button[type=submit]').click();
     cy.getByTestId(manageVegaBtn).should('exist');
+    cy.getByTestId('manage-vega-wallet').click();
+    cy.getByTestId('keypair-list').should('exist');
   });
 
   it('doesnt connect with invalid credentials', () => {
+    // 0002-WCON-020
+
     // Mock incorrect username/password
     cy.intercept('POST', 'https://wallet.testnet.vega.xyz/api/v1/auth/token', {
       body: {
@@ -99,6 +113,10 @@ describe('connect vega wallet', { tags: '@smoke' }, () => {
   });
 
   it('can connect', () => {
+    // 0002-WCON-002
+    // 0002-WCON-005
+    // 0002-WCON-007
+
     mockConnectWallet();
     cy.getByTestId(connectVegaBtn).click();
     cy.getByTestId('connectors-list')
@@ -110,16 +128,40 @@ describe('connect vega wallet', { tags: '@smoke' }, () => {
   });
 
   it('can change selected public key and disconnect', () => {
+    // 0002-WCON-022
+    // 0002-WCON-023
+    // 0002-WCON-025
+    // 0002-WCON-026
+    // 0002-WCON-021
+    // 0002-WCON-027
+    // 0002-WCON-030
+    // 0002-WCON-029
+    // 0002-WCON-008
+    // 0002-WCON-035
+    // 0002-WCON-014
+    // 0002-WCON-010
+
+    mockConnectWallet();
     const key2 = Cypress.env('VEGA_PUBLIC_KEY2');
     const truncatedKey2 = Cypress.env('TRUNCATED_VEGA_PUBLIC_KEY2');
     cy.connectVegaWallet();
     cy.getByTestId('manage-vega-wallet').click();
     cy.getByTestId('keypair-list').should('exist');
     cy.getByTestId(`key-${key2}`).should('contain.text', truncatedKey2);
+    cy.getByTestId(`key-${key2}`)
+      .find('[data-testid="copy-vega-public-key"]')
+      .should('be.visible');
     cy.get(`[data-testid="key-${key2}"] > .mr-2`).click();
+    cy.getByTestId('keypair-list')
+      .find('[data-state="checked"]')
+      .should('be.visible');
     cy.getByTestId('disconnect').click();
     cy.getByTestId('connect-vega-wallet').should('exist');
     cy.getByTestId('manage-vega-wallet').should('not.exist');
+    cy.getByTestId('connect-vega-wallet').click();
+    cy.contains(
+      'Choose wallet app to connect, or to change port or server URL enter a custom wallet location first'
+    );
   });
 });
 
