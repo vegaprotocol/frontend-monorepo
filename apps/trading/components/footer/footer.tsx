f62140d848fb309281054ac183a17d6e21503346
maciek <maciek@vegaprotocol.io>
2023-03-14 15:59:14 +0100

    chore: switch to .toml files network conf - fix test errs

--- apps/trading/components/footer/footer.tsx
+++ apps/trading/components/footer/footer.tsx
@@ -1,69 +1,51 @@
 import { useEnvironment, useNodeHealth } from '@vegaprotocol/environment';
-import { useNavigatorOnline } from '@vegaprotocol/react-helpers';
 import { t } from '@vegaprotocol/i18n';
-import { Indicator, Intent } from '@vegaprotocol/ui-toolkit';
+import type { Intent } from '@vegaprotocol/ui-toolkit';
+import { Indicator } from '@vegaprotocol/ui-toolkit';
 import classNames from 'classnames';
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
 import { useGlobalStore } from '../../stores';
+import { useCallback } from 'react';
 
 export const Footer = () => {
-  const { VEGA_URL } = useEnvironment();
-  const setNodeSwitcher = useGlobalStore(
-    (store) => (open: boolean) => store.update({ nodeSwitcherDialog: open })
-  );
-  const { blockDiff, datanodeBlockHeight, datanodeVegaTime } = useNodeHealth();
-  const blockUpdateMsLatency = datanodeVegaTime
-    ? Date.now() - datanodeVegaTime.valueOf()
-    : 0;
-
   return (
     <footer className="px-4 py-1 text-xs border-t border-default text-vega-light-300 dark:text-vega-dark-300 lg:fixed bottom-0 left-0 border-r bg-white dark:bg-black">
       {/* Pull left to align with top nav, due to button padding */}
       <div className="-ml-2">
-        {VEGA_URL && (
-          <NodeHealth
-            url={VEGA_URL}
-            blockHeight={datanodeBlockHeight}
-            blockDiff={blockDiff}
-            blockUpdateMsLatency={blockUpdateMsLatency}
-            onClick={() => setNodeSwitcher(true)}
-          />
-        )}
+        <NodeHealth />
       </div>
     </footer>
   );
 };
-interface NodeHealthProps {
-  url: string;
-  blockHeight: number | undefined;
-  blockDiff: number | null;
-  onClick: () => void;
-  blockUpdateMsLatency: number;
-}
 
-export const NodeHealth = ({
-  url,
-  blockHeight,
-  blockDiff,
-  onClick,
-  blockUpdateMsLatency,
-}: NodeHealthProps) => {
-  return (
+export const NodeHealth = () => {
+  const { VEGA_URL } = useEnvironment();
+  console.log('useGlobalStore', useGlobalStore);
+  const setNodeSwitcher = useGlobalStore(
+    (store) => (open: boolean) => store.update({ nodeSwitcherDialog: open })
+  );
+
+  console.log('setNodeSwitcher', setNodeSwitcher);
+
+  const { datanodeBlockHeight, text, intent } = useNodeHealth();
+
+  const onClick = useCallback(() => {
+    setNodeSwitcher(true);
+  }, [setNodeSwitcher]);
+  console.log('VEGA_URL', VEGA_URL);
+  return VEGA_URL ? (
     <FooterButton onClick={onClick} data-testid="node-health">
       <FooterButtonPart>
-        <HealthIndicator
-          blockDiff={blockDiff}
-          blockUpdateMsLatency={blockUpdateMsLatency}
-        />
+        <HealthIndicator text={text} intent={intent} />
       </FooterButtonPart>
       <FooterButtonPart>
-        <NodeUrl url={url} />
+        <NodeUrl url={VEGA_URL} />
       </FooterButtonPart>
       <FooterButtonPart>
-        <span title={t('Block height')}>{blockHeight}</span>
+        <span title={t('Block height')}>{datanodeBlockHeight}</span>
       </FooterButtonPart>
     </FooterButton>
-  );
+  ) : null;
 };
 
 interface NodeUrlProps {
@@ -78,21 +60,18 @@ export const NodeUrl = ({ url }: NodeUrlProps) => {
 };
 
 interface HealthIndicatorProps {
-  blockDiff: number | null;
-  blockUpdateMsLatency: number;
+  text: string;
+  intent: Intent;
 }
 
 // How many blocks behind the most advanced block that is
 // deemed acceptable for "Good" status
-const BLOCK_THRESHOLD = 3;
+/*const BLOCK_THRESHOLD = 3;
 const ERROR_LATENCY = 20000;
-const WARNING_LATENCY = 10000;
+const WARNING_LATENCY = 10000;*/
 
-export const HealthIndicator = ({
-  blockDiff,
-  blockUpdateMsLatency,
-}: HealthIndicatorProps) => {
-  const online = useNavigatorOnline();
+export const HealthIndicator = ({ text, intent }: HealthIndicatorProps) => {
+  /*const online = useNavigatorOnline();
 
   let intent = Intent.Success;
   let text = 'Operational';
@@ -119,7 +98,7 @@ export const HealthIndicator = ({
       (blockUpdateMsLatency / 1000).toFixed(2),
     ]);
     intent = Intent.Warning;
-  }
+  }*/
 
   return (
     <span title={t('Node health')}>
