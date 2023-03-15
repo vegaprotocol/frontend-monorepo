66a85cce2aede32200f46862a56bcc3cde3ef060
Art <artur@vegaprotocol.io>
2023-03-15 09:21:12 +0100

    chore(explorer): market info shared panels (#3192)

--- libs/market-info/src/components/market-info/index.ts
+++ libs/market-info/src/components/market-info/index.ts
@@ -3,3 +3,4 @@ export * from './info-market';
 export * from './tooltip-mapping';
 export * from './__generated__/MarketInfo';
 export * from './market-info-data-provider';
+export * from './market-info-panels';
