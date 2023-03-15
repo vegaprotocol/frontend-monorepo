da7c0b84f749e42bbb09fae1cd438b7afe9c38bf
Maciek <maciek@vegaprotocol.io>
2023-03-15 12:49:31 +0100

    chore(trading): display latency in block height progressing as warn or error (#3169)

--- apps/trading/components/footer/footer.spec.tsx
+++ apps/trading/components/footer/footer.spec.tsx
@@ -1,20 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { NodeHealth, NodeUrl, HealthIndicator } from './footer';
+import { MockedProvider } from '@apollo/client/testing';
+import { Intent } from '@vegaprotocol/ui-toolkit';
+
+jest.mock('@vegaprotocol/environment', () => ({
+  ...jest.requireActual('@vegaprotocol/environment'),
+  useEnvironment: jest
+    .fn()
+    .mockImplementation(() => ({ VEGA_URL: 'https://vega-url.wtf' })),
+}));
+
+const mockSetNodeSwitcher = jest.fn();
+jest.mock('../../stores', () => ({
+  ...jest.requireActual('../../stores'),
+  useGlobalStore: () => mockSetNodeSwitcher,
+}));
 
 describe('NodeHealth', () => {
   it('controls the node switcher dialog', async () => {
-    const mockOnClick = jest.fn();
-    render(
-      <NodeHealth
-        onClick={mockOnClick}
-        url={'https://api.n99.somenetwork.vega.xyz'}
-        blockHeight={100}
-        blockDiff={0}
-      />
-    );
+    render(<NodeHealth />, { wrapper: MockedProvider });
+    await waitFor(() => {
+      expect(screen.getByRole('button')).toBeInTheDocument();
+    });
     await userEvent.click(screen.getByRole('button'));
-    expect(mockOnClick).toHaveBeenCalled();
+    expect(mockSetNodeSwitcher).toHaveBeenCalled();
   });
 });
 
@@ -31,14 +41,22 @@ describe('NodeUrl', () => {
 
 describe('HealthIndicator', () => {
   const cases = [
-    { diff: 0, classname: 'bg-vega-green-550', text: 'Operational' },
-    { diff: 5, classname: 'bg-warning', text: '5 Blocks behind' },
-    { diff: null, classname: 'bg-danger', text: 'Non operational' },
+    {
+      intent: Intent.Success,
+      text: 'Operational',
+      classname: 'bg-vega-green-550',
+    },
+    {
+      intent: Intent.Warning,
+      text: '5 Blocks behind',
+      classname: 'bg-warning',
+    },
+    { intent: Intent.Danger, text: 'Non operational', classname: 'bg-danger' },
   ];
   it.each(cases)(
     'renders correct text and indicator color for $diff block difference',
     (elem) => {
-      render(<HealthIndicator blockDiff={elem.diff} />);
+      render(<HealthIndicator text={elem.text} intent={elem.intent} />);
       expect(screen.getByTestId('indicator')).toHaveClass(elem.classname);
       expect(screen.getByText(elem.text)).toBeInTheDocument();
     }
