19a9e17a9c3e6fda04961e4fc201bd0a05209ffe
Madalina Raicu <madalina@raygroup.uk>
2023-04-04 17:27:39 +0100

    fix: add resize observer to test

--- libs/accounts/src/setup-tests.ts
+++ libs/accounts/src/setup-tests.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom';
+import ResizeObserver from 'resize-observer-polyfill';
+import { defaultFallbackInView } from 'react-intersection-observer';
+
+defaultFallbackInView(true);
+global.ResizeObserver = ResizeObserver;
