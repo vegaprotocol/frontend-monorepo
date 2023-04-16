82b667bd1e1877bdaeb6aa57437b1d97bd724ecb
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-04-16 22:51:03 +0200

    test: mocha reporter for e2e tests

--- apps/trading-e2e/cypress.config.js
+++ apps/trading-e2e/cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: '../../node_modules/cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       require('@cypress/grep/src/plugin')(config);
       return config;
     },
