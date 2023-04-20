45213020335436267cfa1c15268977c03c1cea34
sam-keen <samuel.kleinmann@gmail.com>
2023-04-20 09:40:30 +0100

    fix(2567): simplified code and provided connector of type 'unknown' for e2e tests

--- apps/governance/src/app.tsx
+++ apps/governance/src/app.tsx
@@ -114,7 +114,7 @@ const Web3Container = ({
     return [<EthWallet />, <VegaWallet />];
   }, []);
 
-  if (connectors.length === 0 && !('Cypress' in window)) {
+  if (connectors.length === 0) {
     // Prevent loading when the connectors are not initialized
     return <SplashLoader />;
   }
--- apps/governance/src/lib/web3-connectors.ts
+++ apps/governance/src/lib/web3-connectors.ts
@@ -1,42 +1,4 @@
 import { ethers } from 'ethers';
-import type { Web3ReactHooks } from '@web3-react/core';
-import { initializeConnector } from '@web3-react/core';
-import { MetaMask } from '@web3-react/metamask';
-import { WalletConnect } from '@web3-react/walletconnect';
-import type { Connector } from '@web3-react/types';
-import { ENV } from '../config/env';
-import { UrlConnector } from '@vegaprotocol/web3';
-
-const [metamask, metamaskHooks] = initializeConnector<MetaMask>(
-  (actions) => new MetaMask(actions)
-);
-
-const [urlConnector, urlHooks] = initializeConnector<UrlConnector>(
-  (actions) =>
-    new UrlConnector(actions, ENV.localProviderUrl, ENV.ethWalletMnemonic)
-);
-
 export const createDefaultProvider = (providerUrl: string, chainId: number) => {
   return new ethers.providers.JsonRpcProvider(providerUrl, chainId);
-};
-
-export const createConnectors = (providerUrl: string, chainId: number) => {
-  if (isNaN(chainId)) {
-    throw new Error('Invalid Ethereum chain ID for environment');
-  }
-  const [walletconnect, walletconnectHooks] =
-    initializeConnector<WalletConnect>(
-      (actions) =>
-        new WalletConnect(actions, {
-          rpc: {
-            [chainId]: providerUrl,
-          },
-        }),
-      [chainId]
-    );
-  return [
-    ENV.urlConnect ? [urlConnector, urlHooks] : null,
-    [metamask, metamaskHooks],
-    [walletconnect, walletconnectHooks],
-  ].filter(Boolean) as [Connector, Web3ReactHooks][];
 };
