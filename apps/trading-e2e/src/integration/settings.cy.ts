8fe139374534722cf6459ad4aed1afec56c5343d
maciek <maciek@vegaprotocol.io>
2023-04-25 10:49:39 +0200

    feat: make sentry as opt in - fix lints and test after refactor

--- apps/trading-e2e/src/integration/settings.cy.ts
+++ apps/trading-e2e/src/integration/settings.cy.ts
@@ -4,7 +4,9 @@ describe('Settings page', { tags: '@smoke' }, () => {
       cy.mockTradingPage();
       cy.mockSubscription();
       cy.visit('/');
-      cy.getByTestId('dialog-close').click({ multiple: true });
+      cy.get('[role=dialog]').within(() => {
+        cy.getByTestId('dialog-close').click();
+      });
       cy.get('[aria-label="cog icon"]').click();
     });
   });
