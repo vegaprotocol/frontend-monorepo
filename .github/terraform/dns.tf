ae0ed4171b15ead81fb90585136e48c1c7beb687
fryd <fkondej@gmail.com>
2023-05-16 15:06:41 +0200

    fix: change record type to URL and value to full redirect url

--- .github/terraform/dns.tf
+++ .github/terraform/dns.tf
@@ -2,6 +2,6 @@ resource "dnsimple_zone_record" "vega_trading" {
   zone_name = "vega.trading"
   name      = ""
   value     = file("${path.module}/mainnet-dns.txt")
-  type      = "CNAME"
+  type      = "URL"
   ttl       = 60
 }
