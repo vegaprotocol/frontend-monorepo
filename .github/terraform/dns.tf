82fdad708ff6e0fa871946bb0c269b69d3d6f0f3
fryd <fkondej@gmail.com>
2023-05-16 15:36:38 +0200

    feat: update script to get cid and store it in txt file, modify tf to read the cid only and generate proper url from it

--- .github/terraform/dns.tf
+++ .github/terraform/dns.tf
@@ -1,7 +1,7 @@
 resource "dnsimple_zone_record" "vega_trading" {
   zone_name = "vega.trading"
   name      = ""
-  value     = file("${path.module}/mainnet-dns.txt")
+  value     = format("https://%s.ipfs.cf-ipfs.com/", file("${path.module}/console-cid.txt"))
   type      = "URL"
   ttl       = 60
 }
