b56f84c334e1e7d9db77eb724e220f845720728f
fryd <fryderyk@vega.xyz>
2023-05-17 13:09:37 +0200

    chore: remove terraform, and use DNSimple API instead

--- .github/terraform/dns.tf
+++
@@ -1,7 +0,0 @@
-resource "dnsimple_zone_record" "vega_trading" {
-  zone_name = "vega.trading"
-  name      = ""
-  value     = format("https://%s.ipfs.cf-ipfs.com/", file("${path.module}/console-cid.txt"))
-  type      = "URL"
-  ttl       = 60
-}
