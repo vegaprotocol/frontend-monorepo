443560989b4b410482d2df5e01ce3d6c8843f80f
Matthew Russell <mattrussell36@gmail.com>
2023-05-21 18:51:45 -0700

    chore: adjust nav styles to match design

--- libs/announcements/src/lib/announcements.tsx
+++ libs/announcements/src/lib/announcements.tsx
@@ -5,6 +5,8 @@ import {
   Icon,
   AnnouncementBanner as Banner,
   ExternalLink,
+  VegaIcon,
+  VegaIconNames,
 } from '@vegaprotocol/ui-toolkit';
 
 export type AnnouncementBannerProps = {
@@ -75,22 +77,22 @@ export const AnnouncementBanner = ({
 
   return (
     <Banner className="relative px-10">
-      <button
-        className="absolute left-0 top-0 w-10 h-full flex items-center justify-center"
-        data-testid="app-announcement-close"
-        onClick={() => setVisible(false)}
-      >
-        <Icon name="cross" className="w-6 h-6" ariaLabel="dismiss" />
-      </button>
       <div
         data-testid="app-announcement"
-        className="relative font-alpha flex gap-2 justify-center calt uppercase text-center text-lg text-white"
+        className="relative font-alpha flex gap-2 justify-center text-center text-lg text-white"
       >
-        <span className="pr-4">{data.text}</span>
+        <span>{data.text}</span>{' '}
         {data.urlText && data.url && (
           <ExternalLink href={data.url}>{data.urlText}</ExternalLink>
         )}
       </div>
+      <button
+        className="absolute right-0 top-0 p-4 w-10 h-full flex items-center justify-center"
+        data-testid="app-announcement-close"
+        onClick={() => setVisible(false)}
+      >
+        <VegaIcon name={VegaIconNames.CROSS} size={24} />
+      </button>
     </Banner>
   );
 };
