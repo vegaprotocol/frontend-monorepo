3ed615c36e0f01413e24b2164cde9086094b408d
m.ray <16125548+MadalinaRaicu@users.noreply.github.com>
2023-05-25 13:07:17 +0300

    fix(oracles): word breakdown oracles (#3946)

--- libs/markets/src/lib/components/oracle-full-profile/oracle-full-profile.tsx
+++ libs/markets/src/lib/components/oracle-full-profile/oracle-full-profile.tsx
@@ -104,7 +104,7 @@ export const OracleFullProfile = ({
         <div className="mb-2">{message}</div>
         <div className="mb-2">
           <ReactMarkdown
-            className="react-markdown-container"
+            className="react-markdown-container [word-break:break-word]"
             skipHtml={true}
             disallowedElements={['img']}
             linkTarget="_blank"
