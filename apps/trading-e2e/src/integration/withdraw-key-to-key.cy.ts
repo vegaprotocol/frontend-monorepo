3300268defd179f5a59fce12397b7ef1f533fd10
Ben Wallace <ben@vega.xyz>
2023-06-01 10:44:45 +0100

    test: key to key transfer tests

--- apps/trading-e2e/src/integration/withdraw-key-to-key.cy.ts
+++ apps/trading-e2e/src/integration/withdraw-key-to-key.cy.ts
@@ -135,7 +135,81 @@ describe('transfer fees', { tags: '@regression', testIsolation: true }, () => {
       .should('contain.text', '1.00');
   });
 });
-describe('transfer form validation',{ tags: '@regression', testIsolation: true }, () => {
+describe(
+  'transfer form validation',
+  { tags: '@regression', testIsolation: true },
+  () => {
+    beforeEach(() => {
+      cy.mockWeb3Provider();
+      cy.mockTradingPage();
+      cy.mockSubscription();
+      cy.setVegaWallet();
+
+      cy.visit('/#/portfolio');
+      cy.getByTestId(manageVegaWallet).click();
+      cy.getByTestId(walletTransfer).click();
+
+      cy.wait('@Accounts');
+      cy.wait('@Assets');
+    });
+
+    it('transfer Text', () => {
+      // 1003-TRAN-003
+      cy.getByTestId(dialogTransferText)
+        .should('exist')
+        .get(keyID)
+        .invoke('text')
+        .should('match', /[\w.]{6}…[\w.]{6}/);
+    });
+
+    it('invalid vega key validation', function () {
+      //1003-TRAN-013
+      //1003-TRAN-004
+      cy.getByTestId(transferForm).should('be.visible');
+      cy.contains('Enter manually').click();
+      cy.getByTestId(transferForm).find(toAddressField).type('asd');
+      cy.getByTestId(transferForm).find(submitTransferBtn).click();
+      cy.getByTestId(formFieldError).should('contain.text', 'Invalid Vega key');
+      cy.contains('label', 'Vega key').should('be.visible');
+      cy.contains('label', 'Asset').should('be.visible');
+      cy.contains('label', 'Amount').should('be.visible');
+    });
+
+    it('empty fields', () => {
+      // 1003-TRAN-012
+      cy.getByTestId(transferForm).find(submitTransferBtn).click();
+      cy.getByTestId(formFieldError).should('contain.text', 'Required');
+      cy.getByTestId(formFieldError).should('have.length', 3);
+    });
+
+    it('min amount', () => {
+      // 1002-WITH-010
+      // 1003-TRAN-014
+      selectAsset(ASSET_SEPOLIA_TBTC);
+      cy.get(amountField).clear().type('0');
+      cy.getByTestId(transferForm).find(submitTransferBtn).click();
+      cy.getByTestId(errorText).should(
+        'contain.text',
+        'Value is below minimum'
+      );
+    });
+
+    it('max amount', () => {
+      // 1003-TRAN-002
+      // 1003-TRAN-011
+      // 1003-TRAN-002
+      selectAsset(ASSET_EURO); // Will be above maximum because the vega wallet doesn't have any collateral
+      cy.get(amountField).clear().type('1001', { delay: 100 });
+      cy.getByTestId(transferForm).find(submitTransferBtn).click();
+      cy.getByTestId(errorText).should(
+        'contain.text',
+        'You cannot transfer more than your available collateral'
+      );
+      cy.getByTestId(closeDialog).click();
+    });
+  }
+);
+describe('withdraw actions', { tags: '@smoke', testIsolation: true }, () => {
   beforeEach(() => {
     cy.mockWeb3Provider();
     cy.mockTradingPage();
@@ -143,141 +217,70 @@ describe('transfer form validation',{ tags: '@regression', testIsolation: true }
     cy.setVegaWallet();
 
     cy.visit('/#/portfolio');
-    cy.getByTestId(manageVegaWallet).click();
-    cy.getByTestId(walletTransfer).click();
+    cy.getByTestId(collateralTab).click();
+    cy.getByTestId(openTransferDialog).click();
 
     cy.wait('@Accounts');
     cy.wait('@Assets');
+    cy.mockVegaWalletTransaction();
   });
 
-  it('transfer Text', () => {
-    // 1003-TRAN-003
-    cy.getByTestId(dialogTransferText)
-      .should('exist')
-      .get(keyID)
+  it('key to key transfers by select key', function () {
+    // 1003-TRAN-001
+    // 1003-TRAN-006
+    // 1003-TRAN-007
+    // 1003-TRAN-008
+    // 1003-TRAN-009
+    // 1003-TRAN-010
+    // 1003-TRAN-023
+    cy.getByTestId(transferForm).should('be.visible');
+    cy.getByTestId(transferForm).find(toAddressField).select(1);
+
+    cy.getByTestId(assetSelection).click();
+    cy.getByTestId(assetOption);
+    cy.getByTestId(assetBalance).should('not.be.empty');
+    cy.getByTestId(assetOption).should('have.length.gt', 4);
+
+    let optionText: string;
+    cy.getByTestId(assetOption)
+      .eq(2)
       .invoke('text')
-      .should('match', /[\w.]{6}…[\w.]{6}/);
+      .then((text: string) => {
+        optionText = text;
+        cy.getByTestId(assetOption).eq(2).click();
+        cy.getByTestId(assetSelection).should('have.text', optionText);
+      });
+
+    cy.getByTestId(transferForm)
+      .find(amountField)
+      .type('1', { delay: 100, force: true });
+
+    cy.getByTestId(transferForm).find(amountShortName).should('not.be.empty');
+
+    cy.getByTestId(transferForm).find(submitTransferBtn).click();
+    cy.getByTestId(toastContent).should(
+      'contain.text',
+      'Awaiting confirmation'
+    );
+    cy.getByTestId(toastCloseBtn).click();
   });
 
-  it('invalid vega key validation', function () {
-    //1003-TRAN-013
-    //1003-TRAN-004
+  it('key to key transfers by enter manual key', function () {
+    //1003-TRAN-005
     cy.getByTestId(transferForm).should('be.visible');
     cy.contains('Enter manually').click();
-    cy.getByTestId(transferForm).find(toAddressField).type('asd');
-    cy.getByTestId(transferForm).find(submitTransferBtn).click();
-    cy.getByTestId(formFieldError).should('contain.text', 'Invalid Vega key');
-    cy.contains('label', 'Vega key').should('be.visible');
-    cy.contains('label', 'Asset').should('be.visible');
-    cy.contains('label', 'Amount').should('be.visible');
-  });
-
-  it('empty fields', () => {
-    // 1003-TRAN-012
-    cy.getByTestId(transferForm).find(submitTransferBtn).click();
-    cy.getByTestId(formFieldError).should('contain.text', 'Required');
-    cy.getByTestId(formFieldError).should('have.length', 3);
-  });
-
-  it('min amount', () => {
-    // 1002-WITH-010
-    // 1003-TRAN-014
+    cy.getByTestId(transferForm)
+      .find(toAddressField)
+      .type('7f9cf07d3a9905b1a61a1069f7a758855da428bc0f4a97de87f48644bfc25535');
     selectAsset(ASSET_SEPOLIA_TBTC);
-    cy.get(amountField).clear().type('0');
+    cy.getByTestId(transferForm)
+      .find(amountField)
+      .type('1', { delay: 100, force: true });
     cy.getByTestId(transferForm).find(submitTransferBtn).click();
-    cy.getByTestId(errorText).should(
+    cy.getByTestId(toastContent).should(
       'contain.text',
-      'Value is below minimum'
+      'Awaiting confirmation'
     );
+    cy.getByTestId(toastCloseBtn).click();
   });
-
-  it('max amount', () => {
-    // 1003-TRAN-002
-    // 1003-TRAN-011
-    // 1003-TRAN-002
-    selectAsset(ASSET_EURO); // Will be above maximum because the vega wallet doesn't have any collateral
-    cy.get(amountField).clear().type('1001', { delay: 100 });
-    cy.getByTestId(transferForm).find(submitTransferBtn).click();
-    cy.getByTestId(errorText).should(
-      'contain.text',
-      'You cannot transfer more than your available collateral'
-    );
-    cy.getByTestId(closeDialog).click();
-  });
-}
-);
-describe('withdraw actions', { tags: '@smoke', testIsolation: true }, () => {
-beforeEach(() => {
-  cy.mockWeb3Provider();
-  cy.mockTradingPage();
-  cy.mockSubscription();
-  cy.setVegaWallet();
-
-  cy.visit('/#/portfolio');
-  cy.getByTestId(collateralTab).click();
-  cy.getByTestId(openTransferDialog).click();
-
-  cy.wait('@Accounts');
-  cy.wait('@Assets');
-  cy.mockVegaWalletTransaction();
-});
-
-it('key to key transfers by select key', function () {
-  // 1003-TRAN-001
-  // 1003-TRAN-006
-  // 1003-TRAN-007
-  // 1003-TRAN-008
-  // 1003-TRAN-009
-  // 1003-TRAN-010
-  // 1003-TRAN-023
-  cy.getByTestId(transferForm).should('be.visible');
-  cy.getByTestId(transferForm).find(toAddressField).select(1);
-
-  cy.getByTestId(assetSelection).click();
-  cy.getByTestId(assetOption);
-  cy.getByTestId(assetBalance).should('not.be.empty');
-  cy.getByTestId(assetOption).should('have.length.gt', 4);
-
-  let optionText: string;
-  cy.getByTestId(assetOption)
-    .eq(2)
-    .invoke('text')
-    .then((text: string) => {
-      optionText = text;
-      cy.getByTestId(assetOption).eq(2).click();
-      cy.getByTestId(assetSelection).should('have.text', optionText);
-    });
-
-  cy.getByTestId(transferForm)
-    .find(amountField)
-    .type('1', { delay: 100, force: true });
-
-  cy.getByTestId(transferForm).find(amountShortName).should('not.be.empty');
-
-  cy.getByTestId(transferForm).find(submitTransferBtn).click();
-  cy.getByTestId(toastContent).should(
-    'contain.text',
-    'Awaiting confirmation'
-  );
-  cy.getByTestId(toastCloseBtn).click();
-});
-
-it('key to key transfers by enter manual key', function () {
-  //1003-TRAN-005
-  cy.getByTestId(transferForm).should('be.visible');
-  cy.contains('Enter manually').click();
-  cy.getByTestId(transferForm)
-    .find(toAddressField)
-    .type('7f9cf07d3a9905b1a61a1069f7a758855da428bc0f4a97de87f48644bfc25535');
-  selectAsset(ASSET_SEPOLIA_TBTC);
-  cy.getByTestId(transferForm)
-    .find(amountField)
-    .type('1', { delay: 100, force: true });
-  cy.getByTestId(transferForm).find(submitTransferBtn).click();
-  cy.getByTestId(toastContent).should(
-    'contain.text',
-    'Awaiting confirmation'
-  );
-  cy.getByTestId(toastCloseBtn).click();
-});
 });
