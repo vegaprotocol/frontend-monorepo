bd8d131e2ff083a853242beefbee81997a25da0a
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-06-02 15:15:34 +0200

    test: 6001-MARK-find_markets e2e tests

--- apps/trading-e2e/src/integration/markets.cy.ts
+++ apps/trading-e2e/src/integration/markets.cy.ts
@@ -3,6 +3,9 @@ import { aliasGQLQuery, checkSorting } from '@vegaprotocol/cypress';
 import { marketsQuery } from '@vegaprotocol/mock';
 import { getDateTimeFormat } from '@vegaprotocol/utils';
 
+const rowSelector =
+  '[data-testid="tab-all-markets"] .ag-center-cols-container .ag-row';
+
 describe('markets table', { tags: '@smoke' }, () => {
   beforeEach(() => {
     cy.clearLocalStorage().then(() => {
@@ -16,147 +19,7 @@ describe('markets table', { tags: '@smoke' }, () => {
     });
   });
 
-  it('renders markets correctly', () => {
-    cy.wait('@Markets');
-    cy.wait('@MarketsData');
-    cy.get('[data-testid^="market-link-"]').should('not.be.empty');
-    cy.getByTestId('price').invoke('text').should('not.be.empty');
-    cy.getByTestId('settlement-asset').should('not.be.empty');
-    cy.getByTestId('price-change-percentage').should('not.be.empty');
-    cy.getByTestId('price-change').should('not.be.empty');
-  });
-
-  it('able to open and sort full market list - market page', () => {
-    const ExpectedSortedMarkets = [
-      'AAPL.MF21',
-      'BTCUSD.MF21',
-      'ETHBTC.QM21',
-      'SOLUSD',
-    ];
-    cy.url().should('eq', Cypress.config('baseUrl') + '/#/markets/all');
-    cy.contains('AAPL.MF21').should('be.visible');
-    cy.get('.ag-header-cell-label').contains('Market').click(); // sort by market name
-    for (let i = 0; i < ExpectedSortedMarkets.length; i++) {
-      cy.get(`[row-index=${i}]`)
-        .find('[col-id="tradableInstrument.instrument.code"]')
-        .should('have.text', ExpectedSortedMarkets[i]);
-    }
-  });
-
-  it('proposed markets tab should be rendered properly', () => {
-    cy.get('[data-testid="All markets"]').should(
-      'have.attr',
-      'data-state',
-      'active'
-    );
-    cy.get('[data-testid="Proposed markets"]').should(
-      'have.attr',
-      'data-state',
-      'inactive'
-    );
-    cy.get('[data-testid="Proposed markets"]').click();
-    cy.get('[data-testid="Proposed markets"]').should(
-      'have.attr',
-      'data-state',
-      'active'
-    );
-    cy.getByTestId('tab-proposed-markets').should('be.visible');
-    cy.get('.ag-body-viewport .ag-center-cols-container .ag-row').should(
-      'have.length',
-      10
-    );
-    cy.getByTestId('tab-proposed-markets')
-      .find('[data-testid="external-link"]')
-      .should('have.length', 11)
-      .last()
-      .should('have.text', 'Propose a new market')
-      .and(
-        'have.attr',
-        'href',
-        `${Cypress.env('VEGA_TOKEN_URL')}/proposals/propose/new-market`
-      );
-  });
-
-  it('proposed markets tab should be sorted properly', () => {
-    cy.get('[data-testid="Proposed markets"]').click();
-    const marketColDefault = [
-      'ETHUSD',
-      'LINKUSD',
-      'ETHUSD',
-      'ETHDAI.MF21',
-      'AAPL.MF21',
-      'BTCUSD.MF21',
-      'TSLA.QM21',
-      'AAVEDAI.MF21',
-      'ETHBTC.QM21',
-      'UNIDAI.MF21',
-    ];
-    const marketColAsc = [
-      'AAPL.MF21',
-      'AAVEDAI.MF21',
-      'BTCUSD.MF21',
-      'ETHBTC.QM21',
-      'ETHDAI.MF21',
-      'ETHUSD',
-      'ETHUSD',
-      'LINKUSD',
-      'TSLA.QM21',
-      'UNIDAI.MF21',
-    ];
-    const marketColDesc = [
-      'UNIDAI.MF21',
-      'TSLA.QM21',
-      'LINKUSD',
-      'ETHUSD',
-      'ETHUSD',
-      'ETHDAI.MF21',
-      'ETHBTC.QM21',
-      'BTCUSD.MF21',
-      'AAVEDAI.MF21',
-      'AAPL.MF21',
-    ];
-    checkSorting('market', marketColDefault, marketColAsc, marketColDesc);
-
-    const stateColDefault = [
-      'Open',
-      'Passed',
-      'Waiting for Node Vote',
-      'Open',
-      'Passed',
-      'Open',
-      'Passed',
-      'Open',
-      'Waiting for Node Vote',
-      'Open',
-    ];
-    const stateColAsc = [
-      'Open',
-      'Open',
-      'Open',
-      'Open',
-      'Open',
-      'Passed',
-      'Passed',
-      'Passed',
-      'Waiting for Node Vote',
-      'Waiting for Node Vote',
-    ];
-    const stateColDesc = [
-      'Waiting for Node Vote',
-      'Waiting for Node Vote',
-      'Passed',
-      'Passed',
-      'Passed',
-      'Open',
-      'Open',
-      'Open',
-      'Open',
-      'Open',
-    ];
-    checkSorting('state', stateColDefault, stateColAsc, stateColDesc);
-  });
-
-  it.skip('opening auction subsets should be properly displayed', () => {
+  it('opening auction subsets should be properly displayed', () => {
     cy.mockTradingPage(
       Schema.MarketState.STATE_ACTIVE,
       Schema.MarketTradingMode.TRADING_MODE_OPENING_AUCTION
