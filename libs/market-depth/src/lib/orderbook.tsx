1cec69d7c50b126ee15258e903280407006bc03c
maciek <maciek@vegaprotocol.io>
2023-06-05 15:46:33 +0200

    feat: orderbook enhancements - tweak some sizes

--- libs/market-depth/src/lib/orderbook.tsx
+++ libs/market-depth/src/lib/orderbook.tsx
@@ -23,8 +23,8 @@ interface OrderbookProps {
   asks: PriceLevelFieldsFragment[];
 }
 
-// 17px of row height plus 3.5px gap
-export const rowHeight = 20.5;
+// 17px of row height plus 3px gap
+export const rowHeight = 20;
 const MID_PRICE_HEIGHT = 40;
 
 const OrderbookTable = ({
@@ -48,7 +48,6 @@ const OrderbookTable = ({
         'text-right w-full flex flex-col',
         type === VolumeType.ask ? 'justify-end' : 'justify-start'
       )}
-      style={{ height: `calc(50% - ${MID_PRICE_HEIGHT / 2}px)` }}
     >
       <div className="grid auto-rows-[17px] gap-1">
         {rows.map((data, i) => (
@@ -94,91 +93,93 @@ export const Orderbook = ({
 
   /* eslint-disable jsx-a11y/no-static-element-interactions */
   return (
-    <ReactVirtualizedAutoSizer>
-      {({ width, height }) => {
-        const limit = Math.floor((height - 60) / 2 / rowHeight);
-        const askRows =
-          groupedAsks?.slice(
-            Math.max(
-              0,
-              Math.min(groupedAsks?.length || 0, groupedAsks?.length - limit)
-            )
-          ) ?? [];
-        const bidRows = groupedBids?.slice(0, Math.max(0, limit)) ?? [];
-        return (
-          <div
-            className="h-full w-full pl-1 text-xs grid grid-rows-[1fr_min-content] grid-cols-1"
-            style={{ width, height }}
-          >
-            <div
-              className="text-right w-full overflow-hidden"
-              data-testid="orderbook-grid-element"
-            >
-              {askRows.length || bidRows.length ? (
-                <>
-                  <OrderbookTable
-                    rows={askRows}
-                    type={VolumeType.ask}
-                    resolution={resolution}
-                    decimalPlaces={decimalPlaces}
-                    positionDecimalPlaces={positionDecimalPlaces}
-                    onClick={onClick}
-                  />
-                  <div
-                    className={`flex flex-shrink h-[${MID_PRICE_HEIGHT}px] items-center justify-center text-lg`}
-                  >
-                    <PriceCell
-                      value={Number(markPrice)}
-                      valueFormatted={addDecimalsFormatNumber(
-                        markPrice,
-                        decimalPlaces
-                      )}
-                      testId={`middle-mark-price-${markPrice}`}
+    <div className="h-full w-full pl-1 text-xs grid grid-rows-[1fr_min-content] grid-cols-1 overflow-hidden">
+      <div>
+        <ReactVirtualizedAutoSizer disableWidth>
+          {({ height }) => {
+            const limit = Math.floor((height - 60) / 2 / rowHeight);
+            const askRows =
+              groupedAsks?.slice(
+                Math.max(
+                  0,
+                  Math.min(
+                    groupedAsks?.length || 0,
+                    groupedAsks?.length - limit
+                  )
+                )
+              ) ?? [];
+            const bidRows = groupedBids?.slice(0, Math.max(0, limit)) ?? [];
+            return (
+              <div
+                className="text-right w-full overflow-hidden grid grid-rows-[1fr_min-content_1fr]"
+                data-testid="orderbook-grid-element"
+                style={{ height: height + 'px' }}
+              >
+                {askRows.length || bidRows.length ? (
+                  <>
+                    <OrderbookTable
+                      rows={askRows}
+                      type={VolumeType.ask}
+                      resolution={resolution}
+                      decimalPlaces={decimalPlaces}
+                      positionDecimalPlaces={positionDecimalPlaces}
+                      onClick={onClick}
                     />
+                    <div
+                      className={`flex flex-shrink h-[${MID_PRICE_HEIGHT}px] items-center justify-center text-lg`}
+                    >
+                      <PriceCell
+                        value={Number(markPrice)}
+                        valueFormatted={addDecimalsFormatNumber(
+                          markPrice,
+                          decimalPlaces
+                        )}
+                        testId={`middle-mark-price-${markPrice}`}
+                      />
+                    </div>
+                    <OrderbookTable
+                      rows={bidRows}
+                      type={VolumeType.bid}
+                      resolution={resolution}
+                      decimalPlaces={decimalPlaces}
+                      positionDecimalPlaces={positionDecimalPlaces}
+                      onClick={onClick}
+                    />
+                  </>
+                ) : (
+                  <div className="inset-0 absolute">
+                    <Splash>{t('No data')}</Splash>
                   </div>
-                  <OrderbookTable
-                    rows={bidRows}
-                    type={VolumeType.bid}
-                    resolution={resolution}
-                    decimalPlaces={decimalPlaces}
-                    positionDecimalPlaces={positionDecimalPlaces}
-                    onClick={onClick}
-                  />
-                </>
-              ) : (
-                <div className="inset-0 absolute">
-                  <Splash>{t('No data')}</Splash>
-                </div>
-              )}
-            </div>
-
-            <div className="relative bottom-0 grid grid-cols-4 grid-rows-1 gap-2 border-t border-default z-10 bg-white dark:bg-black w-full">
-              <div className="col-start-1">
-                <select
-                  onChange={(e) => {
-                    setResolution(Number(e.currentTarget.value));
-                  }}
-                  value={resolution}
-                  className="block bg-neutral-100 dark:bg-neutral-700 font-mono text-right w-full h-full"
-                  data-testid="resolution"
-                >
-                  {resolutions.map((r) => (
-                    <option key={r} value={r}>
-                      {formatNumberFixed(
-                        Math.log10(r) - decimalPlaces > 0
-                          ? Math.pow(10, Math.log10(r) - decimalPlaces)
-                          : 0,
-                        decimalPlaces - Math.log10(r)
-                      )}
-                    </option>
-                  ))}
-                </select>
+                )}
               </div>
-            </div>
-          </div>
-        );
-      }}
-    </ReactVirtualizedAutoSizer>
+            );
+          }}
+        </ReactVirtualizedAutoSizer>
+      </div>
+      <div className="relative bottom-0 grid grid-cols-4 grid-rows-1 gap-2 border-t border-default z-10 bg-white dark:bg-black w-full">
+        <div className="col-start-1">
+          <select
+            onChange={(e) => {
+              setResolution(Number(e.currentTarget.value));
+            }}
+            value={resolution}
+            className="block bg-neutral-100 dark:bg-neutral-700 font-mono text-right w-full h-full"
+            data-testid="resolution"
+          >
+            {resolutions.map((r) => (
+              <option key={r} value={r}>
+                {formatNumberFixed(
+                  Math.log10(r) - decimalPlaces > 0
+                    ? Math.pow(10, Math.log10(r) - decimalPlaces)
+                    : 0,
+                  decimalPlaces - Math.log10(r)
+                )}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
   );
   /* eslint-enable jsx-a11y/no-static-element-interactions */
 };
