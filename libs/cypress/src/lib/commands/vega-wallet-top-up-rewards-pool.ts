d6a32f5090419110511a6249fd7324233903fa7d
Joe Tsang <30622993+jtsang586@users.noreply.github.com>
2023-06-06 22:19:23 +0100

    chore(governance): fix flaky test failures (#4043)

--- libs/cypress/src/lib/commands/vega-wallet-top-up-rewards-pool.ts
+++ libs/cypress/src/lib/commands/vega-wallet-top-up-rewards-pool.ts
@@ -7,44 +7,54 @@ declare global {
   namespace Cypress {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Chainable<Subject> {
-      VegaWalletTopUpRewardsPool(
-        transferStartEpoch: number,
-        transferEndEpoch: number
-      ): void;
+      VegaWalletTopUpRewardsPool(): void;
     }
   }
 }
 
 export function addVegaWalletTopUpRewardsPool() {
-  Cypress.Commands.add(
-    'VegaWalletTopUpRewardsPool',
-    (transferStartEpoch, transferEndEpoch) => {
-      const vegaWalletUrl = Cypress.env('VEGA_WALLET_URL');
-      const token = Cypress.env('VEGA_WALLET_API_TOKEN');
-      const vegaPubKey = Cypress.env('VEGA_PUBLIC_KEY');
-      const assetAddress =
-        'b4f2726571fbe8e33b442dc92ed2d7f0d810e21835b7371a7915a365f07ccd9b';
+  Cypress.Commands.add('VegaWalletTopUpRewardsPool', () => {
+    let transferStartEpoch = 0;
+    let transferEndEpoch = 0;
+    const vegaWalletUrl = Cypress.env('VEGA_WALLET_URL');
+    const token = Cypress.env('VEGA_WALLET_API_TOKEN');
+    const vegaPubKey = Cypress.env('VEGA_PUBLIC_KEY');
+    const assetAddress =
+      'b4f2726571fbe8e33b442dc92ed2d7f0d810e21835b7371a7915a365f07ccd9b';
 
-      createWalletClient(vegaWalletUrl, token);
+    cy.getByTestId('epoch-countdown')
+      .within(() => {
+        cy.get('h3')
+          .invoke('text')
+          .then((epochText) => {
+            transferStartEpoch = Number(epochText.replace('Epoch', '')) + 5;
+            transferEndEpoch = transferStartEpoch + 100;
 
-      const transactionBody: TransferBody = {
-        transfer: {
-          fromAccountType: AccountType.ACCOUNT_TYPE_GENERAL,
-          toAccountType: AccountType.ACCOUNT_TYPE_GLOBAL_REWARD,
-          to: '0000000000000000000000000000000000000000000000000000000000000000',
-          asset: assetAddress,
-          amount: '1000000000000000000',
-          recurring: {
-            factor: '1',
-            startEpoch: transferStartEpoch,
-            endEpoch: transferEndEpoch,
+            console.log(transferStartEpoch);
+            console.log(transferEndEpoch);
+          });
+      })
+      .then(() => {
+        createWalletClient(vegaWalletUrl, token);
+
+        const transactionBody: TransferBody = {
+          transfer: {
+            fromAccountType: AccountType.ACCOUNT_TYPE_GENERAL,
+            toAccountType: AccountType.ACCOUNT_TYPE_GLOBAL_REWARD,
+            to: '0000000000000000000000000000000000000000000000000000000000000000',
+            asset: assetAddress,
+            amount: '1000000000000000000',
+            recurring: {
+              factor: '1',
+              startEpoch: transferStartEpoch,
+              endEpoch: transferEndEpoch,
+            },
           },
-        },
-      };
+        };
 
-      cy.highlight('Topping up rewards pool');
+        cy.highlight('Topping up rewards pool');
 
-      sendVegaTx(vegaPubKey, transactionBody);
-    }
-  );
+        sendVegaTx(vegaPubKey, transactionBody);
+      });
+  });
 }
