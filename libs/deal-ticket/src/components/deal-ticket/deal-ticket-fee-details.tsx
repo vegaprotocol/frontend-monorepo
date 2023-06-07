2ba0e9a1b2369aa7fd0ded2a82336905abefee6d
m.ray <16125548+MadalinaRaicu@users.noreply.github.com>
2023-06-07 13:49:50 +0300

    chore(trading): add quantum formatting to deal ticket (#4030)

--- libs/deal-ticket/src/components/deal-ticket/deal-ticket-fee-details.tsx
+++ libs/deal-ticket/src/components/deal-ticket/deal-ticket-fee-details.tsx
@@ -33,24 +33,35 @@ export const DealTicketFeeDetails = (props: FeeDetails) => {
   const details = getFeeDetailsValues(props);
   return (
     <div>
-      {details.map(({ label, value, labelDescription, symbol, indent }) => (
-        <div
-          key={typeof label === 'string' ? label : 'value-dropdown'}
-          className={classnames(
-            'text-xs mt-2 flex justify-between items-center gap-4 flex-wrap',
-            { 'ml-2': indent }
-          )}
-        >
-          <div>
-            <Tooltip description={labelDescription}>
-              <div>{label}</div>
+      {details.map(
+        ({
+          label,
+          value,
+          labelDescription,
+          symbol,
+          indent,
+          formattedValue,
+        }) => (
+          <div
+            key={typeof label === 'string' ? label : 'value-dropdown'}
+            className={classnames(
+              'text-xs mt-2 flex justify-between items-center gap-4 flex-wrap',
+              { 'ml-2': indent }
+            )}
+          >
+            <div>
+              <Tooltip description={labelDescription}>
+                <div>{label}</div>
+              </Tooltip>
+            </div>
+            <Tooltip description={`${value ?? '-'} ${symbol || ''}`}>
+              <div className="text-neutral-500 dark:text-neutral-300">{`${
+                formattedValue ?? '-'
+              } ${symbol || ''}`}</div>
             </Tooltip>
           </div>
-          <div className="text-neutral-500 dark:text-neutral-300">{`${
-            value ?? '-'
-          } ${symbol || ''}`}</div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 };
