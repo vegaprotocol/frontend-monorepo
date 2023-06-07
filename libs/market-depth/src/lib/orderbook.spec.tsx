43d3754c64977b66dcf431dce124b744e13c61af
Maciek <maciek@vegaprotocol.io>
2023-06-07 10:58:34 +0200

    feat(trading): 3945 orderbook enhancements (#4016)
    Co-authored-by: Bartłomiej Głownia <bglownia@gmail.com>
    Co-authored-by: Matthew Russell <mattrussell36@gmail.com>

--- libs/market-depth/src/lib/orderbook.spec.tsx
+++ libs/market-depth/src/lib/orderbook.spec.tsx
@@ -1,260 +1,89 @@
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import { generateMockData } from './orderbook-data';
-import { Orderbook, rowHeight } from './orderbook';
+import { generateMockData, VolumeType } from './orderbook-data';
+import { Orderbook } from './orderbook';
+import * as orderbookData from './orderbook-data';
+
+function mockOffsetSize(width: number, height: number) {
+  Object.defineProperty(HTMLElement.prototype, 'getBoundingClientRect', {
+    configurable: true,
+    value: () => ({ height, width }),
+  });
+  Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
+    configurable: true,
+    value: height,
+  });
+  Object.defineProperty(HTMLElement.prototype, 'offsetWidth', {
+    configurable: true,
+    value: width,
+  });
+}
 
 describe('Orderbook', () => {
   const params = {
     numberOfSellRows: 100,
     numberOfBuyRows: 100,
     step: 1,
-    midPrice: 122900,
+    midPrice: '122900',
     bestStaticBidPrice: 122905,
     bestStaticOfferPrice: 122895,
     decimalPlaces: 3,
-    overlap: 10,
-    indicativePrice: 122900,
-    indicativeVolume: 11,
+    overlap: 0,
     resolution: 1,
   };
-  const onResolutionChange = jest.fn();
   const decimalPlaces = 3;
-  it('should scroll to mid price on init', async () => {
-    window.innerHeight = 11 * rowHeight;
+
+  beforeEach(() => {
+    mockOffsetSize(800, 768);
+  });
+  it('markPrice should be in the middle', async () => {
     render(
       <Orderbook
         decimalPlaces={decimalPlaces}
         positionDecimalPlaces={0}
-        fillGaps
         {...generateMockData(params)}
-        onResolutionChange={onResolutionChange}
       />
     );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    expect(screen.getByTestId('scroll').scrollTop).toBe(91 * rowHeight);
-  });
-
-  it('should keep mid price row in the middle', async () => {
-    window.innerHeight = 11 * rowHeight;
-    const result = render(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData(params)}
-        onResolutionChange={onResolutionChange}
-      />
+    await waitFor(() =>
+      screen.getByTestId(`middle-mark-price-${params.midPrice}`)
     );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    expect(screen.getByTestId('scroll').scrollTop).toBe(91 * rowHeight);
-    result.rerender(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData({
-          ...params,
-          numberOfSellRows: params.numberOfSellRows - 1,
-        })}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    expect(result.getByTestId('scroll').scrollTop).toBe(90 * rowHeight);
-  });
-
-  it('should scroll to mid price when it will change', async () => {
-    window.innerHeight = 11 * rowHeight;
-    const result = render(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData(params)}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    expect(result.getByTestId('scroll').scrollTop).toBe(91 * rowHeight);
-    result.rerender(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData({
-          ...params,
-          bestStaticBidPrice: params.bestStaticBidPrice + 1,
-          bestStaticOfferPrice: params.bestStaticOfferPrice + 1,
-        })}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    expect(result.getByTestId('scroll').scrollTop).toBe(90 * rowHeight);
-  });
-
-  it('should keep price it the middle', async () => {
-    window.innerHeight = 11 * rowHeight;
-    const result = render(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData(params)}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    const scrollElement = result.getByTestId('scroll');
-    expect(scrollElement.scrollTop).toBe(91 * rowHeight);
-    scrollElement.scrollTop = 92 * rowHeight + 0.01;
-    fireEvent.scroll(scrollElement);
-    result.rerender(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData({
-          ...params,
-          numberOfSellRows: params.numberOfSellRows - 1,
-        })}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    expect(screen.getByTestId('scroll').scrollTop).toBe(91 * rowHeight + 0.01);
-  });
-
-  it('should get back to mid price on click', async () => {
-    window.innerHeight = 11 * rowHeight;
-    const result = render(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData(params)}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    const scrollElement = result.getByTestId('scroll');
-    expect(scrollElement.scrollTop).toBe(91 * rowHeight);
-    scrollElement.scrollTop = 1;
-    fireEvent.scroll(scrollElement);
-    expect(result.getByTestId('scroll').scrollTop).toBe(1);
-    const scrollToMidPriceButton = result.getByTestId('scroll-to-midprice');
-    fireEvent.click(scrollToMidPriceButton);
-    expect(screen.getByTestId('scroll').scrollTop).toBe(91 * rowHeight + 1);
-  });
-
-  it('should get back to mid price on resolution change', async () => {
-    window.innerHeight = 11 * rowHeight;
-    const result = render(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData(params)}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    await waitFor(() => screen.getByTestId(`bid-vol-${params.midPrice}`));
-    const scrollElement = screen.getByTestId('scroll');
-    expect(scrollElement.scrollTop).toBe(91 * rowHeight);
-    scrollElement.scrollTop = 1;
-    fireEvent.scroll(scrollElement);
-    expect(screen.getByTestId('scroll').scrollTop).toBe(1);
-    const resolutionSelect = screen.getByTestId(
-      'resolution'
-    ) as HTMLSelectElement;
-    fireEvent.change(resolutionSelect, { target: { value: '10' } });
-    expect(onResolutionChange.mock.calls.length).toBe(1);
-    expect(onResolutionChange.mock.calls[0][0]).toBe(10);
-    result.rerender(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData({
-          ...params,
-          resolution: 10,
-        })}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    expect(screen.getByTestId('scroll').scrollTop).toBe(6 * rowHeight);
+    expect(
+      screen.getByTestId(`middle-mark-price-${params.midPrice}`)
+    ).toHaveTextContent('122.90');
   });
 
   it('should format correctly the numbers on resolution change', async () => {
     const onClickSpy = jest.fn();
-    const result = render(
+    jest.spyOn(orderbookData, 'compactRows');
+    const mockedData = generateMockData(params);
+    render(
       <Orderbook
         decimalPlaces={decimalPlaces}
         positionDecimalPlaces={0}
         onClick={onClickSpy}
-        fillGaps
-        {...generateMockData(params)}
-        onResolutionChange={onResolutionChange}
+        {...mockedData}
       />
     );
     expect(
-      await screen.findByTestId(`bid-vol-${params.midPrice}`)
+      await screen.findByTestId(`middle-mark-price-${params.midPrice}`)
     ).toBeInTheDocument();
     // Before resolution change the price is 122.934
-    await fireEvent.click(await screen.getByTestId('price-122934'));
-    expect(onClickSpy).toBeCalledWith('122.934');
+    await fireEvent.click(await screen.getByTestId('price-122901'));
+    expect(onClickSpy).toBeCalledWith('122.901');
     const resolutionSelect = screen.getByTestId(
       'resolution'
     ) as HTMLSelectElement;
     await fireEvent.change(resolutionSelect, { target: { value: '10' } });
-    await result.rerender(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        onClick={onClickSpy}
-        fillGaps
-        {...generateMockData({
-          ...params,
-          resolution: 10,
-        })}
-        onResolutionChange={onResolutionChange}
-      />
+    expect(orderbookData.compactRows).toHaveBeenCalledWith(
+      mockedData.bids,
+      VolumeType.bid,
+      10
     );
-    await fireEvent.click(await screen.getByTestId('price-12299'));
-    // After resolution change the price is 122.99
-    expect(onResolutionChange.mock.calls[0][0]).toBe(10);
-    expect(onClickSpy).toBeCalledWith('122.99');
-  });
-
-  it('should have three or four columns', async () => {
-    window.innerHeight = 11 * rowHeight;
-    const { rerender } = render(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData({
-          ...params,
-          overlap: 0,
-        })}
-        onResolutionChange={onResolutionChange}
-      />
+    expect(orderbookData.compactRows).toHaveBeenCalledWith(
+      mockedData.asks,
+      VolumeType.ask,
+      10
     );
-    await waitFor(() => {
-      expect(screen.queryByText('Bid / Ask vol')).toBeInTheDocument();
-    });
-    rerender(
-      <Orderbook
-        decimalPlaces={decimalPlaces}
-        positionDecimalPlaces={0}
-        fillGaps
-        {...generateMockData(params)}
-        onResolutionChange={onResolutionChange}
-      />
-    );
-    await waitFor(() => {
-      expect(screen.getByText('Bid vol')).toBeInTheDocument();
-      expect(screen.getByText('Ask vol')).toBeInTheDocument();
-    });
-    await expect(screen.queryByText('Bid / Ask vol')).not.toBeInTheDocument();
+    await fireEvent.click(await screen.getByTestId('price-12294'));
+    expect(onClickSpy).toBeCalledWith('122.94');
   });
 });
