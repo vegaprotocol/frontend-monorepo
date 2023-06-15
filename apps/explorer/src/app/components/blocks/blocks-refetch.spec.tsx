5a90ebe12ee0d95b807bc48753b758b3bd5f036d
Edd <edd@vega.xyz>
2023-06-15 17:10:17 +0100

    feat(explorer): transaction list filtering

--- apps/explorer/src/app/components/blocks/blocks-refetch.spec.tsx
+++ apps/explorer/src/app/components/blocks/blocks-refetch.spec.tsx
@@ -1,29 +1,13 @@
 import WS from 'jest-websocket-mock';
-import useWebSocket from 'react-use-websocket';
-import {
-  render,
-  screen,
-  fireEvent,
-  act,
-  waitFor,
-} from '@testing-library/react';
-import { TendermintWebsocketContext } from '../../contexts/websocket/tendermint-websocket-context';
+import { render, screen } from '@testing-library/react';
 import { BlocksRefetch } from './blocks-refetch';
 
 const BlocksRefetchInWebsocketProvider = ({
   callback,
-  mocketLocation,
 }: {
   callback: () => null;
-  mocketLocation: string;
 }) => {
-  const contextShape = useWebSocket(mocketLocation);
-
-  return (
-    <TendermintWebsocketContext.Provider value={{ ...contextShape }}>
-      <BlocksRefetch refetch={callback} />
-    </TendermintWebsocketContext.Provider>
-  );
+  return <BlocksRefetch refetch={callback} />;
 };
 
 describe('Blocks refetch', () => {
@@ -32,111 +16,8 @@ describe('Blocks refetch', () => {
     const mocket = new WS(mocketLocation, { jsonProtocol: true });
     new WebSocket(mocketLocation);
 
-    render(
-      <BlocksRefetchInWebsocketProvider
-        callback={() => null}
-        mocketLocation={mocketLocation}
-      />
-    );
-    await mocket.connected;
-    expect(screen.getByTestId('new-blocks')).toHaveTextContent('new blocks');
+    render(<BlocksRefetchInWebsocketProvider callback={() => null} />);
     expect(screen.getByTestId('refresh')).toBeInTheDocument();
-    mocket.close();
-  });
-
-  it('should initiate callback when the button is clicked', async () => {
-    const mocketLocation = 'wss:localhost:3003';
-    const mocket = new WS(mocketLocation, { jsonProtocol: true });
-    new WebSocket(mocketLocation);
-
-    const callback = jest.fn();
-    render(
-      <BlocksRefetchInWebsocketProvider
-        callback={callback}
-        mocketLocation={mocketLocation}
-      />
-    );
-    await mocket.connected;
-    const button = screen.getByTestId('refresh');
-
-    act(() => {
-      fireEvent.click(button);
-    });
-
-    expect(callback.mock.calls.length).toEqual(1);
-    mocket.close();
-  });
-
-  it('should show new blocks as websocket is correctly updated', async () => {
-    const mocketLocation = 'wss:localhost:3004';
-    const mocket = new WS(mocketLocation, { jsonProtocol: true });
-    new WebSocket(mocketLocation);
-    render(
-      <BlocksRefetchInWebsocketProvider
-        callback={() => null}
-        mocketLocation={mocketLocation}
-      />
-    );
-    await mocket.connected;
-
-    // Ensuring we send an ID equal to the one the client subscribed with.
-    await waitFor(() => expect(mocket.messages.length).toEqual(1));
-    // @ts-ignore id on messages
-    const id = mocket.messages[0].id;
-
-    const newBlockMessage = {
-      id,
-      result: {
-        query: "tm.event = 'NewBlock'",
-      },
-    };
-
-    expect(screen.getByTestId('new-blocks')).toHaveTextContent('0 new blocks');
-
-    act(() => {
-      mocket.send(newBlockMessage);
-    });
-
-    expect(screen.getByTestId('new-blocks')).toHaveTextContent('1 new blocks');
-
-    act(() => {
-      mocket.send(newBlockMessage);
-    });
-
-    expect(screen.getByTestId('new-blocks')).toHaveTextContent('2 new blocks');
-    mocket.close();
-  });
-
-  it('will not show new blocks if websocket has wrong ID', async () => {
-    const mocketLocation = 'wss:localhost:3005';
-    const mocket = new WS(mocketLocation, { jsonProtocol: true });
-    new WebSocket(mocketLocation);
-
-    render(
-      <BlocksRefetchInWebsocketProvider
-        callback={() => null}
-        mocketLocation={mocketLocation}
-      />
-    );
-    await mocket.connected;
-
-    // Ensuring we send an ID equal to the one the client subscribed with.
-    await waitFor(() => expect(mocket.messages.length).toEqual(1));
-
-    const newBlockMessageBadId = {
-      id: 'blahblahblah',
-      result: {
-        query: "tm.event = 'NewBlock'",
-      },
-    };
-
-    expect(screen.getByTestId('new-blocks')).toHaveTextContent('0 new blocks');
-
-    act(() => {
-      mocket.send(newBlockMessageBadId);
-    });
-
-    expect(screen.getByTestId('new-blocks')).toHaveTextContent('0 new blocks');
     mocket.close();
   });
 });
