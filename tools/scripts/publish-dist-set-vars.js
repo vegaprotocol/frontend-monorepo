2acfc06c600d600ba72718c83bc19cd33ce6ca79
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2023-06-16 15:36:09 +0200

    feat(ci): format

--- tools/scripts/publish-dist-set-vars.js
+++ tools/scripts/publish-dist-set-vars.js
@@ -14,46 +14,40 @@ if (ref.contains('release/')) {
   // we take last part of release/* branch as environment name
   envName = argv['ref'].split('/').slice(-1);
   console.log(`env name from release/* ref is: ${envName}`);
-}
-else if(ref.endsWith('develop')) {
+} else if (ref.endsWith('develop')) {
   // normal apps are deployed from develop to stagnet1
   envName = 'stagnet1';
   // those apps are deployed from develop to mainnet
-  if(app == 'multisig-signer') {
+  if (app == 'multisig-signer') {
     envName = 'mainnet';
     bucketName = 'tools.vega.xyz';
-  }
-  else if(app == 'static') {
+  } else if (app == 'static') {
     envName = 'mainnet';
     bucketName = 'static.vega.xyz';
-  }
-  else if(app == 'ui-toolkit') {
+  } else if (app == 'ui-toolkit') {
     envName = 'mainnet';
     bucketName = 'ui.vega.rocks';
   }
-}
-else if(ref.endsWith('main')) {
+} else if (ref.endsWith('main')) {
   envName = 'mainnet';
 }
 
 // mainnet apps do not contain
 if (envName == 'mainnet') {
-  domain = 'vega.xyz'
-  if(!bucketName) {
+  domain = 'vega.xyz';
+  if (!bucketName) {
+    bucketName = `${app}.${domain}`;
+  }
+} else if (envName == 'testnet') {
+  domain = 'fairground.wtf';
+  if (!bucketName) {
     bucketName = `${app}.${domain}`;
   }
 }
-else if (envName == 'testnet') {
-  domain = 'fairground.wtf'
-  if(!bucketName) {
-    bucketName = `${app}.${domain}`;
-  }
-}
-if(!bucketName) {
+if (!bucketName) {
   bucketName = `${app}.${envName}.${domain}`;
 }
 
 // share env vars accross steps https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#example-of-writing-an-environment-variable-to-github_env
 fs.appendFileSync(process.env.GITHUB_ENV, `BUCKET_NAME=${bucketName}`);
 fs.appendFileSync(process.env.GITHUB_ENV, `ENV_NAME=${envName}`);
-
