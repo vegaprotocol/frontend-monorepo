888f6706913d38c4c40b71422192fb6a2e5a704c
maciek <maciek@vegaprotocol.io>
2023-06-19 16:52:00 +0200

    chore: use generated types from vega protos - fix failing unit test

--- setup-test.ts
+++ setup-test.ts
@@ -1,6 +1,7 @@
-import { TextEncoder } from 'util';
+import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (BigInt.prototype as any).toJSON = function () {
