72bad93ef58922452117329485bdb5d5b475ee16
Matthew Russell <mattrussell36@gmail.com>
2023-06-23 16:54:29 -0700

    chore: lint

--- apps/trading/components/welcome-dialog/telemetry-approval.spec.tsx
+++ apps/trading/components/welcome-dialog/telemetry-approval.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { TelemetryApproval } from './telemetry-approval';
 
