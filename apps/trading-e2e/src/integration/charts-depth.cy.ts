2454332f093bd5d1a6f4bc69b357c34fbeebd010
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-06-23 13:23:33 +0200

    test: added ac

--- apps/trading-e2e/src/integration/charts-depth.cy.ts
+++ apps/trading-e2e/src/integration/charts-depth.cy.ts
@@ -7,6 +7,7 @@ describe('charts', { tags: '@smoke' }, () => {
   });
 
   it('can see market depth chart', () => {
+    // 6006-DEPC-001
     cy.getByTestId('tab-depth').should('be.visible');
     cy.get('.depth-chart-module_canvas__260De').should('be.visible');
   });
