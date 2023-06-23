ea69e916ad9789435ff6041f67e4e9e820a913a4
Matthew Russell <mattrussell36@gmail.com>
2023-06-23 15:58:22 -0700

    test: add additional environment mock for telemetry approval tests

--- apps/trading/components/welcome-dialog/telemetry-approval.spec.tsx
+++ apps/trading/components/welcome-dialog/telemetry-approval.spec.tsx
@@ -1,17 +1,25 @@
 import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { TelemetryApproval } from './telemetry-approval';
 
+jest.mock('@vegaprotocol/logger', () => ({
+  SentryInit: () => undefined,
+  SentryClose: () => undefined,
+}));
+
+jest.mock('@vegaprotocol/environment', () => ({
+  useEnvironment: () => ({ VEGA_ENV: 'test', SENTRY_DSN: 'sentry-dsn' }),
+}));
+
 describe('TelemetryApproval', () => {
-  it('click on checkbox should be properly handled', () => {
+  it('click on checkbox should be properly handled', async () => {
     const helpText = 'My help text';
     render(<TelemetryApproval helpText={helpText} />);
     expect(screen.getByRole('checkbox')).toHaveAttribute(
       'data-state',
       'unchecked'
     );
-    act(() => {
-      screen.getByRole('checkbox').click();
-    });
+    await userEvent.click(screen.getByRole('checkbox'));
     expect(screen.getByRole('checkbox')).toHaveAttribute(
       'data-state',
       'checked'
