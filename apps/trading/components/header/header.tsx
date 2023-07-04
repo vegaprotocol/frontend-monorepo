5e3ea30046f77e14f9e3d64ad9828dce8bfd36b5
Matthew Russell <mattrussell36@gmail.com>
2023-07-04 11:38:49 -0700

    chore: adjust layout to work for mobile

--- apps/trading/components/header/header.tsx
+++ apps/trading/components/header/header.tsx
@@ -12,17 +12,17 @@ export const Header = ({ title, children }: TradeMarketHeaderProps) => {
     'grid',
     'grid-rows-[min-content_min-content]',
     'xl:grid-cols-[min-content_1fr]',
-    'border-l border-b border-default',
+    'lg:border-l border-b border-default',
     'bg-vega-clight-800 dark:bg-vega-cdark-800'
   );
   return (
-    <header className="pl-1">
+    <header className="lg:pl-1">
       <div className={headerClasses}>
-        <div className="flex flex-col justify-center pl-4 pt-2 xl:pb-2 pb-0 xl:border-r border-default xl:pr-4">
+        <div className="flex flex-col justify-center pl-3 lg:pl-4 pt-2 xl:pb-2 pb-0 xl:border-r border-default xl:pr-4">
           {title}
         </div>
         <div data-testid="header-summary" className="min-w-0">
-          <div className="px-4 py-2 flex flex-nowrap gap-4 items-center text-xs overflow-x-auto">
+          <div className="px-3 lg:px-4 py-2 flex flex-nowrap gap-4 items-center text-xs overflow-x-auto">
             {children}
           </div>
         </div>
