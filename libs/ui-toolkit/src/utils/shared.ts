8d6d6a9ad5defd86d5517356f802e8ad75470a20
Matthew Russell <mattrussell36@gmail.com>
2023-07-11 16:20:37 +0100

    chore: make select only have additional padding to accomodate dropdown chevron

--- libs/ui-toolkit/src/utils/shared.ts
+++ libs/ui-toolkit/src/utils/shared.ts
@@ -1,12 +1,12 @@
 import classnames from 'classnames';
 
 export const defaultSelectElement = (hasError?: boolean) =>
-  classnames(defaultFormElement(hasError), 'dark:bg-black');
+  classnames(defaultFormElement(hasError), 'pr-10 dark:bg-black');
 
 export const defaultFormElement = (hasError?: boolean) =>
   classnames(
     'flex items-center w-full text-sm',
-    'p-2 pr-10 border-2 rounded whitespace-nowrap text-ellipsis overflow-hidden',
+    'p-2 border-2 rounded whitespace-nowrap text-ellipsis overflow-hidden',
     'bg-transparent',
     'border border-vega-light-200 dark:border-vega-dark-200',
     'focus:border-vega-light-300 dark:focus:border-vega-dark-300',
