56dd5062e18380d4c15bc335691282c57d88d8ca
Matthew Russell <mattrussell36@gmail.com>
2023-07-12 01:24:25 +0100

    test: fix settings test

--- apps/trading-e2e/src/integration/settings.cy.ts
+++ apps/trading-e2e/src/integration/settings.cy.ts
@@ -1,24 +1,29 @@
 describe('Settings page', { tags: '@smoke' }, () => {
   beforeEach(() => {
-    cy.clearLocalStorage().then(() => {
-      cy.mockTradingPage();
-      cy.mockSubscription();
-      cy.visit('/');
-      cy.getByTestId('Settings').click();
+    cy.clearLocalStorage();
+
+    cy.mockTradingPage();
+    cy.mockSubscription();
+    cy.visit('/');
+
+    // Only click if not already active otherwise sidebar will close
+    cy.get('[data-testid="sidebar-content"]').then(($sidebarContent) => {
+      if ($sidebarContent.find('h2').text() !== 'Settings') {
+        cy.get('[data-testid="sidebar"] [data-testid="Settings"]').click();
+      }
     });
   });
+
   it('telemetry checkbox should work well', () => {
     const telemetrySwitch = '#switch-settings-telemetry-switch';
     cy.get(telemetrySwitch).should('have.attr', 'data-state', 'unchecked');
     cy.get(telemetrySwitch).click();
     cy.get(telemetrySwitch).should('have.attr', 'data-state', 'checked');
     cy.reload();
-    cy.getByTestId('Settings').click();
     cy.get(telemetrySwitch).should('have.attr', 'data-state', 'checked');
     cy.get(telemetrySwitch).click();
     cy.get(telemetrySwitch).should('have.attr', 'data-state', 'unchecked');
     cy.reload();
-    cy.getByTestId('Settings').click();
     cy.get(telemetrySwitch).should('have.attr', 'data-state', 'unchecked');
   });
 });
