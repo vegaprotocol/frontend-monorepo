f474672d03e17906e70ea8bb9bf966f807e698a3
Matthew Russell <mattrussell36@gmail.com>
2023-07-14 16:58:42 +0100

    fix: move default bg and text classes to body tag

--- apps/trading/pages/_document.page.tsx
+++ apps/trading/pages/_document.page.tsx
@@ -21,7 +21,7 @@ export default function Document() {
         />
         <script src="/theme-setter.js" type="text/javascript" async />
       </Head>
-      <body>
+      <body className="bg-white dark:bg-vega-cdark-900 text-default font-alpha">
         <Main />
         <NextScript />
       </body>
