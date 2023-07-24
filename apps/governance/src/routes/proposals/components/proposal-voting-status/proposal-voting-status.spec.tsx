20789307453cbd7a6ef93c17f901a3738ccf7832
sam-keen <samuel.kleinmann@gmail.com>
2023-07-24 12:29:41 +0100

    feat(3939): updated test that failed after changes from develop

--- apps/governance/src/routes/proposals/components/proposal-voting-status/proposal-voting-status.spec.tsx
+++ apps/governance/src/routes/proposals/components/proposal-voting-status/proposal-voting-status.spec.tsx
@@ -29,7 +29,6 @@ const mockAppState: AppState = {
   totalAssociated: new BigNumber('50063005'),
   decimals: 18,
   totalSupply: mockTotalSupply,
-  vegaWalletOverlay: false,
   vegaWalletManageOverlay: false,
   transactionOverlay: false,
   bannerMessage: '',
