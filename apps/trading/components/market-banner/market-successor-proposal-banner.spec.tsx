596a125f3aed1a87d995cbb3f6b5844fc1e9b70a
maciek <maciek@vegaprotocol.io>
2023-07-27 12:55:55 +0200

    feat: add market successor proposal banner - add some fixes on proposed markets

--- apps/trading/components/market-banner/market-successor-proposal-banner.spec.tsx
+++ apps/trading/components/market-banner/market-successor-proposal-banner.spec.tsx
@@ -1,4 +1,5 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
+import type { SingleExecutionResult } from '@apollo/client';
 import type { MockedResponse } from '@apollo/react-testing';
 import { MockedProvider } from '@apollo/react-testing';
 import { MarketSuccessorProposalBanner } from './market-successor-proposal-banner';
@@ -48,6 +49,101 @@ describe('MarketSuccessorProposalBanner', () => {
       expect(
         screen.getByText('A successors to this market has been proposed')
       ).toBeInTheDocument();
+    });
+    expect(screen.getByRole('link')).toHaveAttribute(
+      'href',
+      'https://stagnet1.token.vega.xyz/proposals/proposal-1'
+    );
+  });
+  it('should display plural proposals', async () => {
+    const dualProposalMock = {
+      ...marketProposalMock,
+      result: {
+        ...marketProposalMock.result,
+        data: {
+          proposalsConnection: {
+            edges: [
+              ...((
+                marketProposalMock?.result as SingleExecutionResult<SuccessorProposalsListQuery>
+              )?.data?.proposalsConnection?.edges ?? []),
+              {
+                node: {
+                  __typename: 'Proposal',
+                  id: 'proposal-2',
+                  terms: {
+                    __typename: 'ProposalTerms',
+                    change: {
+                      __typename: 'NewMarket',
+                      instrument: {
+                        name: 'New second proposal of the market successor',
+                      },
+                      successorConfiguration: {
+                        parentMarketId: 'marketId',
+                      },
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    render(
+      <MockedProvider mocks={[dualProposalMock]}>
+        <MarketSuccessorProposalBanner marketId="marketId" />
+      </MockedProvider>
+    );
+    await waitFor(() => {
+      expect(
+        screen.getByText('Successors to this market have been proposed')
+      ).toBeInTheDocument();
+    });
+    expect(screen.getAllByRole('link')[0]).toHaveAttribute(
+      'href',
+      'https://stagnet1.token.vega.xyz/proposals/proposal-1'
+    );
+    expect(screen.getAllByRole('link')[1]).toHaveAttribute(
+      'href',
+      'https://stagnet1.token.vega.xyz/proposals/proposal-2'
+    );
+  });
+
+  it('banner should be hidden because no proposals', () => {
+    const { container } = render(
+      <MockedProvider>
+        <MarketSuccessorProposalBanner marketId="marketId" />
+      </MockedProvider>
+    );
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('banner should be hidden because no proposals for the market', () => {
+    const { container } = render(
+      <MockedProvider mocks={[marketProposalMock]}>
+        <MarketSuccessorProposalBanner marketId="otherMarketId" />
+      </MockedProvider>
+    );
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('banner should be hidden after user close click', async () => {
+    const { container } = render(
+      <MockedProvider mocks={[marketProposalMock]}>
+        <MarketSuccessorProposalBanner marketId="marketId" />
+      </MockedProvider>
+    );
+    await waitFor(() => {
+      expect(
+        screen.getByText('A successors to this market has been proposed')
+      ).toBeInTheDocument();
+    });
+    await act(() => {
+      screen.getByTestId('notification-banner-close').click();
+    });
+    await waitFor(() => {
+      expect(container).toBeEmptyDOMElement();
     });
   });
 });
