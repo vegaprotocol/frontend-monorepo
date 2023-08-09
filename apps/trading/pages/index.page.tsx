b5996eae390e0d133fa1e845bb32a2c5cbc83a53
Madalina Raicu <madalina@raygroup.uk>
2023-08-09 17:12:44 +0300

    feat(trading): metadata console

--- apps/trading/pages/index.page.tsx
+++ apps/trading/pages/index.page.tsx
@@ -18,7 +18,27 @@ export default function Index() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
+
         <meta name="description" content="Vega Protocol - VEGA Console" />
+
+        <meta name="og:type" content="website" />
+        <meta name="og:url" content="https://console.vega.xyz/" />
+        <meta name="og:title" content="Vega Protocol - Console" />
+        <meta name="og:site_name" content="Vega Protocol - Console" />
+        <meta name="og:image" content="https://static.vega.xyz/favicon.ico" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Vega Protocol - Console" />
+        <meta name="twitter:description" content="Vega Protocol - Console" />
+        <meta
+          name="twitter:image"
+          content="https://static.vega.xyz/favicon.ico"
+        />
+        <meta name="twitter:image:alt" content="VEGA logo" />
+        <meta name="twitter:site" content="@vegaprotocol" />
+
+        <meta name="description" content="Vega Protocol - Console" />
+
         <link rel="apple-touch-icon" href="assets/apple-touch-icon.png" />
         <link rel="manifest" href="assets/manifest.json" />
         <link
