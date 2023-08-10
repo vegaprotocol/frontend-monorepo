22c893dc737960ecb0d28b2ece22a92b8a84a31c
Madalina Raicu <madalina@raygroup.uk>
2023-08-10 23:45:37 +0300

    chore: update metadata

--- apps/trading/pages/index.page.tsx
+++ apps/trading/pages/index.page.tsx
@@ -8,7 +8,7 @@ import { ClientRouter } from './client-router';
  */
 export default function Index() {
   return (
-    <>
+    <div>
       <Head>
         <meta charSet="utf-8" />
         <link
@@ -18,25 +18,6 @@ export default function Index() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-
-        <meta name="description" content="Vega Protocol - VEGA Console" />
-
-        <meta name="og:type" content="website" />
-        <meta name="og:url" content="https://console.vega.xyz/" />
-        <meta name="og:title" content="Vega Protocol - Console" />
-        <meta name="og:site_name" content="Vega Protocol - Console" />
-        <meta name="og:image" content="https://static.vega.xyz/favicon.ico" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Vega Protocol - Console" />
-        <meta name="twitter:description" content="Vega Protocol - Console" />
-        <meta
-          name="twitter:image"
-          content="https://static.vega.xyz/favicon.ico"
-        />
-        <meta name="twitter:image:alt" content="VEGA logo" />
-        <meta name="twitter:site" content="@vegaprotocol" />
-
         <meta name="description" content="Vega Protocol - Console" />
 
         <link rel="apple-touch-icon" href="assets/apple-touch-icon.png" />
@@ -50,6 +31,6 @@ export default function Index() {
         <title>VEGA Console dApp</title>
       </Head>
       <ClientRouter />
-    </>
+    </div>
   );
 }
