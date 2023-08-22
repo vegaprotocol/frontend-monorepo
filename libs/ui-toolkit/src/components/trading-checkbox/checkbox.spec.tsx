ea41e55791575cdf3905eee5493c347e87d3cf72
Matthew Russell <mattrussell36@gmail.com>
2023-08-22 11:30:34 +0100

    chore: update checkbox test

--- libs/ui-toolkit/src/components/trading-checkbox/checkbox.spec.tsx
+++ libs/ui-toolkit/src/components/trading-checkbox/checkbox.spec.tsx
@@ -9,12 +9,12 @@ describe('Checkbox', () => {
 
   it('should render a checked checkbox if specified in state', () => {
     render(<TradingCheckbox label="label" checked={true} />);
-    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByTestId(/icon-/)).toBeInTheDocument();
   });
 
   it('should render an unchecked checkbox if specified in state', () => {
     render(<TradingCheckbox label="unchecked" checked={false} />);
-    expect(screen.queryByRole('img')).not.toBeInTheDocument();
+    expect(screen.queryByTestId(/icon-/)).not.toBeInTheDocument();
   });
 
   it('should render an indeterminate checkbox if specified in state', () => {
