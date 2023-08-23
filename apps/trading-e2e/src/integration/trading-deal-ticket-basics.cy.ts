29f3374c6169f2827eb2e159fd48d0c7275bc2ba
Maciek <maciek@vegaprotocol.io>
2023-08-23 09:23:36 +0200

    fix(trading): fix hidden sidebars on load (#4583)

--- apps/trading-e2e/src/integration/trading-deal-ticket-basics.cy.ts
+++ apps/trading-e2e/src/integration/trading-deal-ticket-basics.cy.ts
@@ -54,6 +54,15 @@ describe('deal ticket basics', { tags: '@smoke' }, () => {
     cy.getByTestId(toggleLimit).next('input').should('be.checked');
     cy.getByTestId(orderPriceField).should('have.value', '101');
   });
+
+  it('sidebar should be open after reload', () => {
+    cy.mockTradingPage();
+    cy.getByTestId('deal-ticket-form').should('be.visible');
+    cy.getByTestId('Order').click();
+    cy.getByTestId('deal-ticket-form').should('not.exist');
+    cy.reload();
+    cy.getByTestId('deal-ticket-form').should('be.visible');
+  });
 });
 
 describe(
