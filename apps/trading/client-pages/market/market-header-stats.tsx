f0bba23560333f975e295c8e8dd4490287b3cbd8
Matthew Russell <mattrussell36@gmail.com>
2023-08-24 16:45:54 -0700

    chore: remove unnecessary null/undefined checks

--- apps/trading/client-pages/market/market-header-stats.tsx
+++ apps/trading/client-pages/market/market-header-stats.tsx
@@ -18,26 +18,24 @@ import { MarketState } from '../../components/market-state';
 import { MarketLiquiditySupplied } from '../../components/liquidity-supplied';
 
 interface MarketHeaderStatsProps {
-  market: Market | null;
+  market: Market;
 }
 
 export const MarketHeaderStats = ({ market }: MarketHeaderStatsProps) => {
   const { VEGA_EXPLORER_URL } = useEnvironment();
   const { open: openAssetDetailsDialog } = useAssetDetailsDialogStore();
 
-  const asset = market && getAsset(market);
+  const asset = getAsset(market);
 
   return (
     <>
       <HeaderStat
         heading={t('Expiry')}
         description={
-          market && (
-            <ExpiryTooltipContent
-              market={market}
-              explorerUrl={VEGA_EXPLORER_URL}
-            />
-          )
+          <ExpiryTooltipContent
+            market={market}
+            explorerUrl={VEGA_EXPLORER_URL}
+          />
         }
         testId="market-expiry"
       >
@@ -45,25 +43,25 @@ export const MarketHeaderStats = ({ market }: MarketHeaderStatsProps) => {
       </HeaderStat>
       <HeaderStat heading={t('Price')} testId="market-price">
         <MarketMarkPrice
-          marketId={market?.id}
-          decimalPlaces={market?.decimalPlaces}
+          marketId={market.id}
+          decimalPlaces={market.decimalPlaces}
         />
       </HeaderStat>
       <HeaderStat heading={t('Change (24h)')} testId="market-change">
         <Last24hPriceChange
-          marketId={market?.id}
-          decimalPlaces={market?.decimalPlaces}
+          marketId={market.id}
+          decimalPlaces={market.decimalPlaces}
         />
       </HeaderStat>
       <HeaderStat heading={t('Volume (24h)')} testId="market-volume">
         <Last24hVolume
-          marketId={market?.id}
-          positionDecimalPlaces={market?.positionDecimalPlaces}
+          marketId={market.id}
+          positionDecimalPlaces={market.positionDecimalPlaces}
         />
       </HeaderStat>
       <HeaderStatMarketTradingMode
-        marketId={market?.id}
-        initialTradingMode={market?.tradingMode}
+        marketId={market.id}
+        initialTradingMode={market.tradingMode}
       />
       <MarketState market={market} />
       {asset ? (
@@ -83,27 +81,26 @@ export const MarketHeaderStats = ({ market }: MarketHeaderStatsProps) => {
         </HeaderStat>
       ) : null}
       <MarketLiquiditySupplied
-        marketId={market?.id}
+        marketId={market.id}
         assetDecimals={asset?.decimals || 0}
       />
-      <MarketProposalNotification marketId={market?.id} />
+      <MarketProposalNotification marketId={market.id} />
     </>
   );
 };
 
 type ExpiryLabelProps = {
-  market: Market | null;
+  market: Market;
 };
 
 const ExpiryLabel = ({ market }: ExpiryLabelProps) => {
-  const content =
-    market && market.tradableInstrument.instrument.metadata.tags
-      ? getExpiryDate(
-          market.tradableInstrument.instrument.metadata.tags,
-          market.marketTimestamps.close,
-          market.state
-        )
-      : '-';
+  const content = market.tradableInstrument.instrument.metadata.tags
+    ? getExpiryDate(
+        market.tradableInstrument.instrument.metadata.tags,
+        market.marketTimestamps.close,
+        market.state
+      )
+    : '-';
   return <div data-testid="trading-expiry">{content}</div>;
 };
 
@@ -116,7 +113,7 @@ const ExpiryTooltipContent = ({
   market,
   explorerUrl,
 }: ExpiryTooltipContentProps) => {
-  if (market?.marketTimestamps.close === null) {
+  if (market.marketTimestamps.close === null) {
     const oracleId =
       market.tradableInstrument.instrument.product.__typename === 'Future'
         ? market.tradableInstrument.instrument.product
