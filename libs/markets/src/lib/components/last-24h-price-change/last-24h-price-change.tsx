3422b9949147b8d8c02f596073bbef78393c171f
Matthew Russell <mattrussell36@gmail.com>
2023-08-25 15:24:57 -0700

    feat(trading): change to system monospace font (#4634)

--- libs/markets/src/lib/components/last-24h-price-change/last-24h-price-change.tsx
+++ libs/markets/src/lib/components/last-24h-price-change/last-24h-price-change.tsx
@@ -1,8 +1,16 @@
-import { isNumeric } from '@vegaprotocol/utils';
-import { PriceChangeCell } from '@vegaprotocol/datagrid';
-import { Tooltip } from '@vegaprotocol/ui-toolkit';
+import {
+  addDecimalsFormatNumber,
+  formatNumberPercentage,
+  isNumeric,
+  priceChange,
+  priceChangePercentage,
+} from '@vegaprotocol/utils';
+import { PriceChangeCell, signedNumberCssClass } from '@vegaprotocol/datagrid';
+import { Tooltip, VegaIcon, VegaIconNames } from '@vegaprotocol/ui-toolkit';
 import { t } from '@vegaprotocol/i18n';
 import { useCandles } from '../../hooks/use-candles';
+import BigNumber from 'bignumber.js';
+import classNames from 'classnames';
 
 interface Props {
   marketId?: string;
@@ -47,10 +55,39 @@ export const Last24hPriceChange = ({
   if (error || !isNumeric(decimalPlaces)) {
     return <span>-</span>;
   }
+
+  const candles = oneDayCandles?.map((c) => c.close) || initialValue || [];
+  const change = priceChange(candles);
+  const changePercentage = priceChangePercentage(candles);
+
   return (
-    <PriceChangeCell
-      candles={oneDayCandles?.map((c) => c.close) || initialValue || []}
-      decimalPlaces={decimalPlaces}
-    />
+    <span
+      className={classNames(
+        'flex items-center gap-1',
+        signedNumberCssClass(change)
+      )}
+    >
+      <Arrow value={change} />
+      <span data-testid="price-change-percentage">
+        {formatNumberPercentage(new BigNumber(changePercentage.toString()), 2)}
+      </span>
+      <span data-testid="price-change">
+        {addDecimalsFormatNumber(change.toString(), decimalPlaces ?? 0, 3)}
+      </span>
+    </span>
   );
+};
+
+const Arrow = ({ value }: { value: number | bigint }) => {
+  const size = 10;
+
+  if (value > 0) {
+    return <VegaIcon name={VegaIconNames.ARROW_UP} size={size} />;
+  }
+
+  if (value < 0) {
+    return <VegaIcon name={VegaIconNames.ARROW_DOWN} size={size} />;
+  }
+
+  return null;
 };
