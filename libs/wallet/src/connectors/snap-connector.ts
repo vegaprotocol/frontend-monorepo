c9adde0f06a3a81d11a2e68f2dc212316c4605fc
asiaznik <artur@vegaprotocol.io>
2023-08-28 10:19:26 +0200

    fix: ts shenanigans

--- libs/wallet/src/connectors/snap-connector.ts
+++ libs/wallet/src/connectors/snap-connector.ts
@@ -1,4 +1,3 @@
-import type EthereumProvider from '@walletconnect/ethereum-provider/dist/types/EthereumProvider';
 import type { TransactionResponse } from './vega-connector';
 import {
   WalletError,
@@ -7,14 +6,42 @@ import {
   type VegaConnector,
 } from './vega-connector';
 import { clearConfig, setConfig } from '../storage';
+import '@walletconnect/ethereum-provider/dist/types/EthereumProvider';
 
-declare global {
-  interface Window {
-    ethereum?: EthereumProvider;
+type RequestArguments = {
+  method: string;
+  params?: unknown[] | object;
+};
+type WindowEthereumProvider = {
+  isMetaMask: boolean;
+  request<T = unknown>(args: RequestArguments): Promise<T>;
+};
+
+export const ERR_ETHEREUM_UNDEFINED = new Error(
+  'MetaMask extension could not be found'
+);
+export const ERR_NODE_ADDRESS_NOT_SET = new Error('nodeAddress is not set');
+const ERR_SNAP_ID_NOT_SET = new Error('snapId is not set');
+const ERR_TRANSACTION_PARSE = new Error('could not parse transaction data');
+
+const ethereumRequest = <T>(args: RequestArguments): Promise<T> => {
+  // can't declare `EthereumProvider` here because of the conflict with
+  // type definitions of `@web3-react`
+  if (
+    'ethereum' in window &&
+    typeof window.ethereum === 'object' &&
+    window.ethereum &&
+    'request' in window.ethereum &&
+    'isMetaMask' in window.ethereum &&
+    window.ethereum.isMetaMask &&
+    typeof window.ethereum.request === 'function'
+  ) {
+    return (window.ethereum as WindowEthereumProvider).request<T>(args);
   }
-}
+  throw ERR_ETHEREUM_UNDEFINED;
+};
 
-// export const LOCAL_SNAP_ID = 'local:http://localhost:8080';
+export const LOCAL_SNAP_ID = 'local:http://localhost:8080';
 export const DEFAULT_SNAP_ID = 'npm:@vegaprotocol/snap';
 
 type GetSnapsResponse = Record<string, Snap>;
@@ -31,7 +58,6 @@ type InvokeSnapRequest = {
   method: string;
   params?: object;
 };
-type InvokeSnapResponse<T> = Promise<T>;
 
 type SendTransactionResponse =
   | {
@@ -55,13 +81,6 @@ type GetChainIdResponse = {
 };
 type ListKeysResponse = { keys: PubKey[] };
 
-export const ERR_ETHEREUM_UNDEFINED = new Error(
-  'MetaMask extension could not be found'
-);
-export const ERR_NODE_ADDRESS_NOT_SET = new Error('nodeAddress is not set');
-const ERR_SNAP_ID_NOT_SET = new Error('snapId is not set');
-const ERR_TRANSACTION_PARSE = new Error('could not parse transaction data');
-
 /**
  * Requests permission for a website to communicate with the specified snaps
  * and attempts to install them if they're not already installed.
@@ -72,9 +91,8 @@ export const requestSnap = async (
   snapId: string,
   params: Record<'version' | string, unknown> = {}
 ) => {
-  if (!window.ethereum) throw ERR_ETHEREUM_UNDEFINED;
   try {
-    await window.ethereum.request({
+    await ethereumRequest({
       method: 'wallet_requestSnaps',
       params: {
         [snapId]: params,
@@ -90,8 +108,7 @@ export const requestSnap = async (
  * More information here: https://docs.metamask.io/snaps/reference/rpc-api/#wallet_getsnaps
  */
 export const getSnaps = async (): Promise<GetSnapsResponse> => {
-  if (!window.ethereum) throw ERR_ETHEREUM_UNDEFINED;
-  return (await window.ethereum.request({
+  return (await ethereumRequest({
     method: 'wallet_getSnaps',
   })) as GetSnapsResponse;
 };
@@ -116,8 +133,7 @@ export const getSnap = async (
 export const invokeSnap = async <T>(
   snapId: string,
   request: InvokeSnapRequest
-): InvokeSnapResponse<T> => {
-  if (!window.ethereum) throw ERR_ETHEREUM_UNDEFINED;
+) => {
   const req = {
     method: 'wallet_invokeSnap',
     params: {
@@ -125,7 +141,7 @@ export const invokeSnap = async <T>(
       request,
     },
   };
-  return await window.ethereum.request(req);
+  return await ethereumRequest<T>(req);
 };
 
 export class SnapConnector implements VegaConnector {
