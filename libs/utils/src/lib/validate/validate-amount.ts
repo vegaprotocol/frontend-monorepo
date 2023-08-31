2ff5cc0ec81f2d303a1f9b34694a1a827da71031
maciek <maciek@vegaprotocol.io>
2023-08-31 16:56:07 +0200

    chore: handle negative decimals - add validation for negative decimals in size and price

--- libs/utils/src/lib/validate/validate-amount.ts
+++ libs/utils/src/lib/validate/validate-amount.ts
@@ -4,6 +4,12 @@ export const validateAmount = (step: number | string, field: string) => {
   const [, stepDecimals = ''] = String(step).split('.');
 
   return (value?: string) => {
+    if (Number(step) >= 1) {
+      if (Number(value) % Number(step) > 0) {
+        return t(`${field} must be a multiple of ${step} for this market`);
+      }
+      return true;
+    }
     const [, valueDecimals = ''] = (value || '').split('.');
     if (stepDecimals.length < valueDecimals.length) {
       if (stepDecimals === '') {
