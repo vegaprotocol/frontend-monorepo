a619b4a87e8ed653f53886b05a5016023911bfb7
maciek <maciek@vegaprotocol.io>
2023-09-01 10:57:13 +0200

    chore: add back telemetry approval - adjust to fix failing int test

--- libs/utils/src/lib/validate/validate-amount.ts
+++ libs/utils/src/lib/validate/validate-amount.ts
@@ -4,7 +4,7 @@ export const validateAmount = (step: number | string, field: string) => {
   const [, stepDecimals = ''] = String(step).split('.');
 
   return (value?: string) => {
-    if (Number(step) >= 1) {
+    if (Number(step) > 1) {
       if (Number(value) % Number(step) > 0) {
         return t(`${field} must be a multiple of ${step} for this market`);
       }
