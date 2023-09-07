0ac2bca27f0dc64f05b03ef3c7af8f95e0fdd1dc
Matthew Russell <mattrussell36@gmail.com>
2023-09-06 17:08:25 -0700

    chore: rename markets component to open-markets

--- apps/trading/client-pages/markets/open-markets.tsx
+++ apps/trading/client-pages/markets/open-markets.tsx
@@ -2,7 +2,7 @@ import { MarketsContainer } from '@vegaprotocol/markets';
 import { useMarketClickHandler } from '../../lib/hooks/use-market-click-handler';
 import { SuccessorMarketRenderer } from './successor-market-cell';
 
-export const Markets = () => {
+export const OpenMarkets = () => {
   const handleOnSelect = useMarketClickHandler();
   return (
     <MarketsContainer
