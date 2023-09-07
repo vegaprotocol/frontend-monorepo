a1df856f103dc7d612892915326139c2d14b2ad6
maciek <maciek@vegaprotocol.io>
2023-09-07 18:02:34 +0200

    chore: standalone pages for withdraw and transfer

--- apps/trading/client-pages/transfer/transfer.tsx
+++ apps/trading/client-pages/transfer/transfer.tsx
@@ -1,18 +1,23 @@
 import { GetStarted } from '../../components/welcome-dialog';
 import { t } from '@vegaprotocol/i18n';
-import { TransferContainer } from '@vegaprotocol/accounts';
+import {
+  TransferContainer,
+  useTransferAssetIdStore,
+} from '@vegaprotocol/accounts';
 import { useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 
 export const Transfer = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const assetId = searchParams.get('assetId') || undefined;
+  const selectedAssetId = useTransferAssetIdStore((store) => store.assetId);
 
   useEffect(() => {
-    if (assetId) {
+    if (assetId && selectedAssetId && selectedAssetId !== assetId) {
       setTimeout(() => setSearchParams({}));
     }
-  }, [assetId, setSearchParams]);
+  }, [assetId, selectedAssetId, setSearchParams]);
+
   return (
     <div className="py-16 px-8 flex w-full justify-center">
       <div className="lg:min-w-[700px] min-w-[300px] max-w-[700px]">
