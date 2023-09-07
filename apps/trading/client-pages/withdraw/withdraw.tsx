82275db2a228a29c304f03839adb6eb93956086a
maciek <maciek@vegaprotocol.io>
2023-09-07 17:28:08 +0200

    chore: standalone pages for withdrop and transfer

---
+++ apps/trading/client-pages/withdraw/withdraw.tsx
@@ -0,0 +1,30 @@
+import { useSearchParams } from 'react-router-dom';
+import { useWithdrawStore } from '@vegaprotocol/withdraws';
+import { GetStarted } from '../../components/welcome-dialog';
+import { t } from '@vegaprotocol/i18n';
+import { WithdrawContainer } from '../../components/withdraw-container';
+import { useEffect } from 'react';
+
+export const Withdraw = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const assetId = searchParams.get('assetId');
+  const asset = useWithdrawStore((store) => store.asset);
+  useEffect(() => {
+    if (assetId && asset?.id && assetId !== asset.id) {
+      setSearchParams({});
+    }
+  }, [assetId, asset, setSearchParams]);
+  return (
+    <div className="py-16 px-8 flex w-full justify-center">
+      <div className="lg:min-w-[700px] min-w-[300px] max-w-[700px]">
+        <h1 className="text-4xl xl:text-5xl uppercase font-alpha calt">
+          {t('Withdraw')}
+        </h1>
+        <div className="mt-10">
+          <WithdrawContainer assetId={assetId} />
+          <GetStarted />
+        </div>
+      </div>
+    </div>
+  );
+};
