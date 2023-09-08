f456d3aa56d08803b7d5ca736e24a0741963c1be
Matthew Russell <mattrussell36@gmail.com>
2023-09-08 16:03:37 -0700

    test: simplify withdraw test

--- apps/trading/client-pages/deposit/deposit.tsx
+++ apps/trading/client-pages/deposit/deposit.tsx
@@ -4,9 +4,9 @@ import { t } from '@vegaprotocol/i18n';
 
 export const Deposit = () => {
   return (
-    <div className="py-16 px-8 flex w-full justify-center">
+    <div className="flex justify-center w-full px-8 py-16">
       <div className="lg:min-w-[700px] min-w-[300px] max-w-[700px]">
-        <h1 className="text-4xl xl:text-5xl uppercase font-alpha calt">
+        <h1 className="text-4xl uppercase xl:text-5xl font-alpha calt">
           {t('Deposit')}
         </h1>
         <div className="mt-10">
