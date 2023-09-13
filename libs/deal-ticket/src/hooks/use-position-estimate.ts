2178f89007c76e0fa68473a8256ea880721109b9
maciek <maciek@vegaprotocol.io>
2023-09-13 14:36:00 +0200

    chore: preserve estimate position cache in deal ticket

---
+++ libs/deal-ticket/src/hooks/use-position-estimate.ts
@@ -0,0 +1,38 @@
+import type {
+  EstimatePositionQueryVariables,
+  EstimatePositionQuery,
+} from '@vegaprotocol/positions';
+import { useEstimatePositionQuery } from '@vegaprotocol/positions';
+import { useEffect, useState } from 'react';
+
+interface PositionEstimateProps extends EstimatePositionQueryVariables {
+  skip: boolean;
+}
+
+export const usePositionEstimate = ({
+  marketId,
+  openVolume,
+  orders,
+  collateralAvailable,
+  skip,
+}: PositionEstimateProps) => {
+  const [estimates, setEstimates] = useState<EstimatePositionQuery | undefined>(
+    undefined
+  );
+  const { data } = useEstimatePositionQuery({
+    variables: {
+      marketId,
+      openVolume,
+      orders,
+      collateralAvailable,
+    },
+    skip,
+    fetchPolicy: 'no-cache',
+  });
+  useEffect(() => {
+    if (data) {
+      setEstimates(data);
+    }
+  }, [data]);
+  return estimates;
+};
