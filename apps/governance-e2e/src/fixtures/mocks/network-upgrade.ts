aa38e0c001d652485f9279b20db05940d9c8dcf0
Joe Tsang <30622993+jtsang586@users.noreply.github.com>
2023-09-13 13:02:47 +0100

    test(governance): add assert for numerical sort (#4742)

--- apps/governance-e2e/src/fixtures/mocks/network-upgrade.ts
+++ apps/governance-e2e/src/fixtures/mocks/network-upgrade.ts
@@ -19,7 +19,22 @@ export const upgradeProposalsData = {
       },
       {
         node: {
-          upgradeBlockHeight: '1955065',
+          upgradeBlockHeight: '10001',
+          vegaReleaseTag: 'v0.71.0+dev-12156-bca1d57e',
+          approvers: [
+            '02a6531716b7a6d82779b7793c3ad2fcb47290ea2ff0912c56f61219bd9675ff',
+            '121934387281812a2d5e6913e5d57c0f85a8f169e2752347ee2e23b52d46623c',
+            '65c80e2f5f84e2109eec30810f137ba04cbbecaba8f27706c146cc6c6f90db29',
+            'bd6339d2428c79ac3bc9011771236d17bac92bcb1806423388d52fb440043aef',
+          ],
+          status: 'PROTOCOL_UPGRADE_PROPOSAL_STATUS_APPROVED',
+          __typename: 'ProtocolUpgradeProposal',
+        },
+        __typename: 'ProtocolUpgradeProposalEdge',
+      },
+      {
+        node: {
+          upgradeBlockHeight: '20',
           vegaReleaseTag: 'v0.71.0+dev-12156-bca1d57e',
           approvers: [
             '02a6531716b7a6d82779b7793c3ad2fcb47290ea2ff0912c56f61219bd9675ff',
