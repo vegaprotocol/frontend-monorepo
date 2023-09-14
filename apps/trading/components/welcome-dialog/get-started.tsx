fe02d924d508903aa2cedff03168f58e5c02cd06
Matthew Russell <mattrussell36@gmail.com>
2023-09-13 19:16:45 -0700

    chore: re add pubkey message

--- apps/trading/components/welcome-dialog/get-started.tsx
+++ apps/trading/components/welcome-dialog/get-started.tsx
@@ -70,6 +70,9 @@ const GetStartedButton = ({ step }: { step: OnboardingStep }) => {
 export const GetStarted = ({ lead }: Props) => {
   const { pubKey } = useVegaWallet();
   const { VEGA_ENV, VEGA_NETWORKS } = useEnvironment();
+  const openVegaWalletDialog = useVegaWalletDialogStore(
+    (store) => store.openVegaWalletDialog
+  );
   const CANONICAL_URL = VEGA_NETWORKS[VEGA_ENV] || 'https://console.vega.xyz';
   const currentStep = useGetOnboardingStep();
   const dismissed = useOnboardingStore((store) => store.dismissed);
@@ -81,56 +84,78 @@ export const GetStarted = ({ lead }: Props) => {
     { 'mt-8': !lead }
   );
 
-  if (dismissed) {
-    return null;
+  if (!dismissed) {
+    return (
+      <div className={wrapperClasses} data-testid="get-started-banner">
+        {lead && <h2>{lead}</h2>}
+        <h3 className="text-lg">{t('Get started')}</h3>
+        <div>
+          <ul className="list-none">
+            <Step
+              step={1}
+              text={t('Connect')}
+              complete={Boolean(
+                currentStep > OnboardingStep.ONBOARDING_CONNECT_STEP || pubKey
+              )}
+            />
+            <Step
+              step={2}
+              text={t('Deposit funds')}
+              complete={currentStep > OnboardingStep.ONBOARDING_DEPOSIT_STEP}
+            />
+            <Step
+              step={3}
+              text={t('Open a position')}
+              complete={currentStep > OnboardingStep.ONBOARDING_ORDER_STEP}
+            />
+          </ul>
+        </div>
+        <div>
+          <GetStartedButton step={currentStep} />
+        </div>
+        {VEGA_ENV === Networks.MAINNET && (
+          <p className="text-sm">
+            {t('Experiment for free with virtual assets on')}{' '}
+            <ExternalLink href={CANONICAL_URL}>
+              {t('Fairground Testnet')}
+            </ExternalLink>
+          </p>
+        )}
+        {VEGA_ENV === Networks.TESTNET && (
+          <p className="text-sm">
+            {t('Ready to trade with real funds?')}{' '}
+            <ExternalLink href={CANONICAL_URL}>
+              {t('Switch to Mainnet')}
+            </ExternalLink>
+          </p>
+        )}
+      </div>
+    );
   }
 
-  return (
-    <div className={wrapperClasses} data-testid="get-started-banner">
-      {lead && <h2>{lead}</h2>}
-      <h3 className="text-lg">{t('Get started')}</h3>
-      <div>
-        <ul className="list-none">
-          <Step
-            step={1}
-            text={t('Connect')}
-            complete={Boolean(
-              currentStep > OnboardingStep.ONBOARDING_CONNECT_STEP || pubKey
-            )}
-          />
-          <Step
-            step={2}
-            text={t('Deposit funds')}
-            complete={currentStep > OnboardingStep.ONBOARDING_DEPOSIT_STEP}
-          />
-          <Step
-            step={3}
-            text={t('Open a position')}
-            complete={currentStep > OnboardingStep.ONBOARDING_ORDER_STEP}
-          />
-        </ul>
-      </div>
-      <div>
-        <GetStartedButton step={currentStep} />
-      </div>
-      {VEGA_ENV === Networks.MAINNET && (
-        <p className="text-sm">
-          {t('Experiment for free with virtual assets on')}{' '}
-          <ExternalLink href={CANONICAL_URL}>
-            {t('Fairground Testnet')}
-          </ExternalLink>
+  if (!pubKey) {
+    return (
+      <div className={wrapperClasses}>
+        <p className="mb-1 text-sm">
+          You need a{' '}
+          <ExternalLink href="https://vega.xyz/wallet">
+            Vega wallet
+          </ExternalLink>{' '}
+          to start trading in this market.
         </p>
-      )}
-      {VEGA_ENV === Networks.TESTNET && (
-        <p className="text-sm">
-          {t('Ready to trade with real funds?')}{' '}
-          <ExternalLink href={CANONICAL_URL}>
-            {t('Switch to Mainnet')}
-          </ExternalLink>
-        </p>
-      )}
-    </div>
-  );
+        <TradingButton
+          onClick={openVegaWalletDialog}
+          size="small"
+          data-testid="order-connect-wallet"
+          intent={Intent.Info}
+        >
+          {t('Connect wallet')}
+        </TradingButton>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 const Step = ({
