78afecc2e51409f3973b20450d09e5b2e75d23e5
Matthew Russell <mattrussell36@gmail.com>
2023-09-15 05:24:47 -0700

    chore(trading): layout adjustments to accomodate default 13 inch screens (#4798)

--- apps/trading/client-pages/market/trade-grid.tsx
+++ apps/trading/client-pages/market/trade-grid.tsx
@@ -73,7 +73,7 @@ const MainGrid = memo(
             </ResizableGridPanel>
             <ResizableGridPanel
               minSize={200}
-              preferredSize={sizesMiddle[1] || 300}
+              preferredSize={sizesMiddle[1] || 275}
             >
               <TradeGridChild>
                 <Tabs storageKey="console-trade-grid-main-right">
@@ -181,7 +181,7 @@ const TradeGridChild = ({ children }: TradeGridChildProps) => {
         {({ width, height }) => (
           <div
             style={{ width, height }}
-            className="border border-default rounded-sm"
+            className="border rounded-sm border-default"
           >
             {children}
           </div>
