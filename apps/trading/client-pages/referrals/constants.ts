92a291522b485780bb493148657253bea7c618cf
Matthew Russell <mattrussell36@gmail.com>
2023-09-18 16:25:54 -0700

    fix: sky bgs

--- apps/trading/client-pages/referrals/constants.ts
+++ apps/trading/client-pages/referrals/constants.ts
@@ -3,4 +3,4 @@ export const GRADIENT =
   'bg-gradient-to-b from-vega-clight-800 dark:from-vega-cdark-800 to-transparent';
 
 export const SKY_BACKGROUND =
-  'bg-[url(/assets/sky-light.png)] dark:bg-[url(/assets/sky-dark.png)] bg-[40%_0px] bg-[length:1440px] bg-no-repeat';
+  'bg-[url(/sky-light.png)] dark:bg-[url(/sky-dark.png)] bg-[40%_0px] bg-[length:1440px] bg-no-repeat';
