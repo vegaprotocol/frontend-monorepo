896245813e1c0c1185ef2c70c0e534dca80bac3d
m.ray <16125548+MadalinaRaicu@users.noreply.github.com>
2023-09-19 16:20:05 +0100

    Update libs/markets/src/lib/components/market-info/tooltip-mapping.tsx
    Co-authored-by: candida-d <62548908+candida-d@users.noreply.github.com>

--- libs/markets/src/lib/components/market-info/tooltip-mapping.tsx
+++ libs/markets/src/lib/components/market-info/tooltip-mapping.tsx
@@ -140,7 +140,7 @@ export const tooltipMapping: Record<string, ReactNode> = {
     'This is a network parameter. It sets how much an LP forfeits of their bond if they reduce their commitment while the market is below target stake. If set to 0 there is no penalty for early exit, if set to 1 their entire bond is forfeited if they exit their entire commitment, if set >1, their entire bond will be forfeited for exiting 1/earlyExitPenalty of their commitment amount. Must be a decimal ≥0. '
   ),
   probabilityOfTradingTauScaling: t(
-    `This is a network parameter. It sets how the probability of trading is calculated from the risk model. This is used to measure the relative competitiveness of an LP's supplied volume.`
+    `Determines how the probability of trading is scaled from the risk model, and is used to measure the relative competitiveness of an LP's supplied volume. This is a network parameter.`
   ),
   minimumProbabilityOfTradingLPOrders: t(
     'This is a network parameter. It sets a lower bound on the result of the probability of trading calculation.'
