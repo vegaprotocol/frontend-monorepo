ae59a7809801fd7bd2652cbbdac2f653eb69da63
Madalina Raicu <madalina@raygroup.uk>
2023-09-19 15:15:01 +0100

    chore(trading): remove static assets console coming from vega.xyz

--- apps/trading/pages/_document.page.tsx
+++ apps/trading/pages/_document.page.tsx
@@ -4,33 +4,16 @@ export default function Document() {
   return (
     <>
       <Head>
-        {/* 
+        {/*
           meta tags
-          - next advised against using _document for this, so they exist in our 
+          - next advised against using _document for this, so they exist in our
           - single page index.page.tsx
         */}
 
         {/* icons */}
-        <link
-          rel="icon"
-          type="image/x-icon"
-          href="https://static.vega.xyz/favicon.ico"
-        />
-        <link
-          rel="apple-touch-icon"
-          content="https://static.vega.xyz/favicon.ico"
-        />
+        <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+        <link rel="apple-touch-icon" content="./favicon.ico" />
 
-        {/* fonts */}
-        <link
-          rel="preload"
-          href="https://static.vega.xyz/AlphaLyrae-Medium.woff2"
-          as="font"
-          type="font/woff2"
-        />
-
-        {/* styles */}
-        <link rel="stylesheet" href="https://static.vega.xyz/fonts.css" />
         {/* eslint-disable-next-line @next/next/no-css-tags */}
         <link rel="stylesheet" href="/preloader.css" media="all" />
 
