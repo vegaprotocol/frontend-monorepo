9039b0304c61de9ff778683da5a1ac7b86f57055
Matthew Russell <mattrussell36@gmail.com>
2023-09-21 00:09:46 -0700

    feat: handle approvals

--- apps/trading/client-pages/deposit/deposit.tsx
+++ apps/trading/client-pages/deposit/deposit.tsx
@@ -1,27 +1,34 @@
 import type { ReactNode} from 'react';
+import { useCallback, useEffect } from 'react';
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import * as Radio from '@radix-ui/react-radio-group';
 import { DepositGetStarted } from './deposit-get-started';
-import type {
-  AssetFieldsFragment} from '@vegaprotocol/assets';
+import type { AssetFieldsFragment } from '@vegaprotocol/assets';
+import { useAssetsDataProvider } from '@vegaprotocol/assets';
 import {
-  useAssetsDataProvider,
-} from '@vegaprotocol/assets';
-import { isAssetTypeERC20, truncateByChars } from '@vegaprotocol/utils';
+  addDecimal,
+  isAssetTypeERC20,
+  truncateByChars,
+} from '@vegaprotocol/utils';
 import { AssetStatus } from '@vegaprotocol/types';
 import { t } from '@vegaprotocol/i18n';
 import {
   Intent,
+  Loader,
   TradingButton,
   TradingInput,
-  VegaIcon,
-  VegaIconNames,
 } from '@vegaprotocol/ui-toolkit';
 import { useWeb3React } from '@web3-react/core';
-import { useWeb3ConnectStore } from '@vegaprotocol/web3';
-import { useDepositBalances } from '@vegaprotocol/deposits';
-import type BigNumber from 'bignumber.js';
+import {
+  EthTxStatus,
+  useEthereumConfig,
+  useEthTransactionStore,
+  useWeb3ConnectStore,
+} from '@vegaprotocol/web3';
+import BigNumber from 'bignumber.js';
+import { Token } from '@vegaprotocol/smart-contracts';
+import { MaxUint256 } from '@ethersproject/constants';
 
 export const Deposit = () => {
   return (
@@ -40,10 +47,18 @@ const DepositFlowContainer = () => {
   const [searchParams] = useSearchParams();
   const assetId = searchParams.get('assetId') || undefined;
   const { data } = useAssetsDataProvider();
+  const { config } = useEthereumConfig();
 
   if (!data) return null;
+  if (!config) return null;
 
-  return <DepositFlow assets={data} assetId={assetId} />;
+  return (
+    <DepositFlow
+      assets={data}
+      assetId={assetId}
+      bridgeAddress={config.collateral_bridge_contract.address}
+    />
+  );
 };
 
 const DepositSteps = {
@@ -57,71 +72,121 @@ interface DepositState {
   step: Step;
   asset: AssetFieldsFragment | undefined;
   amount: string;
+  allowance: BigNumber | undefined;
 }
 
 const DepositFlow = ({
   assets,
   assetId,
+  bridgeAddress,
 }: {
   assets: AssetFieldsFragment[];
   assetId?: string;
+  bridgeAddress: string;
 }) => {
+  const { provider, account } = useWeb3React();
   const [state, setState] = useState<DepositState>(() => {
     const asset = assets.find((a) => a.id === assetId);
     return {
       step: asset ? 'Approve' : 'Asset',
       asset,
       amount: '',
+      allowance: undefined,
     };
   });
 
-  const { getBalances, reset, balances } = useDepositBalances(state.asset);
+  const onApproved = useCallback(async () => {
+    if (!provider) throw new Error('no provider');
+    if (!account) throw new Error('no account');
+    if (!isAssetTypeERC20(state.asset)) throw new Error('no asset');
+
+    const signer = provider.getSigner();
+    const tokenContract = new Token(
+      state.asset.source.contractAddress,
+      signer || provider
+    );
+    const res = await tokenContract.allowance(account, bridgeAddress);
+    const allowance = new BigNumber(
+      addDecimal(res.toString(), state.asset.decimals)
+    );
+
+    setState((curr) => ({ ...curr, step: DepositSteps.Deposit, allowance }));
+  }, [state.asset, bridgeAddress, account, provider]);
+
+  const handleAssetChanged = async (asset: AssetFieldsFragment | undefined) => {
+    if (!asset) {
+      setState((curr) => ({
+        ...curr,
+        step: DepositSteps.Asset,
+        asset: undefined,
+        allowance: undefined,
+      }));
+      return;
+    }
+
+    if (!provider) throw new Error('no provider');
+    if (!account) throw new Error('no account');
+    if (!isAssetTypeERC20(asset)) throw new Error('invalid asset');
+
+    const signer = provider.getSigner();
+    const tokenContract = new Token(
+      asset.source.contractAddress,
+      signer || provider
+    );
+    const res = await tokenContract.allowance(account, bridgeAddress);
+    const allowance = new BigNumber(addDecimal(res.toString(), asset.decimals));
+
+    const step = allowance.isGreaterThan(0)
+      ? DepositSteps.Deposit
+      : DepositSteps.Approve;
+
+    setState((curr) => ({
+      ...curr,
+      step,
+      asset,
+      allowance,
+    }));
+  };
 
   return (
-    <div className="flex flex-col border rounded border-default">
-      <StepWrapper title={t('Select asset')}>
-        <AssetSelector
-          asset={state.asset}
-          assets={assets}
-          onSelect={(asset) =>
-            setState((curr) => ({
-              ...curr,
-              step: DepositSteps.Approve,
-              asset,
-            }))
-          }
-        />
-      </StepWrapper>
-      <StepWrapper title={t('Approve')}>
-        <Approval
-          step={state.step}
-          asset={state.asset}
-          allowance={balances?.allowance}
-        />
-      </StepWrapper>
-      <StepWrapper title={t('Submit deposit')}>
-        <SendDeposit
-          step={state.step}
-          asset={state.asset}
-          allowance={balances?.allowance}
-          amount={state.amount}
-          setAmount={(amount) => setState((curr) => ({ ...curr, amount }))}
-        />
-      </StepWrapper>
-    </div>
+    <>
+      <div className="flex flex-col border rounded border-default">
+        <StepWrapper>
+          <AssetSelector
+            asset={state.asset}
+            assets={assets}
+            onSelect={handleAssetChanged}
+          />
+        </StepWrapper>
+        <StepWrapper>
+          <Approval
+            step={state.step}
+            asset={state.asset}
+            allowance={state.allowance}
+            bridgeAddress={bridgeAddress}
+            onApproved={onApproved}
+          />
+        </StepWrapper>
+        <StepWrapper>
+          <SendDeposit
+            step={state.step}
+            asset={state.asset}
+            allowance={state.allowance}
+            amount={state.amount}
+            setAmount={(amount) => setState((curr) => ({ ...curr, amount }))}
+          />
+        </StepWrapper>
+      </div>
+      <pre className="fixed bottom-0 right-0 p-2 text-xs text-white bg-vega-pink">
+        {JSON.stringify(state, null, 2)}
+      </pre>
+    </>
   );
 };
 
-const StepWrapper = ({
-  children,
-  title,
-}: {
-  children: ReactNode;
-  title: string;
-}) => {
+const StepWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col items-start p-4 border-b rounded gap-3 last:border-b-0 border-default">
-      <h3 className="text-lg">{title}</h3>
       {children}
     </div>
   );
@@ -140,18 +205,24 @@ const AssetSelector = ({
 
   if (asset && isAssetTypeERC20(asset)) {
     return (
-      <div>
-        <div className="flex items-center gap-2">
-          <button
-            onClick={() => onSelect(undefined)}
-            className="w-5 h-5 border-2 rounded border-vega-clight-600"
-          >
-            <div className="block w-4 h-4 border-2 border-white bg-vega-clight-300" />
-          </button>
-          <p className="block text-lg">
-            {asset.symbol} <small>({asset.source.contractAddress})</small>
-          </p>
-        </div>
+      <div className="flex items-center w-full gap-2">
+        <button
+          onClick={() => onSelect(undefined)}
+          className="w-5 h-5 border-2 rounded border-vega-clight-600"
+        >
+          <div className="block w-4 h-4 border-2 border-white bg-vega-clight-300" />
+        </button>
+        <p className="block text-lg">
+          {asset.symbol}{' '}
+          <small>({truncateByChars(asset.source.contractAddress)})</small>
+        </p>
+        <TradingButton
+          onClick={() => onSelect(undefined)}
+          size="small"
+          className="ml-auto"
+        >
+          Change asset
+        </TradingButton>
       </div>
     );
   }
@@ -160,6 +231,11 @@ const AssetSelector = ({
     .filter((a) => a.status === AssetStatus.STATUS_ENABLED)
     .filter((a) => a.source.__typename === 'ERC20')
     .filter((a) => a.symbol.toLowerCase().includes(search.toLowerCase()));
+  filteredAssets.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
 
   return (
     <div className="flex flex-col w-full gap-4">
@@ -213,48 +289,118 @@ const Approval = ({
   step,
   asset,
   allowance,
+  bridgeAddress,
+  onApproved,
 }: {
   step: Step;
   asset: AssetFieldsFragment | undefined;
   allowance: BigNumber | undefined;
+  bridgeAddress: string;
+  onApproved: () => void;
 }) => {
   const openDialog = useWeb3ConnectStore((store) => store.open);
-  const { account } = useWeb3React();
+  const { account, provider } = useWeb3React();
+  const [id, setId] = useState<number | null>(null);
 
-  if (step !== DepositSteps.Approve) return null;
+  const tx = useEthTransactionStore((store) => {
+    return store.transactions.find((t) => t?.id == id);
+  });
+  const send = useEthTransactionStore((store) => store.create);
+
+  const handleApprove = () => {
+    if (!provider) throw new Error('no provider');
+    if (!isAssetTypeERC20(asset)) throw new Error('no provider');
+    const signer = provider.getSigner();
+    const contract = new Token(
+      asset.source.contractAddress,
+      signer || provider
+    );
+    const id = send(contract, 'approve', [
+      bridgeAddress,
+      MaxUint256.toString(),
+    ]);
+    setId(id);
+  };
+
+  useEffect(() => {
+    if (tx?.status === EthTxStatus.Confirmed) {
+      onApproved();
+    }
+  }, [tx?.status, onApproved]);
 
   if (!asset) {
-    return <p>Please select asset</p>;
+    return <h3 className="text-lg">Approval</h3>;
+  }
+
+  if (tx) {
+    let title = t('Approval');
+    if (tx.status === EthTxStatus.Confirmed) {
+      title = t('Approved');
+    } else if (tx.status === EthTxStatus.Pending) {
+      title = t('Waiting for approval...');
+    }
+    return (
+      <div className="flex items-center justify-between w-full">
+        <h3 className="text-lg">{title}</h3>
+        <TradingButton onClick={handleApprove} size="small">
+          {t('Re-approve')}
+        </TradingButton>
+      </div>
+    );
+  }
+
+  if (step !== DepositSteps.Approve) {
+    if (!allowance) {
+      return <h3 className="text-lg">Approval</h3>;
+    }
+    if (allowance.isGreaterThan(0)) {
+      return (
+        <div className="flex items-center justify-between w-full">
+          <h3 className="text-lg">{t('Approved')}</h3>
+          <TradingButton onClick={handleApprove} size="small">
+            {t('Re-approve')}
+          </TradingButton>
+        </div>
+      );
+    }
   }
 
   if (!account) {
     return (
-      <TradingButton onClick={openDialog}>
-        {t('Connect Ethereum wallet')}
-      </TradingButton>
+      <div className="flex items-center justify-between w-full">
+        <h3 className="text-lg">{t('Approved')}</h3>
+        <TradingButton onClick={openDialog} size="small">
+          {t('Connect Ethereum wallet')}
+        </TradingButton>
+      </div>
     );
   }
 
   if (!allowance) {
-    return (
-      <div>
-        <p>{t('Confirming allowance...')}</p>
-      </div>
-    );
-  }
-
-  if (!allowance.isZero()) {
-    return (
-      <div>
-        <p>{t('Deposits approved')}</p>
-      </div>
-    );
+    return <h3 className="text-lg">Loading balances...</h3>;
   }
 
   return (
     <div className="flex items-center justify-between w-full">
-      <p>{t('Deposits not approved')}</p>
-      <TradingButton onClick={() => alert('TODO:')}>
+      <h3 className="text-lg">{t('Deposits not approved')}</h3>
+      <TradingButton
+        onClick={() => {
+          if (!provider) throw new Error('no provider');
+          if (!isAssetTypeERC20(asset)) throw new Error('no provider');
+          const signer = provider.getSigner();
+          const contract = new Token(
+            asset.source.contractAddress,
+            signer || provider
+          );
+          const id = send(contract, 'approve', [
+            bridgeAddress,
+            MaxUint256.toString(),
+          ]);
+          setId(id);
+        }}
+        size="small"
+        intent={Intent.Success}
+      >
         {t('Approve %s deposits', asset.symbol)}
       </TradingButton>
     </div>
@@ -277,38 +423,44 @@ const SendDeposit = ({
   const openDialog = useWeb3ConnectStore((store) => store.open);
   const { account } = useWeb3React();
 
-  if (step === DepositSteps.Asset) return null;
+  if (step !== DepositSteps.Deposit) {
+    return <h3 className="text-lg">Confirm deposit</h3>;
+  }
 
   if (!asset) {
     return <p>Please select asset</p>;
   }
 
-  if (!allowance) return null;
+  if (!allowance) {
+    return <h3 className="text-lg">Confirm deposit</h3>;
+  }
   if (allowance.isZero()) return null;
 
   if (!account) {
     return (
-      <TradingButton onClick={openDialog}>
+      <TradingButton onClick={openDialog} size="small">
         {t('Connect Ethereum wallet')}
       </TradingButton>
     );
   }
 
   return (
-    <div className="w-full">
+    <div className="flex items-center justify-between w-full">
+      <h3 className="text-lg">{t('Confirm deposit')}</h3>
       <form
-        className="flex items-center justify-between gap-4"
+        className="flex items-center justify-between w-1/2 gap-2"
         onSubmit={() => alert('TODO')}
       >
-        <div className="max-w-[300px]">
+        <div className="flex-1">
           <TradingInput
             name="amount"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
             placeholder="Enter amount"
+            className="w-full"
           />
         </div>
-        <TradingButton type="submit" intent={Intent.Success}>
+        <TradingButton type="submit" size="small" intent={Intent.Success}>
           {t('Deposit %s', asset.symbol)}
         </TradingButton>
       </form>
