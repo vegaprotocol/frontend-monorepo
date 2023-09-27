7a03692c2518d51c0c4832282762691b5ac12e7f
maciek <maciek@vegaprotocol.io>
2023-09-27 17:34:33 +0200

    feat: add market termination warning banner - small improvements

--- apps/trading/components/market-banner/market-termination-banner.tsx
+++ apps/trading/components/market-banner/market-termination-banner.tsx
@@ -13,7 +13,7 @@ import { formatDateWithLocalTimezone } from '@vegaprotocol/utils';
 import { formatDuration, intervalToDuration } from 'date-fns';
 
 export const MarketTerminationBanner = () => {
-  const [visible, setVisible] = useState(true);
+  const [hiddens, setHiddens] = useState<string[]>([]);
   const { pubKey, pubKeys } = useVegaWallet();
 
   const partyIds = pubKeys?.map((item) => item.publicKey) || [];
@@ -26,7 +26,7 @@ export const MarketTerminationBanner = () => {
   });
   const marketsId = (positionsData || []).map((item) => item.market.id);
 
-  const skip = !pubKey || !visible || !marketsId.length;
+  const skip = !pubKey || !marketsId.length;
   const proposalsData = useGetTerminationProposals({ skip });
 
   const marketsMatched = (proposalsData || [])
@@ -49,8 +49,9 @@ export const MarketTerminationBanner = () => {
           : '',
     }));
 
-  if (visible && marketsMatched.length) {
+  if (marketsMatched.length) {
     return marketsMatched.map((item) => {
+      const hide = hiddens.includes(item.id);
       const dayMonthDate = formatDateWithLocalTimezone(
         new Date(item.enactmentDatetime),
         'dd MMMM'
@@ -62,33 +63,33 @@ export const MarketTerminationBanner = () => {
       const formattedDuration = formatDuration(duration, {
         format: ['days', 'hours'],
       });
+      if (hide) return null;
       return (
-        <>
-          <NotificationBanner
-            intent={Intent.Warning}
-            onClose={() => {
-              setVisible(false);
-            }}
-            data-testid={`termination-warning-banner-${item.id}`}
-          >
-            <div className="uppercase mb-1">
-              {t('Trading on Market %s will stop on %s', [
-                item.name,
-                dayMonthDate,
-              ])}
-            </div>
-            <div>
-              {t('Market')}{' '}
-              <ExternalLink href={`/#/markets/${item.id}`}>
-                {item.name}
-              </ExternalLink>
-              {t(
-                'will close to trading in %s. You will not be able to hold a position on this market after %s.',
-                [formattedDuration, dayMonthDate]
-              )}
-            </div>
-          </NotificationBanner>
-        </>
+        <NotificationBanner
+          key={item.id}
+          intent={Intent.Warning}
+          onClose={() => {
+            setHiddens((hiddens) => [...hiddens, item.id]);
+          }}
+          data-testid={`termination-warning-banner-${item.id}`}
+        >
+          <div className="uppercase mb-1">
+            {t('Trading on Market %s will stop on %s', [
+              item.name,
+              dayMonthDate,
+            ])}
+          </div>
+          <div>
+            {t('Market')}{' '}
+            <ExternalLink href={`/#/markets/${item.id}`}>
+              {item.name}
+            </ExternalLink>
+            {t(
+              'will close to trading in %s. You will not be able to hold a position on this market after %s.',
+              [formattedDuration, dayMonthDate]
+            )}
+          </div>
+        </NotificationBanner>
       );
     });
   }
