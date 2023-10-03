a6f7de1f728cabdb7ba9369dbf64073faa269ad6
Bartłomiej Głownia <bglownia@gmail.com>
2023-10-03 15:16:26 +0200

    feat: fix checkbox data-testid

--- libs/ui-toolkit/src/components/trading-radio-group/radio-group.tsx
+++ libs/ui-toolkit/src/components/trading-radio-group/radio-group.tsx
@@ -78,11 +78,12 @@ export const TradingRadio = ({ id, value, label, disabled }: RadioProps) => {
     'border-vega-clight-700 dark:border-vega-cdark-700'
   );
   return (
-    <label className={wrapperClasses} htmlFor={id} data-testid={id}>
+    <label className={wrapperClasses} htmlFor={id}>
       <RadioGroupPrimitive.Item
         value={value}
         className={itemClasses}
         id={id}
+        data-testid={id}
         disabled={disabled}
       >
         <RadioGroupPrimitive.Indicator className={indicatorClasses} />
