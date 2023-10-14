b207650edf63f74dd1113b3ac1fe13a7f8d36e8b
Matthew Russell <mattrussell36@gmail.com>
2023-10-13 19:45:45 -0700

    feat: align market card design

--- apps/trading/client-pages/deposit/markets.tsx
+++ apps/trading/client-pages/deposit/markets.tsx
@@ -8,6 +8,7 @@ import {
 } from '@vegaprotocol/utils';
 import { BigNumber } from 'bignumber.js';
 import { t } from '@vegaprotocol/i18n';
+import classNames from 'classnames';
 
 export const Markets = ({
   markets,
@@ -37,39 +38,49 @@ const MarketCard = ({ market }: { market: MarketMaybeWithDataAndCandles }) => {
     vol && vol !== '0'
       ? addDecimalsFormatNumber(vol, market.positionDecimalPlaces)
       : '0.00';
-  const change = priceChangePercentage(
-    oneDayCandles?.map((c) => c.close) || []
+  const change = new BigNumber(
+    priceChangePercentage(oneDayCandles?.map((c) => c.close) || [])
   );
+
   return (
-    <div className="flex w-1/4 p-2 rounded gap-2 bg-vega-clight-600 dark:bg-vega-cdark-600">
-      <div className="flex-1">
-        <h3 className="overflow-hidden text-sm leading-4 whitespace-nowrap text-ellipsis">
+    <div className="flex flex-col w-1/4 p-4 rounded-lg gap-2 bg-vega-clight-600 dark:bg-vega-cdark-600">
+      <div>
+        <h3 className="leading-none">
           {market.tradableInstrument.instrument.code}
         </h3>
-
-        <p className="font-mono text-xs">{volume}</p>
+        <p className="text-muted">Vol {volume}</p>
       </div>
-      <div className="flex justify-end w-1/3">
-        {oneDayCandles && (
-          <Sparkline
-            width={60}
-            height={20}
-            data={oneDayCandles.map((c) => Number(c.close))}
-          />
-        )}
-      </div>
-      <div className="w-1/3 font-mono text-xs text-right">
-        {market.data?.markPrice && (
-          <>
-            <div>
-              {addDecimalsFormatNumber(
-                market.data.markPrice,
-                market.decimalPlaces
-              )}
-            </div>
-            <div>{formatNumberPercentage(new BigNumber(change), 2)}</div>
-          </>
-        )}
+      <div className="flex items-end justify-between gap-8">
+        <div>
+          {market.data?.markPrice && (
+            <>
+              <p className="leading-none">
+                {addDecimalsFormatNumber(
+                  market.data.markPrice,
+                  market.decimalPlaces
+                )}
+              </p>
+              <p
+                className={classNames('text-sm leading-tight align-baseline', {
+                  'text-market-green': change.isGreaterThan(0),
+                  'text-market-red': change.isLessThan(0),
+                  'text-muted': change.isZero(),
+                })}
+              >
+                {formatNumberPercentage(change, 2)}
+              </p>
+            </>
+          )}
+        </div>
+        <div className="flex-1 max-w-[50%]">
+          {oneDayCandles && (
+            <Sparkline
+              width="100%"
+              height={36}
+              data={oneDayCandles.map((c) => Number(c.close))}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
