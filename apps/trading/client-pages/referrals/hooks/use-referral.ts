c1ea5631eaa690b2f131b003a1e7361051a31f03
asiaznik <artur@vegaprotocol.io>
2023-10-19 14:01:47 +0200

    chore: really more timeout

--- apps/trading/client-pages/referrals/hooks/use-referral.ts
+++ apps/trading/client-pages/referrals/hooks/use-referral.ts
@@ -77,7 +77,7 @@ export const useReferral = (args: UseReferralArgs) => {
     },
     skip: !referralSet?.id,
     fetchPolicy: 'cache-and-network',
-    //context: { isEnlargedTimeout: true },
+    context: { isEnlargedTimeout: true },
   });
 
   const referees = compact(
