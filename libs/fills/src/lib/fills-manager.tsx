0ef3ba4ae9db2a8bef089a4fac4d49cdd953bb08
Bartłomiej Głownia <bglownia@gmail.com>
2023-10-20 16:37:31 +0200

    feat: add load more and rows matching selected filter

--- libs/fills/src/lib/fills-manager.tsx
+++ libs/fills/src/lib/fills-manager.tsx
@@ -1,11 +1,12 @@
 import type { AgGridReact } from 'ag-grid-react';
-import { useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { t } from '@vegaprotocol/i18n';
 import { FillsTable } from './fills-table';
 import type { useDataGridEvents } from '@vegaprotocol/datagrid';
 import { useDataProvider } from '@vegaprotocol/data-provider';
 import type * as Schema from '@vegaprotocol/types';
 import { fillsWithMarketProvider } from './fills-data-provider';
+import { TradingButton as Button } from '@vegaprotocol/ui-toolkit';
 
 interface FillsManagerProps {
   partyId: string;
@@ -22,45 +23,68 @@ export const FillsManager = ({
   const filter: Schema.TradesFilter | Schema.TradesSubscriptionFilter = {
     partyIds: [partyId],
   };
-  const [dateRange, setDateRange] = useState<Schema.DateRange | null>(null);
-  const { data, error } = useDataProvider({
+  const [hasFilteredRows, setHasFilteredRowsCount] = useState<
+    boolean | undefined
+  >(undefined);
+  const { data, error, load, pageInfo } = useDataProvider({
     dataProvider: fillsWithMarketProvider,
-    update: ({ data }) => {
-      if (data?.length && gridRef.current?.api) {
-        gridRef.current?.api.setRowData(data);
-        return true;
-      }
-      return false;
-    },
-    variables: { filter, dateRange },
+    variables: { filter },
   });
   const { onFilterChanged, ...props } = gridProps;
-  const overlayNoRowsTemplate = dateRange
-    ? t('No fills matching selected date range')
-    : t('No fills');
+  const onRowDataUpdated = useCallback(
+    ({ api }: { api: AgGridReact['api'] }) => {
+      let hasFilteredRows = false;
+      try {
+        api.forEachNodeAfterFilter(() => {
+          throw new Error();
+        });
+      } catch (e) {
+        hasFilteredRows = true;
+      }
+      setHasFilteredRowsCount(hasFilteredRows);
+    },
+    []
+  );
   return (
-    <FillsTable
-      ref={gridRef}
-      rowData={data}
-      onFilterChanged={(event) => {
-        const { api } = event;
-        const filterModel = api.getFilterModel();
-        if (filterModel['createdAt']?.value) {
-          if (
-            filterModel['createdAt'].value.start !== dateRange?.start ||
-            filterModel['createdAt'].value.end !== dateRange?.end
-          ) {
-            setDateRange(filterModel['createdAt'].value);
-          }
-        } else {
-          setDateRange(null);
-        }
-        onFilterChanged(event);
-      }}
-      partyId={partyId}
-      onMarketClick={onMarketClick}
-      overlayNoRowsTemplate={error ? error.message : overlayNoRowsTemplate}
-      {...props}
-    />
+    <div className="flex flex-col h-full">
+      <FillsTable
+        ref={gridRef}
+        rowData={data}
+        onFilterChanged={(event) => {
+          onRowDataUpdated(event);
+          onFilterChanged(event);
+        }}
+        onRowDataUpdated={onRowDataUpdated}
+        partyId={partyId}
+        onMarketClick={onMarketClick}
+        overlayNoRowsTemplate={error ? error.message : t('No fills')}
+        {...props}
+      />
+
+      <div className="flex justify-between border-t border-default p-1 items-center">
+        <div className="text-xs">
+          {t(
+            'Depending on data node retention you may not be able see the "full" history'
+          )}
+        </div>
+        <div className="flex text-xs items-center">
+          {data?.length && !pageInfo?.hasNextPage
+            ? t('all %s items loaded', [data.length.toString()])
+            : t('%s items loaded', [
+                data?.length ? data.length.toString() : ' ',
+              ])}
+          {pageInfo?.hasNextPage ? (
+            <Button size="extra-small" className="ml-1" onClick={() => load()}>
+              {t('Load more')}
+            </Button>
+          ) : null}
+        </div>
+        {data?.length && hasFilteredRows === false ? (
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs">
+            {t('No fills matching selected filters')}
+          </div>
+        ) : null}
+      </div>
+    </div>
   );
 };
