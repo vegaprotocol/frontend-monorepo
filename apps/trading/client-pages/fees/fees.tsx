429b7e75e110640d2d26150cf688d5df3fa85483
Matthew Russell <mattrussell36@gmail.com>
2023-10-23 10:06:02 -0700

    fix: allow scrolling of main section in layout with sidebar

--- apps/trading/client-pages/fees/fees.tsx
+++ apps/trading/client-pages/fees/fees.tsx
@@ -3,8 +3,8 @@ import { FeesContainer } from '../../components/fees-container';
 
 export const Fees = () => {
   return (
-    <div className="container px-4 mx-auto">
-      <h1 className="p-4 text-2xl">{t('Fees')}</h1>
+    <div className="container p-4 mx-auto">
+      <h1 className="px-4 pb-4 text-2xl">{t('Fees')}</h1>
       <FeesContainer />
     </div>
   );
