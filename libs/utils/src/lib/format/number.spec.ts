2fd23c4adb4393a1650699c38e0db69ae6e954cd
Matthew Russell <mattrussell36@gmail.com>
2023-11-01 12:14:40 -0700

    test: update tests to use natural dps in tests

--- libs/utils/src/lib/format/number.spec.ts
+++ libs/utils/src/lib/format/number.spec.ts
@@ -77,8 +77,8 @@ describe('number utils', () => {
     { v: new BigNumber(123.123), d: 3, o: '123.123%' },
     { v: new BigNumber(123.123), d: 6, o: '123.123%' },
     { v: new BigNumber(123.123), d: 0, o: '123%' },
-    { v: new BigNumber(123), d: undefined, o: '123.00%' }, // it default to 2 decimal places
-    { v: new BigNumber(30000), d: undefined, o: '30,000.00%' },
+    { v: new BigNumber(123), d: undefined, o: '123%' }, // it default to 2 decimal places
+    { v: new BigNumber(30000), d: undefined, o: '30,000%' },
     { v: new BigNumber(3.000001), d: undefined, o: '3.000001%' },
   ])('formats given number correctly', ({ v, d, o }) => {
     expect(formatNumberPercentage(v, d)).toStrictEqual(o);
