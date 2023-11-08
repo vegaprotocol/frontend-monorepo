73f37c24770fb31e115d613e1c28e8bb49caaeff
Ben <ben@vega.xyz>
2023-11-08 09:43:36 +0000

    chore(ci): fix prepare dist (#5214)

--- docker/prepare-dist.sh
+++ docker/prepare-dist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 yarn --pure-lockfile
 app=${1:-trading}
-envCmd="envCmd="yarn env-cmd -f ./apps/${app}/.env.${2:-mainnet}"
+envCmd="envCmd="yarn -f ./apps/${app}/.env.${2:-mainnet}"
 yarn install
 if [ "${app}" = "trading" ]; then
   $envCmd yarn nx export trading
-  DIST_LOCATION=dist/apps/trading/exported
+  DIST_LOCATION=dist/apps/trading/exported/
 else
   $envCmd yarn nx build ${app}
   DIST_LOCATION=dist/apps/${app}
