4cbee21cc531e5d5b62fe6356cde882439b8d385
Matthew Russell <mattrussell36@gmail.com>
2023-11-11 13:36:56 -0800

    chore: add missing d3 deps, fix imports for jest

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,9 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  moduleNameMapper: {
+    ...nxPreset.moduleNameMapper,
+    '^d3-(.*)$': 'd3-$1/dist/d3-$1',
+  },
+};
