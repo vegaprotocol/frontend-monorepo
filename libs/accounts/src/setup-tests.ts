fb7938d31e793e363e105bceaeca3977b0e0765d
Bartłomiej Głownia <bglownia@gmail.com>
2023-11-13 13:15:37 +0100

    feat: fix accounts unit tests

--- libs/accounts/src/setup-tests.ts
+++ libs/accounts/src/setup-tests.ts
@@ -1,6 +1,21 @@
 import '@testing-library/jest-dom';
 import ResizeObserver from 'resize-observer-polyfill';
 import { defaultFallbackInView } from 'react-intersection-observer';
+import { locales } from '@vegaprotocol/i18n';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
 defaultFallbackInView(true);
+global.ResizeObserver = ResizeObserver;
+
+// Set up i18n instance so that components have the correct default
+// en translations
+i18n.use(initReactI18next).init({
+  // we init with resources
+  resources: locales,
+  fallbackLng: 'en',
+  ns: ['accounts'],
+  defaultNS: 'accounts',
+});
+
 global.ResizeObserver = ResizeObserver;
