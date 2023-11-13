e565b3887c9d596b44b56a1e1bc5cd0b82897048
Matthew Russell <mattrussell36@gmail.com>
2023-11-12 23:46:19 -0800

    chore: comment

--- jest.preset.js
+++ jest.preset.js
@@ -4,6 +4,7 @@ module.exports = {
   ...nxPreset,
   moduleNameMapper: {
     ...nxPreset.moduleNameMapper,
+    // this mapping fixes jest breaking if anything tries to import d3 due to esm exports
     '^d3-(.*)$': 'd3-$1/dist/d3-$1',
   },
 };
