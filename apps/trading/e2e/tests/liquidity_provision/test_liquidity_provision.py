0270b16af665f486631e630f203f6464a73364f7
bwallacee <ben@vega.xyz>
2023-11-14 17:36:00 +0000

    chore(trading): update lp test

--- apps/trading/e2e/tests/liquidity_provision/test_liquidity_provision.py
+++ apps/trading/e2e/tests/liquidity_provision/test_liquidity_provision.py
@@ -27,7 +27,7 @@ def test_liquidity_provision_amendment(continuous_market, vega: VegaService, pag
     page.reload()
     row = page.get_by_test_id("tab-myLP").locator(".ag-center-cols-container .ag-row").first
     expect(row).to_contain_text(
-        "10,000.0010,000.000.00%10,000.00100.00%10,000.00-100.00%0.00%---Active"
+        "Active"
     )
     vega.submit_simple_liquidity(
         key_name="mm",
@@ -42,11 +42,11 @@ def test_liquidity_provision_amendment(continuous_market, vega: VegaService, pag
     page.reload()
     row = page.get_by_test_id("tab-myLP").locator(".ag-center-cols-container .ag-row").first
     expect(row).to_contain_text(
-        "10,000.00/100.00100.000.00%/0.10%10,000.00100.00%10,000.00-100.00%100.00%---Updating next epoch5/9/2023"
+        "Updating next epoch"
     )
     next_epoch(vega=vega)
     page.reload()
     row = page.get_by_test_id("tab-myLP").locator(".ag-center-cols-container .ag-row").first
     expect(row).to_contain_text(
-        "100.00100.000.10%100.00100.00%100.00-100.00%0.00%100.00%0.00%0.00%Active"
+        "Active"
     )
