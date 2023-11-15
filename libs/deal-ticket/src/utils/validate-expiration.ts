925263a8b9032c93be805adf390ae4588dfc1feb
Matthew Russell <mattrussell36@gmail.com>
2023-11-14 18:40:43 -0800

    chore: fix type error

--- libs/deal-ticket/src/utils/validate-expiration.ts
+++ libs/deal-ticket/src/utils/validate-expiration.ts
@@ -2,7 +2,7 @@ import type { Validate } from 'react-hook-form';
 
 export const validateExpiration: (
   errorMessage: string
-) => Validate<string | undefined> =
+) => Validate<string | undefined, object> =
   (errorMessage: string) => (value?: string) => {
     const now = new Date();
     const valueAsDate = value ? new Date(value) : now;
