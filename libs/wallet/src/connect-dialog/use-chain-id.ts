ee2909cc8475a6a4753379b1dd6cabe8b38075a1
Art <artur@vegaprotocol.io>
2023-11-27 17:15:18 +0100

    fix(wallet): empty connect dialog (#5354)

--- libs/wallet/src/connect-dialog/use-chain-id.ts
+++ libs/wallet/src/connect-dialog/use-chain-id.ts
@@ -3,17 +3,37 @@ import { useVegaWallet } from '../use-vega-wallet';
 
 const cache: Record<string, string> = {};
 
+/**
+ * Gets the statistics url for a given vega url.
+ * Example:
+ * https://graphql.example.com/graphql -> https://graphql.example.com/statistics
+ */
+const getNodeStatisticsUrl = (vegaUrl: string) => {
+  try {
+    const url = new URL(vegaUrl);
+    url.pathname = 'statistics';
+    return url.toString();
+  } catch (err) {
+    return undefined;
+  }
+};
+
 export const useChainId = () => {
   const { vegaUrl } = useVegaWallet();
   const [chainId, setChainId] = useState<undefined | string>(cache[vegaUrl]);
   const [fetchAttempt, setFetchAttempt] = useState(1);
+
+  const statisticsUrl = getNodeStatisticsUrl(vegaUrl);
+
   useEffect(() => {
+    // abort when `/statistics` URL could not be determined
+    if (!statisticsUrl) return;
     let isCancelled = false;
     if (cache[vegaUrl]) {
       setChainId(cache[vegaUrl]);
       return;
     }
-    fetch(vegaUrl.replace('graphql', 'statistics'))
+    fetch(statisticsUrl)
       .then((response) => response.json())
       .then((response) => {
         if (isCancelled) {
@@ -32,6 +52,6 @@ export const useChainId = () => {
     return () => {
       isCancelled = true;
     };
-  }, [fetchAttempt, vegaUrl]);
+  }, [fetchAttempt, statisticsUrl, vegaUrl]);
   return chainId;
 };
