5357883cfac7afa3fa40019f924edcb3672f2349
Matthew Russell <mattrussell36@gmail.com>
2023-11-28 11:34:36 -0800

    fix: theme change reload, remove some features

--- libs/trading-view/src/lib/trading-view.tsx
+++ libs/trading-view/src/lib/trading-view.tsx
@@ -52,55 +52,64 @@ export const TradingView = ({
   const language = useLanguage();
   const chartContainerRef =
     useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+  // Cant get types as charting_library is externally loaded
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const widgetRef = useRef<any>();
 
   const datafeed = useDatafeed();
 
-  useEffect(() => {
-    // @ts-ignore cant import types as chartin_library is external
-    // eslint-disable-next-line
-    const widgetOptions: ChartingLibraryWidgetOptions = {
-      symbol: marketId,
-      // BEWARE: no trailing slash is expected in feed URL
+  useEffect(
+    () => {
+      // @ts-ignore cant import types as charting_library is external
       // eslint-disable-next-line
+      const widgetOptions: ChartingLibraryWidgetOptions = {
+        symbol: marketId,
+        // BEWARE: no trailing slash is expected in feed URL
+        // eslint-disable-next-line
 
-      datafeed,
-      // @ts-ignore cant import types as chartin_library is external
-      interval: '1' as ResolutionString,
-      container: chartContainerRef.current,
-      library_path: libraryPath,
-      custom_css_url: 'vega_styles.css',
-      // @ts-ignore cant import types as chartin_library is external
-      locale: language as LanguageCode,
-      disabled_features: [
-        'use_localstorage_for_settings',
-        'header_symbol_search',
-        'header_compare',
-      ],
-      enabled_features: ['study_templates'],
-      // TODO: remove
-      charts_storage_url: 'https://saveload.tradingview.com',
-      charts_storage_api_version: '1.1',
-      // TODO: remove
-      client_id: 'tradingview.com',
-      // TODO: remove
-      user_id: 'public_user_id',
-      fullscreen: false,
-      autosize: true,
-      theme,
-      overrides: {
-        // colors set here, trading view lets the user set a color
-        'paneProperties.background': theme === 'dark' ? '#05060C' : '#fff',
-        'paneProperties.backgroundType': 'solid',
-      },
-    };
+        datafeed,
+        // @ts-ignore cant import types as charting_library is external
+        interval: '1' as ResolutionString,
+        container: chartContainerRef.current,
+        library_path: libraryPath,
+        custom_css_url: 'vega_styles.css',
+        // @ts-ignore cant import types as charting_library is external
+        locale: language as LanguageCode,
+        disabled_features: [
+          'header_symbol_search',
+          'header_compare',
+          'show_object_tree',
+          'timeframes_toolbar',
+        ],
+        fullscreen: false,
+        autosize: true,
+        theme,
+        overrides: {
+          // colors set here, trading view lets the user set a color
+          'paneProperties.background': theme === 'dark' ? '#05060C' : '#fff',
+          'paneProperties.backgroundType': 'solid',
+        },
+      };
 
-    // @ts-ignore parent component loads TradingView onto window obj
-    const tvWidget = new window.TradingView.widget(widgetOptions);
+      // @ts-ignore parent component loads TradingView onto window obj
+      widgetRef.current = new window.TradingView.widget(widgetOptions);
 
-    return () => {
-      tvWidget.remove();
-    };
-  }, [theme, datafeed, marketId, libraryPath]);
+      return () => {
+        if (!widgetRef.current) return;
+        widgetRef.current.remove();
+      };
+    },
+
+    // No theme in deps to avoid full chart reload when the theme changes
+    // Instead the theme is changed programmitcally in a separate useEffect
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [datafeed, marketId, language, libraryPath]
+  );
+
+  useEffect(() => {
+    if (!widgetRef.current || !widgetRef.current._ready) return;
+    widgetRef.current.changeTheme(theme);
+  }, [theme]);
 
   return <div ref={chartContainerRef} className="w-full h-full" />;
 };
