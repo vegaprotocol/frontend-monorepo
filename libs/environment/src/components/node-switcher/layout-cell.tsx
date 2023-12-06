f56d34fe6ec279288e5cd42cde0c3c8945e25aed
Art <artur@vegaprotocol.io>
2023-12-06 14:31:08 +0100

    fix(environment): node switcher on mobile (#5455)

--- libs/environment/src/components/node-switcher/layout-cell.tsx
+++ libs/environment/src/components/node-switcher/layout-cell.tsx
@@ -19,16 +19,27 @@ export const LayoutCell = ({
 }: LayoutCellProps) => {
   const t = useT();
   const classes = [
-    'lg:text-right flex justify-between lg:block',
+    'lg:text-right flex lg:block justify-stretch gap-2',
     'my-2 lg:my-0',
   ];
 
   return (
     <div className={classnames(classes)}>
-      {label && <span className="lg:hidden">{label}</span>}
+      {label && (
+        <>
+          <span className="lg:hidden text-xs text-vega-clight-200 dark:text-vega-cdark-200 whitespace-nowrap">
+            {label}
+          </span>
+          <span
+            /* separator */
+            aria-hidden
+            className="border-b border-dashed border-b-vega-clight-400 dark:border-b-vega-cdark-400 w-full h-[9px]"
+          ></span>
+        </>
+      )}
       <span
         data-testid={dataTestId}
-        className={classnames('font-mono', {
+        className={classnames('font-mono text-xs lg:text-sm', {
           'text-danger': !isLoading && hasError,
           'text-muted': isLoading,
         })}
