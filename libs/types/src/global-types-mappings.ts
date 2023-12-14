b0b602e068221fb0b6d78a0135cd9573f01d1002
m.ray <16125548+MadalinaRaicu@users.noreply.github.com>
2023-12-14 16:13:38 +0000

    Update libs/types/src/global-types-mappings.ts
    Co-authored-by: candida-d <62548908+candida-d@users.noreply.github.com>

--- libs/types/src/global-types-mappings.ts
+++ libs/types/src/global-types-mappings.ts
@@ -601,7 +601,7 @@ export const DispatchMetricDescription: DispatchMetricLabel = {
   DISPATCH_METRIC_MAKER_FEES_RECEIVED:
     'Get rewards for making prices on the order book.',
   DISPATCH_METRIC_MARKET_VALUE: 'Get rewards for total market value',
-  DISPATCH_METRIC_AVERAGE_POSITION: 'Get rewards for Average position',
+  DISPATCH_METRIC_AVERAGE_POSITION: 'Get rewards for having an open position that is consistenly larger than that of other traders.',
   DISPATCH_METRIC_RELATIVE_RETURN: 'Get rewards for relative return',
   DISPATCH_METRIC_RETURN_VOLATILITY: 'Get rewards for having the least amount of variance in your returns while you have a position open during the rewards window.',
   DISPATCH_METRIC_VALIDATOR_RANKING: 'Get rewards for validator ranking',
