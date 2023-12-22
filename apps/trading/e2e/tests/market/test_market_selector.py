b943adfe1310b16f1f2268f6c7969b8956ee9171
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2023-12-22 13:34:29 +0100

    chore: revert changes for e2e tests

--- apps/trading/e2e/tests/market/test_market_selector.py
+++ apps/trading/e2e/tests/market/test_market_selector.py
@@ -16,7 +16,7 @@ def test_market_selector(continuous_market, page: Page):
     btc_market = page.locator('[data-testid="market-selector-list"] a')
     expect(btc_market.locator("h3")).to_have_text("BTC:DAI_2023Futr")
     expect(btc_market.locator('[data-testid="market-selector-volume"]')).to_have_text(
-        "1"
+        "0.00"
     )
     expect(btc_market.locator('[data-testid="market-selector-price"]')).to_have_text(
         "107.50 tDAI"
@@ -57,7 +57,7 @@ def test_market_selector_filter(continuous_market, page: Page):
     page.get_by_test_id("search-term").fill("btc")
     expect(page.locator('[data-testid="market-selector-list"] a')).to_have_count(1)
     expect(page.locator('[data-testid="market-selector-list"] a').nth(0)).to_have_text(
-        "BTC:DAI_2023107.50 tDAI1"
+        "BTC:DAI_2023107.50 tDAI0.00"
     )
 
     page.get_by_test_id("search-term").clear()
@@ -82,5 +82,5 @@ def test_market_selector_filter(continuous_market, page: Page):
     page.get_by_role("menuitemcheckbox").nth(0).get_by_text("tDAI").click()
     expect(page.locator('[data-testid="market-selector-list"] a')).to_have_count(1)
     expect(page.locator('[data-testid="market-selector-list"] a').nth(0)).to_have_text(
-        "BTC:DAI_2023107.50 tDAI1"
+        "BTC:DAI_2023107.50 tDAI0.00"
     )
