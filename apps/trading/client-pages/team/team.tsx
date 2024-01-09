fdbd13a095040c9f981549e739fe4f555f84df5e
Matthew Russell <mattrussell36@gmail.com>
2024-01-09 15:36:00 -0500

    fix: use correct bg color for gradient

--- apps/trading/client-pages/team/team.tsx
+++ apps/trading/client-pages/team/team.tsx
@@ -73,7 +73,7 @@ export const TeamPage = ({
   return (
     <div className="relative h-full overflow-y-auto">
       <div className="absolute top-0 left-0 w-full h-[40%] -z-10 bg-[40%_0px] bg-cover bg-no-repeat bg-local bg-[url(/cover.png)]">
-        <div className="absolute top-o left-0 w-full h-full bg-gradient-to-t from-white dark:from-black to-transparent from-20% to-60%" />
+        <div className="absolute top-o left-0 w-full h-full bg-gradient-to-t from-white dark:from-vega-cdark-900 to-transparent from-20% to-60%" />
       </div>
       <div className="flex flex-col gap-4 lg:gap-6 container p-4 mx-auto">
         <header className="flex gap-2 lg:gap-4 pt-5 lg:pt-10">
