b3bf4b76399837df87515ebed45e352416ddce29
Matthew Russell <mattrussell36@gmail.com>
2024-01-16 17:15:47 -0500

    feat: add box component for shared competitions styles

---
+++ apps/trading/components/competitions/box.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import { type HTMLAttributes } from 'react';
+
+export const BORDER_COLOR = 'border-vega-clight-500 dark:border-vega-cdark-500';
+export const GRADIENT =
+  'bg-gradient-to-b from-vega-clight-800 dark:from-vega-cdark-800 to-transparent';
+
+export const Box = (props: HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      {...props}
+      className={classNames(
+        BORDER_COLOR,
+        GRADIENT,
+        'border rounded-lg',
+        'p-6 flex flex-col items-center gap-6 text-center',
+        props.className
+      )}
+    />
+  );
+};
