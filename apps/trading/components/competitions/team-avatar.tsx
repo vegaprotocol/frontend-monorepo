6a810d00578011144dd8020b64782692c234d193
asiaznik <artur@vegaprotocol.io>
2024-01-16 14:12:23 +0100

    chore: team avatar sizes

--- apps/trading/components/competitions/team-avatar.tsx
+++ apps/trading/components/competitions/team-avatar.tsx
@@ -1,11 +1,25 @@
-export const TeamAvatar = ({ imgUrl }: { imgUrl: string }) => {
+import classNames from 'classnames';
+
+export const TeamAvatar = ({
+  imgUrl,
+  size = 'large',
+}: {
+  imgUrl: string;
+  size?: 'large' | 'small';
+}) => {
   // TODO: add fallback avatars
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
       src={imgUrl}
       alt="Team avatar"
-      className="rounded-full w-20 h-20 lg:w-[112px] lg:h-[112px] bg-vega-clight-700 dark:bg-vega-cdark-700 shrink-0"
+      className={classNames(
+        'rounded-full bg-vega-clight-700 dark:bg-vega-cdark-700 shrink-0',
+        {
+          'w-20 h-20 lg:w-[112px] lg:h-[112px]': size === 'large',
+          'w-10 h-10': size === 'small',
+        }
+      )}
       referrerPolicy="no-referrer"
     />
   );
