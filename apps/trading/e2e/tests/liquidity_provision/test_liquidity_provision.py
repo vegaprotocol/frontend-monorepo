913948569d30d6473f31db5583b17045d537ea32
bwallacee <ben@vega.xyz>
2024-01-18 15:55:28 +0000

    chore(trading): fix skipped tests

--- apps/trading/e2e/tests/liquidity_provision/test_liquidity_provision.py
+++ apps/trading/e2e/tests/liquidity_provision/test_liquidity_provision.py
@@ -16,34 +16,44 @@ def vega(request):
 def continuous_market(vega):
     return setup_continuous_market(vega)
 
+
 @pytest.mark.usefixtures("auth", "risk_accepted")
-def test_liquidity_provision_amendment(continuous_market, vega: VegaServiceNull, page: Page):
+def test_liquidity_provision_amendment(
+    continuous_market, vega: VegaServiceNull, page: Page
+):
     # TODO Refactor asserting the grid
     page.goto(f"/#/liquidity/{continuous_market}")
     change_keys(page, vega, "market_maker")
-    row = page.get_by_test_id(
-        "tab-myLP").locator(".ag-center-cols-container .ag-row").first
-    expect(row).to_contain_text(
-        "Active"
+    row = (
+        page.get_by_test_id("tab-myLP")
+        .locator(".ag-center-cols-container .ag-row")
+        .first
     )
+    expect(row).to_contain_text("Active")
     # 5002-LIQP-006
-    expect(page.get_by_test_id("target-stake")
-           ).to_have_text("Target stake5.82757 tDAI")
+    expect(page.get_by_test_id("target-stake")).to_have_text("Target stake5.82757 tDAI")
     # 5002-LIQP-007
-    expect(page.get_by_test_id("supplied-stake")
-           ).to_have_text("Supplied stake10,000.00 tDAI")
+    expect(page.get_by_test_id("supplied-stake")).to_have_text(
+        "Supplied stake10,000.00 tDAI"
+    )
     # 5002-LIQP-008
-    expect(page.get_by_test_id("liquidity-supplied")
-           ).to_have_text("Liquidity supplied 171,598.11%")
+    expect(page.get_by_test_id("liquidity-supplied")).to_have_text(
+        "Liquidity supplied 171,598.11%"
+    )
     expect(page.get_by_test_id("fees-paid")).to_have_text("Fees paid-")
     # 5002-LIQP-009
-    expect(page.get_by_test_id("liquidity-market-id")
-           ).to_have_text("Market ID" + truncate_middle(continuous_market))
-    expect(page.get_by_test_id("liquidity-learn-more")
-           ).to_have_text("Learn moreProviding liquidity")
+    expect(page.get_by_test_id("liquidity-market-id")).to_have_text(
+        "Market ID" + truncate_middle(continuous_market)
+    )
+    expect(page.get_by_test_id("liquidity-learn-more")).to_have_text(
+        "Learn moreProviding liquidity"
+    )
     # 002-LIQP-010
-    expect(page.get_by_test_id("liquidity-learn-more").get_by_test_id("external-link")
-           ).to_have_attribute("href", "https://docs.vega.xyz/testnet/concepts/liquidity/provision")
+    expect(
+        page.get_by_test_id("liquidity-learn-more").get_by_test_id("external-link")
+    ).to_have_attribute(
+        "href", "https://docs.vega.xyz/testnet/concepts/liquidity/provision"
+    )
 
     vega.submit_simple_liquidity(
         key_name="market_maker",
@@ -56,41 +66,50 @@ def test_liquidity_provision_amendment(continuous_market, vega: VegaServiceNull,
     vega.wait_fn(1)
     vega.wait_for_total_catchup()
     page.reload()
-    row = page.get_by_test_id(
-        "tab-myLP").locator(".ag-center-cols-container .ag-row").first
-    expect(row).to_contain_text(
-        "Updating next epoch"
+    row = (
+        page.get_by_test_id("tab-myLP")
+        .locator(".ag-center-cols-container .ag-row")
+        .first
+    )
+    expect(row).to_contain_text("Updating next epoch")
+    next_epoch(vega=vega)
+    page.reload()
+    expect(page.get_by_test_id("supplied-stake")).to_have_text(
+        "Supplied stake1.00001 tDAI"
+    )
+    expect(page.get_by_test_id("liquidity-supplied")).to_have_text(
+        "Liquidity supplied 17.16%"
+    )
+    row = (
+        page.get_by_test_id("tab-myLP")
+        .locator(".ag-center-cols-container .ag-row")
+        .first
+    )
+    expect(row).to_contain_text("Active")
+
+
+@pytest.mark.usefixtures("auth", "risk_accepted")
+def test_liquidity_provision_cancelled(
+    continuous_market, vega: VegaServiceNull, page: Page
+):
+    page.goto(f"/#/liquidity/{continuous_market}")
+    change_keys(page, vega, "market_maker")
+    row = (
+        page.get_by_test_id("tab-myLP")
+        .locator(".ag-center-cols-container .ag-row")
+        .first
+    )
+    expect(row).to_contain_text("Active")
+    vega.cancel_liquidity(
+        key_name="market_maker",
+        market_id=continuous_market,
     )
     next_epoch(vega=vega)
     page.reload()
-    expect(page.get_by_test_id("supplied-stake")
-           ).to_have_text("Supplied stake1.00001 tDAI")
-    expect(page.get_by_test_id("liquidity-supplied")
-           ).to_have_text("Liquidity supplied 17.16%")
-    row = page.get_by_test_id(
-        "tab-myLP").locator(".ag-center-cols-container .ag-row").first
-    expect(row).to_contain_text(
-        "Active"
+    expect(page.get_by_test_id("supplied-stake")).to_have_text(
+        "Supplied stake0.00 tDAI"
     )
-
-
-@pytest.mark.skip("Waiting for the ability to cancel LP")
-@pytest.mark.usefixtures("auth", "risk_accepted")
-def test_liquidity_provision_inactive(continuous_market, vega: VegaServiceNull, page: Page):
-    # TODO Refactor asserting the grid
-    page.goto(f"/#/liquidity/{continuous_market}")
-    change_keys(page, vega, "market_maker")
-    row = page.get_by_test_id(
-        "tab-myLP").locator(".ag-center-cols-container .ag-row").first
-    expect(row).to_contain_text(
-        "Active"
+    expect(page.get_by_test_id("liquidity-supplied")).to_have_text(
+        "Liquidity supplied 0.00%"
     )
-    vega.submit_simple_liquidity(
-        key_name="market_maker",
-        market_id=continuous_market,
-        commitment_amount=0,
-        fee=0,
-        is_amendment=False,
-    )
-    vega.wait_fn(1)
-    vega.wait_for_total_catchup()
+    expect(page.locator(".ag-overlay-panel")).to_have_text("No data")
