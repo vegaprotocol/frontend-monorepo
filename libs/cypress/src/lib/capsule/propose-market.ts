f29360de8674ae7ce82e4ad440e762ac2ac50147
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2024-01-20 19:05:16 +0100

    fix(trading): revert because of issue 10343 - propose market

--- libs/cypress/src/lib/capsule/propose-market.ts
+++ libs/cypress/src/lib/capsule/propose-market.ts
@@ -35,6 +35,7 @@ function createNewMarketProposal(): ProposalSubmissionBody {
             decimalPlaces: '5',
             positionDecimalPlaces: '5',
             linearSlippageFactor: '0.001',
+            quadraticSlippageFactor: '0',
             instrument: {
               name: 'Test market 1',
               code: 'TEST.24h',
