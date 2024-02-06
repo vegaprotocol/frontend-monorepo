556cd13396761d943d81b7ca34949dbf62c99de9
Art <artur@vegaprotocol.io>
2024-02-06 15:33:29 +0100

    fix(environment): pick best node (#5758)

--- libs/environment/src/components/node-switcher/row-data.spec.tsx
+++ libs/environment/src/components/node-switcher/row-data.spec.tsx
@@ -23,7 +23,6 @@ import {
   SUBSCRIPTION_TIMEOUT,
   useNodeBasicStatus,
   useNodeSubscriptionStatus,
-  useResponseTime,
 } from './row-data';
 import { BLOCK_THRESHOLD, RowData } from './row-data';
 import { CUSTOM_NODE_KEY } from '../../types';
@@ -159,19 +158,6 @@ describe('useNodeBasicStatus', () => {
       expect(result.current.status).toBe(Result.Failed);
       expect(result.current.currentBlockHeight).toBeNaN();
     });
-  });
-});
-
-describe('useResponseTime', () => {
-  it('returns response time when url is valid', () => {
-    const { result } = renderHook(() =>
-      useResponseTime('https://localhost:1234')
-    );
-    expect(result.current.responseTime).toBe(50);
-  });
-  it('does not return response time when url is invalid', () => {
-    const { result } = renderHook(() => useResponseTime('nope'));
-    expect(result.current.responseTime).toBeUndefined();
   });
 });
 
