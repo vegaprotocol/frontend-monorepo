b4e98e285e3a2d32fb7f2de345938591a879c28a
Bartłomiej Głownia <bglownia@gmail.com>
2024-02-06 08:42:11 +0100

    feat(deal-ticket): update fields in margin details (#5722)

--- apps/trading/e2e/tests/deal_ticket/test_trading_deal_ticket_submit_account.py
+++ apps/trading/e2e/tests/deal_ticket/test_trading_deal_ticket_submit_account.py
@@ -33,7 +33,7 @@ def test_should_display_info_and_button_for_deposit(continuous_market, page: Pag
         "You may not have enough margin available to open this position.")
     page.get_by_test_id(deal_ticket_warning_margin).hover()
     expect(page.get_by_test_id("tooltip-content").nth(0)).to_have_text(
-        "1,661,896.6317 tDAI is currently required.You have only 1,000,000.00.Deposit tDAI")
+        "1,661,888.12901 tDAI is currently required.You have only 999,991.49731.Deposit tDAI")
     page.get_by_test_id(deal_ticket_deposit_dialog_button).nth(0).click()
     expect(page.get_by_test_id("sidebar-content")
            ).to_contain_text("DepositFrom")
