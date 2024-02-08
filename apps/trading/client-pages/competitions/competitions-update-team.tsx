5ddcb613e226b149bcbf7af6753b1e80a0e08f01
Art <artur@vegaprotocol.io>
2024-02-08 15:07:48 +0100

    chore(trading): create and update team form traversing (#5764)

--- apps/trading/client-pages/competitions/competitions-update-team.tsx
+++ apps/trading/client-pages/competitions/competitions-update-team.tsx
@@ -3,7 +3,14 @@ import { usePageTitle } from '../../lib/hooks/use-page-title';
 import { Box } from '../../components/competitions/box';
 import { useT } from '../../lib/use-t';
 import { useVegaWallet, useVegaWalletDialogStore } from '@vegaprotocol/wallet';
-import { Loader, Splash } from '@vegaprotocol/ui-toolkit';
+import {
+  Intent,
+  Loader,
+  Splash,
+  TradingAnchorButton,
+  VegaIcon,
+  VegaIconNames,
+} from '@vegaprotocol/ui-toolkit';
 import { RainbowButton } from '../../components/rainbow-button';
 import { Link, Navigate, useParams } from 'react-router-dom';
 import { Links } from '../../lib/links';
@@ -11,6 +18,7 @@ import { useReferralSetTransaction } from '../../lib/hooks/use-referral-set-tran
 import { type FormFields, TeamForm, TransactionType } from './team-form';
 import { useTeam } from '../../lib/hooks/use-team';
 import { LayoutWithGradient } from '../../components/layouts-inner';
+import { useEffect, useState } from 'react';
 
 export const CompetitionsUpdateTeam = () => {
   const t = useT();
@@ -29,6 +37,19 @@ export const CompetitionsUpdateTeam = () => {
       <LayoutWithGradient>
         <div className="mx-auto md:w-2/3 max-w-xl">
           <Box className="flex flex-col gap-4">
+            <Link
+              to={Links.COMPETITIONS_TEAM(teamId)}
+              className="text-xs inline-flex items-center gap-1 group"
+            >
+              <VegaIcon
+                name={VegaIconNames.CHEVRON_LEFT}
+                size={12}
+                className="text-vega-clight-100 dark:text-vega-cdark-100"
+              />{' '}
+              <span className="group-hover:underline">
+                {t('Go back to the team profile')}
+              </span>
+            </Link>
             <h1 className="calt text-2xl lg:text-3xl xl:text-5xl">
               {t('Update a team')}
             </h1>
@@ -57,7 +78,8 @@ const UpdateTeamFormContainer = ({
   pubKey: string;
 }) => {
   const t = useT();
-  const { team, loading, error } = useTeam(teamId, pubKey);
+  const [refetching, setRefetching] = useState<boolean>(false);
+  const { team, loading, error, refetch } = useTeam(teamId, pubKey);
 
   const { err, status, onSubmit } = useReferralSetTransaction({
     onSuccess: () => {
@@ -65,7 +87,15 @@ const UpdateTeamFormContainer = ({
     },
   });
 
-  if (loading) {
+  // refetch when saved
+  useEffect(() => {
+    if (refetch && status === 'confirmed') {
+      refetch();
+      setRefetching(true);
+    }
+  }, [refetch, status]);
+
+  if (loading && !refetching) {
     return <Loader size="small" />;
   }
   if (error) {
@@ -82,6 +112,33 @@ const UpdateTeamFormContainer = ({
   const isMyTeam = team?.referrer === pubKey;
   if (!isMyTeam) {
     return <Navigate to={Links.COMPETITIONS_TEAM(teamId)} />;
+  }
+
+  if (status === 'confirmed') {
+    return (
+      <div
+        className="flex flex-col items-start gap-2"
+        data-testid="team-creation-success-message"
+      >
+        <p className="text-sm">
+          <VegaIcon
+            name={VegaIconNames.TICK}
+            size={18}
+            className="text-vega-green-500"
+          />{' '}
+          {t('Changes successfully saved to your team.')}
+        </p>
+
+        <TradingAnchorButton
+          href={Links.COMPETITIONS_TEAM(teamId)}
+          intent={Intent.Info}
+          size="small"
+          data-testid="view-team-button"
+        >
+          {t('View team')}
+        </TradingAnchorButton>
+      </div>
+    );
   }
 
   const defaultValues: FormFields = {
