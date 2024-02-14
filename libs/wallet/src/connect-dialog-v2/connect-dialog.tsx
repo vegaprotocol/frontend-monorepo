7c5ac31b7392e0d991a791797b6b2bc3409c079d
Matthew Russell <mattrussell36@gmail.com>
2024-02-13 23:51:33 -0800

    feat: add some ui for the connect dialog v2

--- libs/wallet/src/connect-dialog-v2/connect-dialog.tsx
+++ libs/wallet/src/connect-dialog-v2/connect-dialog.tsx
@@ -1,6 +1,13 @@
-import { Dialog, VegaIcon, VegaIconNames } from '@vegaprotocol/ui-toolkit';
+import {
+  Dialog,
+  Tooltip,
+  VLogo,
+  VegaIcon,
+  VegaIconNames,
+} from '@vegaprotocol/ui-toolkit';
 import { useConfig, useConnect, useWallet } from '../wallet';
 import { type Status } from '../types';
+import { useT } from '../use-t';
 
 export const ConnectDialog = ({
   open,
@@ -52,22 +59,50 @@ const ConnectionOptions = ({
   }
 
   return (
-    <ul>
+    <ul className="flex flex-col gap-2">
       {connectors.map((c) => {
         return (
-          <li key={c.id}>
-            <button
-              onClick={async () => {
-                await connect(c.id);
-                onConnect();
-              }}
-            >
-              {c.id}
-            </button>
-          </li>
+          <ConnectionOption
+            key={c.id}
+            id={c.id as any}
+            onClick={async () => {
+              await connect(c.id);
+              onConnect();
+            }}
+          />
         );
       })}
     </ul>
+  );
+};
+
+const ConnectionOption = ({
+  id,
+  onClick,
+}: {
+  id: 'injected' | 'jsonRpc' | 'snap' | 'readOnly';
+  onClick: () => void;
+}) => {
+  const t = useT();
+  return (
+    <li>
+      <Tooltip
+        description={t(`connector-${id}-desc`)}
+        align="center"
+        side="right"
+        sideOffset={10}
+      >
+        <button
+          className="flex gap-2 items-center capitalize"
+          onClick={onClick}
+        >
+          <span className="flex items-center justify-center w-8 h-8 rounded bg-vega-cdark-600 dark:bg-vega-clight-600 text-vega-clight-800 dark:text-vega-cdark-800">
+            <ConnectorIcon id={id} />
+          </span>
+          {t(`connector-${id}-title`)}
+        </button>
+      </Tooltip>
+    </li>
   );
 };
 
@@ -90,4 +125,29 @@ const ConnectionStatus = ({ status }: { status: Status }) => {
   }
 
   return null;
+};
+
+const ConnectorIcon = ({
+  id,
+}: {
+  id: 'injected' | 'jsonRpc' | 'snap' | 'readOnly';
+}) => {
+  switch (id) {
+    case 'injected': {
+      return <VLogo className="w-4 h-4" />;
+    }
+    case 'jsonRpc': {
+      return (
+        <span className="text-xs">
+          <span className="relative -top-0.5">{'>_'}</span>
+        </span>
+      );
+    }
+    case 'snap': {
+      return <VegaIcon name={VegaIconNames.METAMASK} size={24} />;
+    }
+    case 'readOnly': {
+      return <VegaIcon name={VegaIconNames.EYE} size={20} />;
+    }
+  }
 };
