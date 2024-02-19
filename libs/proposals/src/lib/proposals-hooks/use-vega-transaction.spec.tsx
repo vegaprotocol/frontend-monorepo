7e29b3031286d029a4a53905fb6b50a6bedf99ac
Matthew Russell <mattrussell36@gmail.com>
2024-02-18 22:42:37 -0800

    chore: fix user rejected error test for transaction hook

--- libs/proposals/src/lib/proposals-hooks/use-vega-transaction.spec.tsx
+++ libs/proposals/src/lib/proposals-hooks/use-vega-transaction.spec.tsx
@@ -1,5 +1,5 @@
 import { act, renderHook } from '@testing-library/react';
-import type { Transaction } from '@vegaprotocol/wallet';
+import { ConnectorErrors, type Transaction } from '@vegaprotocol/wallet';
 import {
   initialState,
   useVegaTransaction,
@@ -40,7 +40,7 @@ describe('useVegaTransaction', () => {
   it('resets state if sendTx returns null (user rejects)', async () => {
     jest
       .spyOn(mockConfig, 'sendTransaction')
-      .mockReturnValue(Promise.resolve({ error: 'user rejected' }));
+      .mockRejectedValue(ConnectorErrors.userRejected);
     const { result } = setup();
     await act(async () => {
       result.current.send(mockPubKey, {} as Transaction);
