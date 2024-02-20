67aa01f6e23e88a560e5bd77086b66d4c2924026
Matthew Russell <mattrussell36@gmail.com>
2024-02-19 19:37:07 -0800

    feat: add update profile transaction

--- apps/trading/components/profile-dialog/profile-dialog.tsx
+++ apps/trading/components/profile-dialog/profile-dialog.tsx
@@ -1,12 +1,89 @@
-import { Dialog } from '@vegaprotocol/ui-toolkit';
+import {
+  Dialog,
+  FormGroup,
+  Input,
+  InputError,
+  TradingButton,
+} from '@vegaprotocol/ui-toolkit';
 import { useProfileDialogStore } from '../../stores/profile-dialog-store';
+import { useForm } from 'react-hook-form';
+import { useT } from '../../lib/use-t';
+import { useRequired } from '@vegaprotocol/utils';
+import { useSimpleTransaction } from '@vegaprotocol/wallet';
 
 export const ProfileDialog = () => {
   const open = useProfileDialogStore((store) => store.open);
   const setOpen = useProfileDialogStore((store) => store.setOpen);
+
   return (
     <Dialog open={open} onChange={setOpen}>
-      <div>Content</div>
+      <ProfileFormContainer />
     </Dialog>
+  );
+};
+
+const ProfileFormContainer = () => {
+  const { send, status } = useSimpleTransaction();
+
+  const sendTx = (field: FormFields) => {
+    send({
+      updatePartyProfile: {
+        // TODO: alias validation
+        alias: field.alias,
+        metadata: [],
+      },
+    });
+  };
+
+  if (status === 'pending') {
+    return <p>Pending</p>;
+  }
+
+  if (status === 'requested') {
+    return <p>Confirm in wallet</p>;
+  }
+
+  if (status === 'confirmed') {
+    return <p>Transaction sent</p>;
+  }
+
+  if (status === 'idle') {
+    return <ProfileForm onSubmit={sendTx} />;
+  }
+};
+
+interface FormFields {
+  alias: string;
+}
+
+const ProfileForm = ({
+  onSubmit,
+}: {
+  onSubmit: (fields: FormFields) => void;
+}) => {
+  const t = useT();
+  const required = useRequired();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormGroup label="Profile alias" labelFor="alias">
+        <Input
+          {...register('alias', {
+            validate: {
+              required,
+            },
+          })}
+        />
+        {errors.alias?.message && (
+          <InputError>{errors.alias.message}</InputError>
+        )}
+      </FormGroup>
+      <TradingButton type="submit">{t('Submit')}</TradingButton>
+    </form>
   );
 };
