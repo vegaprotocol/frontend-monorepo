4719703481b240a8fdede6a1a2139d9415419624
Matthew Russell <mattrussell36@gmail.com>
2024-02-26 23:19:54 +0000

    chore: remove hard coded translations

--- libs/wallet-react/src/components/connect-dialog/connection-status.tsx
+++ libs/wallet-react/src/components/connect-dialog/connection-status.tsx
@@ -1,20 +1,26 @@
 import { VegaIcon, VegaIconNames } from '@vegaprotocol/ui-toolkit';
 import { type Status } from '@vegaprotocol/wallet';
+import { useT } from '../../hooks/use-t';
 
 export const ConnectionStatus = ({ status }: { status: Status }) => {
+  const t = useT();
+
   if (status === 'connecting') {
     return (
       <>
-        <h3 className="text-lg">Connecting...</h3>
-        <p className="text-secondary">Confirm connection in your wallet</p>
+        <h3 className="text-lg">{t('Connecting...')}</h3>
+        <p className="text-secondary">
+          {t('Approve the connection from your Vega wallet app.')}
+        </p>
       </>
     );
   }
+
   if (status === 'connected') {
     return (
       <div className="flex items-center gap-3">
         <VegaIcon name={VegaIconNames.TICK} className="text-vega-green" />
-        <h3 className="text-lg">Connected</h3>
+        <h3 className="text-lg">{t('Successfully connected')}</h3>
       </div>
     );
   }
