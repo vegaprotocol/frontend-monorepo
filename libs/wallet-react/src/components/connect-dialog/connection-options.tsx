2af090f2ba52b382215e20166f312b72fe7a74b6
Matthew Russell <mattrussell36@gmail.com>
2024-02-27 09:57:42 +0000

    chore: handle custom connection option better

--- libs/wallet-react/src/components/connect-dialog/connection-options.tsx
+++ libs/wallet-react/src/components/connect-dialog/connection-options.tsx
@@ -33,14 +33,28 @@ export const ConnectionOptions = ({
         data-testid="connectors-list"
       >
         {connectors.map((c) => {
+          if (c.id === 'injected') {
+            return (
+              <li key={c.id}>
+                <ConnectionOptionInjected
+                  id={c.id}
+                  name={c.name}
+                  description={c.description}
+                  onClick={() => onConnect(c.id)}
+                />
+              </li>
+            );
+          }
+
           return (
-            <ConnectionOption
-              key={c.id}
-              id={c.id}
-              name={c.name}
-              description={c.description}
-              onClick={() => onConnect(c.id)}
-            />
+            <li key={c.id}>
+              <ConnectionOption
+                id={c.id}
+                name={c.name}
+                description={c.description}
+                onClick={() => onConnect(c.id)}
+              />
+            </li>
           );
         })}
       </ul>
@@ -58,7 +72,7 @@ export const ConnectionOptions = ({
         rel="noreferrer"
         className="text-sm underline underline-offset-4"
       >
-        Dont have a wallet?
+        {t("Don't have a wallet?")}
       </a>
     </div>
   );
@@ -75,45 +89,54 @@ export const ConnectionOption = ({
   description: string;
   onClick: () => void;
 }) => {
+  return (
+    <Tooltip
+      description={description}
+      align="center"
+      side="right"
+      sideOffset={10}
+      delayDuration={400}
+    >
+      <button
+        className="w-full flex gap-2 items-center p-2 rounded capitalize hover:bg-vega-clight-800 dark:hover:bg-vega-cdark-800"
+        onClick={onClick}
+        data-testid={`connector-${id}`}
+      >
+        <ConnectorIcon id={id} />
+        {name}
+      </button>
+    </Tooltip>
+  );
+};
+
+/**
+ * This component is specific for the Vega wallet connection option,
+ * if its not installed we want to link to the extension url
+ */
+export const ConnectionOptionInjected = ({
+  id,
+  name,
+  description,
+  onClick,
+}: {
+  id: 'injected';
+  name: string;
+  description: string;
+  onClick: () => void;
+}) => {
   const t = useT();
   const userAgent = useUserAgent();
-
-  if (id === 'injected' && !isBrowserWalletInstalled()) {
-    const link = userAgent ? extensionLinks[userAgent] : Links.walletOverview;
-
-    return (
-      <li>
-        <Tooltip
-          description={description}
-          align="center"
-          side="right"
-          sideOffset={10}
-          delayDuration={400}
-        >
-          <a
-            href={link}
-            target="_blank"
-            rel="noreferrer"
-            className="w-full flex gap-2 items-center p-2 rounded capitalize hover:bg-vega-clight-800 dark:hover:bg-vega-cdark-800"
-            data-testid={`connector-${id}`}
-          >
-            <ConnectorIcon id={id} />
-            {t('Get the Vega Wallet')}
-          </a>
-        </Tooltip>
-      </li>
-    );
-  }
+  const link = userAgent ? extensionLinks[userAgent] : Links.walletOverview;
 
   return (
-    <li>
-      <Tooltip
-        description={description}
-        align="center"
-        side="right"
-        sideOffset={10}
-        delayDuration={400}
-      >
+    <Tooltip
+      description={description}
+      align="center"
+      side="right"
+      sideOffset={10}
+      delayDuration={400}
+    >
+      {isBrowserWalletInstalled() ? (
         <button
           className="w-full flex gap-2 items-center p-2 rounded capitalize hover:bg-vega-clight-800 dark:hover:bg-vega-cdark-800"
           onClick={onClick}
@@ -122,7 +145,18 @@ export const ConnectionOption = ({
           <ConnectorIcon id={id} />
           {name}
         </button>
-      </Tooltip>
-    </li>
+      ) : (
+        <a
+          href={link}
+          target="_blank"
+          rel="noreferrer"
+          className="w-full flex gap-2 items-center p-2 rounded capitalize hover:bg-vega-clight-800 dark:hover:bg-vega-cdark-800"
+          data-testid={`connector-${id}`}
+        >
+          <ConnectorIcon id={id} />
+          {t('Get the Vega Wallet')}
+        </a>
+      )}
+    </Tooltip>
   );
 };
