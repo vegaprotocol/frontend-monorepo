860f094a7bc1d4e5d6f8b0fa78490375029c0eb3
Matthew Russell <mattrussell36@gmail.com>
2024-03-04 15:47:17 +0000

    feat: add alias to dropdown, dont show key in trigger

--- apps/trading/components/profile-dialog/profile-dialog.tsx
+++ apps/trading/components/profile-dialog/profile-dialog.tsx
@@ -10,19 +10,40 @@ import { useProfileDialogStore } from '../../stores/profile-dialog-store';
 import { useForm } from 'react-hook-form';
 import { useT } from '../../lib/use-t';
 import { useRequired } from '@vegaprotocol/utils';
-import { useSimpleTransaction, type Status } from '@vegaprotocol/wallet';
+import {
+  useSimpleTransaction,
+  type Status,
+  useVegaWallet,
+} from '@vegaprotocol/wallet-react';
+import {
+  usePartyProfilesQuery,
+  type PartyProfilesQuery,
+} from '../vega-wallet-connect-button/__generated__/PartyProfiles';
 
 export const ProfileDialog = () => {
   const t = useT();
+  const { pubKeys } = useVegaWallet();
+  const { data, refetch } = usePartyProfilesQuery({
+    variables: { partyIds: pubKeys.map((pk) => pk.publicKey) },
+    skip: pubKeys.length <= 0,
+  });
   const open = useProfileDialogStore((store) => store.open);
+  const pubKey = useProfileDialogStore((store) => store.pubKey);
   const setOpen = useProfileDialogStore((store) => store.setOpen);
 
-  const { send, status, error, reset } = useSimpleTransaction();
+  const { send, status, error, reset } = useSimpleTransaction({
+    onSuccess: () => {
+      refetch();
+    },
+  });
+
+  const profileEdge = data?.partiesProfilesConnection?.edges.find(
+    (e) => e.node.partyId === pubKey
+  );
 
   const sendTx = (field: FormFields) => {
     send({
       updatePartyProfile: {
-        // TODO: alias validation ?
         alias: field.alias,
         metadata: [],
       },
@@ -32,13 +53,18 @@ export const ProfileDialog = () => {
   return (
     <Dialog
       open={open}
-      onChange={(o) => {
-        setOpen(o);
+      onChange={() => {
+        setOpen(undefined);
         reset();
       }}
-      title={t('Set party alias')}
+      title={t('Edit profile')}
     >
-      <ProfileForm status={status} error={error} onSubmit={sendTx} />
+      <ProfileForm
+        profile={profileEdge?.node}
+        status={status}
+        error={error}
+        onSubmit={sendTx}
+      />
     </Dialog>
   );
 };
@@ -47,11 +73,17 @@ interface FormFields {
   alias: string;
 }
 
+type Profile = NonNullable<
+  PartyProfilesQuery['partiesProfilesConnection']
+>['edges'][number]['node'];
+
 const ProfileForm = ({
+  profile,
   onSubmit,
   status,
   error,
 }: {
+  profile: Profile | undefined;
   onSubmit: (fields: FormFields) => void;
   status: Status;
   error: string | undefined;
@@ -62,7 +94,11 @@ const ProfileForm = ({
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormFields>();
+  } = useForm<FormFields>({
+    defaultValues: {
+      alias: profile?.alias,
+    },
+  });
 
   const renderButtonText = () => {
     if (status === 'requested') {
@@ -90,8 +126,16 @@ const ProfileForm = ({
         />
         {errorMessage && (
           <InputError>
-            <p className="break-words max-w-full">{errorMessage}</p>
+            <p className="break-words max-w-full first-letter:uppercase">
+              {errorMessage}
+            </p>
           </InputError>
+        )}
+
+        {status === 'confirmed' && (
+          <p className="mt-2 mb-4 text-sm text-success">
+            {t('Profile updated')}
+          </p>
         )}
       </FormGroup>
       <TradingButton
--- apps/trading/stores/profile-dialog-store.ts
+++ apps/trading/stores/profile-dialog-store.ts
@@ -2,10 +2,18 @@ import { create } from 'zustand';
 
 interface ProfileDialogStore {
   open: boolean;
-  setOpen: (open: boolean) => void;
+  pubKey: string | undefined;
+  setOpen: (pubKey: string | undefined) => void;
 }
 
 export const useProfileDialogStore = create<ProfileDialogStore>((set) => ({
   open: false,
-  setOpen: (open) => set({ open }),
+  pubKey: undefined,
+  setOpen: (pubKey) => {
+    if (pubKey) {
+      set({ open: true, pubKey });
+    } else {
+      set({ open: false, pubKey: undefined });
+    }
+  },
 }));
