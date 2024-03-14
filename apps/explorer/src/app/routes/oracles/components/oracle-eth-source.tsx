5d3f51a034db9531fb452378686e7df7c3589424
Edd <edd@vega.xyz>
2024-03-14 12:46:30 +0000

    feat(explorer): tidyup normaliser output

--- apps/explorer/src/app/routes/oracles/components/oracle-eth-source.tsx
+++ apps/explorer/src/app/routes/oracles/components/oracle-eth-source.tsx
@@ -8,6 +8,9 @@ import { getExternalChainLabel } from '@vegaprotocol/environment';
 import { t } from 'i18next';
 import { SyntaxHighlighter } from '@vegaprotocol/ui-toolkit';
 import isArray from 'lodash/isArray';
+import type { components } from '../../../../types/explorer';
+
+type Normalisers = components['schemas']['vegaNormaliser'][];
 
 interface OracleDetailsEthSourceProps {
   sourceType: SourceType;
@@ -36,8 +39,9 @@ export function OracleEthSource({
 
   const chainLabel = getExternalChainLabel(chain);
 
-  const abi = prepareAbi(sourceType?.sourceType?.abi);
-  const args = prepareAbi(sourceType?.sourceType?.args);
+  const abi = prepareOracleSpecField(sourceType?.sourceType?.abi);
+  const args = prepareOracleSpecField(sourceType?.sourceType?.args);
+  const normalisers = serialiseNormalisers(sourceType.sourceType.normalisers);
 
   return (
     <TableRow modifier="bordered">
@@ -79,35 +83,72 @@ export function OracleEthSource({
             </>
           )}
 
-          <h2 className={'mt-5 mb-1 text-xl'}>{t('Normalisers')}</h2>
-          <div className="max-w-3 mb-3">
-            <SyntaxHighlighter
-              data={JSON.parse(
-                JSON.stringify(
-                  sourceType.sourceType.normalisers as unknown as string
-                )
-              )}
-            />
-          </div>
+          {normalisers && (
+            <>
+              <h2 className={'mt-5 mb-1 text-xl'}>{t('Normalisers')}</h2>
+              <div className="max-w-3 mb-3">
+                <SyntaxHighlighter data={normalisers} />
+              </div>
+            </>
+          )}
         </details>
       </TableCell>
     </TableRow>
   );
 }
 
+// Constant to define the absence of a valid string from the Oracle Spec fields
 const NO_DATA = false;
 
-export function prepareAbi(abi?: string[] | null): string | false {
-  if (!abi) {
+/**
+ * The ABI and args are stored as either a (JSON escaped, probably) string
+ * or array of strings. Given that OracleEthSource is simply throwing the
+ * data in to a SyntaxHighlighter, we don't really care about the format,
+ * so this function will just try to parse the data and return it as a string.
+ *
+ * @param abi
+ * @returns
+ */
+export function prepareOracleSpecField(
+  specField?: string[] | null
+): string | false {
+  if (!specField) {
     return NO_DATA;
   }
 
   try {
-    if (isArray(abi)) {
-      return JSON.parse(abi.join(''));
+    if (isArray(specField)) {
+      return JSON.parse(specField.join(''));
     } else {
-      return JSON.parse(abi);
+      return JSON.parse(specField);
     }
+  } catch (e) {
+    return NO_DATA;
+  }
+}
+
+/**
+ * Similar to prepareOracleSpecField above, but processes an array of normaliser objects
+ * removing the __typename and returning a serialised array of normalisers for
+ * SyntaxHighlighter
+ *
+ * @param normalisers
+ * @returns
+ */
+export function serialiseNormalisers(
+  normalisers?: Normalisers | null
+): Normalisers | false {
+  if (!normalisers) {
+    return NO_DATA;
+  }
+
+  try {
+    return normalisers.map((normaliser) => {
+      return {
+        name: normaliser.name,
+        expression: normaliser.expression,
+      };
+    });
   } catch (e) {
     return NO_DATA;
   }
