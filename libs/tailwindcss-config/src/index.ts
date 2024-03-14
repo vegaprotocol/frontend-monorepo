e77a4de06c41099393ceffbcfef31cca1741c867
Matthew Russell <mattrussell36@gmail.com>
2024-03-14 15:17:47 +0000

    chore: remove theme-lite and tw warning

--- libs/tailwindcss-config/src/index.ts
+++ libs/tailwindcss-config/src/index.ts
@@ -1,4 +1,3 @@
 export { theme } from './theme';
-export { themeLite } from './theme-lite';
 export { vegaCustomClasses } from './vega-custom-classes';
 export { VegaColours } from './vega-colours';
