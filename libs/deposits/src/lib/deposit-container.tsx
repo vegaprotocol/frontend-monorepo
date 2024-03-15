b58cbc5bf10c843173132b0444828c2e5a1417a9
Matthew Russell <mattrussell36@gmail.com>
2024-03-15 14:14:27 +0000

    chore: add squid

--- libs/deposits/src/lib/deposit-container.tsx
+++ libs/deposits/src/lib/deposit-container.tsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Networks, useEnvironment } from '@vegaprotocol/environment';
 import { AsyncRendererInline } from '@vegaprotocol/ui-toolkit';
 import { DepositManager } from './deposit-manager';
 import { useEnabledAssets } from '@vegaprotocol/assets';
 
+export const DepositContainer = () => {
+  const [mod, setMod] = useState();
+  const config = {
+    companyName: 'Squid Widget',
+    integratorId: '<your integrator id>',
+    slippage: 1,
+    instantExec: true,
+    infiniteApproval: false,
+    apiUrl: 'https://api.squidrouter.com',
+  };
+
+  useEffect(() => {
+    (async () => {
+      const res = await import('@0xsquid/widget');
+      setMod(res);
+    })();
+  }, []);
+
+  if (mod) {
+    return <mod.SquidWidget config={config} />;
+  }
+
+  return <div>here</div>;
+};
+
 /**
  *  Fetches data required for the Deposit page
  */
-export const DepositContainer = ({ assetId }: { assetId?: string }) => {
+export const DepositContainerOld = ({ assetId }: { assetId?: string }) => {
   const { VEGA_ENV } = useEnvironment();
   const { data, error, loading } = useEnabledAssets();
 
