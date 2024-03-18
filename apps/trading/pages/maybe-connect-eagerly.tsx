6f9e9109df1937d497017ec3c8fd52a876739aef
Matthew Russell <mattrussell36@gmail.com>
2024-03-18 10:46:03 +0000

    chore: remove comment

--- apps/trading/pages/maybe-connect-eagerly.tsx
+++ apps/trading/pages/maybe-connect-eagerly.tsx
@@ -3,12 +3,6 @@ import { useEagerConnect as useEthereumEagerConnect } from '@vegaprotocol/web3';
 import { connectors } from '../lib/web3-connectors';
 
 export const MaybeConnectEagerly = () => {
-  // TODO: check dsn/env removal
-  // const { VEGA_ENV, SENTRY_DSN } = useEnvironment();
-  // const [isTelemetryApproved] = useTelemetryApproval();
-  // useEthereumEagerConnect(
-  //   isTelemetryApproved ? { dsn: SENTRY_DSN, env: VEGA_ENV } : {}
-  // );
   useEthereumEagerConnect({ connectors });
   useVegaEagerConnect();
 
