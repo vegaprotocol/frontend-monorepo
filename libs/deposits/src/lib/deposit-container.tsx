2f173401896c8418beca2c7b3a9e35d876ee2ed9
Matthew Russell <mattrussell36@gmail.com>
2024-03-21 15:09:35 +0000

    chore: should be working but no assets

--- libs/deposits/src/lib/deposit-container.tsx
+++ libs/deposits/src/lib/deposit-container.tsx
@@ -5,12 +5,18 @@ import { TradingButton } from '@vegaprotocol/ui-toolkit';
 import { useWeb3React } from '@web3-react/core';
 import { ethers } from 'ethers';
 import { FormEvent, useEffect, useRef, useState } from 'react';
+import { BRIDGE_ABI, ERC20_ABI } from '@vegaprotocol/smart-contracts';
+import { useVegaWallet } from '@vegaprotocol/wallet-react';
+import { useWeb3ConnectStore } from '@vegaprotocol/web3';
 
 /**
  *  Fetches data required for the Deposit page
  */
 export const DepositContainer = ({ assetId }: { assetId?: string }) => {
   const { account, provider } = useWeb3React();
+  const { pubKey } = useVegaWallet();
+  const openDialog = useWeb3ConnectStore((store) => store.open);
+
   const squid = useRef(
     new Squid({
       // baseUrl: 'https://testnet.v2.api.squidrouter.com',
@@ -29,7 +35,7 @@ export const DepositContainer = ({ assetId }: { assetId?: string }) => {
     const run = async () => {
       await squid.current.init();
       console.log('squid inited');
-      console.log(squid);
+      console.log(squid.current);
       setready(true);
 
       setChains(squid.current.chains);
@@ -43,12 +49,19 @@ export const DepositContainer = ({ assetId }: { assetId?: string }) => {
     return <div>Loading</div>;
   }
 
-  console.log(chains, tokens);
+  console.log(
+    chains,
+    tokens,
+    tokens?.filter((t) => t.chainId.toString() === fromChain?.toString())
+  );
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!provider) return;
+    if (!account) return;
+
+    if (!pubKey) return;
 
     if (!formRef.current) return;
     if (!squid.current) return;
@@ -59,7 +72,6 @@ export const DepositContainer = ({ assetId }: { assetId?: string }) => {
     }
 
     const formData = new FormData(formRef.current);
-    console.log(formData);
 
     const fromChain = formData.get('chain')?.toString();
     const fromToken = formData.get('token')?.toString();
@@ -69,30 +81,84 @@ export const DepositContainer = ({ assetId }: { assetId?: string }) => {
     if (!fromToken) return;
     if (!fromAmount) return;
 
-    const { route } = await squid.current.getRoute({
+    const bridgeAddress = '0x3152207Cb8B251AdF88628554a1422AA2b734F61';
+
+    // aUSDC on sepolia
+    const aUSDCSepolia = '0x254d06f33bDc5b8ee05b2ea472107E300226659A';
+
+    const usdcSepoliaContract = new ethers.Contract(
+      aUSDCSepolia,
+      ERC20_ABI,
+      provider
+    );
+
+    console.log(fromToken);
+
+    // const selectedContract = new ethers.Contract(
+    //   fromToken,
+    //   ERC20_ABI,
+    //   provider
+    // );
+    // console.log(selectedContract);
+    // const balance = await selectedContract.balanceOf(account);
+    // console.log(balance);
+    // return;
+
+    const bridgeContract = new ethers.Contract(
+      bridgeAddress,
+      BRIDGE_ABI,
+      provider.getSigner() || provider
+    );
+
+    const approveEncodeData = usdcSepoliaContract.interface.encodeFunctionData(
+      'approve',
+      [bridgeAddress, 0]
+    );
+
+    const depositEncodedData = bridgeContract.interface.encodeFunctionData(
+      'deposit_asset',
+      [aUSDCSepolia, 0, '0x' + pubKey]
+    );
+
+    console.log(fromAmount);
+
+    const { route, ...rest } = await squid.current.getRoute({
       fromChain,
       fromAmount,
       fromToken,
       toChain: '11155111', // Sepolia
-      toToken: '0x254d06f33bDc5b8ee05b2ea472107E300226659A', // aUSDC on sepolia
+      toToken: aUSDCSepolia,
       fromAddress: account,
-      toAddress: '',
+      toAddress: account,
       slippage: 1,
-      // TODO: customContractCAlls to send your new axlUSDC to the bridge
-      // customContractCalls: [
-      //   {
-      //     callType: SquidCallType.FULL_TOKEN_BALANCE,
-      //     target: '',
-      //     value: '',
-      //     callData: '',
-      //     payload: {
-      //       tokenAddress: '',
-      //       inputPos: 1,
-      //     },
-      //     estimatedGas: '5000',
-      //   },
-      // ],
+      customContractCalls: [
+        {
+          callType: SquidCallType.FULL_TOKEN_BALANCE,
+          target: aUSDCSepolia,
+          value: '0', // native value to be sent with call
+          callData: approveEncodeData,
+          payload: {
+            tokenAddress: aUSDCSepolia, // balance of this token replaces 0 on line 13
+            inputPos: 1,
+          },
+          estimatedGas: '50000',
+        },
+        {
+          callType: SquidCallType.FULL_TOKEN_BALANCE,
+          target: bridgeAddress,
+          value: '0',
+          callData: depositEncodedData,
+          payload: {
+            tokenAddress: bridgeAddress,
+            inputPos: 1,
+          },
+          estimatedGas: '78000',
+        },
+      ],
     });
+
+    console.log('router', 'rest');
+    console.log(route, rest);
 
     // Execute the swap transaction
     const tx = (await squid.current.executeRoute({
@@ -100,44 +166,64 @@ export const DepositContainer = ({ assetId }: { assetId?: string }) => {
       route,
     })) as unknown as TransactionResponse;
 
+    console.log('tx');
+    console.log(tx);
+
     const txReceipt = await tx.wait();
+    console.log('txReceipt');
     console.log(txReceipt);
   };
 
   return (
-    <form onSubmit={handleSubmit} ref={formRef} className="flex flex-col gap-4">
-      <div>
-        <label>From chain</label>
-        <select
-          name="chain"
-          className="p-1 border w-full"
-          value={fromChain}
-          onChange={(e) => setFromChain(e.target.value)}
-        >
-          {chains?.map((c) => (
-            <option key={c.chainId} value={c.chainId}>
-              {c.networkName} ({c.chainId})
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label>From token</label>
-        <select name="token" className="p-1 border w-full">
-          {tokens
-            ?.filter((t) => t.chainId === fromChain)
-            .map((t) => (
-              <option key={t.interchainTokenId} value={t.interchainTokenId}>
-                {t.name}
+    <>
+      {account && pubKey ? (
+        <>
+          <p>{account}</p>
+          <p>{pubKey}</p>
+        </>
+      ) : (
+        <div>
+          <button onClick={openDialog}>Connect</button>
+        </div>
+      )}
+      <form
+        onSubmit={handleSubmit}
+        ref={formRef}
+        className="flex flex-col gap-4"
+      >
+        <div>
+          <label>From chain</label>
+          <select
+            name="chain"
+            className="p-1 border w-full"
+            value={fromChain}
+            onChange={(e) => setFromChain(e.target.value)}
+          >
+            {chains?.map((c) => (
+              <option key={c.chainId} value={c.chainId}>
+                {c.networkName} ({c.chainId})
               </option>
             ))}
-        </select>
-      </div>
-      <div>
-        <label>Amount</label>
-        <input name="amount" type="text" className="p-1 border w-full" />
-      </div>
-      <TradingButton type="submit">Submit</TradingButton>
-    </form>
+          </select>
+        </div>
+        <div>
+          <label>From token</label>
+          <select name="token" className="p-1 border w-full">
+            {tokens
+              ?.filter((t) => t.chainId.toString() === fromChain?.toString())
+              .map((t, i) => (
+                <option key={t.address} value={t.address}>
+                  {t.name} ({t.address})
+                </option>
+              ))}
+          </select>
+        </div>
+        <div>
+          <label>Amount</label>
+          <input name="amount" type="text" className="p-1 border w-full" />
+        </div>
+        <TradingButton type="submit">Submit</TradingButton>
+      </form>
+    </>
   );
 };
--- libs/smart-contracts/src/index.ts
+++ libs/smart-contracts/src/index.ts
@@ -1,2 +1,8 @@
 export * from './contracts';
 export * from './utils';
+
+import bridgeAbi from './abis/erc20_bridge_abi.json';
+import erc20Abi from './abis/erc20_abi.json';
+
+export const BRIDGE_ABI = bridgeAbi;
+export const ERC20_ABI = erc20Abi;
