49b6a24726b64140cc95fb4dabc17f669910b3cd
asiaznik <artur@vegaprotocol.io>
2024-03-25 15:26:30 +0100

    fix: move markets settled condition, fix results rendering

--- apps/trading/lib/hooks/use-games.ts
+++ apps/trading/lib/hooks/use-games.ts
@@ -36,6 +36,16 @@ type GamesData = {
 };
 
 const MAX_EPOCHS = 30;
+/**
+ * Converts the given variables (`teamId`, `epochFrom`, `epochTo`) of
+ * `GamesQuery` into chunks so that the maximum difference between given
+ * `epochFrom` and `epochTo` is not greater than the limit of `MAX_EPOCHS`.
+ *
+ * Example: When `epochFrom == 1` and `epochTo == 59` this function should
+ * produce an array of variables consisting of two entries where:
+ * - 1st chunk: `epochFrom == 1` and `epochTo == 31`
+ * - 2nd chunk: `epochFrom == 32` and `epochTo == 59`
+ */
 const prepareVariables = (
   teamId?: string,
   epochFrom?: number,
@@ -55,8 +65,8 @@ const prepareVariables = (
   ];
 
   if (isSafeInteger(from) && isSafeInteger(to)) {
-    // If the difference between from and to is greater than MAX_EPOCHS
-    // then we need to segment the query into N subqueries.
+    // if the difference between "from" and "to" is greater than MAX_EPOCHS
+    // then we need to divide the variables into N chunks.
     if (to - from > MAX_EPOCHS) {
       const N = Math.ceil((to - from) / MAX_EPOCHS);
       variables = Array(N)
@@ -112,54 +122,60 @@ export const useGames = (
     return prepareVariables(teamId, from, to);
   }, [epochData?.epoch.id, epochFrom, epochTo, teamId]);
 
+  /**
+   * Because of the games API limitation to alway return max up to 30 epochs
+   * worth of data (regardless of the actual span of given variables
+   * `epochFrom` and `epochTo`) we need to do a trick of asking for longer
+   * periods in a way of chunks that are then combined into one `games`.
+   *
+   * The code below uses the direct reference to the `ApolloClient` and runs
+   * N queries (see `prepareVariables` function) in order to obtain the whole
+   * set of data.
+   */
   useEffect(() => {
-    if (!loading && !games) setLoading(true);
+    if (loading || games || variables.length === 0) return;
+    if (!loading) setLoading(true);
+    const processChunks = async () => {
+      const chunks = variables.map((v) =>
+        client
+          .query<GamesQuery>({
+            query: GamesDocument,
+            variables: v,
+            context: { isEnlargedTimeout: true },
+          })
+          .then(({ data, loading, error }) => ({ data, loading, error }))
+          .catch(() => {
+            /* NOOP */
+          })
+      );
+      try {
+        const results = await Promise.allSettled(chunks);
+        const games = results.reduce((all, r) => {
+          if (r.status === 'fulfilled' && r.value) {
+            const { data, error } = r.value;
+            if (error) setError(error);
+            const allGames = removePaginationWrapper(data?.games.edges);
+            const allOrScoped = allGames
+              .map((g) => ({
+                ...g,
+                team: teamId ? findTeam(g.entities, teamId) : undefined,
+              }))
+              .filter((g) => {
+                // passthrough if not scoped to particular team
+                if (!teamId) return true;
+                return isTeamGame(g);
+              });
+            return [...all, ...allOrScoped];
+          }
+          return all;
+        }, [] as Game[]);
 
-    if (!games) {
-      Promise.allSettled(
-        variables.map((v) =>
-          client
-            .query<GamesQuery>({
-              query: GamesDocument,
-              variables: v,
-              context: { isEnlargedTimeout: true },
-            })
-            .then(({ data, loading, error }) => ({ data, loading, error }))
-            .catch(() => {
-              /* NOOP */
-            })
-        )
-      )
-        .then((results) => {
-          const games = results.reduce((all, r) => {
-            if (r.status === 'fulfilled' && r.value) {
-              const { data, error } = r.value;
-              if (error) setError(error);
-              const allGames = removePaginationWrapper(data?.games.edges);
-              const allOrScoped = allGames
-                .map((g) => ({
-                  ...g,
-                  team: teamId ? findTeam(g.entities, teamId) : undefined,
-                }))
-                .filter((g) => {
-                  // passthrough if not scoped to particular team
-                  if (!teamId) return true;
-                  return isTeamGame(g);
-                });
-              return [...all, ...allOrScoped];
-            }
-            return all;
-          }, [] as Game[]);
-
-          setGames(orderBy(games, 'epoch', 'desc'));
-        })
-        .catch(() => {
-          /* NOOP */
-        })
-        .finally(() => {
-          if (loading) setLoading(false);
-        });
-    }
+        if (games.length > 0) setGames(orderBy(games, 'epoch', 'desc'));
+      } finally {
+        setLoading(false);
+      }
+    };
+    processChunks();
   }, [client, games, loading, teamId, variables]);
 
   return {
