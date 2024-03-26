cd4b0e508ead254440b863aa284e52d917d0bb0f
Matthew Russell <mattrussell36@gmail.com>
2024-03-26 09:59:13 +0000

    chore(trading): back merge hotfixes (#6099)
    Co-authored-by: Art <artur@vegaprotocol.io>

--- libs/accounts/src/lib/use-account-balance.tsx
+++ libs/accounts/src/lib/use-account-balance.tsx
@@ -1,42 +1,25 @@
-import { useCallback, useMemo, useState } from 'react';
 import { useVegaWallet } from '@vegaprotocol/wallet-react';
 import { useDataProvider } from '@vegaprotocol/data-provider';
 import { accountsDataProvider } from './accounts-data-provider';
-import type { Account } from './accounts-data-provider';
 import { getSettlementAccount } from './get-settlement-account';
 
 export const useAccountBalance = (assetId?: string) => {
   const { pubKey } = useVegaWallet();
-  const [accountBalance, setAccountBalance] = useState<string>('');
-  const [accountDecimals, setAccountDecimals] = useState<number | null>(null);
-  const variables = useMemo(() => {
-    return { partyId: pubKey || '' };
-  }, [pubKey]);
-  const update = useCallback(
-    ({ data }: { data: Account[] | null }) => {
-      const account = assetId
-        ? getSettlementAccount({ accounts: data, assetId })
-        : undefined;
-      setAccountBalance(account?.balance || '');
-      setAccountDecimals(account?.asset.decimals || null);
-      return true;
-    },
-    [assetId]
-  );
-  const { loading, error } = useDataProvider({
+
+  const { data, loading, error } = useDataProvider({
     dataProvider: accountsDataProvider,
-    variables,
+    variables: { partyId: pubKey || '' },
     skip: !pubKey || !assetId,
-    update,
   });
 
-  return useMemo(
-    () => ({
-      accountBalance: pubKey ? accountBalance : '',
-      accountDecimals: pubKey ? accountDecimals : null,
-      loading,
-      error,
-    }),
-    [accountBalance, accountDecimals, pubKey, loading, error]
-  );
+  const account = assetId
+    ? getSettlementAccount({ accounts: data, assetId })
+    : undefined;
+
+  return {
+    accountBalance: account ? account.balance : '',
+    accountDecimals: account ? account.asset.decimals : null,
+    loading,
+    error,
+  };
 };
