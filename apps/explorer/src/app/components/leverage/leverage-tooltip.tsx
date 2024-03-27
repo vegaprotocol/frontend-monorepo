6957f6143de8d7726f145332c0ff3ccecf418d54
Edd <edd@vega.xyz>
2024-03-27 15:35:01 +0000

    feat(explorer): add leverage from margin factor component

---
+++ apps/explorer/src/app/components/leverage/leverage-tooltip.tsx
@@ -0,0 +1,33 @@
+import { t } from '@vegaprotocol/i18n';
+
+const borderClass =
+  'border-solid border-2 border-vega-dark-200 border-collapse p-2';
+
+export type LeverageTooltipProps = {
+  marginFactor: string;
+  leverage: string;
+};
+
+/**
+ * Simple details renderer for Leverage component. Shows the original
+ * Margin Factor value and the calculated Leverage value. No validation is done.
+ */
+export function LeverageTooltip({
+  marginFactor,
+  leverage,
+}: LeverageTooltipProps) {
+  return (
+    <table className={borderClass}>
+      <tbody>
+        <tr>
+          <th className={`text-left ${borderClass}`}>{t('Margin factor')}</th>
+          <td className={borderClass}>{marginFactor}</td>
+        </tr>
+        <tr>
+          <th className={`text-left ${borderClass}`}>{t('Leverage')}</th>
+          <td className="p-2">{leverage}&times;</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
