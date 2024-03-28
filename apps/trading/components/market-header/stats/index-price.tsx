3b821841b2d3b3c5e10430af920dffddff2b5624
Matthew Russell <mattrussell36@gmail.com>
2024-03-28 13:23:50 +0000

    chore: fix mark price oracle display

--- apps/trading/components/market-header/stats/index-price.tsx
+++ apps/trading/components/market-header/stats/index-price.tsx
@@ -1,6 +1,6 @@
-import { DocsLinks } from '@vegaprotocol/environment';
+import { DocsLinks, useEnvironment } from '@vegaprotocol/environment';
 import { ExternalLink } from '@vegaprotocol/ui-toolkit';
-import { useExternalTwap } from '@vegaprotocol/markets';
+import { type MarketFieldsFragment, useExternalTwap } from '@vegaprotocol/markets';
 import { PriceCell } from '@vegaprotocol/datagrid';
 import { addDecimalsFormatNumber } from '@vegaprotocol/utils';
 import { HeaderStat } from '../../../components/header';
@@ -9,25 +9,47 @@ import { useT } from '../../../lib/use-t';
 export const IndexPriceStat = ({
   marketId,
   assetDecimals,
+  markPriceConfiguration,
 }: {
   marketId: string;
   assetDecimals: number;
+  markPriceConfiguration: MarketFieldsFragment['markPriceConfiguration'];
 }) => {
+  const { VEGA_EXPLORER_URL } = useEnvironment();
   const t = useT();
+
+  const dataSourceSpec = markPriceConfiguration.dataSourcesSpec?.[1];
+  const sourceType =
+    dataSourceSpec?.sourceType.__typename === 'DataSourceDefinitionExternal' &&
+    dataSourceSpec?.sourceType.sourceType.__typename === 'EthCallSpec' &&
+    dataSourceSpec?.sourceType.sourceType;
 
   return (
     <HeaderStat
       heading={`${t('Index Price')}`}
       description={
         <div className="p1">
-          {t(
-            'The external time weighted average price (TWAP) received from the data source defined in the data sourcing specification.'
-          )}
-          {DocsLinks && (
-            <ExternalLink href={DocsLinks.ETH_DATA_SOURCES} className="mt-2">
-              {t('Find out more')}
-            </ExternalLink>
-          )}
+          <p>
+            {t(
+              'The external time weighted average price (TWAP) received from the data source defined in the data sourcing specification.'
+            )}
+          </p>
+          <p className="flex flex-col gap-1">
+            {DocsLinks && (
+              <ExternalLink href={DocsLinks.ETH_DATA_SOURCES} className="mt-2">
+                {t('Find out more')}
+              </ExternalLink>
+            )}
+            {sourceType && (
+              <ExternalLink
+                data-testid="oracle-spec-links"
+                href={`${VEGA_EXPLORER_URL}/markets/${marketId}/oracles#${sourceType.address}`}
+                className="text-xs my-1"
+              >
+                {t('Oracle specification')}
+              </ExternalLink>
+            )}
+          </p>
         </div>
       }
       data-testid="index-price"
