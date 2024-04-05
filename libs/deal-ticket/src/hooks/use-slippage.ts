99422b84cb728537a32b425b9e62b26cbde1f625
Matthew Russell <mattrussell36@gmail.com>
2024-04-05 13:46:25 +0100

    fix: handle half volumn in a price level

--- libs/deal-ticket/src/hooks/use-slippage.ts
+++ libs/deal-ticket/src/hooks/use-slippage.ts
@@ -1,4 +1,5 @@
 import { Side, type PriceLevel, type OrderType } from '@vegaprotocol/types';
+import BigNumber from 'bignumber.js';
 
 export const useSlippage = (
   book: {
@@ -9,42 +10,57 @@ export const useSlippage = (
 ) => {
   if (order.side === Side.SIDE_BUY) {
     const lowestAsk = book.sell[0].price;
-    const slippage = calcSlippage(lowestAsk, order.size, book.sell);
-    return { slippage: slippage.toString() };
+    return calcSlippage({
+      price: lowestAsk,
+      size: order.size,
+      priceLevels: book.sell,
+    });
   }
 
   if (order.side === Side.SIDE_SELL) {
     const highestBid = book.buy[0].price;
-    const slippage = calcSlippage(highestBid, order.size, book.buy);
-    return { slippage: slippage.toString() };
+    return calcSlippage({
+      price: highestBid,
+      size: order.size,
+      priceLevels: book.buy,
+    });
   }
 
   return { slippage: '0' };
 };
 
-const calcSlippage = (
-  price: string,
-  size: string,
-  asks: Array<{ volume: string; price: string }>
-) => {
-  let totalVolume = BigInt(0);
-  let weightedSum = BigInt(0);
+export const calcSlippage = ({
+  price,
+  size,
+  priceLevels,
+}: {
+  price: string;
+  size: string;
+  priceLevels: Array<{ volume: string; price: string }>;
+}) => {
+  let remainingSize = new BigNumber(size);
+  let totalVolume = new BigNumber(0);
+  let weightedSum = new BigNumber(0);
 
-  for (const lvl of asks) {
-    if (totalVolume >= BigInt(size)) {
+  for (const lvl of priceLevels) {
+    if (remainingSize.isLessThanOrEqualTo(0)) {
       break;
     }
 
-    const volume = BigInt(lvl.volume);
-    const price = BigInt(lvl.price);
-    totalVolume = totalVolume + volume;
-    weightedSum = weightedSum + volume * price;
+    // const volume = new BigNumber(lvl.volume);
+    const volume = BigNumber.min(remainingSize, lvl.volume);
+    const price = new BigNumber(lvl.price);
+
+    remainingSize = remainingSize.minus(volume);
+    totalVolume = totalVolume.plus(volume);
+    weightedSum = weightedSum.plus(volume.times(price));
   }
 
-  const weightedAveragePrice = weightedSum / totalVolume;
-  const slippage = weightedAveragePrice - BigInt(price);
+  const weightedAveragePrice = weightedSum.dividedBy(totalVolume);
+  const slippage = weightedAveragePrice.minus(price);
 
-  // console.log(price, weightedSum, weightedAveragePrice, slippage);
-
-  return slippage;
+  return {
+    slippage: slippage.toString(),
+    weightedAveragePrice: weightedAveragePrice.toString(),
+  };
 };
