f75203dabd5953a69fcbe9ba65bf98cc30bc1004
Matthew Russell <mattrussell36@gmail.com>
2024-04-19 18:20:25 -0400

    chore: change title of widget

--- libs/deposits/src/lib/deposit-container.tsx
+++ libs/deposits/src/lib/deposit-container.tsx
@@ -162,6 +162,9 @@ export const DepositContainerInner = ({
       stakeConfig: {
         tokensConfig: compact(tokensConfig),
       },
+      titles: {
+        swap: 'Deposit',
+      },
     };
 
     return config;
