54a949d01da48a4c2110a369c3e62aa9cdd73d94
m.ray <16125548+MadalinaRaicu@users.noreply.github.com>
2024-04-22 10:21:47 +0100

    Update apps/trading/e2e/tests/rewards/test_rewards_combo_tier_1_capped.py

--- apps/trading/e2e/tests/rewards/test_rewards_combo_tier_1_capped.py
+++ apps/trading/e2e/tests/rewards/test_rewards_combo_tier_1_capped.py
@@ -172,7 +172,7 @@ def test_reward_history_capped(
     )
     expect((page.get_by_role(ROW).locator(TOTAL_COL_ID)).nth(1)).to_have_text("62.10")
     page.get_by_test_id(EARNED_BY_ME_BUTTON).click()
-    expect((page.get_by_role(ROW).locator(TOTAL_COL_ID)).nth(1)).to_have_text("31.20525")
+    expect((page.get_by_role(ROW).locator(TOTAL_COL_ID)).nth(1)).to_have_text("31.05")
 
 
 def test_reward_card_capped(
