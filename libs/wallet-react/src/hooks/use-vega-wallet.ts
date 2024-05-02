c7aa566bf0dd7144df233a0f4868c0f7babb5ddd
Madalina Raicu <madalina@raygroup.uk>
2024-05-02 12:11:40 +0100

    fix: return promise

--- libs/wallet-react/src/hooks/use-vega-wallet.ts
+++ libs/wallet-react/src/hooks/use-vega-wallet.ts
@@ -14,9 +14,8 @@ export const useVegaWallet = () => {
     selectPubKey: (pubKey: string) => config.store.setState({ pubKey }),
     isReadOnly: store.current === 'viewParty',
     disconnect: () => {
-      config.disconnect().then(() => {
-        config.store.setState({ pubKey: undefined });
-      });
+      config.store.setState({ pubKey: undefined });
+      return config.disconnect();
     },
     refreshKeys: config.refreshKeys,
     sendTx: (pubKey: string, transaction: Transaction) => {
