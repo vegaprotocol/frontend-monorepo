b8a9e46d00560f160acc7cfc39459d4f0bc2808d
Dariusz Majcherczyk <dariusz.majcherczyk@gmail.com>
2024-05-05 14:36:24 +0200

    chore: update address

--- apps/governance-e2e/cypress.config.js
+++ apps/governance-e2e/cypress.config.js
@@ -35,17 +35,17 @@ module.exports = defineConfig({
     ethWalletPublicKey: '0xEe7D375bcB50C26d52E1A4a472D8822A2A22d94F',
     ethWalletPublicKeyTruncated: '0xEe7D…d94F',
     ethStakingBridgeContractAddress:
-      '0x9135f5afd6F055e731bca2348429482eE614CFfA',
+      '0x3E29DFc2Acc4b63160aEC422BC3f0Fd29804fD06',
     vegaWalletName: 'capsule_wallet',
     vegaWalletLocation: '~/.vegacapsule/testnet/wallet',
     vegaWalletPassphrase: '123',
     vegaWalletMnemonic:
       'ozone access unlock valid olympic save include omit supply green clown session',
     vegaWalletPublicKey:
-      '02eceaba4df2bef76ea10caf728d8a099a2aa846cced25737cccaa9812342f65',
+      'b4f2726571fbe8e33b442dc92ed2d7f0d810e21835b7371a7915a365f07ccd9b',
     vegaWalletPublicKeyShort: '02ecea…2f65',
     vegaWalletPublicKey2:
-      '7f9cf07d3a9905b1a61a1069f7a758855da428bc0f4a97de87f48644bfc25535',
+      'c1607f28ec1d0a0b36842c8327101b18de2c5f172585870912f5959145a9176c',
     vegaWalletPublicKey2Short: '7f9cf0…5535',
     vegaTokenContractAddress: '0x331BC11227cB7925F90E37949590a34A876464F9',
     vegaTokenAddress: '0xFcE3C7CBba976414621887F2D762e7fB0f90b5c1',
