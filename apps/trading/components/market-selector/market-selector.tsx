eb7998f8094f06e4cf7f6c2f68ffd8e2c73e5e32
m.ray <16125548+MadalinaRaicu@users.noreply.github.com>
2024-05-08 10:32:26 +0100

    fix(trading): market dropdown on mobile in market selector (#6377)

--- apps/trading/components/market-selector/market-selector.tsx
+++ apps/trading/components/market-selector/market-selector.tsx
@@ -182,13 +182,16 @@ const MarketList = ({
           'p-2 mx-2 border-b border-default text-xs text-secondary'
         )}
       >
-        <div className="w-2/6" role="columnheader">
+        <div className="w-4/6 sm:w-2/6" role="columnheader">
           {t('Name')}
         </div>
         <div className="w-2/6 text-right pr-4" role="columnheader">
           {t('Price')}
         </div>
-        <div className="w-2/6 sm:w-1/6 text-right" role="columnheader">
+        <div
+          className="hidden sm:w-1/6 sm:flex justify-end text-right"
+          role="columnheader"
+        >
           {t('24h volume')}
         </div>
         <div className="sm:w-1/6" role="columnheader" />
