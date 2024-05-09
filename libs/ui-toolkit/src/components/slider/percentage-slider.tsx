36369b7e6cff216ea8c327acd2768229e9e34c74
Bartłomiej Głownia <bglownia@gmail.com>
2024-05-09 13:51:46 +0200

    feat(ui-toolkit): add tooltip on hover on percentage slider (#6374)

--- libs/ui-toolkit/src/components/slider/percentage-slider.tsx
+++ libs/ui-toolkit/src/components/slider/percentage-slider.tsx
@@ -4,16 +4,27 @@ import classNames from 'classnames';
 import { SliderThumb } from './slider';
 import BigNumber from 'bignumber.js';
 
-export const PercentageSlider = (
-  props: Omit<SliderProps, 'min' | 'max'> &
-    Required<Pick<SliderProps, 'max' | 'min'>>
-) => {
+export const PercentageSlider = ({
+  min,
+  max,
+  ...props
+}: Omit<SliderProps, 'min' | 'max'> &
+  Required<Pick<SliderProps, 'max' | 'min'>>) => {
   const markers = [0, 25, 50, 75, 100];
-  const value = props.value?.[0] || props.defaultValue?.[0];
+  const disabled = min === max;
+  const value = disabled ? 0 : props.value?.[0] || props.defaultValue?.[0];
+  const percentageValue =
+    value !== undefined && !disabled
+      ? `${(((value - min) / (max - min)) * 100).toFixed(1)}%`
+      : undefined;
+
   return (
     <div className="relative">
       <SliderPrimitive.Root
+        disabled={disabled}
         {...props}
+        min={disabled ? 0 : min}
+        max={disabled ? 1 : max}
         className="relative flex items-center select-none touch-none h-10 pb-5 w-full"
       >
         <SliderPrimitive.Track className=" relative grow h-[4px]">
@@ -24,8 +35,7 @@ export const PercentageSlider = (
           <span className="absolute top-[-3px] left-[8px] right-[8px]">
             {markers.map((v, i) => {
               const left = `${v}%`;
-              const valueToSet =
-                props.min + ((props.max - props.min) * v) / 100;
+              const valueToSet = min + ((max - min) * v) / 100;
               const lowerThanValue = value && valueToSet < value;
               return (
                 <span
@@ -47,7 +57,7 @@ export const PercentageSlider = (
             })}
           </span>
         </SliderPrimitive.Track>
-        <SliderThumb />
+        <SliderThumb tooltip={percentageValue} />
       </SliderPrimitive.Root>
       <span className="block absolute top-[15px] left-[8px] right-[8px]">
         {markers.map((v, i) => {
@@ -56,17 +66,15 @@ export const PercentageSlider = (
             <button
               type="button"
               key={label}
+              disabled={disabled}
               onClick={
                 props.onValueChange
                   ? (event) => {
                       if (!props.onValueChange) {
                         return;
                       }
-                      let valueToSet = BigNumber(props.min).plus(
-                        BigNumber(props.max)
-                          .minus(props.min)
-                          .multipliedBy(v)
-                          .dividedBy(100)
+                      let valueToSet = BigNumber(min).plus(
+                        BigNumber(max).minus(min).multipliedBy(v).dividedBy(100)
                       );
                       if (props.step) {
                         valueToSet = valueToSet.minus(
