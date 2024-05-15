444012a42978d3ae885bc6c9fd55dd7fbf66a5e8
Matthew Russell <mattrussell36@gmail.com>
2024-05-15 14:03:58 -0400

    feat: add sidebar to resizable grid

--- apps/trading/client-pages/market/trade-grid.tsx
+++ apps/trading/client-pages/market/trade-grid.tsx
@@ -38,164 +38,187 @@ const MainGrid = memo(
     });
 
     return (
-      <ResizableGrid vertical onChange={handleOnLayoutChange}>
-        <ResizableGridPanel
-          preferredSize={sizes[0]}
-          priority={LayoutPriority.High}
-          minSize={200}
-        >
-          <ResizableGrid onChange={handleOnMiddleLayoutChange}>
+      <ResizableGrid onChange={handleOnLayoutChange}>
+        <ResizableGridPanel>
+          <ResizableGrid vertical onChange={handleOnLayoutChange}>
             <ResizableGridPanel
+              preferredSize={sizes[0]}
               priority={LayoutPriority.High}
               minSize={200}
-              preferredSize={sizesMiddle[0] || '75%'}
+            >
+              <ResizableGrid onChange={handleOnMiddleLayoutChange}>
+                <ResizableGridPanel
+                  priority={LayoutPriority.High}
+                  minSize={200}
+                  preferredSize={sizesMiddle[0] || '75%'}
+                >
+                  <TradeGridChild>
+                    <Tabs storageKey="console-trade-grid-main-left">
+                      <Tab
+                        id="chart"
+                        overflowHidden
+                        name={t('Chart')}
+                        menu={<TradingViews.chart.menu />}
+                      >
+                        <ErrorBoundary feature="chart">
+                          <TradingViews.chart.component marketId={market.id} />
+                        </ErrorBoundary>
+                      </Tab>
+                      <Tab id="depth" name={t('Depth')}>
+                        <ErrorBoundary feature="depth">
+                          <TradingViews.depth.component marketId={market.id} />
+                        </ErrorBoundary>
+                      </Tab>
+                      <Tab id="liquidity" name={t('Liquidity')}>
+                        <ErrorBoundary feature="liquidity">
+                          <TradingViews.liquidity.component
+                            marketId={market.id}
+                          />
+                        </ErrorBoundary>
+                      </Tab>
+                      {market &&
+                      market.tradableInstrument.instrument.product
+                        .__typename === 'Perpetual' ? (
+                        <Tab id="funding-history" name={t('Funding history')}>
+                          <ErrorBoundary feature="funding-history">
+                            <TradingViews.funding.component
+                              marketId={market.id}
+                            />
+                          </ErrorBoundary>
+                        </Tab>
+                      ) : null}
+                    </Tabs>
+                  </TradeGridChild>
+                </ResizableGridPanel>
+                <ResizableGridPanel
+                  minSize={200}
+                  preferredSize={sizesMiddle[1] || 275}
+                >
+                  <TradeGridChild>
+                    <Tabs storageKey="console-trade-grid-main-right">
+                      <Tab id="orderbook" name={t('Orderbook')}>
+                        <ErrorBoundary feature="orderbook">
+                          <TradingViews.orderbook.component
+                            marketId={market.id}
+                          />
+                        </ErrorBoundary>
+                      </Tab>
+                      <Tab
+                        id="trades"
+                        name={t('Trades')}
+                        settings={<TradingViews.trades.settings />}
+                      >
+                        <ErrorBoundary feature="trades">
+                          <TradingViews.trades.component marketId={market.id} />
+                        </ErrorBoundary>
+                      </Tab>
+                    </Tabs>
+                  </TradeGridChild>
+                </ResizableGridPanel>
+              </ResizableGrid>
+            </ResizableGridPanel>
+            <ResizableGridPanel
+              preferredSize={sizes[1] || '25%'}
+              minSize={50}
+              priority={LayoutPriority.Low}
             >
               <TradeGridChild>
-                <Tabs storageKey="console-trade-grid-main-left">
+                <Tabs storageKey="console-trade-grid-bottom">
                   <Tab
-                    id="chart"
-                    overflowHidden
-                    name={t('Chart')}
-                    menu={<TradingViews.chart.menu />}
+                    id="positions"
+                    name={t('Positions')}
+                    menu={<TradingViews.positions.menu />}
+                    settings={<TradingViews.positions.settings />}
                   >
-                    <ErrorBoundary feature="chart">
-                      <TradingViews.chart.component marketId={market.id} />
+                    <ErrorBoundary feature="positions">
+                      <TradingViews.positions.component />
                     </ErrorBoundary>
                   </Tab>
-                  <Tab id="depth" name={t('Depth')}>
-                    <ErrorBoundary feature="depth">
-                      <TradingViews.depth.component marketId={market.id} />
+                  <Tab
+                    id="open-orders"
+                    name={t('Open')}
+                    menu={
+                      <TradingViews.activeOrders.menu marketId={market.id} />
+                    }
+                    settings={<TradingViews.activeOrders.settings />}
+                  >
+                    <ErrorBoundary feature="activeOrders">
+                      <TradingViews.activeOrders.component
+                        marketId={market.id}
+                      />
                     </ErrorBoundary>
                   </Tab>
-                  <Tab id="liquidity" name={t('Liquidity')}>
-                    <ErrorBoundary feature="liquidity">
-                      <TradingViews.liquidity.component marketId={market.id} />
+                  <Tab
+                    id="inactive-orders"
+                    name={t('Order history')}
+                    menu={
+                      <TradingViews.inactiveOrders.menu marketId={market.id} />
+                    }
+                    settings={<TradingViews.inactiveOrders.settings />}
+                  >
+                    <ErrorBoundary feature="inactiveOrders">
+                      <TradingViews.inactiveOrders.component
+                        marketId={market.id}
+                      />
+                    </ErrorBoundary>
+                  </Tab>
+                  {featureFlags.STOP_ORDERS ? (
+                    <Tab
+                      id="stop-orders"
+                      name={t('Advanced orders')}
+                      settings={<TradingViews.stopOrders.settings />}
+                      menu={<TradingViews.stopOrders.menu />}
+                    >
+                      <ErrorBoundary feature="stop-orders">
+                        <TradingViews.stopOrders.component
+                          marketId={market.id}
+                        />
+                      </ErrorBoundary>
+                    </Tab>
+                  ) : null}
+                  <Tab
+                    id="fills"
+                    name={t('Trades')}
+                    settings={<TradingViews.fills.settings />}
+                    menu={<TradingViews.fills.menu />}
+                  >
+                    <TradingViews.fills.component marketId={market.id} />
+                  </Tab>
+                  <Tab
+                    id="accounts"
+                    name={t('Collateral')}
+                    menu={<TradingViews.collateral.menu />}
+                    settings={<TradingViews.collateral.settings />}
+                  >
+                    <ErrorBoundary feature="collateral">
+                      <TradingViews.collateral.component
+                        pinnedAssets={pinnedAssets}
+                      />
                     </ErrorBoundary>
                   </Tab>
                   {market &&
                   market.tradableInstrument.instrument.product.__typename ===
                     'Perpetual' ? (
-                    <Tab id="funding-history" name={t('Funding history')}>
-                      <ErrorBoundary feature="funding-history">
-                        <TradingViews.funding.component marketId={market.id} />
+                    <Tab
+                      id="funding-payments"
+                      name={t('Funding payments')}
+                      settings={<TradingViews.fundingPayments.settings />}
+                      menu={<TradingViews.fundingPayments.menu />}
+                    >
+                      <ErrorBoundary feature="funding-payments">
+                        <TradingViews.fundingPayments.component
+                          marketId={market.id}
+                        />
                       </ErrorBoundary>
                     </Tab>
                   ) : null}
                 </Tabs>
               </TradeGridChild>
             </ResizableGridPanel>
-            <ResizableGridPanel
-              minSize={200}
-              preferredSize={sizesMiddle[1] || 275}
-            >
-              <TradeGridChild>
-                <Tabs storageKey="console-trade-grid-main-right">
-                  <Tab id="orderbook" name={t('Orderbook')}>
-                    <ErrorBoundary feature="orderbook">
-                      <TradingViews.orderbook.component marketId={market.id} />
-                    </ErrorBoundary>
-                  </Tab>
-                  <Tab
-                    id="trades"
-                    name={t('Trades')}
-                    settings={<TradingViews.trades.settings />}
-                  >
-                    <ErrorBoundary feature="trades">
-                      <TradingViews.trades.component marketId={market.id} />
-                    </ErrorBoundary>
-                  </Tab>
-                </Tabs>
-              </TradeGridChild>
-            </ResizableGridPanel>
           </ResizableGrid>
         </ResizableGridPanel>
-        <ResizableGridPanel
-          preferredSize={sizes[1] || '25%'}
-          minSize={50}
-          priority={LayoutPriority.Low}
-        >
-          <TradeGridChild>
-            <Tabs storageKey="console-trade-grid-bottom">
-              <Tab
-                id="positions"
-                name={t('Positions')}
-                menu={<TradingViews.positions.menu />}
-                settings={<TradingViews.positions.settings />}
-              >
-                <ErrorBoundary feature="positions">
-                  <TradingViews.positions.component />
-                </ErrorBoundary>
-              </Tab>
-              <Tab
-                id="open-orders"
-                name={t('Open')}
-                menu={<TradingViews.activeOrders.menu marketId={market.id} />}
-                settings={<TradingViews.activeOrders.settings />}
-              >
-                <ErrorBoundary feature="activeOrders">
-                  <TradingViews.activeOrders.component marketId={market.id} />
-                </ErrorBoundary>
-              </Tab>
-              <Tab
-                id="inactive-orders"
-                name={t('Order history')}
-                menu={<TradingViews.inactiveOrders.menu marketId={market.id} />}
-                settings={<TradingViews.inactiveOrders.settings />}
-              >
-                <ErrorBoundary feature="inactiveOrders">
-                  <TradingViews.inactiveOrders.component marketId={market.id} />
-                </ErrorBoundary>
-              </Tab>
-              {featureFlags.STOP_ORDERS ? (
-                <Tab
-                  id="stop-orders"
-                  name={t('Advanced orders')}
-                  settings={<TradingViews.stopOrders.settings />}
-                  menu={<TradingViews.stopOrders.menu />}
-                >
-                  <ErrorBoundary feature="stop-orders">
-                    <TradingViews.stopOrders.component marketId={market.id} />
-                  </ErrorBoundary>
-                </Tab>
-              ) : null}
-              <Tab
-                id="fills"
-                name={t('Trades')}
-                settings={<TradingViews.fills.settings />}
-                menu={<TradingViews.fills.menu />}
-              >
-                <TradingViews.fills.component marketId={market.id} />
-              </Tab>
-              <Tab
-                id="accounts"
-                name={t('Collateral')}
-                menu={<TradingViews.collateral.menu />}
-                settings={<TradingViews.collateral.settings />}
-              >
-                <ErrorBoundary feature="collateral">
-                  <TradingViews.collateral.component
-                    pinnedAssets={pinnedAssets}
-                  />
-                </ErrorBoundary>
-              </Tab>
-              {market &&
-              market.tradableInstrument.instrument.product.__typename ===
-                'Perpetual' ? (
-                <Tab
-                  id="funding-payments"
-                  name={t('Funding payments')}
-                  settings={<TradingViews.fundingPayments.settings />}
-                  menu={<TradingViews.fundingPayments.menu />}
-                >
-                  <ErrorBoundary feature="funding-payments">
-                    <TradingViews.fundingPayments.component
-                      marketId={market.id}
-                    />
-                  </ErrorBoundary>
-                </Tab>
-              ) : null}
-            </Tabs>
-          </TradeGridChild>
+        <ResizableGridPanel minSize={320} preferredSize={320}>
+          <div>Sidebar</div>
         </ResizableGridPanel>
       </ResizableGrid>
     );
