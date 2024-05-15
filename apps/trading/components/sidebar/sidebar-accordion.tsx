114e3d62589059691d68f3122844a8e324975e71
Matthew Russell <mattrussell36@gmail.com>
2024-05-15 19:07:23 -0400

    feat: get animation working correctly with scroll

--- apps/trading/components/sidebar/sidebar-accordion.tsx
+++ apps/trading/components/sidebar/sidebar-accordion.tsx
@@ -1,38 +1,40 @@
-import * as React from 'react';
+import { forwardRef, type ElementRef, type ComponentPropsWithoutRef } from 'react';
 import * as AccordionPrimitive from '@radix-ui/react-accordion';
 import classNames from 'classnames';
 
-const SidebarAccordion = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Root>
+const SidebarAccordion = forwardRef<
+  ElementRef<typeof AccordionPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof AccordionPrimitive.Root>
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Root
     ref={ref}
     {...props}
-    className={classNames('flex flex-col gap-1', className)}
+    className={classNames('flex flex-col gap-1 overflow-hidden', className)}
   />
 ));
 
 SidebarAccordion.displayName = 'SidebarAccordion';
 
-const SidebarAccordionItem = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
->(({ className, ...props }, ref) => (
+const SidebarAccordionItem = forwardRef<
+  ElementRef<typeof AccordionPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
     className={classNames(
-      'data-[state=open]:grow data-[state=closed]:shrink-0 border border-default flex flex-col min-h-0',
+      'data-[state=open]:flex-[1] data-[state=closed]:flex-[0_1_auto] border border-default transition-[flex] flex flex-col min-h-[39px]',
       className
     )}
     {...props}
-  />
+  >
+    {children}
+  </AccordionPrimitive.Item>
 ));
-SidebarAccordionItem.displayName = 'AccordionItem';
+SidebarAccordionItem.displayName = 'SidebarAccordionItem';
 
-const SidebarAccordionTrigger = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
+const SidebarAccordionTrigger = forwardRef<
+  ElementRef<typeof AccordionPrimitive.Trigger>,
+  ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
@@ -47,24 +49,22 @@ const SidebarAccordionTrigger = React.forwardRef<
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
-SidebarAccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
+SidebarAccordionTrigger.displayName = 'SidebarAccordionTrigger';
 
-const SidebarAccordionContent = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+const SidebarAccordionContent = forwardRef<
+  ElementRef<typeof AccordionPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="text-sm grow overflow-hidden"
+    className="flex-1 text-sm min-h-0"
     {...props}
   >
-    <div className={classNames('p-2 h-full overflow-y-auto', className)}>
-      {children}
-    </div>
+    <div className="overflow-auto h-full p-2">{children}</div>
   </AccordionPrimitive.Content>
 ));
 
-SidebarAccordionContent.displayName = AccordionPrimitive.Content.displayName;
+SidebarAccordionContent.displayName = 'SidebarAccordionContent';
 
 export {
   SidebarAccordion,
--- apps/trading/components/sidebar/sidebar.tsx
+++ apps/trading/components/sidebar/sidebar.tsx
@@ -59,11 +59,7 @@ export const Sidebar = () => {
   const params = useParams();
   return (
     <div className="grid grid-rows-[1fr_min-content] p-1 h-full">
-      <SidebarAccordion
-        type="single"
-        defaultValue="trade"
-        className="h-full min-h-0"
-      >
+      <SidebarAccordion type="single" defaultValue="trade">
         <SidebarAccordionItem value="trade">
           <SidebarAccordionTrigger>Trade</SidebarAccordionTrigger>
           <SidebarAccordionContent>
@@ -96,7 +92,7 @@ export const Sidebar = () => {
             <AssetCard marketId={params.marketId} />
           </SidebarAccordionTrigger>
           <SidebarAccordionContent>
-            <div>Assets content</div>
+            <div>Asset list</div>
           </SidebarAccordionContent>
         </SidebarAccordionItem>
       </SidebarAccordion>
