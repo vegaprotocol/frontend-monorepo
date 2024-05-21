70e2aa9b6f4afa8debc783581cfbc04d4df4ef37
m.ray <16125548+MadalinaRaicu@users.noreply.github.com>
2024-05-21 17:23:36 +0100

    fix(trading): sort markets by 24h vol (#6451)

--- apps/trading/client-pages/markets/use-column-defs.tsx
+++ apps/trading/client-pages/markets/use-column-defs.tsx
@@ -241,6 +241,7 @@ export const useMarketsColumnDefs = () => {
         headerName: t('24h Volume'),
         type: 'rightAligned',
         field: 'data.candles',
+        sort: 'desc',
         valueGetter: ({
           data,
         }: VegaValueGetterParams<MarketMaybeWithDataAndCandles>) => {
