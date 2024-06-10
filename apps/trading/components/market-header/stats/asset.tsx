3e01e0a3c225f3f585b885389d5c9ad26603d20d
Matthew Russell <mattrussell36@gmail.com>
2024-06-10 13:31:58 +0100

    chore: use AssetFieldFragment everywhere so chain id is accessible

--- apps/trading/components/market-header/stats/asset.tsx
+++ apps/trading/components/market-header/stats/asset.tsx
@@ -1,11 +1,15 @@
 import { ButtonLink } from '@vegaprotocol/ui-toolkit';
 import { HeaderStat } from '../../header';
-import { useAssetDetailsDialogStore } from '@vegaprotocol/assets';
+import {
+  type AssetFieldsFragment,
+  useAssetDetailsDialogStore,
+} from '@vegaprotocol/assets';
 import { type HTMLAttributes } from 'react';
+import { type ChainId, ChainIdMapShort } from '@vegaprotocol/web3';
 
 type AssetStatProps = HTMLAttributes<HTMLDivElement> & {
   heading: string;
-  asset: { id: string; symbol: string };
+  asset: AssetFieldsFragment;
 };
 
 /**
@@ -13,6 +17,14 @@ type AssetStatProps = HTMLAttributes<HTMLDivElement> & {
  */
 export const AssetStat = ({ heading, asset, ...props }: AssetStatProps) => {
   const { open: openAssetDetailsDialog } = useAssetDetailsDialogStore();
+
+  let symbol = asset.symbol;
+
+  if (asset.source.__typename === 'ERC20') {
+    symbol = `${asset.symbol} (${
+      ChainIdMapShort[asset.source.chainId as unknown as ChainId]
+    })`;
+  }
 
   return (
     <HeaderStat heading={heading} {...props}>
@@ -22,7 +34,7 @@ export const AssetStat = ({ heading, asset, ...props }: AssetStatProps) => {
             openAssetDetailsDialog(asset.id, e.target as HTMLElement);
           }}
         >
-          {asset.symbol}
+          {symbol}
         </ButtonLink>
       </div>
     </HeaderStat>
