1012fda3a49e8d85c3179a4ef765231f311641d6
bwallacee <ben@vega.xyz>
2024-06-11 17:00:03 +0200

    chore(trading): remove pauses

--- apps/trading/e2e/tests/transfer/test_transfer_key_to_key.py
+++ apps/trading/e2e/tests/transfer/test_transfer_key_to_key.py
@@ -33,7 +33,6 @@ def test_transfer_submit(continuous_market, vega: VegaServiceNull, page: Page):
     page.get_by_test_id("rich-select-option").click()
     page.select_option('[data-testid=transfer-form] [name="toVegaKey"]', index=2)
     page.select_option('[data-testid=transfer-form] [name="fromAccount"]', index=1)
-    page.pause()
     expected_asset_text = re.compile(r"tDAI999991\.49731 tDAItDAI")
     actual_asset_text = page.get_by_test_id("select-asset").text_content().strip()
     assert expected_asset_text.search(
