73f5fbaf5248f37b81019d675c1d1d41cc27f9a6
Matthew Russell <mattrussell36@gmail.com>
2024-06-17 16:06:02 +0100

    feat: sort assets

--- libs/accounts/src/lib/accounts-manager.tsx
+++ libs/accounts/src/lib/accounts-manager.tsx
@@ -1,7 +1,6 @@
-import { memo } from 'react';
+import orderBy from 'lodash/orderBy';
 import sortBy from 'lodash/sortBy';
 import { useDataProvider } from '@vegaprotocol/data-provider';
-import { type useDataGridEvents } from '@vegaprotocol/datagrid';
 import { assetsProvider } from '@vegaprotocol/assets';
 import { AccountCard, type AssetActions } from './account-card';
 
@@ -9,7 +8,6 @@ interface AccountManagerProps extends AssetActions {
   partyId?: string;
   isReadOnly: boolean;
   pinnedAssets?: string[];
-  gridProps?: ReturnType<typeof useDataGridEvents>;
 }
 
 export const AccountManager = ({
@@ -21,28 +19,30 @@ export const AccountManager = ({
     variables: undefined,
   });
 
-  const rows =
-    pinnedAssets && pinnedAssets.length
-      ? sortBy(data, (asset) => {
-          if (pinnedAssets.includes(asset.id)) return -1;
-          return 1;
-        })
-      : data;
+  const orderedData = orderBy(data || [], 'name', 'asc');
+  const rows = sortBy(orderedData, (asset) => {
+    if (!pinnedAssets) return 0;
+    if (pinnedAssets.includes(asset.id)) return -1;
+    return 1;
+  });
 
   return (
     <div className="relative h-full" data-testid="accounts-list">
-      {(rows || []).map((asset) => {
+      {rows.map((asset) => {
         return (
           <AccountCard
+            partyId={props.partyId}
+            isReadOnly={props.isReadOnly}
             asset={asset}
             key={asset.id}
-            {...props}
             expanded={pinnedAssets?.includes(asset.id)}
+            onClickAsset={props.onClickAsset}
+            onClickWithdraw={props.onClickWithdraw}
+            onClickDeposit={props.onClickDeposit}
+            onClickTransfer={props.onClickTransfer}
           />
         );
       })}
     </div>
   );
 };
-
-export default memo(AccountManager);
