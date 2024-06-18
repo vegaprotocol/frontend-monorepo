a595595b4b84bab426bfece333c70132da6d66e5
Matthew Russell <mattrussell36@gmail.com>
2024-06-18 12:07:17 +0100

    feat: add transport urls and fix wagmi connectors version conflict

--- apps/trading/lib/wagmi-config.ts
+++ apps/trading/lib/wagmi-config.ts
@@ -2,14 +2,16 @@ import { http, createConfig } from 'wagmi';
 import { mainnet, sepolia, arbitrum, arbitrumSepolia } from 'wagmi/chains';
 import { getDefaultConfig } from 'connectkit';
 
+import { TRANSPORTS } from '@vegaprotocol/web3';
+
 export const wagmiConfig = createConfig(
   getDefaultConfig({
     chains: [mainnet, sepolia, arbitrum, arbitrumSepolia],
     transports: {
-      // TODO: add mainnet
-      [sepolia.id]: http(process.env.NX_ETHEREUM_PROVIDER_URL),
-      [arbitrum.id]: http(),
-      [arbitrumSepolia.id]: http(),
+      [mainnet.id]: http(TRANSPORTS[mainnet.id]),
+      [sepolia.id]: http(TRANSPORTS[sepolia.id]),
+      [arbitrum.id]: http(TRANSPORTS[arbitrum.id]),
+      [arbitrumSepolia.id]: http(TRANSPORTS[arbitrumSepolia.id]),
     },
     walletConnectProjectId: process.env.NX_WALLETCONNECT_PROJECT_ID as string,
     appName: 'Vega',
