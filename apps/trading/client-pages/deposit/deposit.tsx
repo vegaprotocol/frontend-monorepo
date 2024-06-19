b5d5aa2005d8f7986f749bfe8b8333d41638f077
Matthew Russell <mattrussell36@gmail.com>
2024-06-19 15:26:45 +0100

    chore: re add deposit get started

--- apps/trading/client-pages/deposit/deposit.tsx
+++ apps/trading/client-pages/deposit/deposit.tsx
@@ -1,9 +1,15 @@
 import { useSearchParams } from 'react-router-dom';
 import { DepositContainer } from '../../components/deposit-container';
+import { DepositGetStarted } from './deposit-get-started';
 
 export const Deposit = () => {
   const [searchParams] = useSearchParams();
   const assetId = searchParams.get('assetId') || '';
 
-  return <DepositContainer initialAssetId={assetId} />;
+  return (
+    <div className="flex flex-col gap-6">
+      <DepositContainer initialAssetId={assetId} />
+      <DepositGetStarted />
+    </div>
+  );
 };
