ae98e2a68a03d4c9d9b1cafb429814cf7d02045b
asiaznik <artur@vegaprotocol.io>
2024-06-20 12:07:00 +0200

    chore: switch to gen hooks

--- apps/trading/lib/hooks/use-position-closeout-notification.tsx
+++ apps/trading/lib/hooks/use-position-closeout-notification.tsx
@@ -1,18 +1,8 @@
-import { useApolloClient } from '@apollo/client';
 import {
-  PositionsDocument,
-  type PositionsQuery,
-  type PositionsQueryVariables,
-} from '@vegaprotocol/positions';
-import {
-  TradesDocument,
-  TradesUpdateDocument,
   type TradeUpdateFieldsFragment,
-  type TradesUpdateSubscription,
-  type TradesQuery,
-  type TradesQueryVariables,
-  type TradesUpdateSubscriptionVariables,
   type TradeFieldsFragment,
+  useTradesQuery,
+  useTradesUpdateSubscription,
 } from '@vegaprotocol/trades';
 import { Side, TradeType } from '@vegaprotocol/types';
 import {
@@ -22,9 +12,12 @@ import {
   ToastHeading,
   createActiveTabKey,
 } from '@vegaprotocol/ui-toolkit';
-import { addDecimalsFormatNumber } from '@vegaprotocol/utils';
+import {
+  addDecimalsFormatNumber,
+  removePaginationWrapper,
+} from '@vegaprotocol/utils';
 import { useVegaWallet } from '@vegaprotocol/wallet-react';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { useT } from '../use-t';
 import { useMarketsMapProvider } from '@vegaprotocol/markets';
 import { type AssetFieldsFragment } from '@vegaprotocol/assets';
@@ -32,10 +25,11 @@ import { Link } from 'react-router-dom';
 import { Routes } from '../links';
 import { useLocalStorage } from '@vegaprotocol/react-helpers';
 import uniq from 'lodash/uniq';
-
 import { usePositionsStore } from '../../components/positions-container';
 import { PORTFOLIO_TOP_TABS } from '../../client-pages/portfolio/portfolio';
-import { theme } from '@vegaprotocol/tailwindcss-config';
+import groupBy from 'lodash/groupBy';
+import orderBy from 'lodash/orderBy';
+import compact from 'lodash/compact';
 
 const LOCAL_STORAGE_KEY = 'vega_position_close_out_notifications';
 
@@ -74,8 +68,6 @@ const useSeen = () => {
 const PositionClosedOutToastContent = ({
   closeOutPrice,
   asset,
-  marketId,
-  partyId,
   onClose,
 }: {
   closeOutPrice: string;
@@ -91,34 +83,6 @@ const PositionClosedOutToastContent = ({
   const showClosedPositions = usePositionsStore(
     (state) => state.showClosedMarkets
   );
-
-  const highlightPositionElement = () => {
-    let tries = 0;
-    const int = setInterval(() => {
-      tries += 1;
-      if (tries > 10) {
-        clearInterval(int);
-      }
-      const position = document.querySelector(
-        `[data-position][data-market-id="${marketId}"][data-party-id="${partyId}"]`
-      );
-      if (position) {
-        const agGridPinnedRow =
-          position.parentElement?.parentElement?.parentElement?.parentElement;
-        if (agGridPinnedRow) {
-          agGridPinnedRow.style.background = theme.colors.vega.yellow[500];
-          agGridPinnedRow.style.transition = 'all 50ms ease-in-out';
-          setTimeout(() => {
-            agGridPinnedRow.style.background = '';
-            setTimeout(() => {
-              agGridPinnedRow.style.transition = '';
-            }, 200);
-          }, 200);
-        }
-        clearInterval(int);
-      }
-    }, 200);
-  };
 
   return (
     <>
@@ -141,7 +105,6 @@ const PositionClosedOutToastContent = ({
             if (!showClosedPositions) {
               usePositionsStore.setState({ showClosedMarkets: true });
             }
-            highlightPositionElement();
             onClose?.();
           }}
         >
@@ -152,8 +115,28 @@ const PositionClosedOutToastContent = ({
   );
 };
 
+const determinePubKey = (
+  trade: TradeUpdateFieldsFragment | TradeFieldsFragment
+) => {
+  let pubKey: string | undefined = undefined;
+  switch (trade.aggressor) {
+    case Side.SIDE_SELL:
+      if (trade.__typename === 'Trade') pubKey = trade.buyer.id;
+      if (trade.__typename === 'TradeUpdate') pubKey = trade.buyerId;
+      break;
+    case Side.SIDE_BUY:
+      if (trade.__typename === 'Trade') pubKey = trade.seller.id;
+      if (trade.__typename === 'TradeUpdate') pubKey = trade.sellerId;
+      break;
+    default:
+    case Side.SIDE_UNSPECIFIED:
+      pubKey = undefined;
+      break;
+  }
+  return pubKey;
+};
+
 export const usePositionCloseOutNotification = () => {
-  const client = useApolloClient();
   const { pubKeys } = useVegaWallet();
   const { data: markets } = useMarketsMapProvider();
   const { seen, setSeen } = useSeen();
@@ -164,164 +147,119 @@ export const usePositionCloseOutNotification = () => {
     store.update,
   ]);
 
-  const onClose = useCallback(
-    (tradeId: string) => () => {
-      // hides toast instead of removing is so it won't be re-added on rerender
-      updateToast(createToastId(tradeId), { hidden: true });
-      setSeen(tradeId);
-    },
-    [setSeen, updateToast]
-  );
+  const onClose = (tradeId: string) => () => {
+    updateToast(createToastId(tradeId), { hidden: true });
+    setSeen(tradeId);
+  };
 
-  const [errorCount, setErrorCount] = useState(0);
+  const pushNotification = (
+    trade: TradeFieldsFragment | TradeUpdateFieldsFragment
+  ) => {
+    const marketId =
+      trade.__typename === 'Trade'
+        ? trade.market.id
+        : trade.__typename === 'TradeUpdate'
+        ? trade.marketId
+        : undefined;
+    if (!marketId) return;
 
-  const onRejection = useCallback(
-    () => setErrorCount((errorCount) => errorCount + 1),
-    [setErrorCount]
-  );
+    const market = markets?.[marketId];
+    const pubKey = determinePubKey(trade);
+    const toastId = createToastId(trade.id);
 
-  const pushNotification = useCallback(
-    (trade: TradeFieldsFragment | TradeUpdateFieldsFragment) => {
-      const marketId =
-        trade.__typename === 'Trade'
-          ? trade.market.id
-          : trade.__typename === 'TradeUpdate'
-          ? trade.marketId
-          : undefined;
-      if (!marketId) return;
-
-      const market = markets?.[marketId];
-
-      // The pubKey associated with closed out position
-      let pubKey: string | undefined = undefined;
-      switch (trade.aggressor) {
-        case Side.SIDE_SELL:
-          if (trade.__typename === 'Trade') pubKey = trade.buyer.id;
-          if (trade.__typename === 'TradeUpdate') pubKey = trade.buyerId;
-          break;
-        case Side.SIDE_BUY:
-          if (trade.__typename === 'Trade') pubKey = trade.seller.id;
-          if (trade.__typename === 'TradeUpdate') pubKey = trade.sellerId;
-          break;
-        default:
-        case Side.SIDE_UNSPECIFIED:
-          pubKey = undefined;
-          break;
-      }
-
-      const toastId = createToastId(trade.id);
-
-      if (!market || !pubKey || seen.includes(trade.id) || hasToast(toastId)) {
-        return;
-      }
-
-      const closeOutPrice = addDecimalsFormatNumber(
-        trade.price,
-        market.decimalPlaces
-      );
-
-      let asset: Pick<AssetFieldsFragment, 'symbol'> | undefined = undefined;
-      if (
-        market.tradableInstrument.instrument.product.__typename === 'Future' ||
-        market.tradableInstrument.instrument.product.__typename === 'Perpetual'
-      ) {
-        asset = market.tradableInstrument.instrument.product.settlementAsset;
-      } else if (
-        market.tradableInstrument.instrument.product.__typename === 'Spot'
-      ) {
-        asset = market.tradableInstrument.instrument.product.quoteAsset;
-      }
-
-      if (!asset) return;
-
-      const toast: Toast = {
-        id: createToastId(trade.id),
-        intent: Intent.Warning,
-        onClose: onClose(trade.id),
-        content: (
-          <PositionClosedOutToastContent
-            closeOutPrice={closeOutPrice}
-            asset={asset}
-            marketId={market.id}
-            partyId={pubKey}
-            onClose={onClose(trade.id)}
-          />
-        ),
-      };
-      setToast(toast);
-    },
-    [hasToast, markets, onClose, seen, setToast]
-  );
-
-  useEffect(() => {
-    let subscription: { unsubscribe: () => void } | undefined = undefined;
-    let ignore = false;
-
-    if (!pubKeys) {
+    if (!market || !pubKey || seen.includes(trade.id) || hasToast(toastId)) {
       return;
     }
 
-    const runQueries = () => {
-      if (ignore) return;
+    const closeOutPrice = addDecimalsFormatNumber(
+      trade.price,
+      market.decimalPlaces
+    );
 
-      // 1. listen for trades (for all connected pub keys)
-      // 2. notify if a trade is TYPE_NETWORK_CLOSE_OUT_BAD
-      subscription = client
-        .subscribe<TradesUpdateSubscription, TradesUpdateSubscriptionVariables>(
-          {
-            query: TradesUpdateDocument,
-            variables: {
-              partyIds: [...pubKeys.map((k) => k.publicKey)],
-            },
-            fetchPolicy: 'no-cache',
-          }
+    let asset: Pick<AssetFieldsFragment, 'symbol'> | undefined = undefined;
+    if (
+      market.tradableInstrument.instrument.product.__typename === 'Future' ||
+      market.tradableInstrument.instrument.product.__typename === 'Perpetual'
+    ) {
+      asset = market.tradableInstrument.instrument.product.settlementAsset;
+    } else if (
+      market.tradableInstrument.instrument.product.__typename === 'Spot'
+    ) {
+      asset = market.tradableInstrument.instrument.product.quoteAsset;
+    }
+
+    if (!asset) return;
+
+    const toast: Toast = {
+      id: createToastId(trade.id),
+      intent: Intent.Warning,
+      onClose: onClose(trade.id),
+      content: (
+        <PositionClosedOutToastContent
+          closeOutPrice={closeOutPrice}
+          asset={asset}
+          marketId={market.id}
+          partyId={pubKey}
+          onClose={onClose(trade.id)}
+        />
+      ),
+    };
+    setToast(toast);
+  };
+
+  useTradesQuery({
+    variables: {
+      partyIds: [...pubKeys.map((k) => k.publicKey)],
+    },
+    skip: pubKeys.length === 0,
+    onCompleted: (data) => {
+      const tradesByMarket = groupBy(
+        orderBy(
+          removePaginationWrapper(data.trades?.edges),
+          (trade) => trade.createdAt,
+          'desc'
         )
-        .subscribe(({ data }) => {
-          data?.tradesStream?.forEach((trade) => {
-            if (trade.type === TradeType.TYPE_NETWORK_CLOSE_OUT_BAD) {
-              pushNotification(trade);
-            }
-          });
-        }, onRejection);
+          .map((trade) => {
+            return { ...trade, pubKey: determinePubKey(trade) };
+          })
+          .filter(
+            (trade) =>
+              trade.pubKey &&
+              pubKeys.map((pk) => pk.publicKey).includes(trade.pubKey)
+          ),
+        (trade) => {
+          return `${trade.market.id}-${trade.pubKey}`;
+        }
+      );
 
-      // 1. get positions
-      // 2. get latest trades for positions
-      // 3. notify if a trade is TYPE_NETWORK_CLOSE_OUT_BAD
-      client
-        .query<PositionsQuery, PositionsQueryVariables>({
-          query: PositionsDocument,
-          variables: { partyIds: [...pubKeys.map((k) => k.publicKey)] },
-          fetchPolicy: 'no-cache',
-        })
-        .then(({ data }) => {
-          data.positions?.edges?.forEach(({ node: position }) => {
-            client
-              .query<TradesQuery, TradesQueryVariables>({
-                query: TradesDocument,
-                variables: {
-                  marketIds: [position.market.id],
-                  partyIds: [position.party.id],
-                  pagination: { first: 1 },
-                },
-                fetchPolicy: 'no-cache',
-              })
-              .then(({ data }) => {
-                const trade = data.trades?.edges?.[0]?.node;
-                if (
-                  trade &&
-                  trade.type === TradeType.TYPE_NETWORK_CLOSE_OUT_BAD
-                ) {
-                  pushNotification(trade);
-                }
-              }, onRejection);
-          });
-        });
-    };
-    runQueries();
+      for (const trades of Object.values(tradesByMarket)) {
+        if (
+          trades.length > 0 &&
+          trades[0].type === TradeType.TYPE_NETWORK_CLOSE_OUT_BAD
+        ) {
+          pushNotification(trades[0]);
+        }
+      }
+    },
+  });
 
-    return () => {
-      if (subscription) subscription.unsubscribe();
-      ignore = true;
-    };
-  }, [pubKeys, client, pushNotification, onRejection, errorCount]);
+  useTradesUpdateSubscription({
+    variables: {
+      partyIds: [...pubKeys.map((k) => k.publicKey)],
+    },
+    skip: pubKeys.length === 0,
+    onData: (options) => {
+      const updates = compact(options.data.data?.tradesStream);
+      for (const update of updates) {
+        const pubKey = determinePubKey(update);
+        if (
+          pubKey &&
+          pubKeys.map((pk) => pk.publicKey).includes(pubKey) &&
+          update.type === TradeType.TYPE_NETWORK_CLOSE_OUT_BAD
+        ) {
+          pushNotification(update);
+        }
+      }
+    },
+  });
 };
