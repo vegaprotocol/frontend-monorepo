a172133c187794e85f15080b30323ef55748f892
Matthew Russell <mattrussell36@gmail.com>
2024-06-27 12:22:38 +0100

    fix: input zoom on safari

--- apps/trading/pages/index.page.tsx
+++ apps/trading/pages/index.page.tsx
@@ -16,6 +16,10 @@ export default function Index() {
         />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Vega Protocol - Console" />
         <meta name="og:type" content="website" />
