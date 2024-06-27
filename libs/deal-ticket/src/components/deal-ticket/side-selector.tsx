fd3ff8f00c4298b46a98fa6b71d82bbbac27d381
Matthew Russell <mattrussell36@gmail.com>
2024-06-27 11:17:24 +0100

    feat: align colors of side seletor and submit button

--- libs/deal-ticket/src/components/deal-ticket/side-selector.tsx
+++ libs/deal-ticket/src/components/deal-ticket/side-selector.tsx
@@ -5,8 +5,8 @@ import { useT } from '../../use-t';
 import { VegaIcon, VegaIconNames } from '@vegaprotocol/ui-toolkit';
 
 interface SideSelectorProps {
-  value: Schema.Side;
-  onValueChange: (side: Schema.Side) => void;
+  value: Side;
+  onValueChange: (side: Side) => void;
   isSpotMarket?: boolean;
 }
 
@@ -42,22 +42,28 @@ export const SideSelector = ({
   return (
     <RadioGroup.Root
       name="order-side"
-      className="mb-4 flex h-10 leading-10 bg-vega-clight-700 dark:bg-vega-cdark-700 rounded"
+      className="relative mb-4 flex h-10 leading-10 bg-vega-clight-700 dark:bg-vega-cdark-700 rounded"
       {...props}
     >
+      <span
+        className={classNames(
+          'absolute top-0 left-0 rounded w-1/2 h-full transition-transform',
+          {
+            'bg-vega-green-600 dark:bg-vega-green-650':
+              props.value === Side.SIDE_BUY,
+            'bg-vega-red-500 dark:bg-vega-red-600 translate-x-full':
+              props.value === Side.SIDE_SELL,
+          }
+        )}
+      />
       {toggles.map(({ label, value, iconName }) => (
         <RadioGroup.Item value={value} key={value} id={`side-${value}`} asChild>
           <button
             className={classNames(
-              'flex gap-2 justify-center items-center flex-1 relative text-sm rounded',
+              'flex gap-2 justify-center items-center flex-1 relative text-sm rounded transition-colors',
+              'data-[state=unchecked]:hover:bg-vega-clight-500 dark:data-[state=unchecked]:hover:bg-vega-cdark-500',
               'data-[state=unchecked]:text-vega-clight-200 dark:data-[state=unchecked]:text-vega-cdark-200',
-              'data-[state=checked]:text-vega-clight-90 dark:data-[state=checked]:text-vega-cdark-90',
-              {
-                'data-[state=checked]:bg-vega-green-650':
-                  value === Side.SIDE_BUY,
-                'data-[state=checked]:bg-vega-red-600':
-                  value === Side.SIDE_SELL,
-              }
+              'data-[state=checked]:text-vega-clight-800 dark:data-[state=checked]:text-vega-cdark-90'
             )}
             data-testid={`order-side-${value}`}
           >
