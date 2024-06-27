794bd1d003b7fb910b2fa925acfb9cca0564c845
Matthew Russell <mattrussell36@gmail.com>
2024-06-27 12:08:01 +0100

    feat: adjust type selector styles, fix alignment and stop order submit

---
+++ libs/deal-ticket/src/components/deal-ticket/submit-button.tsx
@@ -0,0 +1,38 @@
+import { type ReactNode } from 'react';
+import classNames from 'classnames';
+
+import { Side } from '@vegaprotocol/types';
+
+export const SubmitButton = ({
+  text,
+  subLabel,
+  side,
+}: {
+  text: string;
+  subLabel: ReactNode;
+  side: Side;
+}) => {
+  return (
+    <button
+      data-testid="place-order"
+      type="submit"
+      className={classNames(
+        'w-full flex flex-col justify-center items-center rounded text-vega-clight-800 p-2 transition-colors',
+        {
+          'bg-vega-red-500 enabled:hover:bg-vega-red-550 dark:bg-vega-red-600 dark:enabled:hover:bg-vega-red-650':
+            side === Side.SIDE_SELL,
+          'bg-vega-green-600 enabled:hover:bg-vega-green-650 dark:bg-vega-green-650 dark:enabled:hover:bg-vega-green-600':
+            side === Side.SIDE_BUY,
+        }
+      )}
+    >
+      <span>{text}</span>
+      <span
+        className="text-xs font-mono leading-4 text-vega-clight-800/60"
+        key="trading-button-sub-label"
+      >
+        {subLabel}
+      </span>
+    </button>
+  );
+};
