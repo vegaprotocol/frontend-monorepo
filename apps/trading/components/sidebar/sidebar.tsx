0f1e3ff6f0e87fdc7829743d85e0255077087140
Matthew Russell <mattrussell36@gmail.com>
2024-07-02 10:46:46 +0100

    fix: remove bg from asset-card wrapper

--- apps/trading/components/sidebar/sidebar.tsx
+++ apps/trading/components/sidebar/sidebar.tsx
@@ -92,13 +92,10 @@ export const Sidebar = ({ pinnedAssets }: { pinnedAssets?: string[] }) => {
             <SidebarAccordionHeader>
               <AccordionPrimitive.Trigger
                 data-testid="Assets"
-                className={classNames(
-                  'grid w-full bg-vega-clight-700 dark:bg-vega-cdark-700',
-                  {
-                    'grid-cols-2': pinnedAssets.length === 2,
-                    'grid-cols-1': pinnedAssets.length === 1,
-                  }
-                )}
+                className={classNames('grid w-full', {
+                  'grid-cols-2': pinnedAssets.length === 2,
+                  'grid-cols-1': pinnedAssets.length === 1,
+                })}
               >
                 {pinnedAssets.map((assetId) => (
                   <AssetCard
