b241d1923273da35288eebe98d525cda1fad604a
Matthew Russell <mattrussell36@gmail.com>
2024-07-18 22:18:27 +0100

    feat: make auction text blue

--- apps/trading/components/ticket-container/feedback/market-auction.tsx
+++ apps/trading/components/ticket-container/feedback/market-auction.tsx
@@ -11,10 +11,7 @@ export const MarketAuction = ({
 
   if (marketTradingMode === MarketTradingMode.TRADING_MODE_MONITORING_AUCTION) {
     msg = (
-      <p
-        className="text-xs text-warning"
-        data-testid="feedback-market-monitoring-auction"
-      >
+      <p data-testid="feedback-market-monitoring-auction">
         {t(
           'This market is in auction due to high price volatility. Until the auction ends, you can only place GFA, GTT, or GTC limit orders.'
         )}
@@ -24,10 +21,7 @@ export const MarketAuction = ({
 
   if (marketTradingMode === MarketTradingMode.TRADING_MODE_OPENING_AUCTION) {
     msg = (
-      <p
-        className="text-xs text-warning"
-        data-testid="feedback-market-opening-auction"
-      >
+      <p data-testid="feedback-market-opening-auction">
         {t(
           'This is a new market in an opening auction to determine a fair mid-price before starting continuous trading.'
         )}
@@ -36,9 +30,9 @@ export const MarketAuction = ({
   }
 
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col gap-1 text-xs text-vega-blue-600">
       {msg}
-      <p className="text-xs text-warning" data-testid="feedback-market-auction">
+      <p data-testid="feedback-market-auction">
         {t('Any orders placed now will not trade until the auction ends')}
       </p>
     </div>
