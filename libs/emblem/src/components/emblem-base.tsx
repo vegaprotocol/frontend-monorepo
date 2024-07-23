5070e9764ffefdd96c035eb409779f3f510edb32
Edd <edd@vega.xyz>
2024-07-23 11:37:01 +0100

    feat(emblem): simplify loading and use transparent image instead of black for loading

--- libs/emblem/src/components/emblem-base.tsx
+++ libs/emblem/src/components/emblem-base.tsx
@@ -1,4 +1,4 @@
-import { useState, type ImgHTMLAttributes } from 'react';
+import type ImgHTMLAttributes from 'react';
 import { FALLBACK_URL } from '../config/index';
 import className from 'classnames';
 
@@ -10,33 +10,22 @@ export type ImgProps = ImgHTMLAttributes<HTMLImageElement> & { size?: number };
  * @returns React.Node
  */
 export function EmblemBase({ size = 30, ...p }: ImgProps) {
-  // Used to render a holding image while the emblem is loading
-  const [loading, setLoading] = useState(true);
-
   // If loading the emblem fails, we use a remote fallback image
   const renderFallback = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     e.currentTarget.src = FALLBACK_URL;
   };
 
   return (
-    <>
-      <span style={{ display: loading ? 'inline-block' : 'none' }}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42">
-          <rect width="42" height="42" rx="21" />
-        </svg>
-      </span>
-      <img
-        src={p.src || FALLBACK_URL}
-        onError={renderFallback}
-        alt={p.alt ? p.alt : 'Emblem'}
-        width={size}
-        height={size}
-        className={className(
-          'rounded-full bg-vega-light-600 border-vega-light-600 dark:bg-white dark:border-white',
-          p.className
-        )}
-        onLoad={() => setLoading(false)}
-      />
-    </>
+    <img
+      src={p.src || FALLBACK_URL}
+      onError={renderFallback}
+      alt={p.alt ? p.alt : 'Emblem'}
+      width={size}
+      height={size}
+      className={className(
+        'rounded-full bg-vega-light-600 border-vega-light-600 dark:bg-white dark:border-white',
+        p.className
+      )}
+    />
   );
 }
