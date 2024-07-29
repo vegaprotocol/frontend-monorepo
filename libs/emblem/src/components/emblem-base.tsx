1a18fcf24c49be0a8f7006ba42bba672ef18d51a
Edd <edd@vega.xyz>
2024-07-29 11:26:55 +0100

    feat(emblem): simplify loading and use transparent image instead of black for loading (#6714)

--- libs/emblem/src/components/emblem-base.tsx
+++ libs/emblem/src/components/emblem-base.tsx
@@ -1,8 +1,12 @@
-import { useState, type ImgHTMLAttributes } from 'react';
 import { FALLBACK_URL } from '../config/index';
 import className from 'classnames';
 
-export type ImgProps = ImgHTMLAttributes<HTMLImageElement> & { size?: number };
+export type ImgProps = {
+  size?: number;
+  alt?: string;
+  src?: string;
+  className?: string;
+};
 
 /**
  * Renders an image tag with a known fallback if the emblem does not exist.
@@ -10,33 +14,22 @@ export type ImgProps = ImgHTMLAttributes<HTMLImageElement> & { size?: number };
  * @returns React.Node
  */
 export function EmblemBase({ size = 30, ...p }: ImgProps) {
-  // Used to render a holding image while the emblem is loading
-  const [loading, setLoading] = useState(true);
-
   // If loading the emblem fails, we use a remote fallback image
   const renderFallback = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     e.currentTarget.src = FALLBACK_URL;
   };
 
   return (
-    <>
-      <span style={{ display: loading ? 'inline-block' : 'none' }}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42">
-          <rect width="42" height="42" rx="21" />
-        </svg>
-      </span>
-      <img
-        src={p.src || FALLBACK_URL}
-        onError={renderFallback}
-        alt={p.alt ? p.alt : 'Emblem'}
-        width={size}
-        height={size}
-        className={className(
-          'rounded-full bg-vega-light-600 border-vega-light-600 dark:bg-white dark:border-white',
-          p.className
-        )}
-        onLoad={() => setLoading(false)}
-      />
-    </>
+    <img
+      src={p.src || FALLBACK_URL}
+      onError={renderFallback}
+      alt={p.alt ? p.alt : 'Emblem'}
+      width={size}
+      height={size}
+      className={className(
+        'rounded-full bg-vega-light-600 border-vega-light-600 dark:bg-white dark:border-white',
+        p.className
+      )}
+    />
   );
 }
--- libs/emblem/src/config/index.ts
+++ libs/emblem/src/config/index.ts
@@ -1,7 +1,12 @@
 export const URL_BASE = 'https://icon.vega.xyz';
 export const FILENAME = 'logo.svg';
 export const CHAIN_FILENAME = 'chain.svg';
-export const FALLBACK_URL = `${URL_BASE}/missing.svg`;
 
 export const DEFAULT_VEGA_CHAIN = 'vega-mainnet-0011';
 export const DEFAULT_CHAIN = '1';
+
+// Missing and Loaded are a very simple SVG< b64 encoded.
+// Loading is now TRANSPARENT, missing is BLACK
+// <svg width="250" height="250" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg" ><rect width="42" height="42" rx="21" fill="transparent" /></svg>
+export const FALLBACK_URL = `data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjUwIiBoZWlnaHQ9IjI1MCIgdmlld0JveD0iMCAwIDQyIDQyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciID48cmVjdCB3aWR0aD0iNDIiIGhlaWdodD0iNDIiIHJ4PSIyMSIgZmlsbD0iYmxhY2siIC8+PC9zdmc+`;
+export const LOADING = `data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjUwIiBoZWlnaHQ9IjI1MCIgdmlld0JveD0iMCAwIDQyIDQyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciID48cmVjdCB3aWR0aD0iNDIiIGhlaWdodD0iNDIiIHJ4PSIyMSIgZmlsbD0idHJhbnNwYXJlbnQiIC8+PC9zdmc+`;
