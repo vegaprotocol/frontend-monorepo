3741fd669f1d0df61f179645df0f5bc17d2a80c1
asiaznik <artur@vegaprotocol.io>
2024-07-31 19:10:09 +0200

    chore: hardcode usdt asset id

--- apps/trading/lib/utils/is-asset-usdt-arb.ts
+++ apps/trading/lib/utils/is-asset-usdt-arb.ts
@@ -1,14 +1,8 @@
 import type { AssetFieldsFragment } from '@vegaprotocol/assets';
-import {
-  ARBITRUM_CHAIN_ID,
-  ARBITRUM_SEPOLIA_CHAIN_ID,
-} from '@vegaprotocol/web3';
+
+const USDT_ARB_ID =
+  '29eef0c7fa049e9a866629a23554866313e4cc15a61fb43656e275547a10d305';
 
 export const isAssetUSDTArb = (asset: AssetFieldsFragment) => {
-  const source = asset.source.__typename === 'ERC20' ? asset.source : undefined;
-  return (
-    asset.symbol.toLowerCase() === 'usdt' &&
-    (source?.chainId === String(ARBITRUM_CHAIN_ID) ||
-      source?.chainId === String(ARBITRUM_SEPOLIA_CHAIN_ID))
-  );
+  return asset.id === USDT_ARB_ID;
 };
