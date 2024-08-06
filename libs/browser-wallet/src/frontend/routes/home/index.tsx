49238b57e03f436e7be7ee0c2625b13920fb7cad
Dexter <dexter@vega.xyz>
2024-08-06 17:08:30 +0100

    feat: wallet ui rendering

--- libs/browser-wallet/src/frontend/routes/home/index.tsx
+++ libs/browser-wallet/src/frontend/routes/home/index.tsx
@@ -1,11 +1,13 @@
 import { Navigate } from 'react-router-dom';
 
 import { useGetRedirectPath } from '@/hooks/redirect-path';
+import { FULL_ROUTES } from '../route-names';
 // import { useSentry } from '@/hooks/sentry'
 
 export const Home = () => {
-  const { loading, path } = useGetRedirectPath();
+  console.log('Navigate to get started');
+  // const { loading, path } = useGetRedirectPath();
   // useSentry()
   // If loading then we do not know where to redirect to yet
-  return loading || !path ? null : <Navigate to={path} />;
+  return <Navigate to={FULL_ROUTES.getStarted} />;
 };
