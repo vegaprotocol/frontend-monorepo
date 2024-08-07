4dbabbbf9274e22b8371a0f2d518fb50cfc77717
Dexter <dexter@vega.xyz>
2024-08-07 10:55:04 +0100

    test: adjust test config to function in monorepo envrionment

--- libs/browser-wallet/jest.config.ts
+++ libs/browser-wallet/jest.config.ts
@@ -1,11 +1,28 @@
-/* eslint-disable */
-export default {
-  displayName: 'browser-wallet',
-  preset: '../../jest.preset.js',
+const config = {
+  displayName: 'browser-wallet-ui',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/browser-wallet',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/frontend/$1',
+  },
+  preset: '../../jest.preset.js',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./src/frontend/setup-tests.ts'],
+  globalSetup: './src/frontend/global-setup.ts',
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**', '!test/**'],
+  coverageDirectory: 'coverage/frontend',
+  coverageThreshold: {
+    global: {
+      branches: 98.08,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
+
+export default config;
--- libs/browser-wallet/src/frontend/setup-tests.ts
+++ libs/browser-wallet/src/frontend/setup-tests.ts
@@ -5,11 +5,13 @@
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
 
-import TimeAgo from 'javascript-time-ago';
+// import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
 import ResizeObserver from 'resize-observer-polyfill';
 
-TimeAgo.addDefaultLocale(en);
+// console.log(TimeAgo, en);
+
+// TimeAgo.addDefaultLocale(en);
 
 // Required by radix-ui/react-tooltip
 global.ResizeObserver = ResizeObserver;
