d55b87eb69f546ad7ba41d1495120b0d49a74c5d
Dexter <dexter@vega.xyz>
2024-08-14 10:29:44 +0200

    chore: remove splashes that no longer apply

--- libs/browser-wallet/src/frontend/components/modals/modal-wrapper/index.tsx
+++ libs/browser-wallet/src/frontend/components/modals/modal-wrapper/index.tsx
@@ -1,6 +1,4 @@
 import { ConnectionModal } from '../connection-modal';
-import { OrientationSplash } from '../orientation-splash';
-// import { PopoverOpenSplash } from '../popover-open-splash';
 import { TransactionModal } from '../transaction-modal';
 
 export const ModalWrapper = () => {
@@ -8,9 +6,6 @@ export const ModalWrapper = () => {
     <>
       <ConnectionModal />
       <TransactionModal />
-      {/* Must be at the bottom to ensure that if we have a connection or transaction pending the popover modal is always shown */}
-      {/* <PopoverOpenSplash /> */}
-      <OrientationSplash />
     </>
   );
 };
