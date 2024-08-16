cb9b5a74c52323643ddc2080c1831bd4584ecd14
Dexter <dexter@vega.xyz>
2024-08-16 12:11:48 +0200

    chore: move derived mnemonic creation to a hook

--- libs/browser-wallet/src/frontend/hooks/suggest-mnemonic/index.ts
+++ libs/browser-wallet/src/frontend/hooks/suggest-mnemonic/index.ts
@@ -3,6 +3,50 @@ import { useCallback, useEffect, useState } from 'react';
 import { useJsonRpcClient } from '@/contexts/json-rpc/json-rpc-context';
 import { RpcMethods } from '@/lib/client-rpc-methods';
 
+const SIGN_TYPED_DATA_V4 = 'eth_signTypedData_v4';
+
+export const useDerivedMnemonic = (account: `0x${string}`, chainId: number) => {
+  const [mnemonic, setMnemonic] = useState<string | null>(null);
+  const { request } = useJsonRpcClient();
+
+  useEffect(() => {
+    const run = async () => {
+      const params = {
+        domain: { name: 'Vega', chainId },
+        message: { action: 'Vega Onboarding' },
+        primaryType: 'Vega',
+        types: {
+          EIP712Domain: [
+            { name: 'name', type: 'string' },
+            { name: 'chainId', type: 'uint256' },
+          ],
+          Vega: [{ name: 'action', type: 'string' }],
+        },
+      };
+      const res = await window.ethereum.request({
+        method: SIGN_TYPED_DATA_V4,
+        params: [account, JSON.stringify(params)],
+      });
+      // TODO if res is not successful then throw an error
+      console.log(res);
+      // TODO take the value of the signed message and pass it to the backend
+      const { derivedMnemonic } = await request(
+        RpcMethods.CreateDerivedMnemonic,
+        {
+          signedData: res,
+        }
+      );
+      console.log(derivedMnemonic);
+      setMnemonic(derivedMnemonic);
+    };
+    run();
+  }, [account, chainId, request]);
+
+  return {
+    mnemonic,
+  };
+};
+
 /**
  * Suggests a mnemonic to the user and stores it in memory
  * once the user creates the wallet the mnemonic should be cleared from memory
--- libs/browser-wallet/src/frontend/lib/create-wallet.ts
+++ libs/browser-wallet/src/frontend/lib/create-wallet.ts
@@ -39,36 +39,3 @@ export const createWallet = async (mnemonic: string, request: SendMessage) => {
     wallet: WALLET_NAME,
   });
 };
-
-const SIGN_TYPED_DATA_V4 = 'eth_signTypedData_v4';
-
-export const createDerivedWallet = async (
-  chainId: number,
-  account: string,
-  request: SendMessage
-) => {
-  const params = {
-    domain: { name: 'Vega', chainId: 1 },
-    message: { action: 'Vega Onboarding' },
-    primaryType: 'Vega',
-    types: {
-      EIP712Domain: [
-        { name: 'name', type: 'string' },
-        { name: 'chainId', type: 'uint256' },
-      ],
-      Vega: [{ name: 'action', type: 'string' }],
-    },
-  };
-  const res = await window.ethereum.request({
-    method: SIGN_TYPED_DATA_V4,
-    params: [account, JSON.stringify(params)],
-  });
-  // TODO if res is not successful then throw an error
-  console.log(res);
-  // TODO take the value of the signed message and pass it to the backend
-  const { derivedMnemonic } = await request(RpcMethods.CreateDerivedMnemonic, {
-    signedData: res,
-  });
-  console.log(derivedMnemonic);
-  return createWallet(derivedMnemonic, request);
-};
