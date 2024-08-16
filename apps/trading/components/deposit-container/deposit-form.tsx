fa7f86daec5eecc2222c2696354beacbecefd608
Matthew Russell <mattrussell36@gmail.com>
2024-08-16 08:07:34 +0300

    feat: add chain selector and filtered from asset list

--- apps/trading/components/deposit-container/deposit-form.tsx
+++ apps/trading/components/deposit-container/deposit-form.tsx
@@ -35,18 +35,20 @@ import {
   FormSecondaryActionWrapper,
 } from '../form-secondary-action';
 import { VegaKeySelect } from './vega-key-select';
-import { AssetOption } from '../asset-option';
 import { Approval } from './approval';
 import { useAssetReadContracts } from './use-asset-read-contracts';
 import { Faucet } from './faucet';
 import { isAssetUSDTArb } from '../../lib/utils/is-asset-usdt-arb';
+import { AssetOption } from '../asset-option';
 
 type Configs = Array<EthereumConfig | EVMBridgeConfig>;
 type FormFields = z.infer<typeof depositSchema>;
 
 const depositSchema = z.object({
   fromAddress: z.string().regex(ETHEREUM_ADDRESS_REGEX, 'Connect wallet'),
-  assetId: z.string().regex(VEGA_ID_REGEX, 'Required'),
+  chainId: z.string(),
+  fromAsset: z.string(),
+  toAsset: z.string().regex(VEGA_ID_REGEX, 'Required'),
   toPubKey: z.string().regex(VEGA_ID_REGEX, 'Invalid key'),
   // Use a string but parse it as a number for validation
   amount: z.string().refine(
@@ -91,14 +93,20 @@ export const DepositForm = ({
       // it as a form field so its included with the zodResolver validation
       // and shows up as an error if its not set
       fromAddress: address,
-      assetId: initialAssetId,
+      toAsset: initialAssetId,
       toPubKey: '',
       amount: '',
     },
   });
 
+  const chainIdVal = useWatch({ name: 'chainId', control: form.control });
   const amount = useWatch({ name: 'amount', control: form.control });
-  const assetId = useWatch({ name: 'assetId', control: form.control });
+  const assetId = useWatch({ name: 'toAsset', control: form.control });
+  const tokens = squid.tokens?.filter((t) => {
+    if (!chainIdVal) return true;
+    if (t.chainId === chainIdVal) return true;
+    return false;
+  });
   const asset = assets?.find((a) => a.id === assetId);
 
   // Data relating to the select asset, like balance on address, allowance
@@ -117,7 +125,7 @@ export const DepositForm = ({
     <form
       data-testid="deposit-form"
       onSubmit={form.handleSubmit((fields) => {
-        const asset = assets?.find((a) => a.id === fields.assetId);
+        const asset = assets?.find((a) => a.id === fields.toAsset);
 
         if (!asset || asset.source.__typename !== 'ERC20') {
           throw new Error('invalid asset');
@@ -187,10 +195,38 @@ export const DepositForm = ({
           </TradingInputError>
         )}
       </FormGroup>
-      <FormGroup label="Asset" labelFor="asset">
+      <FormGroup label="From chain" labelFor="chain">
+        <Controller
+          name="chainId"
+          control={form.control}
+          render={({ field }) => {
+            return (
+              <TradingRichSelect
+                placeholder="Select chain"
+                value={field.value}
+                onValueChange={field.onChange}
+              >
+                {squid.chains.map((c) => {
+                  return (
+                    <TradingRichSelectOption value={c.chainId} key={c.chainId}>
+                      <div className="text-sm text-left leading-4">
+                        <div>{c.networkName}</div>
+                        <div className="text-secondary text-xs">
+                          {c.chainId}
+                        </div>
+                      </div>
+                    </TradingRichSelectOption>
+                  );
+                })}
+              </TradingRichSelect>
+            );
+          }}
+        />
+      </FormGroup>
+      <FormGroup label="From asset" labelFor="asset">
         <div className="flex flex-col gap-1">
           <Controller
-            name="assetId"
+            name="fromAsset"
             control={form.control}
             render={({ field }) => {
               return (
@@ -199,10 +235,18 @@ export const DepositForm = ({
                   value={field.value}
                   onValueChange={field.onChange}
                 >
-                  {assets.map((a) => {
+                  {tokens.map((t) => {
                     return (
-                      <TradingRichSelectOption value={a.id} key={a.id}>
-                        <AssetOption asset={a} />
+                      <TradingRichSelectOption
+                        value={t.address}
+                        key={t.address}
+                      >
+                        <div className="text-sm text-left leading-4">
+                          <div>{t.name}</div>
+                          <div className="text-secondary text-xs">
+                            {t.address}
+                          </div>
+                        </div>
                       </TradingRichSelectOption>
                     );
                   })}
@@ -217,9 +261,9 @@ export const DepositForm = ({
               )}
             </TradingInputError>
           )}
-          {form.formState.errors.assetId?.message && (
+          {form.formState.errors.fromAsset?.message && (
             <TradingInputError>
-              {form.formState.errors.assetId.message}
+              {form.formState.errors.fromAsset.message}
             </TradingInputError>
           )}
         </div>
@@ -274,6 +318,53 @@ export const DepositForm = ({
           <TradingInputError>
             {form.formState.errors.toPubKey.message}
           </TradingInputError>
+        )}
+      </FormGroup>
+      <FormGroup label="To asset" labelFor="asset">
+        <div className="flex flex-col gap-1">
+          <Controller
+            name="toAsset"
+            control={form.control}
+            render={({ field }) => {
+              return (
+                <TradingRichSelect
+                  placeholder="Select asset"
+                  value={field.value}
+                  onValueChange={field.onChange}
+                >
+                  {assets.map((a) => {
+                    return (
+                      <TradingRichSelectOption value={a.id} key={a.id}>
+                        <AssetOption asset={a} />
+                      </TradingRichSelectOption>
+                    );
+                  })}
+                </TradingRichSelect>
+              );
+            }}
+          />
+          {asset && !isAssetUSDTArb(asset) && (
+            <TradingInputError intent="warning">
+              {t(
+                'The majority of markets on the network settle in USDT Arb. Are you sure you wish to deposit the selected asset?'
+              )}
+            </TradingInputError>
+          )}
+          {form.formState.errors.toAsset?.message && (
+            <TradingInputError>
+              {form.formState.errors.toAsset.message}
+            </TradingInputError>
+          )}
+        </div>
+        {asset && (
+          <FormSecondaryActionWrapper>
+            <FormSecondaryActionButton
+              onClick={() => openAssetDialog(asset.id)}
+            >
+              {t('View asset details')}
+            </FormSecondaryActionButton>
+            <Faucet asset={asset} queryKey={queryKey} />
+          </FormSecondaryActionWrapper>
         )}
       </FormGroup>
       <FormGroup label="Amount" labelFor="amount">
