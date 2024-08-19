91fc949a9dc209e12b6d0f6d4d28917ca0c6fd53
Dexter <dexter@vega.xyz>
2024-08-19 16:20:59 +0200

    chore: use feature flag rather than commenting

--- apps/trading/lib/hooks/use-vega-wallet-config.ts
+++ apps/trading/lib/hooks/use-vega-wallet-config.ts
@@ -1,7 +1,7 @@
 import {
-  // InjectedConnector,
-  // JsonRpcConnector,
-  // SnapConnector,
+  InjectedConnector,
+  JsonRpcConnector,
+  SnapConnector,
   ViewPartyConnector,
   InBrowserConnector,
   createConfig,
@@ -14,7 +14,7 @@ import {
 import {
   CHAIN_IDS,
   useEnvironment,
-  // useFeatureFlags,
+  useFeatureFlags,
 } from '@vegaprotocol/environment';
 import { useMemo } from 'react';
 
@@ -23,21 +23,21 @@ import { useMemo } from 'react';
  * node address for snap connection we need to re-new once its set or changes
  */
 export const useVegaWalletConfig = () => {
-  const { VEGA_ENV, API_NODE, VEGA_WALLET_URL } = useEnvironment();
+  const { VEGA_ENV, VEGA_URL, VEGA_WALLET_URL } = useEnvironment();
   const { IN_BROWSER_WALLET } = useFeatureFlags((state) => state.flags);
 
   return useMemo(() => {
     if (!API_NODE || !VEGA_WALLET_URL) return;
 
-    // const injected = new InjectedConnector();
+    const injected = new InjectedConnector();
     const inBrowser = new InBrowserConnector();
 
-    // const jsonRpc = new JsonRpcConnector({
-    //   url: VEGA_WALLET_URL,
-    // });
+    const jsonRpc = new JsonRpcConnector({
+      url: VEGA_WALLET_URL,
+    });
 
     const snap = new SnapConnector({
-      node: new URL(API_NODE.graphQLApiUrl).origin,
+      node: new URL(VEGA_URL).origin,
       snapId: 'npm:@vegaprotocol/snap',
       version: '1.0.1',
     });
@@ -47,9 +47,11 @@ export const useVegaWalletConfig = () => {
     const config = createConfig({
       chains: [mainnet, mirror, fairground, validatorsTestnet, stagnet],
       defaultChainId: CHAIN_IDS[VEGA_ENV],
-      connectors: [inBrowser, viewParty],
+      connectors: IN_BROWSER_WALLET
+        ? [inBrowser, viewParty]
+        : [injected, snap, jsonRpc, viewParty],
     });
 
     return config;
-  }, [IN_BROWSER_WALLET, VEGA_ENV, API_NODE, VEGA_WALLET_URL]);
+  }, [IN_BROWSER_WALLET, VEGA_ENV, VEGA_URL, VEGA_WALLET_URL]);
 };
