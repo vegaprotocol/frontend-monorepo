b98e200f9eeacf014855602d1b448085e072c08c
Dexter <dexter@vega.xyz>
2024-08-22 12:16:17 +0200

    chore: fix build

--- libs/browser-wallet/src/frontend/contexts/json-rpc/hooks/create-server.ts
+++ libs/browser-wallet/src/frontend/contexts/json-rpc/hooks/create-server.ts
@@ -3,10 +3,6 @@ import { getExtensionApi } from '@/lib/extension-apis';
 import { log } from '@/lib/logging';
 import { ServerRpcMethods } from '@/lib/server-rpc-methods';
 import type { TransactionMessage } from '@/lib/transactions';
-import type {
-  ConnectionMessage,
-  ConnectionReply,
-} from '@/stores/interaction-store';
 
 const maybeCloseWindow = () => {
   const url = new URL(window.location.href);
@@ -18,19 +14,12 @@ const maybeCloseWindow = () => {
 
 // TODO add own tests
 export const createServer = (
-  handleConnection: (parameters: ConnectionMessage) => Promise<ConnectionReply>,
   handleTransaction: (parameters: TransactionMessage) => Promise<boolean>
 ) => {
   const { runtime } = getExtensionApi();
   const backgroundPort = runtime.connect({ name: 'popup' });
   const server = new JSONRPCServer({
     methods: {
-      async [ServerRpcMethods.Connection](parameters: any, context: any) {
-        log('info', 'Message pushed from background', parameters, context);
-        const response = await handleConnection(parameters);
-        maybeCloseWindow();
-        return response;
-      },
       async [ServerRpcMethods.Transaction](parameters: any, context: any) {
         log('info', 'Message pushed from background', parameters, context);
         const response = await handleTransaction(parameters);
