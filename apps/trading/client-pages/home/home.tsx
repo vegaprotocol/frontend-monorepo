707d02a23c4ebf85ca9a0b3638bee76b1381372d
Matthew Russell <mattrussell36@gmail.com>
2024-08-23 14:14:10 +0100

    feat: clear out old home page redirect

--- apps/trading/client-pages/home/home.tsx
+++ apps/trading/client-pages/home/home.tsx
@@ -1,15 +1,3 @@
-import { Loader, Splash } from '@vegaprotocol/ui-toolkit';
-import { useNavigateToLastMarket } from '../../lib/hooks/use-navigate-to-last-market';
-
-// The home pages only purpose is to redirect to the users last market,
-// the top traded if they are new, or fall back to the list of markets.
-// Thats why we just render a loader here
 export const Home = () => {
-  useNavigateToLastMarket();
-
-  return (
-    <Splash>
-      <Loader />
-    </Splash>
-  );
+  return <div>foo</div>;
 };
