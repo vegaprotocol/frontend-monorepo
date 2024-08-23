dc39ffe1e9488beda75d8056d55e33b55285c6d2
asiaznik <artur@vegaprotocol.io>
2024-08-23 18:26:56 +0200

    fix: e2e

--- apps/trading/e2e/tests/market/test_market_info.py
+++ apps/trading/e2e/tests/market/test_market_info.py
@@ -215,7 +215,7 @@ def test_market_info_risk_factors(page: Page):
     fields = [
         ["Long", "0.05153"],
         ["Short", "0.05421"],
-        ["Max leverage long", "19.406"],
+        ["Max leverage long", "19.405"],
         ["Max leverage short", "18.445"],
         ["Max initial leverage long", "12.937"],
         ["Max initial leverage short", "12.297"],
