63653ad92a1b7f807d89e0f7c7cee0b788e4f794
Matthew Russell <mattrussell36@gmail.com>
2024-08-26 11:18:12 +0200

    chore: remove checking of actual color value and use classname

--- apps/trading/e2e/tests/pnl/test_pnl.py
+++ apps/trading/e2e/tests/pnl/test_pnl.py
@@ -1,18 +1,22 @@
 import pytest
-from playwright.sync_api import Page
+import re
+from playwright.sync_api import Page, expect
 from vega_sim.null_service import VegaServiceNull
 from actions.vega import submit_order
 from actions.utils import change_keys
 
 
-def check_pnl_color_value(element, expected_color, expected_value):
-    color = element.evaluate("element => getComputedStyle(element).color")
+def check_pnl_cell(element, dir, expected_value):
     value = element.inner_text()
-    assert color == expected_color, f"Unexpected color: {color}"
+
+    if dir == "up":
+        expect(element).to_have_class(re.compile(r"text-dir-up-fg"))
+    elif dir == "down":
+        expect(element).to_have_class(re.compile(r"text-dir-down-fg"))
+
     assert value == expected_value, f"Unexpected value: {value}"
 
 # TODO move this test to jest
-
 
 @pytest.mark.usefixtures("auth", "risk_accepted")
 def test_pnl(continuous_market, vega: VegaServiceNull, page: Page):
@@ -30,19 +34,19 @@ def test_pnl(continuous_market, vega: VegaServiceNull, page: Page):
     realised_pnl = row.locator("[col-id='realisedPNL']")
     unrealised_pnl = row.locator("[col-id='unrealisedPNL']")
 
-    check_pnl_color_value(realised_pnl, "rgb(0, 0, 0)", "0.00")
-    check_pnl_color_value(unrealised_pnl, "rgb(236, 0, 60)", "-4.00")
+    check_pnl_cell(realised_pnl, "none", "0.00")
+    check_pnl_cell(unrealised_pnl, "down", "-4.00")
 
     # profit Trading unrealised
     page.locator("label").filter(has_text="Show closed positions").click()
     change_keys(page, vega, "market_maker")
-    check_pnl_color_value(realised_pnl, "rgb(0, 0, 0)", "0.00")
-    check_pnl_color_value(unrealised_pnl, "rgb(1, 145, 75)", "4.00")
+    check_pnl_cell(realised_pnl, "none", "0.00")
+    check_pnl_cell(unrealised_pnl, "up", "4.00")
 
     # neutral Trading unrealised
     change_keys(page, vega, "market_maker_2")
-    check_pnl_color_value(realised_pnl, "rgb(0, 0, 0)", "0.00")
-    check_pnl_color_value(unrealised_pnl, "rgb(0, 0, 0)", "0.00")
+    check_pnl_cell(realised_pnl, "none", "0.00")
+    check_pnl_cell(unrealised_pnl, "none", "0.00")
 
     # Portfolio Unrealised
     page.get_by_role("link", name="Portfolio").click()
@@ -76,27 +80,27 @@ def test_pnl(continuous_market, vega: VegaServiceNull, page: Page):
     )
     key_mm2_realised_pnl = key_mm2.query_selector(
         'xpath=./div[@col-id="realisedPNL"]')
-    check_pnl_color_value(key_1_realised_pnl, "rgb(0, 0, 0)", "0.00")
-    check_pnl_color_value(key_1_unrealised_pnl, "rgb(236, 0, 60)", "-4.00")
+    check_pnl_cell(key_1_realised_pnl, "none", "0.00")
+    check_pnl_cell(key_1_unrealised_pnl, "down", "-4.00")
 
-    check_pnl_color_value(key_mm_realised_pnl, "rgb(0, 0, 0)", "0.00")
-    check_pnl_color_value(key_mm_unrealised_pnl, "rgb(1, 145, 75)", "4.00")
+    check_pnl_cell(key_mm_realised_pnl, "none", "0.00")
+    check_pnl_cell(key_mm_unrealised_pnl, "up", "4.00")
 
-    check_pnl_color_value(key_mm2_realised_pnl, "rgb(0, 0, 0)", "0.00")
-    check_pnl_color_value(key_mm2_unrealised_pnl, "rgb(0, 0, 0)", "0.00")
+    check_pnl_cell(key_mm2_realised_pnl, "none", "0.00")
+    check_pnl_cell(key_mm2_unrealised_pnl, "none", "0.00")
 
     submit_order(vega, "Key 1", continuous_market, "SIDE_SELL", 2, 101.50000)
     vega.wait_fn(1)
     vega.wait_for_total_catchup()
 
-    check_pnl_color_value(key_1_realised_pnl, "rgb(236, 0, 60)", "-8.00")
-    check_pnl_color_value(key_1_unrealised_pnl, "rgb(0, 0, 0)", "0.00")
+    check_pnl_cell(key_1_realised_pnl, "down", "-8.00")
+    check_pnl_cell(key_1_unrealised_pnl, "none", "0.00")
 
-    check_pnl_color_value(key_mm_realised_pnl, "rgb(1, 145, 75)", "8.00")
-    check_pnl_color_value(key_mm_unrealised_pnl, "rgb(0, 0, 0)", "0.00")
+    check_pnl_cell(key_mm_realised_pnl, "up", "8.00")
+    check_pnl_cell(key_mm_unrealised_pnl, "none", "0.00")
 
-    check_pnl_color_value(key_mm2_realised_pnl, "rgb(0, 0, 0)", "0.00")
-    check_pnl_color_value(key_mm2_unrealised_pnl, "rgb(0, 0, 0)", "0.00")
+    check_pnl_cell(key_mm2_realised_pnl, "none", "0.00")
+    check_pnl_cell(key_mm2_unrealised_pnl, "none", "0.00")
 
     page.get_by_role("link", name="Trading").click()
 
@@ -109,15 +113,15 @@ def test_pnl(continuous_market, vega: VegaServiceNull, page: Page):
     unrealised_pnl = row.locator("[col-id='unrealisedPNL']")
 
     # neutral trading realised
-    check_pnl_color_value(realised_pnl, "rgb(0, 0, 0)", "0.00")
-    check_pnl_color_value(unrealised_pnl, "rgb(0, 0, 0)", "0.00")
+    check_pnl_cell(realised_pnl, "none", "0.00")
+    check_pnl_cell(unrealised_pnl, "none", "0.00")
 
     # profit trading realised
     change_keys(page, vega, "market_maker")
-    check_pnl_color_value(realised_pnl, "rgb(1, 145, 75)", "8.00")
-    check_pnl_color_value(unrealised_pnl, "rgb(0, 0, 0)", "0.00")
+    check_pnl_cell(realised_pnl, "up", "8.00")
+    check_pnl_cell(unrealised_pnl, "none", "0.00")
 
     # loss trading realised
     change_keys(page, vega, "Key 1")
-    check_pnl_color_value(realised_pnl, "rgb(236, 0, 60)", "-8.00")
-    check_pnl_color_value(unrealised_pnl, "rgb(0, 0, 0)", "0.00")
+    check_pnl_cell(realised_pnl, "down", "-8.00")
+    check_pnl_cell(unrealised_pnl, "none", "0.00")
