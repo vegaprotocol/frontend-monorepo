e23290f26d5fb8f602723dd1bdafb05c3f25e754
Matthew Russell <mattrussell36@gmail.com>
2024-08-31 08:59:40 +0200

    feat: dont show empty transfers section

--- apps/trading/client-pages/home/home.tsx
+++ apps/trading/client-pages/home/home.tsx
@@ -55,38 +55,40 @@ export const Home = () => {
         </ul>
       </section>
 
-      <section className="py-10">
-        <ul className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          {transfers.map((transfer, i) => {
-            return (
-              <li
-                key={i}
-                className="bg-black/5 dark:bg-black/40 grid grid-rows-[subgrid] row-span-4 gap-4 p-8 rounded-lg"
-              >
-                <div>
-                  <Pill intent={Intent.Primary}>Liquidity</Pill>
-                </div>
-                <h4 className="text-2xl">
-                  Share of all fees ({transfer.amount.toFormat()})
-                </h4>
-                <p>
-                  Commit liquidity to an AMM and receive a share of all fees
-                  paid on that market
-                </p>
-                <p>
-                  <Link
-                    to={Links.COMPETITIONS_GAME('TODO: game-id')}
-                    className="underline underline-offset-4 flex items-center gap-1"
-                  >
-                    {t('View game')}
-                    <VegaIcon name={VegaIconNames.ARROW_RIGHT} />
-                  </Link>
-                </p>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
+      {transfers.length > 0 && (
+        <section className="py-10">
+          <ul className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            {transfers.map((transfer, i) => {
+              return (
+                <li
+                  key={i}
+                  className="bg-black/5 dark:bg-black/40 grid grid-rows-[subgrid] row-span-4 gap-4 p-8 rounded-lg"
+                >
+                  <div>
+                    <Pill intent={Intent.Primary}>Liquidity</Pill>
+                  </div>
+                  <h4 className="text-2xl">
+                    Share of all fees ({transfer.amount.toFormat()})
+                  </h4>
+                  <p>
+                    Commit liquidity to an AMM and receive a share of all fees
+                    paid on that market
+                  </p>
+                  <p>
+                    <Link
+                      to={Links.COMPETITIONS_GAME('TODO: game-id')}
+                      className="underline underline-offset-4 flex items-center gap-1"
+                    >
+                      {t('View game')}
+                      <VegaIcon name={VegaIconNames.ARROW_RIGHT} />
+                    </Link>
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      )}
 
       <section className="flex flex-col items-center gap-6 py-10">
         <h3 className="text-4xl">Explore the Console</h3>
