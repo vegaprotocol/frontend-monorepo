8bbd5931c1d7cf45309d46d603d7cfa046d47f04
Matthew Russell <mattrussell36@gmail.com>
2024-09-02 21:22:27 +0100

    chore: revert emblem changes

--- libs/emblem/src/components/hooks/use-market-info.ts
+++ libs/emblem/src/components/hooks/use-market-info.ts
@@ -1,9 +1,9 @@
+import { useEffect, useState } from 'react';
 import { URL_BASE } from '../../config';
-import { useQuery } from '@tanstack/react-query';
 
 // This is a subset of the fields available in the JSON, but these
 // are the only fields required by the Emblem components
-type MarketInfo = {
+export type MarketInfo = {
   // Full path to the logo for the quote asset
   quoteLogo: string;
   // Full path to the logo for the settlement asset
@@ -18,6 +18,12 @@ type MarketInfo = {
   baseChainLogo?: string;
 };
 
+export type MarketInfoState = {
+  loading: boolean;
+  data?: MarketInfo | null;
+  error: Error | null | undefined;
+};
+
 /**
  * Fetch and parse the market info file
  *
@@ -25,28 +31,57 @@ type MarketInfo = {
  * @returns Promise<MarketInfo>
  */
 export async function fetchMarketInfo(path: string) {
-  return (await fetch(path)).json() as Promise<MarketInfo>;
+  return (await fetch(path)).json();
 }
 
 /**
  * Hook to fetch the information JSON for a specific market. There is a
  * single file that contains the base, quote and settlement logos for all
- * markets
+ * markets, so this hook could be adapted to fetch that once and parse
+ * out the relevant market info. This has not been done yet, nor has caching
+ * been done in the hook, as the headers set by icon.vega.xyz will ensure
+ * that the response is cached, which is sufficient for now.
  *
  * @param vegaChain string representing the vega chain (e.g. 'vega-testnet-1')
  * @param marketId string Market ID
  * @returns MarketInfoState
  */
 export const useMarketInfo = (vegaChain: string, marketId: string) => {
-  const queryResult = useQuery({
-    queryKey: ['emblem', vegaChain, marketId],
-    queryFn: ({ queryKey }) => {
-      const [, vegaChain, marketId] = queryKey;
-      return fetchMarketInfo(
-        `${URL_BASE}/vega/${vegaChain}/market/${marketId}/index.json`
-      );
-    },
+  const [state, setState] = useState<MarketInfoState>({
+    loading: true,
+    data: null,
+    error: null,
   });
 
-  return queryResult;
+  useEffect(() => {
+    let mounted = true;
+
+    const path = `${URL_BASE}/vega/${vegaChain}/market/${marketId}/index.json`;
+
+    fetchMarketInfo(path)
+      .then((market) => {
+        if (mounted) {
+          setState({
+            loading: false,
+            data: market,
+            error: null,
+          });
+        }
+      })
+      .catch((err) => {
+        if (mounted) {
+          setState({
+            loading: false,
+            data: null,
+            error: err,
+          });
+        }
+      });
+
+    return () => {
+      mounted = false;
+    };
+  }, [vegaChain, marketId]);
+
+  return state;
 };
