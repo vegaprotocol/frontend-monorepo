56f631da00ff34f8e3ec06a8ccb09dad758f26fc
Matthew Russell <mattrussell36@gmail.com>
2024-09-04 15:57:58 +0100

    chore(trading,governance,explorer): remove locize (#6846)
    Co-authored-by: Dexter <dexter@vega.xyz>

--- apps/trading/lib/i18n/index.ts
+++ apps/trading/lib/i18n/index.ts
@@ -1,55 +1,13 @@
-import type { Module } from 'i18next';
 import i18n from 'i18next';
 import HttpBackend from 'i18next-http-backend';
-import LocizeBackend from 'i18next-locize-backend';
 import type { HttpBackendOptions, RequestCallback } from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 export const supportedLngs = ['en'];
 
-const isInDev = false;
-const useLocize = isInDev && !!process.env.NX_USE_LOCIZE;
-
-const backend = useLocize
-  ? {
-      projectId: '96ac1231-4bdd-455a-b9d7-f5322a2e7430',
-      apiKey: process.env.NX_LOCIZE_API_KEY,
-      referenceLng: 'en',
-    }
-  : {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-      request: (
-        options: HttpBackendOptions,
-        url: string,
-        payload: string,
-        callback: RequestCallback
-      ) => {
-        if (typeof window === 'undefined') {
-          callback(false, { status: 200, data: {} });
-          return;
-        }
-        fetch(url).then((response) => {
-          if (!response.ok) {
-            return callback(response.statusText || 'Error', {
-              status: response.status,
-              data: {},
-            });
-          }
-          response
-            .text()
-            .then((data) => {
-              callback(null, { status: response.status, data });
-            })
-            .catch((error) => callback(error, { status: 200, data: {} }));
-        });
-      },
-    };
-
-const Backend: Module = useLocize ? LocizeBackend : HttpBackend;
-
 i18n
-  .use(Backend)
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -86,9 +44,35 @@ i18n
     defaultNS: 'trading',
     nsSeparator: false,
     keySeparator: false, // we use content as keys
-    backend,
-    debug: isInDev,
-    saveMissing: useLocize && !!process.env.NX_LOCIZE_API_KEY,
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      request: (
+        options: HttpBackendOptions,
+        url: string,
+        payload: string,
+        callback: RequestCallback
+      ) => {
+        if (typeof window === 'undefined') {
+          callback(false, { status: 200, data: {} });
+          return;
+        }
+        fetch(url).then((response) => {
+          if (!response.ok) {
+            return callback(response.statusText || 'Error', {
+              status: response.status,
+              data: {},
+            });
+          }
+          response
+            .text()
+            .then((data) => {
+              callback(null, { status: response.status, data });
+            })
+            .catch((error) => callback(error, { status: 200, data: {} }));
+        });
+      },
+    },
+    debug: process.env.NODE_ENV === 'development',
     interpolation: {
       escapeValue: false,
     },
