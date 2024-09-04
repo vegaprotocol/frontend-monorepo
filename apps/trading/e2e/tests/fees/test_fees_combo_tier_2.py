689c997b446c3f32fb10df12e1bef13d5e6f1ae5
Matthew Russell <mattrussell36@gmail.com>
2024-09-04 14:36:08 +0100

    test: remove e2e tests that are replaced by unit tests

--- apps/trading/e2e/tests/fees/test_fees_combo_tier_2.py
+++ apps/trading/e2e/tests/fees/test_fees_combo_tier_2.py
@@ -77,72 +77,49 @@ def setup_environment(
             yield vega_instance, market, page_instance
 
 
-def test_fees_page_discount_program_my_trading_fees(
-    setup_environment: Tuple[VegaServiceNull, str, Page],
-) -> None:
-    vega, market, page = setup_environment
-    page.goto("/#/fees")
-    expect(page.get_by_test_id(ADJUSTED_FEES)).to_have_text("6.432%-6.432%")
-    expect(page.get_by_test_id(TOTAL_FEE_BEFORE_DISCOUNT)).to_have_text(
-        "Taker fee before discount10.05%-10.05%"
-    )
-    expect(page.get_by_test_id(INFRASTRUCTURE_FEES)).to_have_text("Infrastructure0.05%")
-    expect(page.get_by_test_id(MAKER_FEES)).to_have_text("Maker10%")
-    expect(page.get_by_test_id(LIQUIDITY_FEES)).to_have_text("Liquidity0%-0%")
+# def test_fees_page_discount_program_my_trading_fees(
+#     setup_environment: Tuple[VegaServiceNull, str, Page],
+# ) -> None:
+#     vega, market, page = setup_environment
+#     page.goto("/#/fees")
+#     expect(page.get_by_test_id(ADJUSTED_FEES)).to_have_text("6.432%-6.432%")
+#     expect(page.get_by_test_id(TOTAL_FEE_BEFORE_DISCOUNT)).to_have_text(
+#         "Taker fee before discount10.05%-10.05%"
+#     )
+#     expect(page.get_by_test_id(INFRASTRUCTURE_FEES)).to_have_text("Infrastructure0.05%")
+#     expect(page.get_by_test_id(MAKER_FEES)).to_have_text("Maker10%")
+#     expect(page.get_by_test_id(LIQUIDITY_FEES)).to_have_text("Liquidity0%-0%")
 
 
-def test_fees_page_discount_program_total_discount(
-    setup_environment: Tuple[VegaServiceNull, str, Page],
-) -> None:
-    vega, market, page = setup_environment
-    page.goto("/#/fees")
-    expect(page.get_by_test_id(TOTAL_DISCOUNT)).to_have_text("36%")
-    expect(page.get_by_test_id(VOLUME_DISCOUNT_ROW)).to_have_text("Volume discount20%")
-    expect(page.get_by_test_id(REFERRAL_DISCOUNT_ROW)).to_have_text(
-        "Referral discount20%"
-    )
-    page.get_by_test_id(TOTAL_DISCOUNT).hover()
-    expect(page.get_by_test_id(TOOLTIP_CONTENT).nth(0)).to_have_text(
-        "The total discount is calculated according to the following formula: 1 - (1 - dvolume) ⋇ (1 - dreferral)"
-    )
+# def test_fees_page_discount_program_total_discount(
+#     setup_environment: Tuple[VegaServiceNull, str, Page],
+# ) -> None:
+#     vega, market, page = setup_environment
+#     page.goto("/#/fees")
+#     expect(page.get_by_test_id(TOTAL_DISCOUNT)).to_have_text("36%")
+#     expect(page.get_by_test_id(VOLUME_DISCOUNT_ROW)).to_have_text("Volume discount20%")
+#     expect(page.get_by_test_id(REFERRAL_DISCOUNT_ROW)).to_have_text(
+#         "Referral discount20%"
+#     )
+#     page.get_by_test_id(TOTAL_DISCOUNT).hover()
+#     expect(page.get_by_test_id(TOOLTIP_CONTENT).nth(0)).to_have_text(
+#         "The total discount is calculated according to the following formula: 1 - (1 - dvolume) ⋇ (1 - dreferral)"
+#     )
 
 
-def test_fees_page_discount_program_fees_by_market(
-    setup_environment: Tuple[VegaServiceNull, str, Page],
-) -> None:
-    vega, market, page = setup_environment
-    page.goto("/#/fees")
-    pinned = page.locator(PINNED_ROW_LOCATOR)
-    row = page.locator(ROW_LOCATOR)
-    expect(pinned.locator(COL_CODE)).to_have_text("BTC:DAI_2023FutrBTC:DAI_2023 ")
-    expect(row.locator(COL_FEE_AFTER_DISCOUNT)).to_have_text("6.432%")
-    expect(row.locator(COL_INFRA_FEE)).to_have_text("0.05%")
-    expect(row.locator(COL_MAKER_FEE)).to_have_text("10%")
-    expect(row.locator(COL_LIQUIDITY_FEE)).to_have_text("0%")
-    expect(row.locator(COL_TOTAL_FEE)).to_have_text("10.05%")
-
-
-def test_deal_ticket_discount_program(
-    setup_environment: Tuple[VegaServiceNull, str, Page],
-) -> None:
-    vega, market, page = setup_environment
-    page.goto(f"/#/markets/{market}")
-    page.get_by_test_id(ORDER_SIZE).fill("1")
-    page.get_by_test_id(ORDER_PRICE).fill("1")
-    expect(page.get_by_test_id(DISCOUNT_PILL)).to_have_text("-36%")
-    page.get_by_test_id(FEES_TEXT).hover()
-    tooltip = page.get_by_test_id(TOOLTIP_CONTENT).first
-    expect(tooltip.get_by_test_id(INFRASTRUCTURE_FEE_FACTOR)).to_have_text("0.05%")
-    expect(tooltip.get_by_test_id(INFRASTRUCTURE_FEE_VALUE)).to_have_text("0.0005 tDAI")
-    expect(tooltip.get_by_test_id(LIQUIDITY_FEE_FACTOR)).to_have_text("0%")
-    expect(tooltip.get_by_test_id(LIQUIDITY_FEE_VALUE)).to_have_text("0.00 tDAI")
-    expect(tooltip.get_by_test_id(MAKER_FEE_FACTOR)).to_have_text("10%")
-    expect(tooltip.get_by_test_id(MAKER_FEE_VALUE)).to_have_text("0.10 tDAI")
-    expect(tooltip.get_by_test_id(SUBTOTAL_FEE_FACTOR)).to_have_text("10.05%")
-    expect(tooltip.get_by_test_id(SUBTOTAL_FEE_VALUE)).to_have_text("0.1005 tDAI")
-    expect(tooltip.get_by_test_id(DISCOUNT_FEE_FACTOR)).to_have_text("-36%")
-    expect(tooltip.get_by_test_id(DISCOUNT_FEE_VALUE)).to_have_text("-0.03618 tDAI")
-    expect(tooltip.get_by_test_id(TOTAL_FEE_VALUE)).to_have_text("0.06432 tDAI")
+# def test_fees_page_discount_program_fees_by_market(
+#     setup_environment: Tuple[VegaServiceNull, str, Page],
+# ) -> None:
+#     vega, market, page = setup_environment
+#     page.goto("/#/fees")
+#     pinned = page.locator(PINNED_ROW_LOCATOR)
+#     row = page.locator(ROW_LOCATOR)
+#     expect(pinned.locator(COL_CODE)).to_have_text("BTC:DAI_2023FutrBTC:DAI_2023 ")
+#     expect(row.locator(COL_FEE_AFTER_DISCOUNT)).to_have_text("6.432%")
+#     expect(row.locator(COL_INFRA_FEE)).to_have_text("0.05%")
+#     expect(row.locator(COL_MAKER_FEE)).to_have_text("10%")
+#     expect(row.locator(COL_LIQUIDITY_FEE)).to_have_text("0%")
+#     expect(row.locator(COL_TOTAL_FEE)).to_have_text("10.05%")
 
 
 def test_fills_taker_discount_program(
@@ -174,34 +151,3 @@ def test_fills_maker_discount_program(
     expect(row.locator(COL_AGGRESSOR)).to_have_text("Maker")
     expect(row.locator(COL_FEE)).to_have_text("-10.5984 tDAI")
     expect(row.locator(COL_FEE_DISCOUNT)).to_have_text("7.452 tDAI")
-
-
-def test_fills_maker_fee_tooltip_discount_program(
-    setup_environment: Tuple[VegaServiceNull, str, Page],
-) -> None:
-    vega, market, page = setup_environment
-    page.goto(f"/#/markets/{market}")
-    change_keys(page, vega, MM_WALLET.name)
-    page.get_by_test_id(TRADES).nth(1).click()
-    row = page.locator(ROW_LOCATOR).first
-    # tbd - tooltip is not visible without this wait
-    page.wait_for_timeout(1000)
-    row.locator(COL_FEE).hover()
-    expect(page.get_by_test_id(FEE_BREAKDOWN_TOOLTIP)).to_have_text(
-        "If the market was activeFee revenue to be received by the maker, takers' fee discounts already applied.During continuous trading the maker pays no infrastructure and liquidity fees.Infrastructure fee0.00 tDAILiquidity fee0.00 tDAIMaker fee-10.5984 tDAITotal fees-10.5984 tDAI"
-    )
-
-
-def test_fills_taker_fee_tooltip_discount_program(
-    setup_environment: Tuple[VegaServiceNull, str, Page],
-) -> None:
-    vega, market, page = setup_environment
-    page.goto(f"/#/markets/{market}")
-    page.get_by_test_id(TRADES).nth(1).click()
-    row = page.locator(ROW_LOCATOR).first
-    # tbd - tooltip is not visible without this wait
-    page.wait_for_timeout(1000)
-    row.locator(COL_FEE).hover()
-    expect(page.get_by_test_id(FEE_BREAKDOWN_TOOLTIP)).to_have_text(
-        "If the market was activeFee revenue to be received by the maker, takers' fee discounts already applied.During continuous trading the maker pays no infrastructure and liquidity fees.Infrastructure fee0.00 tDAILiquidity fee0.00 tDAIMaker fee-10.5984 tDAITotal fees-10.5984 tDAI"
-    )
