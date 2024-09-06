b108ccdc1acca6c912cf59d8250dedb1e5e23c3e
Dexter <dexter@vega.xyz>
2024-09-06 08:58:26 +0700

    chore: refactor to make file more understandable

--- libs/wallet-react/src/components/connect-dialog/connection-options/connection-options.tsx
+++ libs/wallet-react/src/components/connect-dialog/connection-options/connection-options.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
-import { ConnectorErrors, type ConnectorType } from '@vegaprotocol/wallet';
+import {
+  type Connector,
+  ConnectorErrors,
+  type ConnectorType,
+} from '@vegaprotocol/wallet';
 import {
   Accordion,
   AccordionItem,
@@ -13,6 +17,86 @@ import { Trans } from 'react-i18next';
 import { ConnectionOptionRecord } from './custom-connection-options';
 import { ConnectionOptionDefault } from './connection-option-default';
 import { ConnectionOptionDeemphasizedDefault } from './connection-option-deemphasized-default';
+
+const ProminentConnectors = ({
+  onConnect,
+  setIsInstalling,
+  connectors,
+}: {
+  onConnect: (id: ConnectorType) => void;
+  setIsInstalling: (isInstalling: boolean) => void;
+  connectors: Connector[];
+}) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-1 -mx-2">
+      {connectors
+        .filter((c) => c.prominent)
+        .map((c) => {
+          const ConnectionOption = ConnectionOptionRecord[c.id];
+          const props = {
+            id: c.id,
+            name: c.name,
+            description: c.description,
+            showDescription: false,
+            onClick: () => onConnect(c.id),
+            onInstall: () => setIsInstalling(true),
+          };
+
+          if (ConnectionOption) {
+            return (
+              <li key={c.id}>
+                <ConnectionOption {...props} />
+              </li>
+            );
+          }
+
+          return (
+            <li key={c.id}>
+              <ConnectionOptionDefault {...props} />
+            </li>
+          );
+        })}
+    </div>
+  );
+};
+
+export const AdvancedConnectionOptions = ({
+  onConnect,
+  setIsInstalling,
+  connectors,
+}: {
+  onConnect: (id: ConnectorType) => void;
+  setIsInstalling: (isInstalling: boolean) => void;
+  connectors: Connector[];
+}) => {
+  const t = useT();
+
+  return (
+    <Accordion>
+      <AccordionItem
+        itemId="current-fees"
+        title={t('Advanced connection options')}
+        content={connectors
+          .filter((c) => !c.prominent)
+          .map((c) => {
+            const props = {
+              id: c.id,
+              name: c.name,
+              description: c.description,
+              onClick: () => onConnect(c.id),
+              onInstall: () => setIsInstalling(true),
+            };
+
+            return (
+              <li key={c.id}>
+                <ConnectionOptionDeemphasizedDefault {...props} />
+              </li>
+            );
+          })}
+      />
+    </Accordion>
+  );
+};
 
 export const ConnectionOptions = ({
   onConnect,
@@ -42,58 +126,12 @@ export const ConnectionOptions = ({
       ) : (
         <>
           <ul className="w-full" data-testid="connectors-list">
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-1 -mx-2">
-              {connectors
-                .filter((c) => c.prominent)
-                .map((c) => {
-                  const ConnectionOption = ConnectionOptionRecord[c.id];
-                  const props = {
-                    id: c.id,
-                    name: c.name,
-                    description: c.description,
-                    showDescription: false,
-                    onClick: () => onConnect(c.id),
-                    onInstall: () => setIsInstalling(true),
-                  };
-
-                  if (ConnectionOption) {
-                    return (
-                      <li key={c.id}>
-                        <ConnectionOption {...props} />
-                      </li>
-                    );
-                  }
-
-                  return (
-                    <li key={c.id}>
-                      <ConnectionOptionDefault {...props} />
-                    </li>
-                  );
-                })}
-            </div>
-            <Accordion>
-              <AccordionItem
-                itemId="current-fees"
-                title={t('Advanced options')}
-                content={connectors
-                  .filter((c) => !c.prominent)
-                  .map((c) => {
-                    const props = {
-                      id: c.id,
-                      name: c.name,
-                      description: c.description,
-                      onClick: () => onConnect(c.id),
-                      onInstall: () => setIsInstalling(true),
-                    };
-
-                    return (
-                      <li key={c.id}>
-                        <ConnectionOptionDeemphasizedDefault {...props} />
-                      </li>
-                    );
-                  })}
-              />
-            </Accordion>
+            <ProminentConnectors
+              {...{ onConnect, setIsInstalling, connectors }}
+            />
+            <AdvancedConnectionOptions
+              {...{ onConnect, setIsInstalling, connectors }}
+            />
           </ul>
           {error && error.code !== ConnectorErrors.userRejected.code && (
             <p
