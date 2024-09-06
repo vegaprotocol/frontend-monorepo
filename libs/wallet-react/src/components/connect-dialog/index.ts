244d0fe6c5993e5ae7f97d0a7f675674cd0322f0
Dexter <dexter@vega.xyz>
2024-09-06 07:16:26 +0700

    refactor: move moving things around

--- libs/wallet-react/src/components/connect-dialog/index.ts
+++ libs/wallet-react/src/components/connect-dialog/index.ts
@@ -2,5 +2,5 @@ export { ConnectDialogWithRiskAck, ConnectDialog } from './connect-dialog';
 export { ConnectionOptions } from './connection-options';
 export { ConnectionStatus } from './connection-status';
 export { ConnectorIcon } from './connection-options/connector-icon';
-export { ConnectionOptionDefault } from './connection-options/connection-option-default';
-export { ConnectionOptionRecord } from './connection-options/custom-connection-options';
+export { ConnectionOptionDefault } from './connection-options';
+export { ConnectionOptionRecord } from './connection-options';
