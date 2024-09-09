bced2dc6e6c47d2e696b034ec09e576a825cd016
Matthew Russell <mattrussell36@gmail.com>
2024-09-09 12:46:44 +0100

    fix: props not being spread to element

--- apps/trading/components/gradient-text/gradient-text.tsx
+++ apps/trading/components/gradient-text/gradient-text.tsx
@@ -4,6 +4,7 @@ import { type HTMLAttributes } from 'react';
 export const GradientText = (props: HTMLAttributes<HTMLSpanElement>) => {
   return (
     <span
+      {...props}
       className={cn(
         'bg-clip-text text-transparent bg-gradient-to-tr from-highlight-fg via-highlight-secondary-fg to-highlight-tertiary-fg via-30%',
         props.className
