00db07b96512e9c293f89469161eb91ed4fb0ae7
Dexter <dexter@vega.xyz>
2024-09-13 17:32:45 +0700

    chore: recomment missing part of the script

--- libs/i18n/src/locales/en/script.js
+++ libs/i18n/src/locales/en/script.js
@@ -9,35 +9,33 @@ if (!NEW_APP_NAME_LOWERCASE) {
   );
 }
 
-// console.log(__dirname);
-// const files = fs.readdirSync(__dirname);
-// console.log(files);
-// const jsonFiles = files.filter((f) => f.endsWith('.json'));
-// console.log(jsonFiles);
-// for (const file of jsonFiles) {
-//   const filePath = path.resolve(__dirname, file);
-//   const data = fs.readFileSync(filePath, {
-//     encoding: 'utf-8',
-//   });
-//   const json = JSON.parse(data);
-//   const newObject = Object.fromEntries(
-//     Object.entries(json).map(([k, v]) => [
-//       k,
-//       v
-//         .replace(/vega/g, NEW_APP_NAME_LOWERCASE)
-//         .replace(
-//           /Vega/g,
-//           NEW_APP_NAME_LOWERCASE.charAt(0).toUpperCase() +
-//             NEW_APP_NAME_LOWERCASE.slice(1)
-//         )
-//         .replace(/VEGA/g, NEW_APP_NAME_LOWERCASE.toUpperCase())
-//         .replace(/\$VEGA/g, `$${NEW_APP_NAME_LOWERCASE.toLocaleUpperCase()}`),
-//     ])
-//   );
-//   fs.writeFileSync(filePath, JSON.stringify(newObject, null, 2), {
-//     encoding: 'UTF-8',
-//   });
-// }
+// Replace all instances of the word "vega" with the new app name in the translation files
+const files = fs.readdirSync(__dirname);
+const jsonFiles = files.filter((f) => f.endsWith('.json'));
+for (const file of jsonFiles) {
+  const filePath = path.resolve(__dirname, file);
+  const data = fs.readFileSync(filePath, {
+    encoding: 'utf-8',
+  });
+  const json = JSON.parse(data);
+  const newObject = Object.fromEntries(
+    Object.entries(json).map(([k, v]) => [
+      k,
+      v
+        .replace(/vega/g, NEW_APP_NAME_LOWERCASE)
+        .replace(
+          /Vega/g,
+          NEW_APP_NAME_LOWERCASE.charAt(0).toUpperCase() +
+            NEW_APP_NAME_LOWERCASE.slice(1)
+        )
+        .replace(/VEGA/g, NEW_APP_NAME_LOWERCASE.toUpperCase())
+        .replace(/\$VEGA/g, `$${NEW_APP_NAME_LOWERCASE.toLocaleUpperCase()}`),
+    ])
+  );
+  fs.writeFileSync(filePath, JSON.stringify(newObject, null, 2), {
+    encoding: 'UTF-8',
+  });
+}
 
 const otherFiles = [
   'apps/explorer/src/assets/manifest.json',
@@ -49,6 +47,7 @@ const otherFiles = [
   'apps/trading/public/manifest.json',
 ];
 
+// Replace all instances of the word "vega" with the new app name in static assets such as manifests and index.html
 for (const file of otherFiles) {
   const filePath = path.resolve(__dirname, '../../../../../', file);
   const data = fs.readFileSync(filePath, {
