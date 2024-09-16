c83df860190411894729324cd0163bf5cee9d423
oldalbus <oldalbus@protonmail.com>
2024-09-16 16:57:42 -0400

    chore: fix incorrect squid receiver address

--- apps/trading/components/deposit-container/constants.ts
+++ apps/trading/components/deposit-container/constants.ts
@@ -4,4 +4,4 @@ export type Address = ViemAddress;
 
 export const ARBITRUM_CHAIN_ID = '42161';
 export const ARBITRUM_SQUID_RECEIVER_ADDRESS =
-  '0xE7477a9aDb9BA0d00Af8f4d8e5E53A532C650ffa' as Address;
+  '0x35a297F91F356C6830D8F426713Db463B2F66dc8' as Address;
