e633788fcf663b40b0add62a27770ae4775104d2
Dexter <dexter@vega.xyz>
2024-09-17 18:11:11 +0700

    chore: fix build

--- libs/wallet-react/src/testing.ts
+++ libs/wallet-react/src/testing.ts
@@ -13,6 +13,7 @@ export const mockConfig = createConfig({
   chains: [mockChain],
   defaultChainId: mockChain.id,
   connectors: [mockConnector],
+  appName: 'test',
 });
 
 export function MockedWalletProvider({
