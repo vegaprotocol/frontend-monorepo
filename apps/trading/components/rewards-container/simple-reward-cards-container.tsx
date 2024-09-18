34554564598f09b1448c4ee0ae4de66d0c04beb8
Matthew Russell <mattrussell36@gmail.com>
2024-09-18 15:49:17 -0400

    chore: remove comment

--- apps/trading/components/rewards-container/simple-reward-cards-container.tsx
+++ apps/trading/components/rewards-container/simple-reward-cards-container.tsx
@@ -21,17 +21,3 @@ export const SimpleRewardCardsContainer = () => {
     </div>
   );
 };
-
-/**
-
-MD:
-
-This is a description that returns onto a maximum number of lines to be defined. It may also be truncated at 3 or 4 lines.
-
-* **1,666.58 NEB**
-* Distribution strategy: Pro rata 
-* Reward pool amount and asset
-* Another bullet point
-* Another bullet point
-
- */
