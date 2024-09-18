49d03e0a0d422c83e41960d48beaef1398b66082
oldalbus <oldalbus@protonmail.com>
2024-09-17 22:06:14 -0400

    chore: fix backdrop images

--- apps/trading/components/layouts/backdrop.tsx
+++ apps/trading/components/layouts/backdrop.tsx
@@ -5,8 +5,8 @@ export const Backdrop = (props: { backdrop?: number }) => {
   if (props.backdrop !== undefined) {
     const backdrop =
       theme === 'dark'
-        ? `backdrop-dark-${props.backdrop}.jpg`
-        : `backdrop-light-${props.backdrop}.jpg`;
+        ? `backdrop-${props.backdrop}-dark.jpg`
+        : `backdrop-${props.backdrop}-light.jpg`;
     return (
       <div
         className="fixed top-0 left-0 w-full h-full -z-10 bg-cover bg-fixed bg-no-repeat"
