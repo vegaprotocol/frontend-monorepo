b24a95c1bb0c72d86626cbb40d35519de74da9a6
Matthew Russell <mattrussell36@gmail.com>
2024-09-20 14:32:05 -0400

    chore: remove unnecessary constants

--- apps/trading/components/deposit-container/constants.ts
+++ apps/trading/components/deposit-container/constants.ts
@@ -1,7 +1,4 @@
-import { type Address as ViemAddress } from 'viem';
+import { type Address } from 'viem';
 
-export type Address = ViemAddress;
-
-export const ARBITRUM_CHAIN_ID = '42161';
 export const SQUID_RECEIVER_ADDRESS =
   '0xE7477a9aDb9BA0d00Af8f4d8e5E53A532C650ffa' as Address;
