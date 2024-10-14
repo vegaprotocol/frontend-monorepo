d5fabd009a5154fe3b9916bfbc26690fef600fa1
Dexter <dexter@vega.xyz>
2024-10-14 14:52:39 +0200

    chore: reimplement signing logic

--- libs/wallet-react/src/components/connect-dialog/connection-options/custom-connector-options/connection-option-quickstart.tsx
+++ libs/wallet-react/src/components/connect-dialog/connection-options/custom-connector-options/connection-option-quickstart.tsx
@@ -4,6 +4,10 @@ import { ConnectionOptionButton } from '../connection-option-button';
 import { ConnectorIcon } from '../connector-icon';
 import { type ConnectionOptionProps } from '../types';
 import { useQuickstart } from 'libs/wallet-react/src/hooks/use-quickstart';
+import { useWeb3ConnectStore } from '@vegaprotocol/web3';
+import { useWeb3React } from '@web3-react/core';
+import { useCallback, useEffect, useState } from 'react';
+import { useConfig } from '@vegaprotocol/wallet-react';
 
 const USER_REJECTED_CODE = 4001;
 
@@ -43,7 +47,7 @@ export const QuickstartButton = ({
   );
 };
 
-export const ConnectionOptionQuickstart = ({
+export const ConnectionOptionQuickstartWagmi = ({
   connector,
   onClick,
 }: ConnectionOptionProps) => {
@@ -61,5 +65,86 @@ export const ConnectionOptionQuickstart = ({
       isPending={isPending}
       error={error as ConnectorError}
     />
+  );
+};
+
+export const ETHEREUM_CHAIN_ID = 11155111;
+
+export const ConnectionOptionQuickstart = ({
+  connector,
+  onClick,
+}: ConnectionOptionProps) => {
+  const t = useT();
+  if (!(connector instanceof QuickStartConnector)) {
+    throw new Error('Tried to render QuickStartConnector with wrong connector');
+  }
+  const state = useConfig();
+  const [isCreating, setIsCreating] = useState(false);
+  const { open } = useWeb3ConnectStore();
+  const { account, connector: ethConnector } = useWeb3React();
+  const createWallet = useCallback(async () => {
+    try {
+      state.store.setState({
+        status: 'creating',
+      });
+      const { chainId } = await connector.getChainId();
+      if (chainId !== ETHEREUM_CHAIN_ID.toString()) {
+        await ethConnector.activate(ETHEREUM_CHAIN_ID);
+      }
+      const signedMessage = await ethConnector.provider?.request({
+        method: 'eth_signTypedData_v4',
+        params: [
+          account,
+          {
+            types: {
+              EIP712Domain: [
+                { name: 'name', type: 'string' },
+                { name: 'chainId', type: 'uint256' },
+              ],
+              Onboarding: [{ name: 'action', type: 'string' }],
+            },
+            primaryType: 'Onboarding',
+            domain: { name: 'Onboarding', chainId: ETHEREUM_CHAIN_ID },
+            message: { action: `${state.appName} Onboarding` },
+          },
+        ],
+      });
+      const mnemonic = (await connector.deriveMnemonic(
+        signedMessage as unknown as string
+      )) as unknown as string[];
+      const mnemonicString = mnemonic.join(' ');
+      await connector.importWallet(mnemonicString, account!);
+      onClick();
+    } catch (e) {
+      state.store.setState({
+        status: 'disconnected',
+      });
+      throw e;
+    }
+  }, [ethConnector, state, account]);
+  const clickHandler = () => {
+    if (!account) {
+      open(ETHEREUM_CHAIN_ID);
+      setIsCreating(true);
+    } else {
+      createWallet();
+    }
+  };
+
+  useEffect(() => {
+    if (account && isCreating) {
+      createWallet();
+    }
+  }, [account, isCreating]);
+
+  return (
+    <ConnectionOptionButton
+      icon={<ConnectorIcon id="embedded-wallet-quickstart" />}
+      id="embedded-wallet-quickstart"
+      onClick={clickHandler}
+      disabled={false}
+    >
+      {t('Connect with Ethereum')}
+    </ConnectionOptionButton>
   );
 };
